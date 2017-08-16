d401501399242caf7df0b11a65749e6689cbe7f1
Henrique Harman <henriqueharman@gmail.com>
2017-08-16 15:22:28 -0400

    Has one page: quickfiles for user

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+    this.route('quickfiles', {path: '/:user_id/quickfiles'});
 });
 
 export default Router;
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,24 +1,31 @@
 /*jshint node:true*/
 /* global require, module */
+const path = require('path');
+
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
+    var app = new EmberApp(defaults, {
+      'ember-bootstrap': {
+        'bootstrapVersion': 3,
+        'importBootstrapFont': true,
+        'importBootstrapCSS': true
+      }
+    });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+    // osf-style
+    app.import(path.join(app.bowerDirectory, 'osf-style/vendor/prism/prism.css'));
+    app.import(path.join(app.bowerDirectory, 'osf-style/page.css'));
+    app.import(path.join(app.bowerDirectory, 'osf-style/css/base.css'));
+    app.import(path.join(app.bowerDirectory, 'loaders.css/loaders.min.css'));
+    app.import(path.join(app.bowerDirectory, 'osf-style/img/cos-white2.png'), {
+        destDir: 'img'
+    });
+    app.import(path.join(app.bowerDirectory, 'dropzone/dist/basic.css'));
+    app.import(path.join(app.bowerDirectory, 'dropzone/dist/dropzone.css'));
+    app.import(path.join(app.bowerDirectory, 'dropzone/dist/dropzone.js'));
+    
+    app.import('vendor/assets/ember-osf.css');
 
-  return app.toTree();
+    return app.toTree();
 };
