cf1304fe392ee536b00c828b7ad4cb7dc6ad997e
Baylee Swenson <baylee@cos.io>
2017-09-07 15:35:23 -0400

    Initial fixes and WIP fix for change files

--- app/file-detail/controller.js
+++ app/file-detail/controller.js
@@ -3,17 +3,24 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     displays: Ember.A([]),
     userId: Ember.computed('model', function() {
-        // let link = this.get('model')._internalModel._relationships.initializedRelationships.user.link; //hack to get id without having to request it
-        // return link.split('/')[link.split('/').indexOf('users') + 1]; 
-        let model = this.get('model');
-        return model.get('user').then(user => user.get('id'));
+        return this.get('model.user.id');
     }),
     actions: {
+        share() {
+            // Share the document being viewed
+        },
         download() {
             window.location = this.get('model.links.download');
         },
+        changeView() {
+            // Change the view to either the normal view or the revision table view
+            $('#mfrIframeParent').toggle();
+            $('#revisionsPanel').toggle();
+            $('.view-button').toggleClass('btn-default btn-primary');
+        },
         openFile(file) {
-            this.transitionToRoute('file-detail', file);
+            let fileID = file.get('guid') ? file.get('guid') : file.id;
+            this.transitionToRoute('file-detail', fileID);
         }
     }
 });
--- app/file-detail/route.js
+++ app/file-detail/route.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return this.store.findRecord('file', params.file_id);
+        return this.store.findRecord('file', params.file_id).
+               then(file => file.get('user').
+                       then(() => file));    
     }
 });
