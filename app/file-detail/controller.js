a01a89ce09b182ee9363aab8bb39586d9dbab84c
Baylee Swenson <baylee@cos.io>
2017-09-11 10:38:12 -0500

    Change id for file re-route

--- app/file-detail/controller.js
+++ app/file-detail/controller.js
@@ -1,9 +1,15 @@
 import Ember from 'ember';
+import permissions from 'ember-osf/const/permissions';
+
 
 export default Ember.Controller.extend({
     displays: Ember.A([]),
     userId: Ember.computed('model', function() {
         return this.get('model.user.id');
+    }),
+    isAdmin: Ember.computed('model', function() {
+        // True if the current user has admin permissions for the node that contains the preprint
+        //return (this.get('model.user.currentUserPermissions') || []).includes(permissions.ADMIN);
     }),
     actions: {
         share() {
@@ -19,8 +25,31 @@ export default Ember.Controller.extend({
             $('.view-button').toggleClass('btn-default btn-primary');
         },
         openFile(file) {
+            // TODO: When the user chooses a new file, the file-widget temporarily goes AWOL
             let fileID = file.get('guid') ? file.get('guid') : file.id;
             this.transitionToRoute('file-detail', fileID);
-        }
+        },
+        // Custom addATag method that appends tag to list instead of auto-saving
+        addTag(tag) {
+            Ember.get(this, 'metrics')
+                .trackEvent({
+                    category: 'input',
+                    action: 'onchange',
+                    label: `${this.get('editMode') ? 'Edit' : 'Submit'} - Add Tag`
+                });
+
+            this.get('fileTags').pushObject(tag);
+        },
+        // Custom removeATag method that removes tag from list instead of auto-saving
+        removeTag(tag) {
+            Ember.get(this, 'metrics')
+                .trackEvent({
+                    category: 'button',
+                    action: 'click',
+                    label: `${this.get('editMode') ? 'Edit' : 'Submit'} - Remove Tag`
+                });
+
+            this.get('fileTags').removeObject(tag);
+        },
     }
 });
