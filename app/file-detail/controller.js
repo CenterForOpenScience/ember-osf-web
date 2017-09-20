57c73342c5450d5382e1535d95442e482d6ec8b7
Baylee Swenson <baylee@cos.io>
2017-09-20 11:41:08 -0400

    Finish tags, fix some styling, start file versions

--- app/file-detail/controller.js
+++ app/file-detail/controller.js
@@ -1,42 +1,58 @@
 import Ember from 'ember';
-import fixSpecialChar from 'ember-osf/utils/fix-special-char';
 
 
 export default Ember.Controller.extend({
     displays: Ember.A([]),
     fileTags: Ember.A([]),
+
     userId: Ember.computed('model', function() {
         return this.get('model.user');
     }),
+
     fileUrl: Ember.computed('model', function() {
         return encodeURIComponent(window.location.href);
     }),
+    
     twitterUrl: Ember.computed('model', 'fileUrl', function() {
         return 'https://twitter.com/intent/tweet?url=' + this.get('fileUrl') + '&text=' + this.get('model.name') + '&via=OSFramework';
     }),
+    
     facebookUrl: Ember.computed('model', 'fileUrl', function() {
         return 'https://www.facebook.com/sharer/sharer.php?u=' + this.get('fileUrl');
     }),
+    
     linkedInUrl: Ember.computed('model', 'fileUrl', function() {
         return 'https://www.linkedin.com/cws/share?url=' + this.get('fileUrl') + '&title=' + this.get('model.name');
     }),
+    
     emailUrl: Ember.computed('model', 'fileUrl', function() {
         return 'mailto:?subject=' + this.get('model.name') + '&body=' + this.get('fileUrl');
     }),
+    
     mfrUrl: Ember.computed('model', function() {
         return 'https://mfr.osf.io/render?url=' + window.location.href + '?action=download%26mode=render';
     }),
+    
     shareiFrameDynamic: Ember.computed('model', function() {
         return '<style>.embed-responsive{position:relative;height:100%;}.embed-responsive iframe{position:absolute;height:100%;}</style><script>window.jQuery || document.write(\'<script src="//code.jquery.com/jquery-1.11.2.min.js">\x3C/script>\') </script><link href="https://mfr.osf.io/static/css/mfr.css" media="all" rel="stylesheet"><div id="mfrIframe" class="mfr mfr-file"></div><script src="https://mfr.osf.io/static/js/mfr.js"></script> <script>var mfrRender = new mfr.Render("mfrIframe", "' + this.get('mfrUrl') + '");</script>';
     }),
+    
     shareiFrameDirect: Ember.computed('model', function() {
         return '<iframe src="' + this.get('mfrUrl') + '" width="100%" scrolling="yes" height="677px" marginheight="0" frameborder="0" allowfullscreen webkitallowfullscreen>';
     }),
+    
     fileVersions: Ember.computed('model', function() {
-        let versionId = this.get('model.versions.content.record._data.guid');
-        let versionSize = this.get('model.versions.content.record._data.size');
+        let versionArray = Ember.A([]);
+        let versionId = ''; 
+        let versionSize = '';
 
-        let versionArray = {'id': versionId, 'size': versionSize};
+        this.get('model.versions').then(versions => {
+            versions.currentState.forEach(function(version) {
+                versionId = version.id;
+                versionSize = version.__data.size;
+                versionArray.pushObject({'id': versionId, 'size': versionSize});
+            });
+        });
         return versionArray;
     }),
 
@@ -71,10 +87,8 @@ export default Ember.Controller.extend({
         removeTagAtIndex(index) {
             let model = this.get('model');
             this.get('fileTags').removeAt(index);
-            if (this.get('tagsChanged')) {
-                model.set('tags', this.get('fileTags'));
-                model.save();
-            }
+            model.set('tags', this.get('fileTags'));
+            model.save();
         }
     }
 });
--- app/file-detail/route.js
+++ app/file-detail/route.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        return this.store.findRecord('file', params.file_id);  
+        return this.store.findRecord('file', params.file_id);
     }
 });
