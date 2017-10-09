d2d2af6541d0c61439a6e4d482887d7b7f5ef266
Baylee Swenson <baylee@cos.io>
2017-10-09 11:00:37 -0400

    Add ability to edit text documents

--- app/file-detail/controller.js
+++ app/file-detail/controller.js
@@ -4,15 +4,15 @@ import { computed } from '@ember/object';
 import { A } from '@ember/array';
 import Controller from '@ember/controller';
 import { authenticatedAJAX } from 'ember-osf/utils/ajax-helpers';
+import { mimeTypes } from 'ember-osf/const/mime-types';
 
 export default Controller.extend({
     currentUser: service(),
     revision: null,
     openModal: false,
-    editableFileTypes: A(['txt']),
     displays: A([]),
 
-    mfrVersion: computed('model.file', 'revision', function() {
+    mfrVersion: computed('model.file.currentVersion', 'revision', function() {
         return this.get('revision') ? this.get('revision') : this.get('model.file.currentVersion');
     }),
 
@@ -52,7 +52,7 @@ export default Controller.extend({
         return this.get('model.file.tags');
     }),
 
-    fileVersions: computed('model.file', function() {
+    fileVersions: computed('model.file.currentVersion', function() {
         return $.getJSON(`${this.get('model.file.links.download')}?revisions=&`).then(this._returnFileVersion.bind(this));
     }),
 
@@ -62,9 +62,20 @@ export default Controller.extend({
     }),
 
     isEditableFile: computed('model.file', function() {
-        const fileExtension = this.get('model.file.name').split('.').pop();
-        if (this.get('editableFileTypes').includes(fileExtension)) return true;
+        const fileName = this.get('model.file.name');
+        const fileExtension = fileName.split('.').pop();
+        if (fileExtension in mimeTypes) return true;
         return false;
+    }),
+
+    fileText: computed('model.file', function() {
+        return authenticatedAJAX({
+            url: this.get('model.file.links.download'),
+            type: 'GET',
+            xhrFields: { withCredentials: true },
+        }).done((data) => {
+            return data;
+        });
     }),
 
     actions: {
@@ -97,10 +108,54 @@ export default Controller.extend({
             this.set('openModal', false);
         },
 
-        changeViewRevisionPane() {
-            $('#mfrIframeParent').toggle();
-            $('#revisionsPanel').toggle();
-            $('.view-button').toggleClass('btn-default btn-primary');
+        changeViewPanel(panel, button) {
+            if (!this.get('isEditableFile')) {
+                $('#mainViewBtn, #revisionBtn').toggleClass('btn-primary btn-default');
+                $('#mfrIframeParent, #revisionsPanel').toggle();
+                return;
+            }
+
+            if (button === 'revisionBtn' && $(`#${panel}`).css('display') === 'none') {
+                $('.panel-view').hide().removeClass('col-sm-6');
+                $('.view-button').removeClass('btn-primary').addClass('btn-default');
+                $(`#${panel}`).toggle();
+                $(`#${button}`).toggleClass('btn-default btn-primary');
+                return;
+            } else if (button === 'revisionBtn') {
+                $('#mfrIframeParent').toggle();
+                $('#mainViewBtn').toggleClass('btn-default btn-primary');
+                $(`#${panel}`).toggle();
+                $(`#${button}`).toggleClass('btn-default btn-primary');
+                return;
+            }
+
+            if ($(`#${button}`).hasClass('btn-primary') && $('.view-button.btn-primary').length === 1) {
+                return;
+            } else if ($('#revisionsPanel').css('display') !== 'none') {
+                $('#revisionsPanel').toggle();
+                $('#revisionBtn').toggleClass('btn-default btn-primary');
+            } else if ($('#mfrIframeParent').css('display') !== 'none' || $('#editPanel').css('display') !== 'none') {
+                $('.panel-view').toggleClass('col-sm-6');
+            } else {
+                $('.panel-view').removeClass('col-sm-6');
+            }
+
+            $(`#${panel}`).toggle();
+            $(`#${button}`).toggleClass('btn-default btn-primary');
+        },
+
+        save(text) {
+            const controller = this;
+            authenticatedAJAX({
+                url: this.get('model.file.links.upload'),
+                type: 'PUT',
+                xhrFields: { withCredentials: true },
+                data: text,
+            }).done(() => {
+                controller.get('model.file').reload().then(
+                    () => controller.set('revision', null),
+                );
+            });
         },
 
         openFile(file) {
