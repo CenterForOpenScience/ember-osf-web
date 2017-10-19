ace91001d950c93d0090bf005a512a3f6b9dbcb4
Henrique Harman <henriqueharman@gmail.com>
2017-10-19 13:31:04 -0400

    Add filtering

--- app/institutions/controller.js
+++ app/institutions/controller.js
@@ -4,12 +4,17 @@ export default Ember.Controller.extend({
     store: Ember.inject.service(),
     sort: 'asc',
     page: 1,
-    institutions: Ember.computed('model', 'sort', 'page', function() {
-        const sorted = this.get('model').sortBy('name');
+    textValue: '',
+    institutions: Ember.computed('model', 'sort', 'page', 'textValue', function() {
+        const filtered = this.get('textValue.length') ? this.get('model').filter(i => i.get('name').toLowerCase().indexOf(this.get('textValue').toLowerCase()) !== -1) : this.get('model');
+        const sorted = filtered.sortBy('name');
         if (this.get('sort') === 'des') { sorted.reverse(); }
         return sorted.slice(0, 10 * this.get('page'));
     }),
-    hasMore: Ember.computed('institutions', 'model', function() {
+    hasMore: Ember.computed('institutions', 'model', 'textValue', function() {
+        if (this.get('textValue.length')) {
+            return this.get('institutions.length') !== this.get('model').filter(i => i.get('name').toLowerCase().indexOf(this.get('textValue').toLowerCase()) !== -1).length;
+        }
         return this.get('institutions.length') !== this.get('model.length');
     }),
     actions: {
