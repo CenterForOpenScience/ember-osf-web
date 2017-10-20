b1e7ed6d75b4cff518cace3583694df6087af555
Henrique Harman <henriqueharman@gmail.com>
2017-10-20 16:24:56 -0400

    Add ember-i18n, add translations and use them

---
+++ app/locales/en/translations.js
@@ -0,0 +1,25 @@
+export default {
+    general: {
+        share: "Share",
+        embed: "Embed",
+        download: "Download",
+        view: "View",
+        revisions: "Revisions",
+        date: "Date"
+    },
+    quickfiles: {
+        title: "{{user-name}}'s Quick Files",
+        description: "Files uploaded here are <b>publicly accessible</b> and easy to share with others using the share link."
+    },
+    file_detail: {
+        version: {
+            id: "Version ID",
+            title: "Version: {{version-number}})"
+        },
+        embed: {
+            dynamic: "Dynamically render iframe with JavaScript",
+            direct: "Direct iframe with fixed height and width"
+        },
+        tags: "Tags:"
+    }
+};
--- app/user-quickfiles/controller.js
+++ app/user-quickfiles/controller.js
@@ -2,9 +2,6 @@ import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    title: computed('model.fullName', function() {
-        return `${this.get('model.fullName')}'s Quick Files`;
-    }),
     actions: {
         openFile(file) {
             if (file.get('guid')) {
