f03b72a49f67ed77cc47014695af2a78927a2ebb
Henrique Harman <henriqueharman@gmail.com>
2017-10-23 12:43:08 -0400

    Add template-lintrc.js

---
+++ .template-lintrc.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = {
+  extends: 'recommended',
+
+  rules: {
+      'block-indentation': 4,
+      'bare-strings': true,
+      'triple-curlies': false,
+      'nested-interactive': false,
+      'html-comments': false
+  }
+};
