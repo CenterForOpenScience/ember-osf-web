05ba3f76997f4776837e77e3fee653f7c4349e77
Henrique Harman <henriqueharman@gmail.com>
2017-10-26 11:21:09 -0400

    Use ember-modules-codemod to clean up

--- app/institutions/controller.js
+++ app/institutions/controller.js
@@ -1,17 +1,20 @@
-import Ember from 'ember';
+import $ from 'jquery';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
 
-export default Ember.Controller.extend({
-    store: Ember.inject.service(),
+export default Controller.extend({
+    store: service(),
     sort: 'asc',
     page: 1,
     textValue: '',
-    institutions: Ember.computed('model', 'sort', 'page', 'textValue', function() {
+    institutions: computed('model', 'sort', 'page', 'textValue', function() {
         const filtered = this.get('textValue.length') ? this.get('model').filter(i => i.get('name').toLowerCase().indexOf(this.get('textValue').toLowerCase()) !== -1) : this.get('model');
         const sorted = filtered.sortBy('name');
         if (this.get('sort') === 'des') { sorted.reverse(); }
         return sorted.slice(0, 10 * this.get('page'));
     }),
-    hasMore: Ember.computed('institutions', 'model', 'textValue', function() {
+    hasMore: computed('institutions', 'model', 'textValue', function() {
         if (this.get('textValue.length')) {
             return this.get('institutions.length') !== this.get('model').filter(i => i.get('name').toLowerCase().indexOf(this.get('textValue').toLowerCase()) !== -1).length;
         }
@@ -22,8 +25,8 @@ export default Ember.Controller.extend({
             this.incrementProperty('page');
         },
         sort(sortOrder) {
-            Ember.$('.sorting').removeClass('active');
-            Ember.$(`.name${sortOrder}`).addClass('active');
+            $('.sorting').removeClass('active');
+            $(`.name${sortOrder}`).addClass('active');
             this.set('sort', sortOrder);
         },
         goTo(id) {
--- app/institutions/route.js
+++ app/institutions/route.js
@@ -1,7 +1,8 @@
-import Ember from 'ember';
+import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
 
-export default Ember.Route.extend({
-    store: Ember.inject.service(),
+export default Route.extend({
+    store: service(),
     model() {
         return this.get('store').query('institution', { page: 1 });
     },
