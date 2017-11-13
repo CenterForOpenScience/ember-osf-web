a67b72183f4040465a0001ed237ffac3b72144f2
James C. Davis <james@cos.io>
2017-11-13 08:52:50 -0500

    Reload user on file detail route

--- app/file-detail/route.js
+++ app/file-detail/route.js
@@ -5,7 +5,7 @@ export default Route.extend({
     model(params) {
         return hash({
             file: this.store.findRecord('file', params.file_id),
-            user: this.store.findRecord('file', params.file_id).then(file => file.get('user')),
+            user: this.store.findRecord('file', params.file_id).then(file => file.get('user')).then(user => user.reload()),
         });
     },
 });
