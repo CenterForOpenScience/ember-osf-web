464e98a6f5c7adf27f3a81dda221c88beea11920
Henrique Harman <henriqueharman@gmail.com>
2017-12-27 13:19:49 -0500

    Fix base template, add basic html dashboard template and css

---
+++ tests/unit/dashboard/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:dashboard', 'Unit | Route | dashboard', {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+    const route = this.subject();
+    assert.ok(route);
+});
