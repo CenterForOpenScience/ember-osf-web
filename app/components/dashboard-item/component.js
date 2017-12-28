a821b5d14fd21479cbf611dd9c7248cda480d4ab
Henrique Harman <henriqueharman@gmail.com>
2017-12-28 13:37:41 -0500

    Add ember-concurrency, use it, add filtering and pagination

--- app/components/dashboard-item/component.js
+++ app/components/dashboard-item/component.js
@@ -10,16 +10,12 @@ export default Component.extend({
         // Trust contributors are cached due to emdedding done on dashboard
         const contribs = this.get('node.contributors.content.canonicalState');
         const len = contribs.length;
-        const namePath = index => `node._internalModel.__relationships.initializedRelationships.contributors.canonicalState.${index}.__data.links.relationships.users.data.attributes.family_name`;
-        // TODO generalize
-        if (len === 1) {
-            return this.get(namePath(0));
-        } else if (len === 2) {
-            return `${this.get(namePath(0))} and ${this.get(namePath(1))}`;
-        } else if (len === 3) {
-            return `${this.get(namePath(0))}, ${this.get(namePath(1))} and ${this.get(namePath(2))}`;
-        } else {
-            return `${this.get(namePath(0))}, ${this.get(namePath(1))}, ${this.get(namePath(2))} +${len}`;
+        const namePath = index => this.get(`node._internalModel.__relationships.initializedRelationships.contributors.canonicalState.${index}.__data.links.relationships.users.data.attributes.family_name`);
+        switch (len) {
+        case 1: return namePath(0);
+        case 2: return `${namePath(0)} and ${namePath(1)}`;
+        case 3: return `${namePath(0)}, ${namePath(1)} and ${namePath(2)}`;
+        default: return `${namePath(0)}, ${namePath(1)}, ${namePath(2)} +${len - 3}`;
         }
     }),
 });
--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,32 +1,36 @@
 import Ember from 'ember';
 import Controller from '@ember/controller';
+import { task } from 'ember-concurrency';
 
 export default Controller.extend({
     currentUser: Ember.inject.service(),
     init() {
         this.get('currentUser.user').then((user) => {
             user.query('nodes', { embed: 'contributors' }).then((nodes) => {
-                this.set('nodes', nodes);
-                this.set('totalPages', nodes.meta.pagination.total / 10 | 1); // eslint-disable-line no-bitwise
+                this.set('_nodes', nodes.slice());
+                const pages = Math.ceil(nodes.meta.pagination.total / nodes.meta.pagination.per_page);
+                this.set('totalPages', pages);
+                for (let i = 2; i <= pages; i++) { this.get('findNodes').perform(i); }
             });
         });
     },
+    findNodes: task(function* (page) {
+        const user = yield this.get('currentUser.user');
+        const nodes = yield user.query('nodes', { page, embed: 'contributors' });
+        this.get('_nodes').pushObjects(nodes.slice());
+    }).enqueue(),
     curPage: 1,
-    nodes: Ember.A([]),
+    filter: '',
+    _nodes: Ember.A([]),
+    nodes: Ember.computed('_nodes', 'filter', 'curPage', function() {
+        return this.get('_nodes').filter(each => each.get('title').toLowerCase().indexOf(this.get('filter').toLowerCase()) !== -1).slice(0, this.get('curPage') * 10);
+    }),
     hasMore: Ember.computed('totalPages', 'curPage', function() {
         return this.get('totalPages') ? this.get('totalPages') > this.get('curPage') : false;
     }),
     actions: {
         more() {
-            this.set('curPage', this.get('curPage') + 1);
-            this.get('currentUser.user').then((user) => {
-                user.query('nodes', {
-                    embed: 'contributors',
-                    page: this.get('curPage'),
-                }).then((nodes) => {
-                    this.get('nodes').pushObjects(nodes);
-                });
-            });
+            this.incrementProperty('curPage');
         },
     },
 });
