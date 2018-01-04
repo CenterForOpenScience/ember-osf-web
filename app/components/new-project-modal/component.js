abf0e5a012cffbe90b6e6cdd13ec66695f4edf4e
Henrique Harman <henriqueharman@gmail.com>
2018-01-04 11:58:43 -0500

    Work on modal

--- app/components/new-project-modal/component.js
+++ app/components/new-project-modal/component.js
@@ -1,12 +1,37 @@
+import Ember from 'ember';
 import Component from '@ember/component';
 
 export default Component.extend({
+    store: Ember.inject.service(),
+    done: false,
+    more: false,
     actions: {
+        toggleMore() {
+            this.toggleProperty('more');
+        },
         closeModal() {
+            this.set('done', false);
             this.get('closeModal')();
         },
         createNode() {
-
+            const data = {
+                public: false,
+                category: 'project',
+                title: this.get('nodeTitle'),
+                description: this.get('description'),
+            };
+            if (this.get('templateFrom')) {
+                data.templateFrom = this.get('templateFrom');
+            }
+            this.get('store').createRecord('node', data).save().then((node) => {
+                if (this.get('institutionsSelected')) {
+                    node.set('affiliatedInstitutions', this.get('institutionsSelected')).then(() => {
+                        this.set('done', node.get('id'));
+                    });
+                } else {
+                    this.set('done', node.get('id'));
+                }
+            });
         },
     },
 });
