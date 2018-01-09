45df3939ebc9f165f2faf4925363ec645257e0ad
Henrique Harman <henriqueharman@gmail.com>
2018-01-09 14:26:17 -0500

    Deal with hierarchy

--- app/components/dashboard-item/component.js
+++ app/components/dashboard-item/component.js
@@ -3,6 +3,28 @@ import moment from 'moment';
 import Component from '@ember/component';
 
 export default Component.extend({
+    ancestry: Ember.computed('node', 'node.{parent,root}', function() {
+        const rootId = this.get('node._internalModel.__data.links.relationships.root.errors.length') ? undefined : this.get('node.root.id');
+        const parentId = this.get('node._internalModel.__data.links.relationships.parent.errors.length') ? undefined : this.get('node.parent.id');
+        const grandpaLink = this.get('node.parent.content._internalModel._data.links.relationships.parent.links.related.href');
+        const grandpaId = grandpaLink ? grandpaLink.split('nodes')[1].split('/').filter(e => e)[0] : undefined;
+        if (!rootId) {
+            if (parentId) {
+                this.set('_ancestry', this.get('node.parent.title'));
+            }
+            return '--private';
+        }
+        if (!parentId) {
+            return '';
+        }
+        if (parentId === rootId) {
+            return `${this.get('node.root.title')} / `;
+        }
+        if (grandpaId === rootId) {
+            return `${this.get('node.root.title')} / ${this.get('node.parent.title')} / `;
+        }
+        return `${this.get('node.root.title')} / ... / ${this.get('node.parent.title')} / `;
+    }),
     date: Ember.computed('node.dateModified', function() {
         return moment(this.get('node.dateModified')).utc().format('YYYY-MM-DD h:mm A');
     }),
