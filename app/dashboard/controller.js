d0584e5cbc5a7876ccadde6f7a9238e12c478150
Henrique Harman <henriqueharman@gmail.com>
2018-01-16 14:58:39 -0500

    Change controller logic to use api

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -2,9 +2,8 @@ import { computed } from '@ember/object';
 import { A } from '@ember/array';
 import { inject as service } from '@ember/service';
 import Controller from '@ember/controller';
-import { task } from 'ember-concurrency';
+import { task, timeout } from 'ember-concurrency';
 
-// const prodLinkedNodePath = id => `https://api.osf.io/v2/nodes/${id}/linked_nodes/?page[size]=5`;
 const popularNode = '57tnq';
 const noteworthyNode = 'z3sg2';
 
@@ -17,11 +16,11 @@ export default Controller.extend({
                 return;
             }
             user.query('nodes', { embed: 'contributors' }).then((nodes) => {
-                this.set('_nodes', nodes.slice());
+                this.set('nodes', nodes.slice());
+                this.set('totalNodes', nodes.meta.total);
                 const pages = Math.ceil(nodes.meta.pagination.total / nodes.meta.pagination.per_page);
                 this.set('totalPages', pages);
-                this.set('loading', false);
-                for (let i = 2; i <= pages; i++) { this.get('findNodes').perform(i); }
+                this.set('initialLoading', false);
             });
         });
 
@@ -30,59 +29,78 @@ export default Controller.extend({
         this.get('getPopularAndNoteworthy').perform(noteworthyNode, 'noteworthy');
     },
 
-    loading: true,
+    loading: false,
+    loadingMore: false,
+    initialLoading: true,
     curPage: 1,
     filter: '',
     sortBy: 'date',
     sortOrder: 'desc',
     modalOpen: false,
-    _nodes: A([]),
+    nodes: A([]),
     institutions: A([]),
     popular: A([]),
     noteworthy: A([]),
 
-    nodes: computed('_nodes', 'filter', 'curPage', 'sortBy', 'sortOrder', function() {
-        const nodes = this.get('_nodes').filter(each => each.get('title').toLowerCase().indexOf(this.get('filter').toLowerCase()) !== -1);
-        if (this.get('sortBy') === 'date') {
-            if (this.get('sortOrder') === 'asc') {
-                nodes.reverse();
-            }
-        } else if (this.get('sortOrder') === 'asc') {
-            nodes.sort((a, b) => ((a.get('title').toLowerCase() < b.get('title').toLowerCase()) ? -1 : 1));
-        } else {
-            nodes.sort((a, b) => ((a.get('title').toLowerCase() < b.get('title').toLowerCase()) ? 1 : -1));
+    hasMore: computed('nodes.length', function() {
+        if (this.get('nodes.length') < 10) {
+            return false;
         }
-        return nodes.slice(0, this.get('curPage') * 10);
-    }),
-
-    hasMore: computed('totalPages', 'curPage', function() {
         return this.get('totalPages') ? this.get('totalPages') > this.get('curPage') : false;
     }),
 
     actions: {
         more() {
             this.incrementProperty('curPage');
+            this.get('findNodes').perform(true);
         },
         sort(by, order) {
             this.set('sortBy', by);
             this.set('sortOrder', order);
+            this.get('findNodes').perform();
         },
         setSortBy(by) {
             this.set('sortBy', by);
+            this.get('findNodes').perform();
         },
         setSortOrder(order) {
             this.set('sortOrder', order);
+            this.get('findNodes').perform();
         },
         toggleModal() {
             this.toggleProperty('modalOpen');
         },
     },
 
-    findNodes: task(function* (page) {
+    filterNodes: task(function* (term) {
+        this.set('filter', term);
+        yield timeout(500);
+        this.get('findNodes').perform();
+    }).restartable(),
+
+    findNodes: task(function* (more) {
+        this.set(more ? 'loadingMore' : 'loading', true);
         const user = yield this.get('currentUser.user');
-        const nodes = yield user.query('nodes', { page, embed: 'contributors' });
-        this.get('_nodes').pushObjects(nodes.slice());
-    }).enqueue(),
+        const query = { embed: 'contributors', filter: {} };
+        query.sort = `${this.get('sortOrder') === 'desc' ? '-' : ''}${this.get('sortBy')}`;
+        query.filter.title = this.get('filter');
+        if (more) {
+            query.page = this.get('curPage');
+        } else {
+            this.set('loading', true);
+            query.page = 1;
+            this.set('curPage', 1);
+        }
+        const nodes = yield user.query('nodes', query);
+        const pages = Math.ceil(nodes.meta.pagination.total / nodes.meta.pagination.per_page);
+        this.set('totalPages', pages);
+        this.set(more ? 'loadingMore' : 'loading', false);
+        if (more) {
+            this.get('nodes').pushObjects(nodes.slice());
+        } else {
+            this.set('nodes', nodes.slice());
+        }
+    }).restartable(),
 
     getPopularAndNoteworthy: task(function* (id, dest) {
         // const url = prodLinkedNodePath(id);
