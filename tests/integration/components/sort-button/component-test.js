8566bb05fbfe716f1c534144a40b7d801d47051d
Henrique Harman <henriqueharman@gmail.com>
2018-01-23 16:44:49 -0500

    Add sort-button component testing

--- tests/integration/components/sort-button/component-test.js
+++ tests/integration/components/sort-button/component-test.js
@@ -3,9 +3,42 @@ import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('sort-button', 'Integration | Component | sort button', {
     integration: true,
+    beforeEach() {
+        this.set('sortAction', () => {});
+    },
 });
 
-test('it renders', function(assert) {
-    this.render(hbs`{{sort-button}}`);
-    assert.equal(this.$().text().trim(), '');
+test('ascending points up, selected works', function(assert) {
+    this.render(hbs`{{sort-button sort=sortAction curOrder='asc' sortOrder='asc'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') === -1, 'sortOrder matches, should be selected');
+    assert.ok(this.$().html().indexOf('-up') !== -1, 'sortOrder is asc, should point up');
+
+    this.render(hbs`{{sort-button sort=sortAction curOrder='desc' sortOrder='asc'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') !== -1, 'sortOrder doesn\'t match, should be unselected');
+    assert.ok(this.$().html().indexOf('-down') === -1, 'sortOrder is asc, should point up');
 });
+
+test('descending points down, selected works', function(assert) {
+    this.render(hbs`{{sort-button sort=sortAction curOrder='desc' sortOrder='desc'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') === -1, 'sortOrder matches, should be selected');
+    assert.ok(this.$().html().indexOf('-down') !== -1, 'sortOrder is desc should point down');
+
+    this.render(hbs`{{sort-button sort=sortAction curOrder='asc' sortOrder='desc'}}`);
+    assert.ok(this.$().html().indexOf('selected') !== -1, 'sortOrder doesn\'t match, should be unselected');
+    assert.ok(this.$().html().indexOf('-up') === -1, 'sortOrder is desc should point down');
+});
+
+test('selected works with sortBy', function(assert) {
+    this.render(hbs`{{sort-button sort=sortAction curOrder='desc' sortOrder='desc' curBy='speed' sortBy='kindness'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') !== -1, 'sortBy and sortOrder don\'t both match, should be unselected');
+
+    this.render(hbs`{{sort-button sort=sortAction curOrder='desc' sortOrder='asc' curBy='kindness' sortBy='kindness'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') !== -1, 'sortBy and sortOrder don\'t both match, should be unselected');
+
+    this.render(hbs`{{sort-button sort=sortAction curOrder='desc' sortOrder='asc' curBy='speed' sortBy='kindness'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') !== -1, 'sortBy and sortOrder don\'t both match, should be unselected');
+
+    this.render(hbs`{{sort-button sort=sortAction curOrder='desc' sortOrder='desc' curBy='kindness' sortBy='kindness'}}`);
+    assert.ok(this.$().html().indexOf('not-selected') === -1, 'sortBy and sortOrder both match, should be selected');
+});
+
