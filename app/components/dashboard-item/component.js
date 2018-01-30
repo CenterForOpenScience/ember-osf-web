8cc38fc9599213f17fd7fa1b563f7011b5bbafce
Henrique Harman <henriqueharman@gmail.com>
2018-01-30 10:12:32 -0500

    Wrap inputs in their labels, small refactor to dashboard-item

--- app/components/dashboard-item/component.js
+++ app/components/dashboard-item/component.js
@@ -6,9 +6,9 @@ export default Component.extend({
     private: false,
     ancestry: computed('node', 'node.{parent,root}', function() {
         const rootId = this.get('node.root.id');
+        const rootLink = this.get('node.root.links.self');
         const parentId = this.get('node.parent.id');
         const grandpaLink = this.get('node.parent.links.relationships.parent.links.related.href');
-        const grandpaId = grandpaLink ? grandpaLink.split('nodes')[1].split('/').filter(e => e)[0] : undefined;
         if (!rootId) {
             this.set('private', true);
             if (parentId) {
@@ -21,7 +21,7 @@ export default Component.extend({
         if (parentId === rootId) {
             return `${this.get('node.root.title')} / `;
         }
-        if (grandpaId === rootId) {
+        if (grandpaLink === rootLink) {
             return `${this.get('node.root.title')} / ${this.get('node.parent.title')} / `;
         }
         return `${this.get('node.root.title')} / ... / ${this.get('node.parent.title')} / `;
