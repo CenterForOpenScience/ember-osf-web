7cd752ef3ff35cf4da03a32c9baa910179fe8313
Barrett K. Harber <barrett.harber@gmail.com>
2018-02-16 17:31:12 -0500

    Move beforeModel hook into Route.extend for quickfiles route

--- app/quickfiles/route.ts
+++ app/quickfiles/route.ts
@@ -3,13 +3,13 @@ import { inject as service } from '@ember/service';
 
 import CasAuthenticatedRouteMixin from 'ember-osf/mixins/cas-authenticated-route';
 
-export default class Quickfiles extends Route.extend(CasAuthenticatedRouteMixin) {
-    private currentUser = service('currentUser');
-
-    private beforeModel(this: Quickfiles, transition) {
-        this._super(transition);
+export default class Quickfiles extends Route.extend(CasAuthenticatedRouteMixin, {
+    async beforeModel(this: Quickfiles, transition) {
+        await this._super(transition);
         this.transitionTo('user-quickfiles', this.get('currentUser').get('currentUserId'));
-    }
+    },
+}) {
+    private currentUser = service('currentUser');
 }
 
 declare module '@ember/routing/route' {
