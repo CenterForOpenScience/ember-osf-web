b1e3c590524ca7cc9051b92945c7e7840f2f25bf
Henrique Harman <henriqueharman@gmail.com>
2018-03-09 10:42:49 -0500

    Use ember-wormhole to pass seconday nav to where it needs to go, allowing banner to be placed bellow it

--- app/components/quickfile-nav/component.ts
+++ app/components/quickfile-nav/component.ts
@@ -1,11 +1,3 @@
 import Component from '@ember/component';
 
-export default class QuickfileNav extends Component.extend({
-    tagName: 'nav',
-
-    classNames: ['navbar', 'osf-project-navbar', 'row'],
-
-    attributeBindings: ['role'],
-
-    role: 'navigation',
-}) {}
+export default class QuickfileNav extends Component {}
