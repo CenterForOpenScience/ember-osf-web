b65c84af3813dcefd5780288c3af2e71e5093399
Henrique Harman <henriqueharman@gmail.com>
2018-03-14 09:32:31 -0400

    Fix linting according to tslint.json

--- app/components/osf-footer/component.ts
+++ app/components/osf-footer/component.ts
@@ -1,12 +1,12 @@
 import Component from '@ember/component';
 import { serviceLinks } from 'ember-osf-web/const/service-links';
 
-
 export default class OsfFooter extends Component {
+    supportEmail = 'placeholder';
+    serviceLinks = serviceLinks;
+
     didRender() {
         this._super(...arguments);
         this.set('supportEmail', atob('bWFpbHRvOmNvbnRhY3RAb3NmLmlv'));
     }
-    supportEmail = 'placeholder';
-    serviceLinks = serviceLinks;
 }
