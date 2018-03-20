dccb6e46177cda177851244ccbfcf3bdb57d2a8d
Henrique Harman <hmoco@users.noreply.github.com>
2018-03-20 13:16:42 -0400

    Update component.ts

--- app/components/osf-footer/component.ts
+++ app/components/osf-footer/component.ts
@@ -4,9 +4,5 @@ import { serviceLinks } from 'ember-osf-web/const/service-links';
 
 export default class OsfFooter extends Component {
     serviceLinks = serviceLinks;
-    supportEmail: string = '';
-    constructor() {
-        super(...arguments);
-        this.supportEmail = config.support.supportEmail;
-    }
+    supportEmail: string = config.support.supportEmail;
 }
