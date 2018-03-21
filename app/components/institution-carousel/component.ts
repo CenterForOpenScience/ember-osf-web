08485d9524ce30725161aa7f6843147ec6cf88d2
Abram Booth <abram@cos.io>
2018-03-21 12:49:18 -0400

    Use ember-decorators

--- app/components/institution-carousel/component.ts
+++ app/components/institution-carousel/component.ts
@@ -1,33 +1,20 @@
+import { action, computed } from '@ember-decorators/object';
+import { service } from '@ember-decorators/service';
 import { A } from '@ember/array';
 import Component from '@ember/component';
-import { computed } from '@ember/object';
-import { inject as service } from '@ember/service';
 import chunkArray from 'ember-osf-web/utils/chunk-array';
 
 // TODO generalize this as carousel-somehting and add it to ember-osf, to be used by both providers
 // and institutions lot of this logic is copied over from
 // ember-osf-preprints/app/components/provider-carousel (h/t @pattisdr)
-export default class InstitutionCarousel extends Component.extend({
-    store: service('store'),
+export default class InstitutionCarousel extends Component {
+    @service store;
 
-    itemsPerSlide: 5,
+    itemsPerSlide: number = 5;
+    institutions = A();
 
-    institutions: A(),
-
-    didInsertElement() {
-        this.$('.carousel').carousel();
-    },
-
-    actions: {
-        prev() {
-            this.decrementProperty('index');
-        },
-        next() {
-            this.incrementProperty('index');
-        },
-    },
-}) {
-    slides = computed('institutions.[]', 'itemsPerSlide', function(this: InstitutionCarousel) {
+    @computed('institutions.[]', 'itemsPerSlide')
+    get slides(this: InstitutionCarousel) {
         const institutions = this.get('institutions').slice();
 
         const cos = this.get('store').peekRecord('institution', 'cos');
@@ -37,11 +24,26 @@ export default class InstitutionCarousel extends Component.extend({
         }
 
         return A(chunkArray(institutions, this.get('itemsPerSlide')));
-    });
+    }
 
-    columnOffset = computed('institutions.length', 'itemsPerSlide', function() {
+    @computed('institutions.length', 'itemsPerSlide')
+    get columnOffset(this: InstitutionCarousel) {
         const numInstitutions = this.get('institutions.length');
         const itemsPerSlide = this.get('itemsPerSlide');
         return numInstitutions <= itemsPerSlide ? itemsPerSlide - numInstitutions - 1 : 1;
-    });
+    }
+
+    didInsertElement(this: InstitutionCarousel) {
+        this.$('.carousel').carousel();
+    }
+
+    @action
+    prev(this: InstitutionCarousel) {
+        this.decrementProperty('index');
+    }
+
+    @action
+    next(this: InstitutionCarousel) {
+        this.incrementProperty('index');
+    }
 }
