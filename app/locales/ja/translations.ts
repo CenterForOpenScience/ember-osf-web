f16dc0123e4150008b2951bba8f3bf503704df35
Yusuke Komiyama <ykomiyama@bi.a.u-tokyo.ac.jp>
2018-03-30 05:12:16 +0900

    minor fix for ja/translations.ts

--- app/locales/ja/translations.ts
+++ app/locales/ja/translations.ts
@@ -434,5 +434,5 @@ export default {
         line1: '本サイトは<strong>{{start}}から{{end}}</strong>({{utc}} UTC)時の間メンテナンスを実施します。',
         line2: 'ご不自由をおかけいたしますが、ご協力どうぞよろしくお願いいたします。',
         title: 'お知らせ:',
-    },
+    }
 };
