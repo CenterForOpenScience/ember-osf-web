27ae0bbd1461c2472a9dd4ba5ea8ff430fe1cc2f
Brian J. Geiger <bgeiger@pobox.com>
2018-04-13 16:40:22 -0400

    Add component for the join-osf-banner

---
+++ tests/integration/components/join-osf-banner/component-test.ts
@@ -0,0 +1,20 @@
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+import { module, test } from 'qunit';
+
+module('Integration | Component | join-osf-banner', hooks => {
+    setupRenderingTest(hooks);
+
+    test('it renders', async assert => {
+        // Set any properties with this.set('myProperty', 'value');
+        // Handle any actions with this.set('myAction', function(val) { ... });
+
+        // Template block usage:
+        await render(hbs`
+      {{#join-osf-banner}}
+      {{/join-osf-banner}}
+    `);
+        assert.equal(true, true);
+    });
+});
