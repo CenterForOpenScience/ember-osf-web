8a4618a8fe72f470d119f8ef4d8a28c66e8063dd
Henrique Harman <henriqueharman@gmail.com>
2018-04-16 09:22:56 -0400

    Remove unused activeTab

--- app/components/node-navbar/component.ts
+++ app/components/node-navbar/component.ts
@@ -3,20 +3,8 @@ import Component from '@ember/component';
 import Node from 'ember-osf-web/models/node';
 import defaultTo from 'ember-osf-web/utils/default-to';
 
-enum Tabs {
-    Title = 'title',
-    Files = 'files',
-    Wiki = 'wiki',
-    Analytics = 'analytics',
-    Registrations = 'registrations',
-    Contributors = 'contributors',
-    Addons = 'addons',
-    Settings = 'settings',
-}
-
 export default class NodeNavbar extends Component {
     node: Node;
-    activeTab?: Tabs;
     allowComments?: boolean;
     renderInPlace?: boolean = defaultTo(this.renderInPlace, false);
 
