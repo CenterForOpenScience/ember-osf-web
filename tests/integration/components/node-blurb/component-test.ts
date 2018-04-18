995fb3762eb39be0fb7696abf2a29acbc9d6f3f8
Henrique Harman <henriqueharman@gmail.com>
2018-04-18 14:40:59 -0400

    A11y fixes, fix simple-paginator, add request error handling

--- tests/integration/components/node-blurb/component-test.ts
+++ tests/integration/components/node-blurb/component-test.ts
@@ -8,10 +8,9 @@ module('Integration | Component | node-blurb', hooks => {
     setupRenderingTest(hooks);
 
     test('it renders', async function(assert) {
-        // Set any properties with this.set('myProperty', 'value');
-        // Handle any actions with this.set('myAction', function(val) { ... });
         this.set('contributors', []);
-        await render(hbs`{{node-blurb contributors=contributors}}`);
+        this.set('node', { queryHasMany: () => [] });
+        await render(hbs`{{node-blurb contributors=contributors node=node}}`);
 
         assert.ok(this.element.textContent.trim());
     });
