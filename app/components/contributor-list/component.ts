a087d9cdff05e6518f025cf93aaba841e8ab08dc
Henrique Harman <henriqueharman@gmail.com>
2018-04-20 09:30:04 -0400

    Update contributor-list to allow the use of links for each contributor

--- app/components/contributor-list/component.ts
+++ app/components/contributor-list/component.ts
@@ -4,36 +4,77 @@ import { service } from '@ember-decorators/service';
 import { A } from '@ember/array';
 import Component from '@ember/component';
 
+interface Contributor {
+    title: string;
+    id: string | undefined;
+}
+
 @tagName('span')
 export default class ContributorList extends Component {
     @service i18n;
-
+    max = 3;
+    nodeId?: string;
+    useLink?: boolean;
     contributors = this.contributors || A([]);
 
     @computed('contributors.[]')
-    get contributorList(this: ContributorList): string {
+    get contributorList(this: ContributorList): Contributor[] {
+        if (!this.get('contributors')) {
+            return [];
+        }
         const contributors = this.get('contributors').toArray();
 
         if (!(contributors && contributors.length)) {
-            return '';
+            return [];
         }
 
-        const len = contributors.length;
-        const max = 3;
+        const names: Contributor[] = contributors
+            .slice(0, this.get('max'))
+            .map(c => ({
+                title: c.get('users.familyName') || c.get('users.givenName') || c.get('users.fullName'),
+                id: c.get('users.id'),
+            }));
 
-        const names: string[] = contributors
-            .slice(0, max)
-            .map(c => c.get('users.familyName') || c.get('users.givenName') || c.get('users.fullName'));
+        return names;
+    }
 
-        const i18n = this.get('i18n');
-        const and = i18n.t('general.and');
-        const more = i18n.t('general.more');
+    @computed('contributorList')
+    get first(this: ContributorList): Contributor | undefined {
+        return this.get('contributorList').slice(0, 1)[0];
+    }
 
-        if (len < 3) {
-            return names.join(` ${and} `);
+    @computed('contributorList', 'contributors.[]')
+    get last(this: ContributorList): Contributor | void {
+        const contributors = this.get('contributorList');
+
+        if (contributors.length < 2) {
+            return;
         }
 
-        const last = len <= max ? names.splice(-1) : `${len - max} ${more}`;
-        return [...names, `${and} ${last}`].join(', ');
+        const len = this.get('contributors').toArray().length;
+
+        if (len <= this.get('max')) {
+            return contributors.splice(-1)[0];
+        }
+        return {
+            title: `${len - this.get('max')} ${this.get('i18n').t('general.more')}`,
+            id: this.get('nodeId'),
+        };
+    }
+
+    @computed('contributorList', 'contributors.[]')
+    get rest(this: ContributorList): Contributor[] {
+        const contributors = this.get('contributorList');
+
+        if (!(contributors && contributors.length)) {
+            return [];
+        }
+
+        const len = this.get('contributors').toArray().length;
+
+        if (len <= this.get('max')) {
+            return contributors.slice(1, contributors.length - 1);
+        }
+        return contributors.slice(1, this.get('max'));
     }
 }
---
+++ tests/integration/components/contributor-list/contributor/component-test.ts
@@ -0,0 +1,14 @@
+import { render } from '@ember/test-helpers';
+import { setupRenderingTest } from 'ember-qunit';
+import { module, test } from 'qunit';
+
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | contributor-list/contributor', hooks => {
+    setupRenderingTest(hooks);
+
+    test('it renders', async function(assert) {
+        await render(hbs`{{contributor-list/contributor}}`);
+        assert.equal(this.element.textContent.trim(), '');
+    });
+});
