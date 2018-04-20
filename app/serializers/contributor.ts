8f5f9211d38721b4219a88140817d750e729113a
Abram Booth <abram@cos.io>
2018-04-20 15:30:42 -0400

    Revert users => user rename

--- app/serializers/contributor.ts
+++ app/serializers/contributor.ts
@@ -1,10 +1,6 @@
 import OsfSerializer from './osf-serializer';
 
 export default class Contributor extends OsfSerializer {
-    attrs = {
-        ...this.attrs, // from OsfSerializer
-        user: 'users',
-    };
 }
 
 declare module 'ember-data' {
