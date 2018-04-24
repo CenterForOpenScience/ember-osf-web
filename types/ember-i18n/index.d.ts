81345c9b5ad2ef1ca7dd7b26a2e50184f5d207c0
James C. Davis <james@cos.io>
2018-04-24 15:15:34 -0400

    Add stub ember-i18n type defs

---
+++ types/ember-i18n/index.d.ts
@@ -0,0 +1,11 @@
+declare module 'ember-i18n/services/i18n' {
+    import { A } from '@ember/array';
+    import Computed from '@ember/object/computed';
+    import Service from '@ember/service';
+
+    export default class I18N extends Service {
+        locales: Computed<A>;
+        locale: string;
+        t(key: string): string;
+    }
+}
