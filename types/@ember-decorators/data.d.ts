23d8fcf451c903b562b3ca65ca1182736d902e2a
James C. Davis <james@cos.io>
2018-04-25 03:33:25 -0400

    Remove unnecessary ambient module declaration

--- types/@ember-decorators/data.d.ts
+++ types/@ember-decorators/data.d.ts
@@ -1,29 +1,27 @@
-declare module '@ember-decorators/data' {
-    import { ModelRegistry, TransformRegistry } from 'ember-data';
+import { ModelRegistry, TransformRegistry } from 'ember-data';
 
-    interface AttrOptions<T> {
-        defaultValue?: T;
-        allowNull?: boolean;
-    }
-
-    export function attr<K extends keyof TransformRegistry>(
-        type?: K,
-        options?: AttrOptions<TransformRegistry[K]>,
-    ): PropertyDecorator;
-
-    interface RelationshipOptions<Model> {
-        async?: boolean;
-        inverse?: keyof Model | null;
-        polymorphic?: boolean;
-    }
-
-    export function hasMany<K extends keyof ModelRegistry>(
-        type: K,
-        options?: RelationshipOptions<ModelRegistry[K]>,
-    ): PropertyDecorator;
-
-    export function belongsTo<K extends keyof ModelRegistry>(
-        type: K,
-        options?: RelationshipOptions<ModelRegistry[K]>,
-    ): PropertyDecorator;
+interface AttrOptions<T> {
+    allowNull?: boolean;
+    defaultValue?: T;
 }
+
+export function attr<K extends keyof TransformRegistry>(
+    type?: K,
+    options?: AttrOptions<TransformRegistry[K]>,
+): PropertyDecorator;
+
+interface RelationshipOptions<Model> {
+    async?: boolean;
+    inverse?: keyof Model | null;
+    polymorphic?: boolean;
+}
+
+export function hasMany<K extends keyof ModelRegistry>(
+    type: K,
+    options?: RelationshipOptions<ModelRegistry[K]>,
+): PropertyDecorator;
+
+export function belongsTo<K extends keyof ModelRegistry>(
+    type: K,
+    options?: RelationshipOptions<ModelRegistry[K]>,
+): PropertyDecorator;
