9e0cd13723d801304c52d52e0a0fed80c20d49f1
James C. Davis <james@cos.io>
2018-05-16 15:30:15 -0400

    fudge the Application.create return type

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -5,7 +5,7 @@ import Application from 'ember-osf-web/app';
 import manifest from 'ember-osf-web/config/asset-manifest';
 import { start } from 'ember-qunit';
 
-setApplication(Application.create(config.APP));
+setApplication(Application.create(config.APP) as any);
 
 (async () => {
     // This ensures all engine resources are loaded before the tests
