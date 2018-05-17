90c1553478c94ea98cdab186fa6fadee3c49140c
James C. Davis <james@cos.io>
2018-05-17 08:58:09 -0400

    Add helpers for setting up tests for things in engines

---
+++ tests/helpers/engines.ts
@@ -0,0 +1,14 @@
+import engineResolverFor from 'ember-engines/test-support/engine-resolver-for';
+import { setupApplicationTest, setupRenderingTest, setupTest } from 'ember-qunit';
+
+export function setupEngineTest(hooks: any, engine: string) {
+    setupTest(hooks, { resolver: engineResolverFor(engine) });
+}
+
+export function setupEngineRenderingTest(hooks: any, engine: string) {
+    setupRenderingTest(hooks, { resolver: engineResolverFor(engine) });
+}
+
+export function setupEngineApplicationTest(hooks: any, engine: string) {
+    setupApplicationTest(hooks, { resolver: engineResolverFor(engine) });
+}
