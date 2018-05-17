963973b257e746cc8fe89d909512aea87aaeaf18
James C. Davis <james@cos.io>
2018-05-17 08:59:06 -0400

    Refactor collections route tests to use new test syntax

--- tests/unit/collections/provider/discover/route-test.ts
+++ tests/unit/collections/provider/discover/route-test.ts
@@ -1,15 +1,11 @@
-import engineResolverFor from 'ember-engines/test-support/engine-resolver-for';
-import { moduleFor, test } from 'ember-qunit';
+import { setupEngineTest } from 'ember-osf-web/tests/helpers/engines';
+import { module, test } from 'qunit';
 
-const resolver = engineResolverFor('collections');
+module('Unit | Route | collections/provider/discover', hooks => {
+    setupEngineTest(hooks, 'collections');
 
-moduleFor('route:provider/discover', 'Unit | Route | collections/provider/discover', {
-    needs: [
-    ],
-    resolver,
-});
-
-test('it exists', function(assert) {
-    const route = this.subject();
-    assert.ok(route);
+    test('it exists', function(assert) {
+        const route = this.owner.lookup('route:provider/discover');
+        assert.ok(route);
+    });
 });
