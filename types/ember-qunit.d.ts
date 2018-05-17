66121f853abb145f5035b459d9bc7cae45248508
James C. Davis <james@cos.io>
2018-05-17 08:56:59 -0400

    Override types for setup*Test to include options

--- types/ember-qunit.d.ts
+++ types/ember-qunit.d.ts
@@ -1,4 +1,6 @@
 declare module 'ember-qunit' {
+    import Resolver from '@ember/resolver';
+
     export interface StartOptions {
         loadTests?: boolean;
         setupTestContainer?: boolean;
@@ -9,4 +11,12 @@ declare module 'ember-qunit' {
     }
 
     export function start(options?: StartOptions): void;
+
+    export interface SetupTestOptions {
+        resolver: Resolver;
+    }
+
+    export function setupTest(hooks: NestedHooks, options?: SetupTestOptions): void;
+    export function setupRenderingTest(hooks: NestedHooks, options?: SetupTestOptions): void;
+    export function setupApplicationTest(hooks: NestedHooks, options?: SetupTestOptions): void;
 }
