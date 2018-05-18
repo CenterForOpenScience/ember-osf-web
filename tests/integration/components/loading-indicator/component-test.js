0d5cf173493b600df222970136fd1e1d4d9cdcbd
James C. Davis <james@cos.io>
2018-05-18 16:50:44 -0400

    improve loading indicator test

--- tests/integration/components/loading-indicator/component-test.js
+++ tests/integration/components/loading-indicator/component-test.js
@@ -1,13 +1,22 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
+import injectCustomAssertions from 'ember-osf-web/tests/helpers/inject-custom-assertions';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | loading indicator', function(hooks) {
     setupRenderingTest(hooks);
+    injectCustomAssertions(hooks);
 
-    test('it renders', async function(assert) {
+    test('light', async function(assert) {
         await render(hbs`{{loading-indicator}}`);
-        assert.equal(this.$().text().trim(), '');
+        assert.hasText(this.element, '', 'is empty element');
+        assert.hasClass(this.element.querySelector('div.ball-scale'), 'ball-light');
+    });
+
+    test('dark', async function(assert) {
+        await render(hbs`{{loading-indicator dark=true}}`);
+        assert.hasText(this.element, '', 'is empty element');
+        assert.hasClass(this.element.querySelector('div.ball-scale'), 'ball-dark');
     });
 });
