d15cb434cce49c3f6c2e0e75ff57e6d9c283138f
James C. Davis <james@cos.io>
2018-05-23 16:05:26 -0400

    Convert factories to TS

--- types/ember-data-factory-guy/index.d.ts
+++ types/ember-data-factory-guy/index.d.ts
@@ -1,3 +1,6 @@
 export function make(factory: string, model?: object);
 export function setupFactoryGuy(hooks: any);
 export function manualSetup(context: any);
+export function define(factory: string, model?: object);
+export function hasMany(factory: string, count?: number, trait?: string);
+export function belongsTo(factory: string);
