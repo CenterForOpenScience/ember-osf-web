a8ab41bf6b1a35db4a429cf0ffa8eee7ae48d0b1
Abram Booth <abram@cos.io>
2018-05-24 14:48:53 -0400

    Add c3 and d3

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -114,6 +114,15 @@ module.exports = function(defaults) {
     app.import('node_modules/dropzone/dist/dropzone.css');
     app.import('node_modules/dropzone/dist/dropzone.js');
 
+    // TODO: Move analytics page to an engine to get these out of the main build
+    app.import('node_modules/d3/d3.min.js');
+    app.import('node_modules/c3/c3.min.css');
+    app.import('node_modules/c3/c3.min.js', {
+        using: [
+            { transformation: 'amd', as: 'c3' },
+        ],
+    });
+
     app.import({
         test: 'vendor/ember/ember-template-compiler.js',
     });
