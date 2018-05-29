f24ac65a70192afc23d8094663f8577bfe3dd316
Henrique Harman <henriqueharman@gmail.com>
2018-05-29 11:33:22 -0400

    Different support link on institutions, change contact us on institutions page as well

--- app/locales/zh/translations.ts
+++ app/locales/zh/translations.ts
@@ -531,7 +531,7 @@ export default {
     institutions: {
         description: 'OSF Institutions is a free scholarly web tool that enhances transparency, fosters collaboration, and increases the visibility of research outputs at the institutional level.',
         read_more: 'Read more',
-        footer: 'Interested in setting up an OSF Institutions page for your research institution? <a href=\'mailto:contact@osf.io\'>Contact us</a>',
+        footer: 'Interested in setting up an OSF Institutions page for your research institution? <a href=\'http://cos.io/contact\'>Contact us</a>',
         title: 'Institutions',
         search_placeholder: 'Search institutions',
     },
