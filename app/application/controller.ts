2175572a344656d47cc96ea83218edf06d927b28
James C. Davis <james@cos.io>
2018-06-27 21:28:26 -0400

    Disable page headers for handbook

--- app/application/controller.ts
+++ app/application/controller.ts
@@ -1,6 +1,8 @@
 import { action } from '@ember-decorators/object';
+import { match } from '@ember-decorators/object/computed';
 import { service } from '@ember-decorators/service';
 import Controller from '@ember/controller';
+import { Registry as Services } from '@ember/service';
 import config from 'ember-get-config';
 import CurrentUser from 'ember-osf-web/services/current-user';
 import pathJoin from 'ember-osf-web/utils/path-join';
@@ -8,9 +10,12 @@ import $ from 'jquery';
 
 export default class Application extends Controller {
     @service currentUser!: CurrentUser;
+    @service router!: Services['router'];
 
     secondaryNavbarId = config.secondaryNavbarId;
     signupUrl = `${pathJoin(config.OSF.url, 'register')}?${$.param({ next: window.location.href })}`;
+
+    @match('router.currentRouteName', /^handbook/) disableHeaders!: boolean;
 
     @action
     login() {
