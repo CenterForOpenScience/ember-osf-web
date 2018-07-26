76455d73ecd2286966ac43563b51992a65c673c4
Brian J. Geiger <bgeiger@pobox.com>
2018-07-26 13:50:59 -0400

    Add filtering tests and ensure test messages are useful

--- tests/acceptance/dashboard-test.ts
+++ tests/acceptance/dashboard-test.ts
@@ -1,4 +1,4 @@
-import { click, currentURL, visit } from '@ember/test-helpers';
+import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-osf-web/tests/helpers/osf-qunit';
 import { module, test } from 'qunit';
 
@@ -18,7 +18,7 @@ module('Acceptance | dashboard', hooks => {
 
         await visit('/dashboard');
 
-        assert.equal(currentURL(), '/dashboard');
+        assert.equal(currentURL(), '/dashboard', 'We stayed on the proper page');
         assert.found('nav.navbar');
         assert.hasText('nav.navbar .service-name', 'OSF HOME');
         assert.hasText('nav.navbar .secondary-nav-dropdown .nav-profile-name', currentUser.fullName);
@@ -39,10 +39,10 @@ module('Acceptance | dashboard', hooks => {
         );
         // Item 6 should be in page 2 of the carousel and thus not visible
         if (nextPageItem !== null) {
-            assert.equal(nextPageItem.clientWidth, 0, 'Verify that the item is invisible 1/2');
-            assert.equal(nextPageItem.clientHeight, 0, 'Verify that the item is invisible 2/2');
+            assert.equal(nextPageItem.clientWidth, 0, 'Item is invisible since it is on another page 1/2');
+            assert.equal(nextPageItem.clientHeight, 0, 'Item is invisible since it is on another page 2/2');
         } else {
-            assert.ok(false, 'Check for non-existence of item 6 in the institutions list');
+            assert.ok(false, 'Item 6 in the institutions list exists on page load');
         }
 
         // Click next to make item six visible
@@ -59,10 +59,10 @@ module('Acceptance | dashboard', hooks => {
             `[class*="InstitutionCarousel__item"] img[name*="${institutions[6].name}"]`,
         );
         if (nextPageItem !== null) {
-            assert.notEqual(nextPageItem.clientWidth, 0, 'Verify that the item is visible 1/2');
-            assert.notEqual(nextPageItem.clientHeight, 0, 'Verify that the item is visible 2/2');
+            assert.notEqual(nextPageItem.clientWidth, 0, 'Item is visible now that it is on the current page 1/2');
+            assert.notEqual(nextPageItem.clientHeight, 0, 'Item is visible now that it is on the current page 2/2');
         } else {
-            assert.ok(false, 'Check for existence of item 6 in the institutions list after clicking');
+            assert.ok(false, 'Item 6 in the institutions list exists after clicking');
         }
     });
 
@@ -135,14 +135,14 @@ module('Acceptance | dashboard', hooks => {
         // There should be a control to load more projects
         assert.found('#loadMoreProjects');
         let projects = this.element.querySelectorAll('div[class*="DashboardItem"] div[class="row"]');
-        assert.equal(projects.length, 10);
+        assert.equal(projects.length, 10, 'Only the first page of projects loaded');
         await click('#loadMoreProjects');
         projects = this.element.querySelectorAll('div[class*="DashboardItem"] div[class="row"]');
-        assert.equal(projects.length, 20);
+        assert.equal(projects.length, 20, 'Only the first two pages of projects are loaded after clicking `more` once');
         assert.found('#loadMoreProjects');
         await click('#loadMoreProjects');
         projects = this.element.querySelectorAll('div[class*="DashboardItem"] div[class="row"]');
-        assert.equal(projects.length, 30);
+        assert.equal(projects.length, 30, 'All 30 projects are loaded after clicking `more` twice');
 
         // All projects loaded, should not have a loading control any more
         assert.notFound('#loadMoreProjects');
@@ -180,41 +180,90 @@ module('Acceptance | dashboard', hooks => {
 
         // Default sort
         let projectTitles = this.element.querySelectorAll('.di-title>strong');
-        assert.equal(projectTitles.length, 3);
-        assert.equal(projectTitles[0].innerHTML, 'z', 'default sort no filtering item 0');
-        assert.equal(projectTitles[1].innerHTML, 'a', 'default sort no filtering item 1');
-        assert.equal(projectTitles[2].innerHTML, 'az', 'default sort no filtering item 2');
+        assert.equal(projectTitles.length, 3, 'Proper number of items are in list in default sort');
+        assert.equal(projectTitles[0].innerHTML, 'z', 'Default sort item 0 is in proper position');
+        assert.equal(projectTitles[1].innerHTML, 'a', 'Default sort item 1 is in proper position');
+        assert.equal(projectTitles[2].innerHTML, 'az', 'Default sort item 2 is in proper position');
 
         // Sort date ascending
         await click('#last_loggedAscendingSort');
         projectTitles = this.element.querySelectorAll('.di-title>strong');
-        assert.equal(projectTitles.length, 3);
-        assert.equal(projectTitles[0].innerHTML, 'az', 'date asc sort no filtering item 0');
-        assert.equal(projectTitles[1].innerHTML, 'a', 'date asc sort no filtering item 1');
-        assert.equal(projectTitles[2].innerHTML, 'z', 'date asc sort no filtering item 2');
+        assert.equal(projectTitles.length, 3, 'Proper number of items are in list in date asc sort');
+        assert.equal(projectTitles[0].innerHTML, 'az', 'Date asc sort item 0 is in proper position');
+        assert.equal(projectTitles[1].innerHTML, 'a', 'Date asc sort item 1 is in proper position');
+        assert.equal(projectTitles[2].innerHTML, 'z', 'Date asc sort item 2 is in proper position');
 
         // Sort date descending (should be same as default)
         await click('#last_loggedDescendingSort');
         projectTitles = this.element.querySelectorAll('.di-title>strong');
-        assert.equal(projectTitles.length, 3);
-        assert.equal(projectTitles[0].innerHTML, 'z', 'date desc sort no filtering item 0');
-        assert.equal(projectTitles[1].innerHTML, 'a', 'date desc sort no filtering item 1');
-        assert.equal(projectTitles[2].innerHTML, 'az', 'date desc sort no filtering item 2');
+        assert.equal(projectTitles.length, 3, 'Proper number of items are in list in date desc sort');
+        assert.equal(projectTitles[0].innerHTML, 'z', 'Date desc sort item 0 is in proper position');
+        assert.equal(projectTitles[1].innerHTML, 'a', 'Date desc sort item 1 is in proper position');
+        assert.equal(projectTitles[2].innerHTML, 'az', 'Date desc sort item 2 is in proper position');
 
         // Sort title ascending
         await click('#titleAscendingSort');
         projectTitles = this.element.querySelectorAll('.di-title>strong');
-        assert.equal(projectTitles.length, 3);
-        assert.equal(projectTitles[0].innerHTML, 'a', 'title asc sort no filtering item 0');
-        assert.equal(projectTitles[1].innerHTML, 'az', 'title asc sort no filtering item 1');
-        assert.equal(projectTitles[2].innerHTML, 'z', 'title asc sort no filtering item 2');
+        assert.equal(projectTitles.length, 3, 'Proper number of items are in list in title asc sort');
+        assert.equal(projectTitles[0].innerHTML, 'a', 'Title asc sort item 0 is in proper position');
+        assert.equal(projectTitles[1].innerHTML, 'az', 'Title asc sort item 1 is in proper position');
+        assert.equal(projectTitles[2].innerHTML, 'z', 'Title asc sort item 2 is in proper position');
 
         // Sort title descending
         await click('#titleDescendingSort');
         projectTitles = this.element.querySelectorAll('.di-title>strong');
-        assert.equal(projectTitles.length, 3);
-        assert.equal(projectTitles[0].innerHTML, 'z', 'title desc sort no filtering item 0');
-        assert.equal(projectTitles[1].innerHTML, 'az', 'title desc sort no filtering item 1');
-        assert.equal(projectTitles[2].innerHTML, 'a', 'title desc sort no filtering item 2');
+        assert.equal(projectTitles.length, 3, 'Proper number of items are in list in title desc sort');
+        assert.equal(projectTitles[0].innerHTML, 'z', 'Title desc sort item 0 is in proper position');
+        assert.equal(projectTitles[1].innerHTML, 'az', 'Title desc sort item 1 is in proper position');
+        assert.equal(projectTitles[2].innerHTML, 'a', 'Title desc sort item 2 is in proper position');
+    });
+
+    test('filtering projects', async function(assert) {
+        const currentUser = server.create('user');
+        server.create('root', { currentUser });
+        const nodeOne = server.create(
+            'node',
+            { title: 'z', lastLogged: '2017-10-19T12:05:10.571Z', dateModified: '2017-10-19T12:05:10.571Z' },
+        );
+        const nodeTwo = server.create(
+            'node',
+            { title: 'az', lastLogged: '2017-10-17T12:05:10.571Z', dateModified: '2017-10-17T12:05:10.571Z' },
+        );
+        const nodeThree = server.create(
+            'node',
+            { title: 'a', lastLogged: '2017-10-18T12:05:10.571Z', dateModified: '2017-10-18T12:05:10.571Z' },
+        );
+        server.create(
+            'contributor',
+            { node: nodeOne, users: currentUser, index: 0, permission: 'admin', bibliographic: true },
+        );
+        server.create(
+            'contributor',
+            { node: nodeTwo, users: currentUser, index: 0, permission: 'admin', bibliographic: true },
+        );
+        server.create(
+            'contributor',
+            { node: nodeThree, users: currentUser, index: 0, permission: 'admin', bibliographic: true },
+        );
+        await visit('/dashboard');
+        assert.notFound('img[alt*="Missing translation"]');
+
+        // No filtering
+        let projectTitles = this.element.querySelectorAll('.di-title>strong');
+        assert.equal(projectTitles.length, 3, 'Default filtering has correct number of projects');
+        assert.equal(projectTitles[0].innerHTML, 'z', 'Default filtering item 0 is correct');
+        assert.equal(projectTitles[1].innerHTML, 'a', 'Default filtering item 1 is correct');
+        assert.equal(projectTitles[2].innerHTML, 'az', 'Default filtering item 2 is correct');
+
+        await fillIn('#quickSearchInput', 'z');
+        projectTitles = this.element.querySelectorAll('.di-title>strong');
+        assert.equal(projectTitles.length, 2, 'One character filtering has correct number of projects');
+        assert.equal(projectTitles[0].innerHTML, 'z', 'One character filtering item 0 is correct');
+        assert.equal(projectTitles[1].innerHTML, 'az', 'One character filtering item 1 is correct');
+
+        await fillIn('#quickSearchInput', 'az');
+        projectTitles = this.element.querySelectorAll('.di-title>strong');
+        assert.equal(projectTitles.length, 1, 'Two character filtering has correct number of projects');
+        assert.equal(projectTitles[0].innerHTML, 'az', 'Two character filtering item is correct');
     });
 });
