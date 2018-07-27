1bd94e497e4c2e6f93c5ab91124056610b98e60d
James C. Davis <james@cos.io>
2018-07-27 17:10:20 -0400

    Add placeholder and registrations type to node-blurb

--- lib/osf-components/addon/components/node-blurb/component.ts
+++ lib/osf-components/addon/components/node-blurb/component.ts
@@ -1,4 +1,4 @@
-import { computed } from '@ember-decorators/object';
+import { action, computed } from '@ember-decorators/object';
 import { service } from '@ember-decorators/service';
 import Component from '@ember/component';
 import Node from 'ember-osf-web/models/node';
@@ -10,9 +10,15 @@ import styles from './styles';
 import layout from './template';
 
 enum BlurbType {
-    FORK = 'fork',
-    GENERIC = 'generic',
+    Fork = 'fork',
+    Generic = 'generic',
+    Registration = 'registration',
 }
+
+const schemaWithTitles = [
+    'Prereg Challenge',
+    'Registered Report Protocol Preregistration',
+];
 
 export default class NodeBlurb extends Component {
     layout = layout;
@@ -20,8 +26,15 @@ export default class NodeBlurb extends Component {
 
     @service analytics!: Analytics;
 
+    // Optional arguments
     node?: Node | Registration;
-    blurbType: BlurbType = defaultTo(this.blurbType, BlurbType.FORK);
+    blurbType: BlurbType = defaultTo(this.blurbType, BlurbType.Fork);
+    showTags: boolean = defaultTo(this.showTags, false);
+    onClickTag?: (tag: string) => void;
+
+    // Private properties
+    pendingRegistration = true;
+    archivingRegistration = true;
 
     @computed('node.currentUserPermissions')
     get canEdit(this: NodeBlurb): boolean {
@@ -40,7 +53,33 @@ export default class NodeBlurb extends Component {
     }
 
     @computed('blurbType')
-    get dateLabelKey(this: NodeBlurb): string {
+    get dateLabelKey(): string {
         return `node_blurb.${this.blurbType}.dateLabel`;
+    }
+
+    @computed('blurbType')
+    get placeholderBodyLines(): number {
+        if (this.blurbType === BlurbType.Registration) {
+            return 3;
+        }
+        return 2;
+    }
+
+    @computed('node.registrationSchema.name', 'node.registeredMeta.q1.value')
+    get registrationTitle(): string | undefined {
+        if (this.node && this.node.isRegistration) {
+            const registration = this.node as Registration;
+            if (schemaWithTitles.includes(registration.registrationSchema.get('name'))) {
+                return registration.registeredMeta.q1.value;
+            }
+        }
+        return undefined;
+    }
+
+    @action
+    clickTag(tag: string): void {
+        if (this.onClickTag) {
+            this.onClickTag(tag);
+        }
     }
 }
