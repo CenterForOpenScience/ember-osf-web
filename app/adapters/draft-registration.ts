763afa15de7f2837b360a88f8275ff33f31cb1c4
James C. Davis <james@cos.io>
2018-07-30 09:15:00 -0400

    Override urlForCreateRecord in draft-registration adapter

--- app/adapters/draft-registration.ts
+++ app/adapters/draft-registration.ts
@@ -1,6 +1,22 @@
+import DS from 'ember-data';
+import config from 'ember-get-config';
+
+import pathJoin from 'ember-osf-web/utils/path-join';
+
 import OsfAdapter from './osf-adapter';
 
+const { OSF: { apiUrl, apiNamespace } } = config;
+
 export default class DraftRegistration extends OsfAdapter {
+    urlForCreateRecord(_: string, snapshot: DS.Snapshot) {
+        return pathJoin(
+            apiUrl,
+            apiNamespace,
+            'nodes',
+            snapshot.record.belongsTo('branchedFrom').belongsToRelationship.members.list.firstObject.id,
+            'draft_registrations',
+        );
+    }
 }
 
 declare module 'ember-data' {
