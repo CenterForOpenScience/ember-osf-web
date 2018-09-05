4dc6e67e9a4a9b0a6730ac21255ec6c1ebe4f049
James C. Davis <james@cos.io>
2018-09-05 00:49:56 -0400

    set perms on nodes/registrations in default scenario

--- mirage/scenarios/default.ts
+++ mirage/scenarios/default.ts
@@ -2,6 +2,7 @@ import { Server } from 'ember-cli-mirage';
 import config from 'ember-get-config';
 
 import Node from 'ember-osf-web/models/node';
+import { Permission } from 'ember-osf-web/models/osf-model';
 
 import { draftRegisterNodeMultiple, registerNodeMultiple } from '../helpers';
 
@@ -16,16 +17,20 @@ export default function(server: Server) {
     const currentUser = server.create('user', 'loggedIn');
     const firstNode = server.create('node', {});
     server.create('contributor', { node: firstNode, users: currentUser, index: 0 });
-    const nodes = server.createList<Node>('node', 10, {}, 'withContributors');
+    const nodes = server.createList<Node>('node', 10, {
+        currentUserPermissions: Object.values(Permission),
+    }, 'withContributors');
     server.create('node', {
         id: noteworthyNode,
         linkedNodes: nodes.slice(0, 5),
         title: 'NNW',
+        currentUserPermissions: [Permission.Read],
     });
     server.create('node', {
         id: popularNode,
         linkedNodes: nodes.slice(5, 10),
         title: 'Popular',
+        currentUserPermissions: [Permission.Read],
     });
     for (const node of nodes.slice(4, 10)) {
         server.create('contributor', { node, users: currentUser, index: 11 });
@@ -33,6 +38,8 @@ export default function(server: Server) {
     server.createList('institution', 20);
     server.createList('token', 7);
     server.loadFixtures('registration-schemas');
-    registerNodeMultiple(server, nodes[0], 12, {}, 'withRegisteredMeta');
+    registerNodeMultiple(server, nodes[0], 12, {
+        currentUserPermissions: Object.values(Permission),
+    }, 'withRegisteredMeta');
     draftRegisterNodeMultiple(server, nodes[0], 12, {}, 'withRegistrationMetadata');
 }
