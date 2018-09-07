ad008927b7ed3d209eaa181bf793f2c7398cb354
James C. Davis <james@cos.io>
2018-09-07 13:22:59 -0400

    Create interface to descript RegistrationFactorySchema

--- mirage/factories/registration-schema.ts
+++ mirage/factories/registration-schema.ts
@@ -2,8 +2,12 @@ import { Factory, faker } from 'ember-cli-mirage';
 
 import RegistrationSchema from 'ember-osf-web/models/registration-schema';
 
-// eslint-disable-next-line no-spaced-func, func-call-spacing
-export default Factory.extend<RegistrationSchema & { schemaNoConflict: () => RegistrationSchema['schema'] }>({
+export interface RegistrationSchemaFactory extends
+    Pick<RegistrationSchema, Exclude<keyof RegistrationSchema, 'schema'>> {
+    schemaNoConflict: RegistrationSchema['schema'];
+}
+
+export default Factory.extend<RegistrationSchemaFactory>({
     active: true,
     name() {
         return faker.lorem.sentence().replace('.', '');
