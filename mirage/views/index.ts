6ebd403c771d0a61ec642186d7ca2cd1fb5631e1
James C. Davis <james@cos.io>
2018-09-07 15:46:51 -0400

    Use function syntax for mirage views relationshipList

--- mirage/views/index.ts
+++ mirage/views/index.ts
@@ -1,7 +1,11 @@
 import { process } from './utils';
 
-export const relationshipList = (type: string, relationshipName: string, schema: any, request: any, config: any) => {
-    return process(schema, request, config,
+export function relationshipList(type: string, relationshipName: string, schema: any, request: any, config: any) {
+    return process(
+        schema,
+        request,
+        config,
         schema[type].find(request.params.id)[relationshipName].models.map((model: any) => config.serialize(model).data),
-        {});
-};
+        {},
+    );
+}
