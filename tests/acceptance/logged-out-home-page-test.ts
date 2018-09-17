03e986bfafa6718ead07c99bae687ffa7e559158
James C. Davis <james@cos.io>
2018-09-17 16:17:06 -0400

    Just check for existence of sign up form on home page

--- tests/acceptance/logged-out-home-page-test.ts
+++ tests/acceptance/logged-out-home-page-test.ts
@@ -1,4 +1,4 @@
-import { click, currentURL, fillIn, visit } from '@ember/test-helpers';
+import { currentURL, visit } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupApplicationTest } from 'ember-qunit';
 import { module, test } from 'qunit';
@@ -25,25 +25,7 @@ module('Acceptance | logged-out home page', hooks => {
         assert.dom('footer').exists();
 
         // Check sign-up form.
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').doesNotExist('Empty form: no captcha appears.');
-        await fillIn('[data-test-sign-up-full-name] input', 'Test User');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').doesNotExist(
-            'Filled in fullName: no captcha appears.',
-        );
-        await fillIn('[data-test-sign-up-email-1] input', 'test@user.com');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').doesNotExist('Filled in email1: no captcha appears.');
-        await fillIn('[data-test-sign-up-email-2] input', 'test@user.com');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').doesNotExist('Filled in email2: no captcha appears.');
-        await fillIn('[data-test-sign-up-password] input', 'correct horse battery staple');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').doesNotExist(
-            'Filled in password: no captcha appears.',
-        );
-        await click('[data-test-sign-up-tos] input');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').exists('All fields valid: captcha appears.');
-        await click('[data-test-sign-up-tos] input');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').doesNotExist('Invalidate form: captcha disappears.');
-        await click('[data-test-sign-up-tos] input');
-        assert.dom('[class*="SignUpForm"] [data-test-recaptcha]').exists('Revalidate form: captcha reappears.');
+        assert.dom('[data-test-sign-up-full-name]').exists();
 
         // Alt text for integration logos
         assert.dom('[class*="_integrations"] img[alt*="Dropbox logo"]').exists();
