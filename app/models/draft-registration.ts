9080105baaf605ac288410841327d610893346fa
James C. Davis <james@cos.io>
2018-09-27 09:44:47 -0400

    Fix inverse relationships node:draftRegistrations and node:preprints

--- app/models/draft-registration.ts
+++ app/models/draft-registration.ts
@@ -22,7 +22,7 @@ export default class DraftRegistration extends OsfModel {
     @attr('object') registrationMetadata!: RegistrationMetadata;
     @attr('date') datetimeInitiated!: Date;
     @attr('date') datetimeUpdated!: Date;
-    @belongsTo('node', { inverse: null }) branchedFrom!: DS.PromiseObject<Node> & Node;
+    @belongsTo('node', { inverse: 'draftRegistrations' }) branchedFrom!: DS.PromiseObject<Node> & Node;
     @belongsTo('user', { inverse: null }) initiator!: DS.PromiseObject<User> & User;
     @belongsTo('registration-schema', { inverse: null })
     registrationSchema!: DS.PromiseObject<RegistrationSchema> & RegistrationSchema;
