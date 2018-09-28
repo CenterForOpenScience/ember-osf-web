fd2feaa3265b026f429f388c5ac4fa326bd52e57
James C. Davis <james@cos.io>
2018-09-27 20:14:38 -0400

    Also stub router for osf-navbar component integration test

--- tests/integration/components/osf-navbar/component-test.ts
+++ tests/integration/components/osf-navbar/component-test.ts
@@ -1,10 +1,21 @@
+import Service from '@ember/service';
 import { render } from '@ember/test-helpers';
 import { setupRenderingTest } from 'ember-qunit';
+import { TestContext } from 'ember-test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
+const routerStub = Service.extend({
+    currentURL: '',
+});
+
 module('Integration | Component | osf-navbar', hooks => {
     setupRenderingTest(hooks);
+
+    hooks.beforeEach(function(this: TestContext) {
+        // Make sure currentURL is always a string
+        this.owner.register('service:router', routerStub);
+    });
 
     test('it renders', async function(assert) {
         this.set('loginAction', () => { /* stub */ });
