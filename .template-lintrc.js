fc8cd3d3d5403d4d4e15b61819824decc9df9c03
James C. Davis <james@cos.io>
2018-10-03 12:04:22 -0400

    Downgrade to ember-cli-template_lint 0.7.6

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -7,11 +7,6 @@ module.exports = {
 
     rules: {
         'block-indentation': 4,
-        'attribute-indentation': {
-            indentation: 4,
-            'open-invocation-max-len': 120,
-            'process-elements': true,
-        },
         'no-bare-strings': true,
         'no-nested-interactive': false,
     },
