849f14cbc8a10e274708b3f9267ccf19a9610b8f
Barrett K. Harber <barrett.harber@gmail.com>
2018-10-08 12:44:20 -0400

    Handle top-level collections page-not-found properly

--- lib/collections/addon/application/route.ts
+++ lib/collections/addon/application/route.ts
@@ -1,13 +1,18 @@
+import { service } from '@ember-decorators/service';
 import Route from '@ember/routing/route';
+import Theme from 'ember-osf-web/services/theme';
 
 export default class Application extends Route.extend({
-    beforeModel(transition: any) {
+    beforeModel(this: Application, transition: any) {
         this._super(transition);
 
+        this.theme.set('providerType', 'collection');
+
         // If we're not on a provider, redirect
-        if (!/^collections\.provider/.test(transition.targetName)) {
-            this.transitionToExternal('home');
+        if (!/^collections\.(provider|page-not-found)/.test(transition.targetName)) {
+            return this.transitionToExternal('home');
         }
     },
 }) {
+    @service theme!: Theme;
 }
