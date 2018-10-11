4cb86e27d48c1eb7dfb92b7f2e5335f075f77b97
James C. Davis <james@cos.io>
2018-10-11 09:24:05 -0400

    Add panel component

---
+++ lib/osf-components/addon/components/panel/x-footer/component.ts
@@ -0,0 +1,9 @@
+import { layout, tagName } from '@ember-decorators/component';
+import Component from '@ember/component';
+
+import template from './template';
+
+@layout(template)
+@tagName('')
+export default class PanelFooter extends Component {
+}
