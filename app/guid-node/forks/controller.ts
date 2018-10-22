9d12ffaa5a59d8bfb136ae77b040236dd6cbf90b
James C. Davis <james@cos.io>
2018-10-22 09:51:35 -0400

    chore: s/readOnly/reads/ decorator

--- app/guid-node/forks/controller.ts
+++ app/guid-node/forks/controller.ts
@@ -1,5 +1,5 @@
 import { action, computed } from '@ember-decorators/object';
-import { readOnly } from '@ember-decorators/object/computed';
+import { reads } from '@ember-decorators/object/computed';
 import { service } from '@ember-decorators/service';
 import Controller from '@ember/controller';
 import Analytics from 'ember-osf-web/services/analytics';
@@ -24,7 +24,7 @@ export default class GuidNodeForks extends Controller {
 
     forksQueryParams = { embed: 'contributors' };
 
-    @readOnly('model.taskInstance.value')
+    @reads('model.taskInstance.value')
     node?: Node;
 
     @computed('node')
