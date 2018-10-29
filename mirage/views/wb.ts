53a90688fb18a8277f85a2b25a60fb1a5ad31648
Brian J. Geiger <bgeiger@pobox.com>
2018-10-29 11:57:46 -0400

    Respond to CR

--- mirage/views/wb.ts
+++ mirage/views/wb.ts
@@ -2,8 +2,11 @@ import { HandlerContext, Schema } from 'ember-cli-mirage';
 
 export function moveFile(this: HandlerContext, schema: Schema) {
     const fileId = this.request.params.id;
-    const nodeId = this.request.requestBody.resource;
+    const nodeId = JSON.parse(this.request.requestBody).resource;
     const file = schema.files.find(fileId);
-    file.attrs.userId = null;
-    file.attrs.nodeId = nodeId;
+    file.update({
+        userId: null,
+        nodeId,
+    });
+    return file;
 }
