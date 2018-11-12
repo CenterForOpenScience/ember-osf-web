d4a16f3f12026a5eb726610ba5c3612973c9a709
James C. Davis <james@cos.io>
2018-11-12 15:18:47 -0500

    feat(i18n): added documentType for preprint providers
    Add translations keys for documentType as well as documentType computed
    to preprint model.

--- app/models/preprint-provider.ts
+++ app/models/preprint-provider.ts
@@ -1,10 +1,18 @@
 import { attr, hasMany } from '@ember-decorators/data';
+import { computed } from '@ember-decorators/object';
 import { alias } from '@ember-decorators/object/computed';
+import { service } from '@ember-decorators/service';
+import { getOwner } from '@ember/application';
+import { get } from '@ember/object';
 import DS from 'ember-data';
+import I18N from 'ember-i18n/services/i18n';
+
 import Preprint from './preprint';
 import Provider from './provider';
 
 export default class PreprintProvider extends Provider {
+    @service i18n!: I18N;
+
     @attr('array') subjectsAcceptable!: string[];
     @attr('array') additionalProviders!: string[];
     @attr('string') shareSource!: string;
@@ -24,6 +32,12 @@ export default class PreprintProvider extends Provider {
 
     @alias('links.relationships.highlighted_taxonomies.links.related.meta.has_highlighted_subjects')
     hasHighlightedSubjects!: boolean;
+
+    @computed('i18n.locale', 'preprintWord')
+    get documentType() {
+        const locale = getOwner(this).factoryFor(`locale:${this.i18n.locale}/translations`).class;
+        return get(locale, `documentType.${this.preprintWord}`);
+    }
 }
 
 declare module 'ember-data' {
