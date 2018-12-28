ccf0e13639482ae1a6db3572be2cf09845212d84
James C. Davis <james@cos.io>
2018-12-28 16:36:10 -0500

    refactor: remove unneeded urlForCreateRecord override from user-email adapter

--- app/adapters/user-email.ts
+++ app/adapters/user-email.ts
@@ -1,13 +1,7 @@
-import DS from 'ember-data';
 import OsfAdapter from './osf-adapter';
 
 export default class UserEmailAdapter extends OsfAdapter {
     parentRelationship = 'user';
-
-    urlForCreateRecord(_: string, snapshot: DS.Snapshot) {
-        return snapshot.record.belongsTo('user').value()
-            .links.relationships.emails.links.related.href;
-    }
 }
 
 declare module 'ember-data/types/registries/adapter' {
