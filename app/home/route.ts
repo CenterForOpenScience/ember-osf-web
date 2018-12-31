34c6ef1b0cd9e74fa34d1ddeec58bc011a6e5e56
James C. Davis <james@cos.io>
2018-12-31 17:13:47 -0500

    chore: use new location for Transition type

--- app/home/route.ts
+++ app/home/route.ts
@@ -1,7 +1,7 @@
 import { action } from '@ember-decorators/object';
 import { service } from '@ember-decorators/service';
+import Transition from '@ember/routing/-private/transition';
 import Route from '@ember/routing/route';
-import Ember from 'ember';
 import Session from 'ember-simple-auth/services/session';
 
 import Analytics from 'ember-osf-web/services/analytics';
@@ -12,7 +12,7 @@ export default class Home extends Route {
     @service analytics!: Analytics;
     @service session!: Session;
 
-    async beforeModel(this: Home, transition: Ember.Transition) {
+    async beforeModel(this: Home, transition: Transition) {
         await super.beforeModel(transition);
 
         if (this.session.isAuthenticated) {
@@ -25,7 +25,7 @@ export default class Home extends Route {
         this.get('analytics').trackPage();
     }
 
-    resetController(controller: Controller, isExiting: boolean, _: Ember.Transition) {
+    resetController(controller: Controller, isExiting: boolean, _: Transition) {
         if (isExiting) {
             controller.set('goodbye', null);
         }
