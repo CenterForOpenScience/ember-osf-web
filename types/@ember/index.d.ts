9a8dcdab4467e0967453932d8b7dc9f3dd68469a
James C. Davis <james@cos.io>
2019-01-01 00:57:17 -0500

    chore: move transition type augmentation tp @ember/routing/-private/transition

--- types/@ember/index.d.ts
+++ types/@ember/index.d.ts
@@ -15,35 +15,3 @@ declare module 'ember' {
         }
     }
 }
-
-declare module '@ember/routing/transition' {
-    interface AbstractHandlerInfo {
-        isResolved: boolean;
-        name: string;
-        params?: Record<string, string>;
-    }
-
-    interface HandlerInfo extends AbstractHandlerInfo {
-        isResolved: true;
-    }
-
-    interface UnresolvedHandlerInfo extends AbstractHandlerInfo {
-        isResolved: false;
-    }
-
-    interface TransitionState {
-        queryParams: Record<string, any>;
-    }
-
-    export default interface Transition extends Ember.Transition {
-        handlerInfos: Array<HandlerInfo | UnresolvedHandlerInfo>;
-        params: Record<string, Record<string, string>>;
-        queryParams?: Record<string, any>;
-        resolvedModels: Record<string, any>;
-        state: TransitionState;
-        targetName: string;
-        router: {
-            generate(handlerName: string, ...params: any[]): string;
-        }
-    } // eslint-disable-line semi
-}
