29f767ccd8325189742a6dfea0338102a2f5ef2c
Fabrice Mizero <fabrice@cos.io>
2019-03-12 11:21:49 -0400

    Fix mirage bookmarks

--- mirage/views/collection.ts
+++ mirage/views/collection.ts
@@ -1,18 +1,21 @@
 import { HandlerContext, Schema } from 'ember-cli-mirage';
 
-export function updateBookmarks(
-    this: HandlerContext,
-    schema: Schema,
-    request: any,
-) {
-    const { id: collId } = this.request.params;
-    const { data: [{ id: regId, type }] } = JSON.parse(request.requestBody);
+export function bookmarksRegistrationAdd(this: HandlerContext, schema: Schema) {
+    const { parentID: collId } = this.request.params;
+    const { data: [{ id: regId, type }] } = JSON.parse(this.request.requestBody);
     const bookmarksColl = schema.collections.find(collId);
 
-    const existingLinkedRegistrations: string[] = bookmarksColl.linkedRegistrations.models.mapBy('id');
-    const linkedRegistrations = request.method === 'POST' ?
-        [regId, ...existingLinkedRegistrations] :
-        existingLinkedRegistrations.splice(existingLinkedRegistrations.indexOf(regId), 1);
+    const existingLinkedRegistrations: Array<string|number> = bookmarksColl.linkedRegistrations.models.mapBy('id');
+    const linkedRegistrations = [regId, ...existingLinkedRegistrations];
 
     return { data: linkedRegistrations.map(id => ({ id, type })) };
+}
+
+export function bookmarksRegistrationDelete(this: HandlerContext, schema: Schema) {
+    const { id: regId, parentID } = this.request.params;
+    const bookmarksColl = schema.collections.find(parentID);
+    const existingLinkedRegistrations: Array<string|number> = bookmarksColl.linkedRegistrations.models.mapBy('id');
+    const linkedRegistrations = existingLinkedRegistrations.splice(existingLinkedRegistrations.indexOf(regId), 1);
+
+    return { data: linkedRegistrations.map(id => ({ id, type: 'registrations' })) };
 }
