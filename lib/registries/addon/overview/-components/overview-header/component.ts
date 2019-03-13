e65e0d90a281c514a385ece06ab13ffc518a7387
Herman Rogers <herman.rogers@gmail.com>
2019-03-13 10:27:09 -0400

    Add responsive addon header and updated components

--- lib/registries/addon/overview/-components/overview-header/component.ts
+++ lib/registries/addon/overview/-components/overview-header/component.ts
@@ -1,4 +1,14 @@
+import { action } from '@ember-decorators/object';
 import Component from '@ember/component';
 
 export default class OverviewHeader extends Component {
+    @action
+    acceptRegistration() {
+        return 'accept registration';
+    }
+
+    @action
+    rejectRegistration() {
+        return 'reject registration';
+    }
 }
