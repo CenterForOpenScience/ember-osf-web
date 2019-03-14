6740f3490fb6e5fa286efb36ef2814e589b42dac
Herman Rogers <herman.rogers@gmail.com>
2019-03-14 15:57:10 -0400

    Update test names

--- tests/unit/models/registration-test.ts
+++ tests/unit/models/registration-test.ts
@@ -10,55 +10,55 @@ module('Unit | Model | registration', hooks => {
         assert.ok(!!model);
     });
 
-    test('it selects public as default state', function(assert) {
+    test('registraiton state selects public as default', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
 
         assert.equal(model.get('state'), 'public');
     });
 
-    test('it returns pendingRegistrationApproval if field is true', function(assert) {
+    test('registration state returns pendingRegistrationApproval if field is true', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('pendingRegistrationApproval', true);
 
         assert.equal(model.get('state'), 'pendingRegistrationApproval');
     });
 
-    test('it returns pendingEmbargoApproval if field is true', function(assert) {
+    test('registration state returns pendingEmbargoApproval if field is true', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('pendingEmbargoApproval', true);
 
         assert.equal(model.get('state'), 'pendingEmbargoApproval');
     });
 
-    test('it returns pendingEmbargoTerminationApproval if field is true', function(assert) {
+    test('registration state return pendingEmbargoTerminationApproval if field is true', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('pendingEmbargoTerminationApproval', true);
 
         assert.equal(model.get('state'), 'pendingEmbargoTerminationApproval');
     });
 
-    test('it returns pendingWithdrawal if field is true', function(assert) {
+    test('registartion state returns pendingWithdrawal if field is true', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('pendingWithdrawal', true);
 
         assert.equal(model.get('state'), 'pendingWithdrawal');
     });
 
-    test('it returns withdrawn if field is true', function(assert) {
+    test('registartion state returns withdrawn if field is true', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('withdrawn', true);
 
         assert.equal(model.get('state'), 'withdrawn');
     });
 
-    test('it returns embargoed if field is true', function(assert) {
+    test('registration state returns embargoed if field is true', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('embargoed', true);
 
         assert.equal(model.get('state'), 'embargoed');
     });
 
-    test('it updates registrations state when the model changes', function(assert) {
+    test('registration state updates when the model changes', function(assert) {
         const model = run(() => this.owner.lookup('service:store').createRecord('registration'));
         model.set('embargoed', true);
         assert.equal(model.get('state'), 'embargoed');
