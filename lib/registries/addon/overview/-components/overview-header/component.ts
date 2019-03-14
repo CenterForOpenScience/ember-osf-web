0e12d1b86e05dd8b76e8131f6e540d7eb34e3a06
Herman Rogers <herman.rogers@gmail.com>
2019-03-14 11:17:22 -0400

    Remove uneeded features from the topbar

--- lib/registries/addon/overview/-components/overview-header/component.ts
+++ lib/registries/addon/overview/-components/overview-header/component.ts
@@ -1,14 +1,4 @@
-import { action } from '@ember-decorators/object';
 import Component from '@ember/component';
 
 export default class OverviewHeader extends Component {
-    @action
-    acceptRegistration() {
-        return 'accept registration';
-    }
-
-    @action
-    rejectRegistration() {
-        return 'reject registration';
-    }
 }
