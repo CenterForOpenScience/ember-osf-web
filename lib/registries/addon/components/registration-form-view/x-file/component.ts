b0587c33b41d55eacbf1282057e45ecafe96f959
Abram Booth <abram@cos.io>
2019-03-28 12:04:54 -0400

    chore: remove unnecessary styles

--- lib/registries/addon/components/registration-form-view/x-file/component.ts
+++ lib/registries/addon/components/registration-form-view/x-file/component.ts
@@ -2,13 +2,9 @@ import { tagName } from '@ember-decorators/component';
 import { alias } from '@ember-decorators/object/computed';
 import Component from '@ember/component';
 
-import { layout } from 'ember-osf-web/decorators/component';
 import { Answerable } from '../component';
-import styles from './styles';
-import template from './template';
 
 @tagName('')
-@layout(template, styles)
 export default class ViewFile extends Component {
     answerable!: Answerable;
 
