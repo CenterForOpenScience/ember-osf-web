e88f2bdc9198bf324db0d0d3b86bed2a9775e95d
Baylee Swenson <baylee@cos.io>
2019-04-04 19:51:39 -0400

    Add test for search bar

--- tests/integration/components/discover-research/component-test.ts
+++ tests/integration/components/discover-research/component-test.ts
@@ -1,4 +1,4 @@
-import { render } from '@ember/test-helpers';
+import { fillIn, render, settled, triggerKeyEvent } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
@@ -16,5 +16,17 @@ module('Integration | Component | discover-research', hooks => {
             .hasText('Discover projects, data, materials, and collaborators on OSF that might be helpful ' +
             'to your own research.');
         assert.dom('[data-test-search-bar]').exists();
+    });
+
+    test('search bar works', async function(assert) {
+        assert.expect(1);
+
+        this.set('search', () => { assert.ok(true); });
+        await render(hbs`<DiscoverResearch @onSearch={{action this.search}} />`);
+
+        await fillIn('[data-test-discover-input]', 'search string');
+        settled().then(() => {
+            triggerKeyEvent('[data-test-discover-input]', 'keyup', 'Enter');
+        });
     });
 });
