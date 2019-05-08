3d46190bd7cc3018c61e2e74f75d9d6723474e97
James C. Davis <james@cos.io>
2019-05-08 16:31:55 -0400

    feature: add meetings index route and feature flag

---
+++ app/meetings/index/route.ts
@@ -0,0 +1,14 @@
+import { action } from '@ember-decorators/object';
+import { service } from '@ember-decorators/service';
+import Route from '@ember/routing/route';
+
+import Analytics from 'ember-osf-web/services/analytics';
+
+export default class MeetingsIndex extends Route {
+    @service analytics!: Analytics;
+
+    @action
+    didTransition() {
+        this.analytics.trackPage();
+    }
+}
