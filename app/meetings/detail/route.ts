95379002a790219b6e39a9a0b21fee53527d17cc
Yuhuai Liu <yuhuai@cos.io>
2019-05-16 11:42:44 -0400

    add route

--- app/meetings/detail/route.ts
+++ app/meetings/detail/route.ts
@@ -3,21 +3,12 @@ import { service } from '@ember-decorators/service';
 import Route from '@ember/routing/route';
 
 import Analytics from 'ember-osf-web/services/analytics';
-import { task } from 'ember-concurrency';
-
 
 export default class MeetingsDetail extends Route {
     @service analytics!: Analytics;
 
-    getSubmissions = task(function* (this: MeetingsDetail, meetingId: string) {
-        return yield this.store.query('meeting-submissions', { meetingId , page: 1 })
-    });
-
-    model(this: MeetingsDetail, params: { meeting_id: string }) {
-        return {
-            meetingId: params.meeting_id,
-            taskInstance: this.get('getSubmissions').perform(params.meeting_id),
-        }
+    model(params: string) {
+        return params;
     }
 
     @action
