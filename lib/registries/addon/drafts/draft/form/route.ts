12ef72770f5b74d32c664384fd1191041c952119
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2019-07-05 16:00:56 -0400

    Update lib/registries/addon/drafts/draft/form/route.ts
    Co-Authored-By: Abram Booth <aaxelb@users.noreply.github.com>

--- lib/registries/addon/drafts/draft/form/route.ts
+++ lib/registries/addon/drafts/draft/form/route.ts
@@ -1,6 +1,6 @@
 import Route from '@ember/routing/route';
 
-export default class DraftsFormRoute extends Route.extend({}) {
+export default class DraftsFormRoute extends Route {
     model() {
         return this.modelFor('drafts');
     }
