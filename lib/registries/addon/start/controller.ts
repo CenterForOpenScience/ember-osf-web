3118939412d099b9f5e799ffb570658b307d4db4
Futa Ikeda <futa.ikeda@gmail.com>
2019-07-08 09:55:25 -0400

    Remove controllers and simplify some templates

--- lib/registries/addon/start/controller.ts
+++
@@ -1,7 +0,0 @@
-import { service } from '@ember-decorators/service';
-import Controller from '@ember/controller';
-import Analytics from 'ember-osf-web/services/analytics';
-
-export default class Start extends Controller {
-    @service analytics!: Analytics;
-}
