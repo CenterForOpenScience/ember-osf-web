3c4fb26b8214c2c011b647de75aced790be1b8cf
Futa Ikeda <futa.ikeda@gmail.com>
2019-08-01 13:17:28 -0400

    Single block elements renbered using schema blocks

--- lib/registries/addon/drafts/draft/form/route.ts
+++ lib/registries/addon/drafts/draft/form/route.ts
@@ -1,10 +1,16 @@
 import { action } from '@ember-decorators/object';
 import { service } from '@ember-decorators/service';
 import Route from '@ember/routing/route';
+import Store from 'ember-data/store';
 import Analytics from 'ember-osf-web/services/analytics';
 
 export default class DraftsFormRoute extends Route {
     @service analytics!: Analytics;
+    @service store!: Store;
+
+    model() {
+        return this.store.findRecord('registration-schema', 'as_predicted_preregsitration');
+    }
 
     @action
     didTransition() {
--- mirage/fixture-data/registration-schemas/as-predicted-preregistration.ts
+++ mirage/fixture-data/registration-schemas/as-predicted-preregistration.ts
@@ -9,6 +9,111 @@ export default {
     name: 'AsPredicted Preregistration',
     schemaVersion: 2,
     // To avoid collision with mirage schema. Will be changed to 'schema' in serializer.
+    formBlocks: [
+        {
+            blockType: 'page-heading',
+            blockText: 'Page One',
+            blockId: '1',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'Existing Data',
+            blockId: 'data',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'Has any data been collected for this study already?',
+            blockId: 'data',
+        },
+        {
+            blockType: 'single-select input',
+            blockId: 'data',
+        },
+        {
+            blockType: 'single-select option',
+            blockText: 'Yes, at least some data have been collected for this study already',
+            blockId: 'data',
+            value: 'no',
+        },
+        {
+            blockType: 'single-select option',
+            blockText: 'No, no data have been collected for this study yet',
+            blockId: 'data',
+            value: 'no',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'Hypothesis',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'What is the main question being asked or hypothesis being tested in this study?',
+            blockId: 'hypothesis',
+        },
+        {
+            blockType: 'long-text input',
+            blockId: 'hypothesis',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'Dependent variables',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'Describe the key dependent variable(s) specifying how they will be measured.',
+            blockId: 'dependent',
+        },
+        {
+            blockType: 'long-text input',
+            blockId: 'dependent',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'Analyses',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'Specify exactly which analyses you will conduct to examine the main question/hypothesis.',
+            blockId: 'analysis',
+        },
+        {
+            blockType: 'long-text input',
+            blockId: 'analysis',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'More analyses',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'Any secondary analyses?',
+        },
+        {
+            blockType: 'long-text input',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'Sample size',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'How many observations will be collected or what will determine sample size? No need to justify decision, but be precise about exactly how the number will be determined.',
+        },
+        {
+            blockType: 'long-text input',
+        },
+        {
+            blockType: 'section-heading',
+            blockText: 'Other',
+        },
+        {
+            blockType: 'input label',
+            blockText: 'Anything else you would like to pre-register? (e.g., data exclusions, variables collected for exploratory purposes, unusual analyses planned?',
+        },
+        {
+            blockType: 'long-text input',
+        },
+    ],
     schemaNoConflict: {
         description: 'You will be asked to answer 8 questions about your study. This registration form uses the content recommended on the website AsPredicted.org.',
         title: 'AsPredicted Preregistration',
