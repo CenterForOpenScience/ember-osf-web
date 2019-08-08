f870b92347d67f90160b5c6520547ab8c8c18b2f
Baylee Swenson <baylee@cos.io>
2019-08-08 14:11:53 -0400

    Remove ! and replace with ? in schema-block model

--- app/models/schema-block.ts
+++ app/models/schema-block.ts
@@ -3,12 +3,12 @@ import { attr } from '@ember-decorators/data';
 import OsfModel from './osf-model';
 
 export default class SchemaBlockModel extends OsfModel {
-    @attr('string') blockType!: string;
-    @attr('string') chunkId!: string;
-    @attr('string') answerId!: string;
-    @attr('string') displayText!: string;
-    @attr('string') helpText!: string;
-    @attr('boolean') required!: boolean;
+    @attr('string') blockType?: string;
+    @attr('string') chunkId?: string;
+    @attr('string') answerId?: string;
+    @attr('string') displayText?: string;
+    @attr('string') helpText?: string;
+    @attr('boolean') required?: boolean;
 }
 
 declare module 'ember-data/types/registries/model' {
