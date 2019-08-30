af787726169263bcf576d1ebc0b1d403c97d40e8
Futa Ikeda <futa.ikeda@gmail.com>
2019-08-30 12:00:23 -0400

    Add back linebreak

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -37,6 +37,7 @@ module.exports = function(defaults) {
             }],
         };
     }
+
     const app = new EmberApp(defaults, {
         ...handbookOptions,
         hinting: config.lintOnBuild,
