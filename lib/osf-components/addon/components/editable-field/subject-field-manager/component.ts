12b74a5997051db7f5dbc15bbd01f06a5ac0e78a
Fabrice Mizero <fabrice@cos.io>
2019-09-05 11:55:46 -0400

    Add editable field manager for subjects
    Co-authored-by: Abram Booth <abram@cos.io>

---
+++ lib/osf-components/addon/components/editable-field/subject-field-manager/component.ts
@@ -0,0 +1,59 @@
+import { tagName } from '@ember-decorators/component';
+import { action, computed } from '@ember-decorators/object';
+import { alias, and } from '@ember-decorators/object/computed';
+import Component from '@ember/component';
+import { task } from 'ember-concurrency';
+
+import { layout } from 'ember-osf-web/decorators/component';
+import Node from 'ember-osf-web/models/node';
+import { SubjectManager } from 'osf-components/components/subjects/manager/component';
+
+import template from './template';
+
+@tagName('')
+@layout(template)
+export default class SubjectFieldManagerComponent extends Component.extend({
+    save: task(function *(this: SubjectFieldManagerComponent) {
+        try {
+            yield this.subjectsManager.saveChanges();
+        } catch (e) {
+            // TODO
+            throw e;
+        }
+        this.set('requestedEditMode', false);
+    }).drop(),
+}) {
+    // required
+    node!: Node;
+    subjectsManager!: SubjectManager;
+
+    requestedEditMode: boolean = false;
+
+    @alias('node.userHasAdminPermission')
+    userCanEdit!: boolean;
+
+    @and('userCanEdit', 'requestedEditMode')
+    inEditMode!: boolean;
+
+    @computed('subjectsManager.savedSubjects.length')
+    get fieldIsEmpty() {
+        return !this.subjectsManager.savedSubjects.length;
+    }
+
+    @computed('fieldIsEmpty', 'userCanEdit')
+    get shouldShowField() {
+        return this.userCanEdit || !this.fieldIsEmpty;
+    }
+
+    @action
+    startEditing() {
+        this.subjectsManager.discardChanges();
+        this.set('requestedEditMode', true);
+    }
+
+    @action
+    cancel() {
+        this.subjectsManager.discardChanges();
+        this.set('requestedEditMode', false);
+    }
+}
