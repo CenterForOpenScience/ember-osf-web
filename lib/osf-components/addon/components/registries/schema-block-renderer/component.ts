394ebde3ebda2332e26b3512a09e6644a98bef5a
baylee-d <bayleeswenson@Admins-MacBook-Pro-2.local>
2019-09-12 11:54:01 -0400

    add to handbook

--- lib/osf-components/addon/components/registries/schema-block-renderer/component.ts
+++ lib/osf-components/addon/components/registries/schema-block-renderer/component.ts
@@ -2,8 +2,10 @@ import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 import Changeset from 'ember-changeset';
 
+import defaultTo from 'ember-osf-web/utils/default-to';
+
 import { layout } from 'ember-osf-web/decorators/component';
-import { SchemaBlock } from 'ember-osf-web/models/schema-block';
+import SchemaBlock from 'ember-osf-web/models/schema-block';
 import template from './template';
 
 @layout(template)
@@ -12,4 +14,8 @@ export default class SchemaBlockRenderer extends Component {
     // Required params
     schemaBlock!: SchemaBlock;
     changeset!: Changeset;
+
+    // Optional params
+    disabled: boolean = defaultTo(this.disabled, false);
+    shouldShowMessages: boolean = defaultTo(this.shouldShowMessages, true);
 }
