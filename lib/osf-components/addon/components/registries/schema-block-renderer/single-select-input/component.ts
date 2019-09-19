1a0ea29fb4225b2bac7a0e1607258428692f7067
baylee-d <bayleeswenson@Admins-MacBook-Pro-2.local>
2019-09-18 23:05:19 -0400

    Add to handbook

---
+++ lib/osf-components/addon/components/registries/schema-block-renderer/single-select-input/component.ts
@@ -0,0 +1,10 @@
+import { tagName } from '@ember-decorators/component';
+import Component from '@ember/component';
+
+import { layout } from 'ember-osf-web/decorators/component';
+import template from './template';
+
+@layout(template)
+@tagName('')
+export default class SingleSelectInput extends Component {
+}
