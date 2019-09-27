f67872e676ca00b2abab1fb251ee8bba92832436
Rob Cary <r0bc4ry@gmail.com>
2019-09-27 14:25:37 -0400

    Add parent relationship for intitutional users

--- app/adapters/institutional-user.ts
+++ app/adapters/institutional-user.ts
@@ -1,6 +1,7 @@
 import OsfAdapter from './osf-adapter';
 
 export default class InstitutionalUserAdapter extends OsfAdapter {
+    parentRelationship = 'institution';
 }
 
 declare module 'ember-data/types/registries/adapter' {
--- app/models/institutional-user.ts
+++ app/models/institutional-user.ts
@@ -1,6 +1,7 @@
 import { attr, belongsTo } from '@ember-decorators/data';
 import DS from 'ember-data';
 
+import InstitutionModel from 'ember-osf-web/models/institution';
 import UserModel from 'ember-osf-web/models/user';
 import OsfModel from './osf-model';
 
@@ -8,6 +9,9 @@ export default class InstitutionalUserModel extends OsfModel {
     @attr('fixstring') department?: string;
     @attr('number') publicProjectCount!: number;
     @attr('number') privateProjectCount!: number;
+
+    @belongsTo('institution')
+    institution!: DS.PromiseObject<InstitutionModel> & InstitutionModel;
 
     @belongsTo('user')
     user!: DS.PromiseObject<UserModel> & UserModel;
