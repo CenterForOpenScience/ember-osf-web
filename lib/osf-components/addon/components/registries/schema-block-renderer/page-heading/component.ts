df474809e819a54d0064ba4c1560655929eaf956
Yuhuai Liu <yuhuai@cos.io>
2019-10-29 14:20:25 -0400

    update styling for schema-block-renderers

--- lib/osf-components/addon/components/registries/schema-block-renderer/page-heading/component.ts
+++ lib/osf-components/addon/components/registries/schema-block-renderer/page-heading/component.ts
@@ -2,9 +2,10 @@ import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 
 import { layout } from 'ember-osf-web/decorators/component';
+import styles from './styles';
 import template from './template';
 
-@layout(template)
+@layout(template, styles)
 @tagName('')
 export default class PageHeading extends Component {
 }
