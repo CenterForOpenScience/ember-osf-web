2b6e51ca62042666873fb39d04aebc998bd51f6f
Baylee Swenson <baylee@cos.io>
2019-10-29 21:45:32 -0400

    Add styling for schema-blocks

--- lib/osf-components/addon/components/registries/schema-block-renderer/editable/multi-select-input/component.ts
+++ lib/osf-components/addon/components/registries/schema-block-renderer/editable/multi-select-input/component.ts
@@ -5,9 +5,10 @@ import { assert } from '@ember/debug';
 import { computed } from '@ember-decorators/object';
 import { layout } from 'ember-osf-web/decorators/component';
 import { SchemaBlock } from 'ember-osf-web/packages/registration-schema';
+import styles from './styles';
 import template from './template';
 
-@layout(template)
+@layout(template, styles)
 @tagName('')
 export default class MultiSelectInput extends Component {
     // Required param
