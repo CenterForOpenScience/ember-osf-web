ce49a56c5bfecafe4a1aebd062ba396a3c7c5523
James C. Davis <james@cos.io>
2019-11-15 16:35:50 -0500

    fix: example text

--- lib/osf-components/addon/components/registries/schema-block-renderer/label/label-content/component.ts
+++ lib/osf-components/addon/components/registries/schema-block-renderer/label/label-content/component.ts
@@ -3,16 +3,13 @@ import Component from '@ember/component';
 
 import { action } from '@ember-decorators/object';
 import { layout } from 'ember-osf-web/decorators/component';
-import { SchemaBlock } from 'ember-osf-web/packages/registration-schema';
+
 import styles from './styles';
 import template from './template';
 
 @layout(template, styles)
 @tagName('')
-export default class Label extends Component {
-    // Required param
-    schemaBlock!: SchemaBlock;
-
+export default class LabelContent extends Component {
     // Private property
     shouldShowExample = false;
 
