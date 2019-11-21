b37b776da5cb3e0917719306e3efd8a96a7d77d4
James C. Davis <james@cos.io>
2019-11-21 17:14:47 -0500

    feat: remove all borders and clean up whitespace

--- lib/osf-components/addon/components/registries/schema-block-group-renderer/component.ts
+++ lib/osf-components/addon/components/registries/schema-block-group-renderer/component.ts
@@ -8,9 +8,11 @@ import { layout } from 'ember-osf-web/decorators/component';
 import { SchemaBlock, SchemaBlockGroup } from 'ember-osf-web/packages/registration-schema';
 import defaultTo from 'ember-osf-web/utils/default-to';
 import { uniqueId } from 'osf-components/helpers/unique-id';
+
+import styles from './styles';
 import template from './template';
 
-@layout(template)
+@layout(template, styles)
 @tagName('')
 export default class SchemaBlockGroupRenderer extends Component {
     // Required parameters
