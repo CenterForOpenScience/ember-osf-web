6dee6eadb1873aeac2cd35a986a4d3d1e2623864
Fabrice Mizero <fabrice@cos.io>
2019-11-21 15:12:43 -0500

    Initial implementation

--- app/packages/registration-schema/index.ts
+++ app/packages/registration-schema/index.ts
@@ -3,5 +3,11 @@ export { getSchemaBlockGroups } from './get-schema-block-group';
 export { SchemaBlock, SchemaBlockType } from './schema-block';
 export { SchemaBlockGroup } from './schema-block-group';
 export { buildValidation } from './validations';
-export { FileReference, RegistrationResponse, ResponseValue } from './registration-response';
+export {
+    FileReference,
+    RegistrationResponse,
+    ResponseValue,
+    NormalizedResponseValue,
+    NormalizedRegistrationResponse,
+} from './registration-response';
 export { PageManager } from './page-manager';
--- app/serializers/draft-registration.ts
+++ app/serializers/draft-registration.ts
@@ -1,6 +1,75 @@
+import { isEmpty } from '@ember/utils';
+import DS from 'ember-data';
+
+import {
+    FileReference,
+    NormalizedRegistrationResponse,
+    NormalizedResponseValue,
+    RegistrationResponse,
+    ResponseValue,
+} from 'ember-osf-web/packages/registration-schema';
+import { mapKeysAndValues } from 'ember-osf-web/utils/map-keys';
+import { SingleResourceDocument } from 'osf-api';
 import OsfSerializer from './osf-serializer';
 
+interface JsonPayload {
+    attributes: Record<string, {}>;
+}
+
+function normalizeRegistrationResponses(value: ResponseValue, store: DS.Store) {
+    if (Array.isArray(value) && !isEmpty(value) && ('file_id' in value.firstObject)) {
+        return (value as FileReference[]).map((fileRef: FileReference) => {
+            const peekedRecord = store.peekRecord('file', fileRef.file_id);
+            if (peekedRecord) {
+                return peekedRecord;
+            }
+            return store.createRecord('file', { id: fileRef.file_id });
+        });
+    }
+    return value;
+}
+
+function serializeRegistrationResponses(value: NormalizedResponseValue) {
+    if (Array.isArray(value) && !isEmpty(value) && ('materializedPath' in value.firstObject)) {
+        return value.map(file => file.isNew ? file : file.toFileReference());
+    }
+    return value;
+}
+
 export default class DraftRegistrationSerializer extends OsfSerializer {
+    normalizeResponse(
+        store: DS.Store,
+        primaryModelClass: any,
+        payload: SingleResourceDocument,
+        id: string,
+        requestType: string,
+    ) {
+        if (payload.data.attributes) {
+            const registrationResponses = payload.data.attributes.registration_responses as RegistrationResponse;
+            // @ts-ignore
+            payload.data.attributes.registration_responses = mapKeysAndValues(
+                registrationResponses || {},
+                key => key,
+                value => normalizeRegistrationResponses(value, store),
+            ) as NormalizedRegistrationResponse;
+        }
+        return super.normalizeResponse(store, primaryModelClass, payload, id, requestType);
+    }
+
+    serializeAttribute(snapshot: DS.Snapshot, json: JsonPayload, key: string, attribute: object): void {
+        super.serializeAttribute(snapshot, json, key, attribute);
+
+        if (key === 'registrationResponses') {
+            const underscoreKey = this.keyForAttribute(key);
+            const registrationResponses = json.attributes[underscoreKey] as NormalizedRegistrationResponse;
+
+            json.attributes[underscoreKey] = mapKeysAndValues(
+                registrationResponses || {},
+                k => k,
+                value => serializeRegistrationResponses(value),
+            );
+        }
+    }
 }
 
 declare module 'ember-data/types/registries/serializer' {
