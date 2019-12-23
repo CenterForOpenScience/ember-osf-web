84c2ff23c277f17b60693efaff5ccaf2ed2f9e89
Yuhuai Liu <yuhuai@cos.io>
2019-12-23 12:29:27 -0500

    add component

---
+++ lib/osf-components/addon/components/form-controls/power-select/component.ts
@@ -0,0 +1,39 @@
+import { tagName } from '@ember-decorators/component';
+import Component from '@ember/component';
+import { assert } from '@ember/debug';
+import { action } from '@ember/object';
+import { ChangesetDef } from 'ember-changeset/types';
+
+import { layout } from 'ember-osf-web/decorators/component';
+import defaultTo from 'ember-osf-web/utils/default-to';
+
+import styles from './styles';
+import template from './template';
+
+@tagName('')
+@layout(template, styles)
+export default class FormControlPowerSelect extends Component {
+    // Required params
+    options!: string[];
+    valuePath!: string;
+    changeset!: ChangesetDef;
+
+    // Optional params
+    shouldShowMessages?: boolean;
+    disabled: boolean = defaultTo(this.disabled, false);
+    onchange?: (option: string) => void;
+
+    didReceiveAttrs() {
+        assert('FormControls::RadioButton - @options are required', Boolean(this.options));
+        assert('FormControls::RadioButton - @valuePath is required', Boolean(this.valuePath));
+        assert('FormControls::RadioButton - @changeset is required', Boolean(this.changeset));
+    }
+
+    @action
+    updateChangeset(option: any) {
+        this.changeset.set(this.valuePath, option);
+        if (this.onchange) {
+            this.onchange(option);
+        }
+    }
+}
