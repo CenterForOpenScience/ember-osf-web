0e8ee72674a94ab882bc7fa306508335bda6e495
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2019-12-26 10:12:17 -0500

    [ENG-610]Add debounce component (#911)
    * Add debounce component
    
    * Update changelog and remove comments
    
    * Respond to CR
    
    * Remove unnecessary function and revert page template

---
+++ lib/osf-components/addon/components/debouncer/component.ts
@@ -0,0 +1,28 @@
+import Component from '@ember/component';
+import { assert } from '@ember/debug';
+import { timeout } from 'ember-concurrency';
+import { task } from 'ember-concurrency-decorators';
+
+import { layout } from 'ember-osf-web/decorators/component';
+
+import template from './template';
+
+@layout(template)
+export default class Debouncer extends Component {
+    fn!: () => void;
+    timeoutInterval: number = 500;
+
+    @task({ restartable: true })
+    doFnDebounce = task(function *(this: Debouncer) {
+        yield timeout(this.timeoutInterval);
+        try {
+            yield this.fn();
+        } catch (error) {
+            throw error;
+        }
+    });
+
+    didReceiveAttrs() {
+        assert('Debouncer needs a function input', Boolean(this.fn));
+    }
+}
