1ca7d4a10ff71e35759c424a375466258703b477
Fabrice Mizero <fabrice@cos.io>
2020-01-27 20:01:56 -0500

    Use stripHTMLtags util, htmlsafe flag not working in tests

--- tests/integration/components/sign-up-policy/component-test.ts
+++ tests/integration/components/sign-up-policy/component-test.ts
@@ -1,18 +1,26 @@
 import { render } from '@ember/test-helpers';
+import config from 'ember-get-config';
 import { setupIntl, t } from 'ember-intl/test-support';
+import stripHtmlTags from 'ember-osf-web/tests/helpers/strip-html-tags';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
 
+const { signUpPolicy: { termsLink, privacyPolicyLink } } = config;
+
 module('Integration | Component | sign-up-policy', hooks => {
     setupRenderingTest(hooks);
-    setupIntl(hooks);
+    setupIntl(hooks, 'en-us');
 
     test('it renders', async function(assert) {
         await render(hbs`{{sign-up-policy}}`);
-        const link1 = 'fakesignup.link';
-        const link2 = 'fakesignup.link';
+        const link1 = termsLink;
+        const link2 = privacyPolicyLink;
         assert.dom(this.element)
-            .hasText(t('osf-components.sign-up-policy.paragraph', { link1, link2, htmlSafe: true }).toString());
+            .hasText(
+                stripHtmlTags(
+                    t('osf-components.sign-up-policy.paragraph', { link1, link2, htmlSafe: true }).toString(),
+                ),
+            );
     });
 });
