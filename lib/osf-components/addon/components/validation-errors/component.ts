f1b6a0cf9725f95f8d77c08885199327ef3897af
Fabrice Mizero <fabrice@cos.io>
2020-01-31 13:02:27 -0500

    Fix tests; take into account ember-cp-validations default errors

--- lib/osf-components/addon/components/validation-errors/component.ts
+++ lib/osf-components/addon/components/validation-errors/component.ts
@@ -1,3 +1,4 @@
+import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 import { assert } from '@ember/debug';
 import { computed, set } from '@ember/object';
@@ -11,6 +12,7 @@ import { layout } from 'ember-osf-web/decorators/component';
 import styles from './styles';
 import template from './template';
 
+@tagName('')
 @layout(template, styles)
 export default class ValidationErrors extends Component {
     @service intl!: Intl;
@@ -18,17 +20,32 @@ export default class ValidationErrors extends Component {
     changeset!: ChangesetDef;
     key!: string;
     validatorResults!: RawValidationResult[];
+    errors?: string[];
 
     didReceiveAttrs() {
-        assert('validation-errors - requires @changeset and @responseKey!', Boolean(this.changeset && this.key));
+        assert('validation-errors - requires (@changeset and @responseKey!) or @errors',
+            Boolean(this.changeset && this.key) || Boolean(this.errors));
         const results = this.changeset.get('error')[this.key];
         const validatorResults = results ? results.validation : [];
         set(this, 'validatorResults', validatorResults as unknown as RawValidationResult[]);
     }
 
-    @computed('intl.locale', 'validatorResults.[]')
-    get errors() {
-        const { validatorResults } = this;
+    @computed('intl.locale', 'validatorResults.[]', 'errors.[]')
+    get validatorErrors() {
+        const { validatorResults, errors } = this;
+        if (errors) {
+            // TODO: remove when we get rid of ember-cp-validations.
+            if (Array.isArray(errors) && errors.every(error => typeof error === 'string')) {
+                // default validator messages from ember-cp-validations
+                return errors;
+            }
+            if (typeof errors === 'string') {
+                // custom validators that use createErrorMessage...
+                // (and extend ember-cp-validations/validators/base) return a translated string.
+                return [errors];
+            }
+        }
+
         return validatorResults.map(({ context: { type, translationArgs } }) =>
             this.intl.t(`validationErrors.${type}`, { ...translationArgs }));
     }
