badec0cd5cf4655c56a426c343f8af32885cb7b8
Yuhuai Liu <adlius45@hotmail.com>
2020-02-17 16:20:00 -0500

    Update app/utils/fix-special-char.ts
    Co-Authored-By: James C. Davis <james@cos.io>

--- app/utils/fix-special-char.ts
+++ app/utils/fix-special-char.ts
@@ -3,7 +3,7 @@
  * sequences into text, and this will replace `&amp;` sequences with `&`, `&lt;` with `<` and `&gt;` with `>`.
  * Template helper and ember-data field versions of this function are available.
  */
-export default function fixSpecialChar(inputString: string = ''): string | null {
+export default function fixSpecialChar(inputString: string = ''): string {
     return inputString
         .replace(/&amp;/gi, '&')
         .replace(/&lt;/gi, '<')
