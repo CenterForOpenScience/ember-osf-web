e14caf0171342dcd50b10ee265e72ab43e10cc24
Yuhuai Liu <yuhuai@cos.io>
2020-03-09 12:47:04 -0400

    fix register button on draft-registration list

--- app/guid-node/drafts/register/route.ts
+++ app/guid-node/drafts/register/route.ts
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class GuidNodeDraftsRegister extends Route {
     model() {
-        this.replaceWith('registries.drafts.draft.page', this.modelFor('guid-node.drafts'), 'review');
+        this.replaceWith('registries.drafts.draft.review', this.modelFor('guid-node.drafts'));
     }
 }
