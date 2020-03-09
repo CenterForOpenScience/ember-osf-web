a3739d4c1430c6c8f5921da1ba6610b7b810c413
Futa Ikeda <futa.ikeda@gmail.com>
2020-03-09 10:40:15 -0400

    Update test for registration form navigation dropdown

--- tests/integration/components/registries/registration-form-navigation-dropdown/component-test.ts
+++ tests/integration/components/registries/registration-form-navigation-dropdown/component-test.ts
@@ -1,5 +1,6 @@
 import { click, render } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { TestContext } from 'ember-intl/test-support';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 import { module, test } from 'qunit';
@@ -8,44 +9,59 @@ module('Integration | Component | registration-form-navigation-dropdown', hooks 
     setupRenderingTest(hooks);
     setupMirage(hooks);
 
-    test('it renders only schema anchors and no metadata anchors by default', async function(assert) {
+    hooks.beforeEach(async function(this: TestContext) {
+        this.store = this.owner.lookup('service:store');
         server.loadFixtures('schema-blocks');
         server.loadFixtures('registration-schemas');
+        server.loadFixtures('registration-providers');
+        server.loadFixtures('licenses');
+        const initiator = server.create('user', 'loggedIn');
         const registrationSchema = server.schema.registrationSchemas.find('testSchema');
-        this.set('schemaBlocks', registrationSchema.schemaBlocks);
+        const draftRegistration = server.create(
+            'draft-registration',
+            {
+                registrationSchema,
+                initiator,
+            },
+        );
+        const dr = await this.store.findRecord('draft-registration', draftRegistration.id);
+        const schema = await dr.registrationSchema;
+        const schemaBlocks = await schema.loadAll('schemaBlocks');
+        this.set('schemaBlocks', schemaBlocks);
+    });
+
+    test('it renders only schema anchors and no metadata anchors by default', async assert => {
         await render(hbs`
             <Registries::RegistrationFormNavigationDropdown
                 @schemaBlocks={{this.schemaBlocks}}
             />
         `);
+        assert.dom('[data-test-toggle-anchor-nav-button]').exists();
         await click('[data-test-toggle-anchor-nav-button]');
         assert.dom('[data-test-page-anchor="Metadata"]').doesNotExist();
         assert.dom('[data-test-page-anchor="title"').doesNotExist();
         assert.dom('[data-test-page-anchor="license"]').doesNotExist();
         assert.dom('[data-test-page-anchor="SB1"]').exists();
         assert.dom('[data-test-page-anchor="SB4"]').isVisible();
-        assert.dom('.page-heading').exists({ count: 2 });
-        assert.dom('li.question-label').exists({ count: 7 });
+        assert.dom('[data-test-list-item="page-heading"]').exists({ count: 2 });
+        assert.dom('[data-test-list-item="question-label"]').exists({ count: 5 });
     });
 
-    test('it renders schema and metadata anchors with showMetadata flag', async function(assert) {
-        server.loadFixtures('schema-blocks');
-        server.loadFixtures('registration-schemas');
-        const registrationSchema = server.schema.registrationSchemas.find('testSchema');
-        this.set('schemaBlocks', registrationSchema.schemaBlocks);
+    test('it renders schema and metadata anchors with showMetadata flag', async assert => {
         await render(hbs`
             <Registries::RegistrationFormNavigationDropdown
                 @schemaBlocks={{this.schemaBlocks}}
                 @showMetadata={{true}}
             />
         `);
+        assert.dom('[data-test-toggle-anchor-nav-button]').exists();
         await click('[data-test-toggle-anchor-nav-button]');
         assert.dom('[data-test-page-anchor="Metadata"]').exists();
         assert.dom('[data-test-page-anchor="title"]').exists();
         assert.dom('[data-test-page-anchor="license"]').exists();
-        assert.dom('.page-heading > a[data-test-page-anchor="SB1"]').exists();
-        assert.dom('.question-label > a[data-test-page-anchor="SB4"]').exists();
-        assert.dom('.page-heading').exists({ count: 3 });
-        assert.dom('li .question-label').exists({ count: 15 });
+        assert.dom('[data-test-page-anchor="SB1"]').exists();
+        assert.dom('[data-test-page-anchor="SB4"]').exists();
+        assert.dom('[data-test-list-item="page-heading"]').exists({ count: 3 });
+        assert.dom('[data-test-list-item="question-label"]').exists({ count: 12 });
     });
 });
