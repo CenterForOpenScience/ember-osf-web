652ebca33e47d500f46010c843d49ff353f2bf61
Derrick Camerino <robustdj@gmail.com>
2020-03-17 13:33:38 -0700

    [ENG-1613] Set rest of css variables from brand

--- lib/registries/addon/services/brand.ts
+++ lib/registries/addon/services/brand.ts
@@ -2,7 +2,7 @@ import Service from '@ember/service';
 import Brand from 'ember-osf-web/models/brand';
 
 export default class BrandService extends Service {
-    currentBrand = { primaryColor: '' };
+    currentBrand: Brand? = null;
 
     setBrand(brand: Brand) {
         this.set('currentBrand', brand);
@@ -10,8 +10,16 @@ export default class BrandService extends Service {
     }
 
     setCssVariables() {
-        // set --primary-color variable
-        // set --secondary-color variable
-        document.documentElement.style.setProperty('--primary-color', this.currentBrand.primaryColor);
+        if (!this.currentBrand) {
+            return;
+        }
+
+        const documentStyle = document.documentElement.style;
+
+        documentStyle.setProperty('--primary-color', this.currentBrand.primaryColor);
+        documentStyle.setProperty('--secondary-color', this.currentBrand.secondaryColor);
+        documentStyle.setProperty('--navbar-logo-img-url', this.currentBrand.navbarLogoImage);
+        documentStyle.setProperty('--hero-logo-img-url', this.currentBrand.heroLogoImage);
+        documentStyle.setProperty('--hero-background-img-url', this.currentBrand.heroBackgroundImage);
     }
 }
