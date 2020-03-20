7f6eaefda74db417f8873bc1dba8189684602cdb
Derrick Camerino <robustdj@gmail.com>
2020-03-20 10:57:01 -0700

    [ENG-1617] WIP Starting on branding components

--- lib/registries/addon/modifiers/with-branding.ts
+++ lib/registries/addon/modifiers/with-branding.ts
@@ -4,10 +4,12 @@ import Brand from 'registries/services/brand';
 
 class WithBrandingModifier extends Modifier {
     didInsertElement([brand]: [Brand]) {
-        const elementStyle = this.element.style;
+        const element = this.element;
+        const elementStyle = element.style;
         const { currentBrand } = brand;
 
         if (currentBrand) {
+            element.classList.add('brand-container');
             elementStyle.setProperty('--primary-color', currentBrand.primaryColor);
             elementStyle.setProperty('--secondary-color', currentBrand.secondaryColor);
             elementStyle.setProperty('--navbar-logo-img-url', currentBrand.navbarLogoImage);
