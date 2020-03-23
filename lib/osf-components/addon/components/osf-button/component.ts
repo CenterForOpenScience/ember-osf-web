dd165bbacc557d97f89ab84825244eea1ff04532
Derrick Camerino <robustdj@gmail.com>
2020-03-23 15:50:04 -0700

    [ENG-1617] First pass at OsfButton without BsButton

--- lib/osf-components/addon/components/osf-button/component.ts
+++ lib/osf-components/addon/components/osf-button/component.ts
@@ -1,10 +1,4 @@
-import BsButton from 'ember-bootstrap/components/bs-button';
+import Component from '@glimmer/component';
 
-import { layout } from 'ember-osf-web/decorators/component';
-
-import styles from './styles';
-import template from './template';
-
-@layout(template, styles)
-export default class OsfButton extends BsButton {
+export default class OsfButton extends Component {
 }
