2ed151573962eef8b482cd6dfe2559d82eaa567a
Yuhuai Liu <adlius45@hotmail.com>
2020-03-25 12:48:54 -0400

    Update lib/osf-components/addon/components/registries/finalize-registration-modal/manager/component.ts
    Co-Authored-By: Fabrice Mizero <fabrice@cos.io>

--- lib/osf-components/addon/components/registries/finalize-registration-modal/manager/component.ts
+++ lib/osf-components/addon/components/registries/finalize-registration-modal/manager/component.ts
@@ -37,7 +37,7 @@ export default class FinalizeRegistrationModalManagerComponent extends Component
                 this.onSubmitRegistration(this.registration.id);
             }
         } catch (error) {
-            if (error.errors) {
+            if (Array.isArray(error.errors) && error.errors.length) {
                 this.toast.error(error.errors[0].detail);
             } else {
                 this.toast.error(this.intl.t('registries.drafts.draft.submit_error'));
