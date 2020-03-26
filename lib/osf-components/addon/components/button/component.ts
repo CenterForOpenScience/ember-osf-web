5e28272080ad33321b3c8366e9a49952dca1818f
Derrick Camerino <robustdj@gmail.com>
2020-03-26 09:24:34 -0700

    [ENG-1617] Address CR feedback

--- lib/osf-components/addon/components/button/component.ts
+++ lib/osf-components/addon/components/button/component.ts
@@ -1,5 +1,7 @@
 import Component from '@glimmer/component';
 
+import styles from './styles';
+
 type Layout = 'small' | 'medium' | 'large' | 'fake-link';
 type Type = 'primary' | 'secondary' | 'create' | 'destroy';
 
@@ -8,21 +10,32 @@ interface Args {
     type?: Type;
 }
 
+const classMap = {
+    small: styles.SmallButton,
+    medium: styles.MediumButton,
+    large: styles.LargeButton,
+    'fake-link': styles.FakeLink,
+    primary: styles.PrimaryButton,
+    secondary: styles.SecondaryButton,
+    create: styles.CreateButton,
+    destroy: styles.DestroyButton,
+};
+
 export default class Button extends Component<Args> {
     get classList(): string {
         const classes = [];
         const { layout, type } = this.args;
 
         if (layout) {
-            classes.push(layout);
+            classes.push(classMap[layout]);
         } else {
-            classes.push('medium');
+            classes.push(classMap.medium);
         }
 
         if (type) {
-            classes.push(type);
+            classes.push(classMap[type]);
         } else {
-            classes.push('secondary');
+            classes.push(classMap.secondary);
         }
 
         return classes.join(' ');
