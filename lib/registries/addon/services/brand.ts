93b3384e0bbd312db950c9f9f48c706185c86630
Abram Booth <abram@cos.io>
2020-04-22 16:21:58 -0400

    Revert "Revert "Merge pull request #983 from robustdj/ENG-1613_fetch-brand""
    This reverts commit 8bb082900a2b7115c23dc87058ad4be457a3fbfa.

---
+++ lib/registries/addon/services/brand.ts
@@ -0,0 +1,10 @@
+import Service from '@ember/service';
+import Brand from 'ember-osf-web/models/brand';
+
+export default class BrandService extends Service {
+    currentBrand: Brand | null = null;
+
+    setBrand(brand: Brand | null) {
+        this.currentBrand = brand;
+    }
+}
