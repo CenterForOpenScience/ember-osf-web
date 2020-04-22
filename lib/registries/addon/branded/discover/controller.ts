8bb082900a2b7115c23dc87058ad4be457a3fbfa
Abram Booth <abram@cos.io>
2020-04-22 16:18:00 -0400

    Revert "Merge pull request #983 from robustdj/ENG-1613_fetch-brand"
    This reverts commit fb4bb1a72146e828c60970bcf74d1f93cedc4c42, reversing
    changes made to 4d43d0049c31f9b366decb424946f48ead708241.

--- lib/registries/addon/branded/discover/controller.ts
+++
@@ -1,9 +0,0 @@
-import Controller from '@ember/controller';
-
-import Brand from 'registries/services/brand';
-
-import { inject as service } from '@ember/service';
-
-export default class Discover extends Controller {
-    @service brand!: Brand;
-}
--- mirage/fixtures/registration-providers.ts
+++ mirage/fixtures/registration-providers.ts
@@ -10,6 +10,7 @@ function randomAssets() {
 
 interface MirageRegistrationProvider extends RegistrationProvider {
     licensesAcceptableIds: string[];
+    brand: any;
 }
 
 const registrationProviders: Array<Partial<MirageRegistrationProvider>> = [
@@ -19,6 +20,7 @@ const registrationProviders: Array<Partial<MirageRegistrationProvider>> = [
         description: 'The open registries network',
         allowSubmissions: true,
         assets: randomAssets(),
+        brand: null,
         licensesAcceptableIds: [
             '5c252c8e0989e100220edb70',
             '5c252c8e0989e100220edb7a',
