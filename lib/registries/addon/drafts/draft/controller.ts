fcdc300abac7d25c9352b3e5035f9e816a069573
Abram Booth <abram@cos.io>
2020-04-22 16:22:16 -0400

    Revert "Revert "Merge pull request #998 from robustdj/branded-draft-submission-flow""
    This reverts commit bd16d1380be061535acf93b9e5e83430181a06ee.

--- lib/registries/addon/drafts/draft/controller.ts
+++ lib/registries/addon/drafts/draft/controller.ts
@@ -5,10 +5,13 @@ import { inject as service } from '@ember/service';
 
 import Media from 'ember-responsive';
 
+import Brand from 'registries/services/brand';
+
 import Registration from 'ember-osf-web/models/registration';
 
 export default class RegistriesDraft extends Controller {
     @service media!: Media;
+    @service brand!: Brand;
 
     @not('media.isDesktop') showMobileView!: boolean;
 
