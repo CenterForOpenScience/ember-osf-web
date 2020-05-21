a903d679c1a08849f523bc90ae51f874fe6cd238
Fabrice Mizero <fabrice@cos.io>
2020-05-21 10:50:54 -0400

    Remove Brand service usage from registries app and discover route

--- lib/registries/addon/application/route.ts
+++ lib/registries/addon/application/route.ts
@@ -5,11 +5,6 @@ import DS from 'ember-data';
 import config from 'ember-get-config';
 
 import Analytics from 'ember-osf-web/services/analytics';
-import BrandService from 'registries/services/brand';
-
-import BrandModel from 'ember-osf-web/models/brand';
-
-import RegistrationProvider from 'ember-osf-web/models/registration-provider';
 
 const {
     OSF: {
@@ -20,13 +15,8 @@ const {
 export default class ApplicationRoute extends Route {
     @service analytics!: Analytics;
     @service store!: DS.Store;
-    @service brand!: BrandService;
 
     model() {
         return this.store.findRecord('registration-provider', String(osfRegistrationProviderId), { include: 'brand' });
-    }
-
-    afterModel(model: RegistrationProvider) {
-        this.brand.setBrand(model.belongsTo('brand').value() as BrandModel);
     }
 }
