d6381403b84030e4df9f258bec890515aef38f4d
Yuhuai Liu <yuhuai@cos.io>
2020-05-21 11:05:26 -0400

    mob

--- lib/registries/addon/application/controller.ts
+++ lib/registries/addon/application/controller.ts
@@ -1,31 +1,17 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
-import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
-import { camelize } from '@ember/string';
 import Features from 'ember-feature-flags/services/features';
-import config from 'ember-get-config';
 
 import { OSFService } from 'osf-components/components/osf-navbar/component';
 
 import Brand from 'registries/services/brand';
 
-const {
-    featureFlagNames: {
-        routes: {
-            'registries.overview': newStyleFlag,
-        },
-    },
-} = config;
-
 export default class Application extends Controller {
     @service features!: Features;
     @service brand!: Brand;
-    @alias(`features.${camelize(newStyleFlag)}`) newStyleEnabled!: boolean;
 
     activeService = OSFService.REGISTRIES;
-    searchRoute = 'registries.discover';
-    supportRoute = 'https://openscience.zendesk.com/hc/en-us/categories/360001550953';
 
     @action
     search(query: string) {
