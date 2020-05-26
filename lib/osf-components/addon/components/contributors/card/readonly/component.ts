458097eea6bf5c54494010d079972d00c6fead71
Baylee Swenson <baylee@cos.io>
2020-05-25 22:30:50 -0400

    Update styling, remove tags

--- lib/osf-components/addon/components/contributors/card/readonly/component.ts
+++ lib/osf-components/addon/components/contributors/card/readonly/component.ts
@@ -1,3 +1,4 @@
+import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 
 import { layout } from 'ember-osf-web/decorators/component';
@@ -5,5 +6,6 @@ import styles from './styles';
 import template from './template';
 
 @layout(template, styles)
+@tagName('')
 export default class ContributorsCardReadonly extends Component {
 }
--- lib/osf-components/addon/components/contributors/widget/component.ts
+++ lib/osf-components/addon/components/contributors/widget/component.ts
@@ -1,3 +1,4 @@
+import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 
 import { layout } from 'ember-osf-web/decorators/component';
@@ -7,6 +8,7 @@ import styles from './styles';
 import template from './template';
 
 @layout(template, styles)
+@tagName('')
 export default class Widget extends Component {
     // Required arguments
     node!: Node;
