77b9a80bfaa514f32901ffa550ef6fb6931eee31
Futa Ikeda <futa.ikeda@gmail.com>
2020-05-28 13:34:25 -0400

    Handle proxy in with-branding modifier

--- lib/osf-components/addon/modifiers/with-branding.ts
+++ lib/osf-components/addon/modifiers/with-branding.ts
@@ -3,17 +3,18 @@ import Modifier from 'ember-oo-modifiers';
 import BrandModel from 'ember-osf-web/models/brand';
 
 class WithBrandingModifier extends Modifier {
-    didReceiveArguments([brand]: [BrandModel | DS.PromiseObject<BrandModel>]) {
+    didReceiveArguments([brand]: [BrandModel | DS.PromiseObject<BrandModel> | null]) {
+        const brandModel = (brand && 'content' in brand) ? brand.content : brand;
         const { element } = this;
         const elementStyle = element.style;
 
-        if (brand.content) {
+        if (brandModel) {
             element.classList.add('brand-container');
-            elementStyle.setProperty('--primary-color', brand.primaryColor);
-            elementStyle.setProperty('--secondary-color', brand.secondaryColor);
-            elementStyle.setProperty('--navbar-logo-img-url', `url(${brand.topnavLogoImage})`);
-            elementStyle.setProperty('--hero-logo-img-url', `url(${brand.heroLogoImage})`);
-            elementStyle.setProperty('--hero-background-img-url', `url(${brand.heroBackgroundImage})`);
+            elementStyle.setProperty('--primary-color', brandModel.primaryColor);
+            elementStyle.setProperty('--secondary-color', brandModel.secondaryColor);
+            elementStyle.setProperty('--navbar-logo-img-url', `url(${brandModel.topnavLogoImage})`);
+            elementStyle.setProperty('--hero-logo-img-url', `url(${brandModel.heroLogoImage})`);
+            elementStyle.setProperty('--hero-background-img-url', `url(${brandModel.heroBackgroundImage})`);
         } else {
             element.classList.remove('brand-container');
             elementStyle.removeProperty('--primary-color');
