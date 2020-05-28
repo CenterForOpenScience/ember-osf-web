a778be98cf797490ed54117babc1bc9501435f40
Abram Booth <abram@cos.io>
2020-05-28 11:47:45 -0400

    mob mob mob

--- lib/osf-components/addon/modifiers/with-branding.ts
+++ lib/osf-components/addon/modifiers/with-branding.ts
@@ -3,20 +3,25 @@ import Modifier from 'ember-oo-modifiers';
 import Brand from 'ember-osf-web/models/brand';
 
 class WithBrandingModifier extends Modifier {
-    didInsertElement([brand]: [Brand | null]) {
-        if (!brand) {
-            return;
-        }
-
+    didReceiveArguments([brand]: [Brand | null]) {
         const { element } = this;
         const elementStyle = element.style;
 
-        element.classList.add('brand-container');
-        elementStyle.setProperty('--primary-color', brand.primaryColor);
-        elementStyle.setProperty('--secondary-color', brand.secondaryColor);
-        elementStyle.setProperty('--navbar-logo-img-url', `url(${brand.topnavLogoImage})`);
-        elementStyle.setProperty('--hero-logo-img-url', `url(${brand.heroLogoImage})`);
-        elementStyle.setProperty('--hero-background-img-url', `url(${brand.heroBackgroundImage})`);
+        if (brand) {
+            element.classList.add('brand-container');
+            elementStyle.setProperty('--primary-color', brand.primaryColor);
+            elementStyle.setProperty('--secondary-color', brand.secondaryColor);
+            elementStyle.setProperty('--navbar-logo-img-url', `url(${brand.topnavLogoImage})`);
+            elementStyle.setProperty('--hero-logo-img-url', `url(${brand.heroLogoImage})`);
+            elementStyle.setProperty('--hero-background-img-url', `url(${brand.heroBackgroundImage})`);
+        } else {
+            element.classList.remove('brand-container');
+            elementStyle.removeProperty('--primary-color');
+            elementStyle.removeProperty('--secondary-color');
+            elementStyle.removeProperty('--navbar-logo-img-url');
+            elementStyle.removeProperty('--hero-logo-img-url');
+            elementStyle.removeProperty('--hero-background-img-url');
+        }
     }
 }
 
