8ebc516c4083c21c3f8663449b8768cc5b9663aa
Yuhuai Liu <yuhuai@cos.io>
2020-05-29 04:50:14 -0400

    make manager a tagless component

--- lib/osf-components/addon/components/contributors/manager/component.ts
+++ lib/osf-components/addon/components/contributors/manager/component.ts
@@ -1,8 +1,9 @@
+import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
-
 import { layout } from 'ember-osf-web/decorators/component';
 import template from './template';
 
 @layout(template)
+@tagName('')
 export default class ContributorsManager extends Component {
 }
