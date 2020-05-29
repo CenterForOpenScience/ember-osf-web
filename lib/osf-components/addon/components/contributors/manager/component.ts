2eb76b51fa67ec3acddcc9b6b3b240252dd9125f
Yuhuai Liu <yuhuai@cos.io>
2020-05-29 09:51:38 -0400

    make contributors-list load more on scroll

--- lib/osf-components/addon/components/contributors/manager/component.ts
+++ lib/osf-components/addon/components/contributors/manager/component.ts
@@ -1,9 +1,34 @@
 import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency-decorators';
 import { layout } from 'ember-osf-web/decorators/component';
+import ContributorModel from 'ember-osf-web/models/contributor';
+import DraftRegistrationModel from 'ember-osf-web/models/draft-registration';
+import NodeModel from 'ember-osf-web/models/node';
 import template from './template';
 
 @layout(template)
 @tagName('')
 export default class ContributorsManager extends Component {
+    node?: NodeModel | DraftRegistrationModel;
+    @tracked contributors: ContributorModel[] = [];
+    @tracked totalPage: number = 1;
+    @tracked currentPage: number = 1;
+
+    get hasMore() {
+        return this.currentPage <= this.totalPage;
+    }
+
+    @task({ on: 'init', enqueue: true })
+    fetchContributors = task(function *(this: ContributorsManager) {
+        if (this.node && this.hasMore) {
+            const currentPageResult = yield this.node.queryHasMany('contributors', {
+                page: this.currentPage,
+            });
+            this.totalPage = Math.ceil(currentPageResult.meta.total / currentPageResult.meta.per_page);
+            this.contributors.pushObjects(currentPageResult);
+            this.currentPage = this.currentPage + 1;
+        }
+    });
 }
