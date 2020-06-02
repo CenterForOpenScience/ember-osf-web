76b850f72644f7c33b7c7aa01f7489629ce6ca30
Fabrice Mizero <fabrice@cos.io>
2020-06-02 12:05:29 -0400

    Mob

--- mirage/views/share-search.ts
+++ mirage/views/share-search.ts
@@ -16,11 +16,24 @@ export function shareSearch(schema: Schema, request: Request) {
     // TODO:
     // get provider from request body
     // call buildRegistrationSearchResponse
+		const {
+				query: {
+						bool: {
+								filter: filterList,
+						},
+				},
+		} = JSON.parse(request.requestBody);
+
+		const providerFilter = filterList.find(filter => Boolean(filter.terms && filter.terms.sources));
+		if (providerFilter) {
+				const { terms: { sources: providerShareKeys } } = providerFilter;
+		}
+
 }
 
 function buildRegistrationSearchResponse(
     schema: Schema,
-    provider: ModelInstance<RegistrationProviderModel>
+    provider: ModelInstance<RegistrationProviderModel>,
 ) {
     // TODO serialize all registrations for the given provider
 }
@@ -31,32 +44,32 @@ function serializeRegistration(reg: ModelInstance<RegistrationModel>) {
         _source: {
             date: reg.dateRegistered,
             date_published: reg.dateRegistered,
-            justification: null,
+            justification:  null,
             sources: ['OSF'],
             affiliations: [],
-            registration_type: 'Prereg Challenge',
-            type: 'registration',
+            registration_type:  'Prereg Challenge',
+            type:  'registration',
             subject_synonyms: [],
-            id: '4614E-CF4-5BB',
-            language: null,
+            id:  '4614E-CF4-5BB',
+            language:  null,
             types: ['registration', 'publication', 'creative work'],
             lists: {
                 contributors: [],
             },
             subjects: [],
-            title: "2016, Deutchman, The Role of Framing Effects, the Dark Triad, and Empathy in Predicting Behavior in a One-shot Prisoner's Dilemma",
-            retracted: false,
+            title:  "2016, Deutchman, The Role of Framing Effects, the Dark Triad, and Empathy in Predicting Behavior in a One-shot Prisoner's Dilemma",
+            retracted:  false,
             contributors: ['Jess Sullivan', 'Paul Michael Deutchman'],
-            date_updated: '2017-07-30T17:34:31.234631+00:00',
-            description: 'This project is designed to support Haverford College economics majors who produce empirical theses. The structure is based on the TIER Documentation Protocol. Additional information about Project TIER is available at http://projecttier.org',
-            date_modified: '2017-07-31T02:26:51.566149+00:00',
-            date_created: '2016-12-07T18:00:39.914615+00:00',
+            date_updated:  '2017-07-30T17:34:31.234631+00:00',
+            description:  'This project is designed to support Haverford College economics majors who produce empirical theses. The structure is based on the TIER Documentation Protocol. Additional information about Project TIER is available at http://projecttier.org',
+            date_modified:  '2017-07-31T02:26:51.566149+00:00',
+            date_created:  '2016-12-07T18:00:39.914615+00:00',
             tags: ['project'],
-            withdrawn: false,
-            identifiers: ['http://osf.io/aurjt/'],
+            withdrawn:  false,
+            identifiers: ['http://osf.io/aurjt/'], ,
         },
-    }
-};
+    };
+}
 
 function serializeContributor(contributor: ModelInstance<Contributor>) {
 }
