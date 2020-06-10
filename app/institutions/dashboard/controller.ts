accbf83b7ec43d237939f1bd88829f3b76fc293f
Abram Booth <abram@cos.io>
2020-06-10 10:15:19 -0400

    wip

--- app/institutions/dashboard/controller.ts
+++ app/institutions/dashboard/controller.ts
@@ -1,13 +1,17 @@
 import { action, computed } from '@ember/object';
 import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 import Controller from '@ember/controller';
 import { InstitutionsDashboardModel } from 'ember-osf-web/institutions/dashboard/route';
 import InstitutionModel from 'ember-osf-web/models/institution';
 import InstitutionDepartmentModel from 'ember-osf-web/models/institution-department';
 import InstitutionSummaryMetricModel from 'ember-osf-web/models/institution-summary-metric';
+import CurrentUser from 'ember-osf-web/services/current-user';
 
 export default class InstitutionsDashboardController extends Controller {
+    @service currentUser!: CurrentUser;
+
     @alias('model.taskInstance.value') modelValue?: InstitutionsDashboardModel;
     @alias('modelValue.institution') institution?: InstitutionModel;
     @alias('modelValue.summaryMetrics') summaryMetrics?: InstitutionSummaryMetricModel;
@@ -31,6 +35,19 @@ export default class InstitutionsDashboardController extends Controller {
     @action
     onCsvButtonMouseLeave() {
         this.set('csvImgSrc', '/assets/images/institutions/csv.svg');
+    }
+
+    @action
+    async downloadCsv() {
+        const url = this.institution!.hasMany('userMetrics').link();
+        const csvData = await this.currentUser.authenticatedAJAX({
+            url,
+            headers: { Accept: 'text/csv' },
+        });
+        console.log({ url, csvData });
+        const csvBlob = new Blob(csvData, { type: 'text/csv' });
+        const csvUrl = URL.createObjectURL(csvBlob);
+        window.open(csvUrl, '_blank');
     }
 }
 
