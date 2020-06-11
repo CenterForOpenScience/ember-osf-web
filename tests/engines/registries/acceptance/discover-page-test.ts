7493b765d742c758247513eeb7e164c3e5dcea85
Yuhuai Liu <yuhuai@cos.io>
2020-06-11 10:45:31 -0400

    mob

--- tests/engines/registries/acceptance/discover-page-test.ts
+++ tests/engines/registries/acceptance/discover-page-test.ts
@@ -1,23 +1,23 @@
-import Service from '@ember/service';
-import { click, currentRouteName, currentURL, fillIn, settled, triggerKeyEvent } from '@ember/test-helpers';
+// import Service from '@ember/service';
+// import { click, currentRouteName, currentURL, fillIn, settled, triggerKeyEvent } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
-import { t } from 'ember-intl/test-support';
+// import { t } from 'ember-intl/test-support';
 import { percySnapshot } from 'ember-percy';
-import { setBreakpoint } from 'ember-responsive/test-support';
+// import { setBreakpoint } from 'ember-responsive/test-support';
 import { TestContext } from 'ember-test-helpers';
 import { module, test } from 'qunit';
 
 import { visit } from 'ember-osf-web/tests/helpers';
 import { setupEngineApplicationTest } from 'ember-osf-web/tests/helpers/engines';
-import { deserializeResponseKey } from 'ember-osf-web/transforms/registration-response-key';
+// import { deserializeResponseKey } from 'ember-osf-web/transforms/registration-response-key';
 
-const currentUserStub = Service.extend();
-const storeStub = Service.extend();
-const analyticsStub = Service.extend();
+// const currentUserStub = Service.extend();
+// const storeStub = Service.extend();
+// const analyticsStub = Service.extend();
 
-function getHrefAttribute(selector: string) {
-    return document.querySelector(selector)!.getAttribute('href');
-}
+// function getHrefAttribute(selector: string) {
+//     return document.querySelector(selector)!.getAttribute('href');
+// }
 
 module('Registries | Acceptance | aggregate discover', hooks => {
     setupEngineApplicationTest(hooks, 'registries');
@@ -27,9 +27,10 @@ module('Registries | Acceptance | aggregate discover', hooks => {
         /*
         server.loadFixtures('schema-blocks');
         server.loadFixtures('registration-schemas');
-        server.loadFixtures('registration-providers');
         server.loadFixtures('licenses');
         */
+        server.create('registration-provider', { id: 'osf' });
+        server.create('registration-provider', { id: 'another' });
     });
 
     // available assertions:
@@ -44,14 +45,23 @@ module('Registries | Acceptance | aggregate discover', hooks => {
     //
     // happy path:
     // - arrive at the page (no query params)
+    // - take percy snapshot
     // - assert search results exist
     // - important controls exist
     // - enter query -- search happens
     // - click filters -- search happens
+    // - assert paginator exists
     //
     // path with different initial state:
     // - arrive at page WITH query params
+    // - take percy snapshot
     // - assert initial search reflects query params
+    // - assert filter checkbox is checked
+    // - uncheck filter
+    // - assert search reflects new query
+    // - assert filter checkbox is unchecked
     //
-    // pagination tests (getting into edge cases?)
+    // query returns no result:
+    // - assert the page shows no result help text
+    // - take percy snapshot
 });
