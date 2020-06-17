21937ff628c0e910e2d9b95ab497483ffbcd2b2a
Abram Booth <abram@cos.io>
2020-06-17 15:11:30 -0400

    double settle for institution dashboard

--- tests/acceptance/institutions/dashboard-test.ts
+++ tests/acceptance/institutions/dashboard-test.ts
@@ -1,4 +1,4 @@
-import { currentURL, visit } from '@ember/test-helpers';
+import { currentURL, settled, visit } from '@ember/test-helpers';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { percySnapshot } from 'ember-percy';
 import { module, test } from 'qunit';
@@ -16,13 +16,19 @@ module(moduleName, hooks => {
             id: 'has-users',
         }, 'withMetrics');
         await visit('/institutions/has-users/dashboard');
+        await settled();
         assert.equal(
             currentURL(),
             '/institutions/has-users/dashboard',
             "Still at '/institutions/has-users/dashboard'.",
         );
         await percySnapshot(`${moduleName} - default`);
+        await settled();
+        await settled(); // the paginator apparently only renders after two settles
+        assert.dom('[data-test-next-page-button]').exists({ count: 1 }, 'next page button exists!?');
         await click('[data-test-next-page-button]');
+        await settled();
+        await settled();
         await percySnapshot(`${moduleName} - next page`);
     });
 });
--- tests/integration/routes/institutions/dashboard/-components/institutional-users-list/component-test.ts
+++ tests/integration/routes/institutions/dashboard/-components/institutional-users-list/component-test.ts
@@ -1,4 +1,4 @@
-import { click, render } from '@ember/test-helpers';
+import { click, render, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import { OsfLinkRouterStub } from 'ember-osf-web/tests/integration/helpers/osf-link-router-stub';
@@ -37,6 +37,7 @@ module('Integration | routes | institutions | dashboard | -components | institut
                 @institution={{this.model.taskInstance.institution}}
             />
         `);
+        await settled();
         assert.dom('[data-test-header-name]')
             .exists({ count: 1 }, '1 name header');
         assert.dom('[data-test-header-department]')
@@ -56,6 +57,7 @@ module('Integration | routes | institutions | dashboard | -components | institut
             .exists({ count: 10 }, '10 in the list with private projects');
 
         await click('[data-test-next-page-button]');
+        await settled();
 
         assert.dom('[data-test-item-name]')
             .exists({ count: 5 }, '5 in the list with a name');
@@ -105,22 +107,27 @@ module('Integration | routes | institutions | dashboard | -components | institut
                 @institution={{this.model.taskInstance.institution}}
             />
         `);
+        await settled();
         assert.dom('[data-test-item-name]')
             .exists({ count: 3 }, '3 users');
 
         await click('[data-test-ascending-sort="user_name"]');
+        await settled();
         assert.dom('[data-test-item-name]')
             .containsText('Hulk Hogan', 'Sorts by name ascendening');
 
         await click('[data-test-descending-sort="user_name"]');
+        await settled();
         assert.dom('[data-test-item-name]')
             .containsText('John Doe', 'Sorts by name descendening');
 
         await click('[data-test-ascending-sort="department"]');
+        await settled();
         assert.dom('[data-test-item-department]')
             .hasText('Architecture', 'Sorts by department ascendening');
 
         await click('[data-test-descending-sort="department"]');
+        await settled();
         assert.dom('[data-test-item-department]')
             .hasText('Psychology', 'Sorts by department descendening');
     });
