63eaf567d087e207aa65a1cec3602cfa73b7c133
Baylee Swenson <baylee@cos.io>
2020-06-19 11:33:14 -0400

    Add collections analytics

--- lib/collections/addon/provider/discover/route.ts
+++ lib/collections/addon/provider/discover/route.ts
@@ -1,6 +1,8 @@
+import { action } from '@ember/object';
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 import { DS } from 'ember-data';
+import Analytics from 'ember-osf-web/services/analytics';
 import Theme from 'ember-osf-web/services/theme';
 
 export default class ProviderDiscover extends Route {
@@ -9,8 +11,14 @@ export default class ProviderDiscover extends Route {
 
     @service store!: DS.Store;
     @service theme!: Theme;
+    @service analytics!: Analytics;
 
     model() {
         return [];
+    }
+
+    @action
+    didTransition() {
+        this.analytics.trackPage();
     }
 }
