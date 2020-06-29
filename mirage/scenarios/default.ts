b9dd91f9b6abd5ad105b8d52130aa8c6db2c1485
Futa Ikeda <futa.ikeda@gmail.com>
2020-06-29 12:17:25 -0400

    Revert some changes

--- mirage/scenarios/default.ts
+++ mirage/scenarios/default.ts
@@ -5,8 +5,7 @@ import FileProvider from 'ember-osf-web/models/file-provider';
 import { Permission } from 'ember-osf-web/models/osf-model';
 import User from 'ember-osf-web/models/user';
 
-// import { draftRegisterNodeMultiple, forkNode, registerNodeMultiple } from '../helpers';
-import { forkNode } from '../helpers';
+import { draftRegisterNodeMultiple, forkNode, registerNodeMultiple } from '../helpers';
 // import { placekitten } from '../utils';
 
 const {
