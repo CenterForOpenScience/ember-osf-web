9195b9400008ae48eb40010547fbc3f4a8b25b89
Yuhuai Liu <yuhuai@cos.io>
2020-07-06 11:10:37 -0400

    use Permission enum

--- lib/osf-components/addon/components/contributors/card/editable/component.ts
+++ lib/osf-components/addon/components/contributors/card/editable/component.ts
@@ -2,11 +2,12 @@ import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 
 import { layout } from 'ember-osf-web/decorators/component';
+import { Permission } from 'ember-osf-web/models/osf-model';
 import styles from './styles';
 import template from './template';
 
 @layout(template, styles)
 @tagName('')
 export default class ContributorsCardEditable extends Component {
-    permissionOptions = ['read', 'write', 'admin'];
+    permissionOptions = [...Object.values(Permission)];
 }
