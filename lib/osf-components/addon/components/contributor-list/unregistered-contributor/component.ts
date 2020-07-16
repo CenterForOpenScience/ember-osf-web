2572e1d40ed5e8adf51a921407acfc9094c03925
Fabrice Mizero <fabrice@cos.io>
2020-07-16 14:30:36 -0400

    Add actions

--- lib/osf-components/addon/components/contributor-list/unregistered-contributor/component.ts
+++ lib/osf-components/addon/components/contributor-list/unregistered-contributor/component.ts
@@ -3,6 +3,7 @@ import Component from '@ember/component';
 import { action } from '@ember/object';
 import { bool } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
 import { layout } from 'ember-osf-web/decorators/component';
 import CurrentUserService from 'ember-osf-web/services/current-user';
 import styles from './styles';
@@ -13,9 +14,15 @@ import template from './template';
 export default class UnregisteredContributorComponent extends Component {
     @service currentUser!: CurrentUserService;
     @bool('currentUser.currentUserId') isLoggedIn: boolean = false;
+    @tracked shouldOpenClaimDialog: boolean = false;
 
     @action
     claimUser() {
         return null;
+    }
+
+    @action
+    showDialog() {
+        this.shouldOpenClaimDialog = true;
     }
 }
