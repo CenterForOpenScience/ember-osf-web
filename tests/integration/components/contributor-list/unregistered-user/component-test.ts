6d75c5da34ec56e8e3dfba1bceee2e88ecbb18a9
Fabrice Mizero <fabrice@cos.io>
2020-07-21 12:27:14 -0400

    Fix integration tests

--- tests/integration/components/contributor-list/unregistered-user/component-test.ts
+++ tests/integration/components/contributor-list/unregistered-user/component-test.ts
@@ -1,20 +1,12 @@
-// import Service from '@ember/service';
-import { fillIn, pauseTest, render, triggerEvent } from '@ember/test-helpers';
+import { fillIn, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 import CurrentUser from 'ember-osf-web/services/current-user';
 import { setupRenderingTest } from 'ember-qunit';
 import { TestContext } from 'ember-test-helpers';
-
-import { click } from 'ember-osf-web/tests/helpers';
-// import faker from 'faker';
-
 import { module, test } from 'qunit';
 
-// const currentUserStub = Service.extend({
-//     currentUserId: 'dscf',
-//     ajaxHeaders: () => ({}),
-// });
+import { click } from 'ember-osf-web/tests/helpers';
 
 interface ThisTestContext extends TestContext {
     contrib: { email: string, unregisteredContributor: string, users: { fullName: string } };
@@ -48,53 +40,55 @@ module('Integration | Component | contributor-list/unregistered-user', hooks => 
     // - has error messages
     // - "Claim" button disabled when invalid text inputed
     test('logged in scenario', async function(this: ThisTestContext, assert) {
-        const user = server.create('user', { id: 'dscf' });
-        server.create('user-email', { primary: true, user });
-        const userModel = await this.store.findRecord('user', user.id);
-        this.owner.lookup('service:current-user').set('user', userModel);
-        this.owner.lookup('service:current-user').set('currentUserId', user.id);
+        const mirageUser = server.create('user');
+        server.create('user-email', { primary: true, user: mirageUser });
+        const user = await this.store.findRecord('user', mirageUser.id);
+        this.owner.lookup('service:current-user').setProperties({ user, currentUserId: user.id });
 
         await render(hbs`<ContributorList::UnregisteredContributor
             @contributor={{this.contrib}} @nodeId={{this.node.id}}/>`);
-        assert.dom(this.element).hasText(this.contrib.unregisteredContributor, 'Has correct name');
-        await triggerEvent('[data-test-unregistered-contributor-name]', 'mouseover');
-        await pauseTest();
-        // check the popover
-        assert.dom('[data-test-claim-user-tooltip-message]').isVisible('tooltip message is visible');
+        assert.dom(this.element).hasText(this.contrib.unregisteredContributor, 'Has unregistered contrib name');
 
         await click('[data-test-unregistered-contributor-name]');
         // check the modal
-        assert.dom('[data-test-modal-heading]').isVisible();
-        assert.dom('[data-test-modal-heading]').containsText(this.contrib.email);
-        assert.dom('[data-test-modal-main]').isVisible();
+        assert.dom('[data-test-modal-heading]').isVisible('Modal heading is visible');
+        assert.dom('[data-test-modal-heading]').containsText(this.contrib.email,
+            'Modal heading contains unreg. contrib email');
+        assert.dom('[data-test-modal-main]').isVisible('Modal main is visible');
         assert.dom('[data-test-modal-cancel-button]').isVisible();
+        assert.dom('[data-test-modal-claim-button]').isEnabled();
         assert.dom('[data-test-modal-claim-button]').isVisible();
-        assert.dom('[data-test-email-input]').doesNotExist();
+        assert.dom('[data-test-email-input]').doesNotExist('Claim email input does not exist');
     });
 
     test('logged out scenario', async function(this: ThisTestContext, assert) {
+        this.owner.lookup('service:current-user').setProperties({ user: null, currentUserId: null });
         await render(hbs`<ContributorList::UnregisteredContributor
             @contributor={{this.contrib}} @nodeId={{this.node.id}}/>`);
-        assert.dom(this.element).hasText('Lou Bega', 'Has correct name');
-        await triggerEvent('[data-test-unregistered-contributor-name]', 'mouseover');
-        // check the popover
-        assert.dom('[data-test-claim-user-tooltip-message]').isVisible();
+        assert.dom(this.element).hasText(this.contrib.unregisteredContributor, 'Has unregistered contrib name');
 
         await click('[data-test-unregistered-contributor-name]');
         // check the modal
         assert.dom('[data-test-modal-heading]').isVisible();
         assert.dom('[data-test-modal-heading]').doesNotContainText(this.contrib.email);
-        assert.dom('[data-test-modal-heading]').containsText(this.contrib.users.fullName);
+        assert.dom('[data-test-modal-heading]').containsText(
+            this.contrib.unregisteredContributor,
+            'Modal heading contains unreg. contrib name',
+        );
         assert.dom('[data-test-modal-main]').isVisible();
         assert.dom('[data-test-modal-cancel-button]').isVisible();
         assert.dom('[data-test-modal-claim-button]').isVisible();
-        assert.dom('[data-test-modal-claim-button]').isDisabled();
-        assert.dom('[data-test-email-input]').exists();
+        // TODO: figure/fix why is disabled does not work
+        assert.dom('[data-test-modal-claim-button]').isDisabled('Claim button is disabled by default');
+        assert.dom('[data-test-email-input]').exists('Claim email input exists');
 
         await fillIn('[data-test-email-input]', 'lou bega');
-        assert.dom('[data-test-modal-claim-button]').isDisabled();
+        assert.dom('[data-test-unreg-contrib-validation-error]').isVisible('Has invalid email address');
+        // TODO: figure/fix why is disabled does not work
+        assert.dom('[data-test-modal-claim-button]').isDisabled('Claim button is disabled');
 
         await fillIn('[data-test-email-input]', 'lou.bega@bega.lou');
-        assert.dom('[data-test-modal-claim-button]').isEnabled();
+        assert.dom('[data-test-unreg-contrib-validation-error]').doesNotExist('Has valid email address');
+        assert.dom('[data-test-modal-claim-button]').isEnabled('Claim button is now enabled');
     });
 });
