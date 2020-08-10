7fed9e2b273b923a696e385aa6f8f62e6c8fa77d
Fabrice Mizero <fabrice@cos.io>
2020-08-10 17:08:32 -0400

    Redirect branded index to discover

---
+++ lib/registries/addon/branded/index/route.ts
@@ -0,0 +1,8 @@
+import Route from '@ember/routing/route';
+
+export default class BrandedRegistriesRoute extends Route {
+    beforeModel() {
+        const params: { providerId?: string } = this.paramsFor('branded');
+        return this.replaceWith('branded.discover', params.providerId);
+    }
+}
