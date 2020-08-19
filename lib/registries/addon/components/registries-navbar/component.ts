3b79c8a0762abccb5ffbce3e9072e72b6a95def1
baylee-d <baylee@cos.io>
2020-08-19 11:51:35 -0400

    [ENG-2050] Add campaign queryParam for collections and registries (#1045)
    * Add campaign queryparam for registries navbar
    
    * Add collections campaign queryParam
    
    * Fix campaign variable
    
    * Add check for campaign variable to remove wrong query param

--- lib/registries/addon/components/registries-navbar/component.ts
+++ lib/registries/addon/components/registries-navbar/component.ts
@@ -34,6 +34,7 @@ export default class RegistriesNavbar extends AuthBase {
     provider?: RegistrationProviderModel;
 
     @and('media.isMobile', 'searchDropdownOpen') showSearchDropdown!: boolean;
+
     @computed('provider.{allowSubmissions,id}')
     get showAddRegistrationButton() {
         if (!this.provider) {
