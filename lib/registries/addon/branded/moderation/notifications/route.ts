695c1f2534103c0d24f0ff4791a784d457b6b5ca
Fabrice Mizero <fabrice@cos.io>
2020-08-28 10:50:17 -0400

    Add route structure

--- lib/registries/addon/branded/moderation/notifications/route.ts
+++ lib/registries/addon/branded/moderation/notifications/route.ts
@@ -5,12 +5,12 @@ import DS from 'ember-data';
 
 import Analytics from 'ember-osf-web/services/analytics';
 
-export default class BrandedModerationIndexRoute extends Route {
+export default class BrandedModerationNotificationsRoute extends Route {
     @service analytics!: Analytics;
     @service store!: DS.Store;
 
-    beforeModel() {
-        this.replaceWith('branded.moderation.submissions');
+    model() {
+        return this.modelFor('branded');
     }
 
     @action
