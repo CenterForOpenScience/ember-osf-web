e27309bc45d50802f5aa86f69922c3b1149f3ae3
Futa Ikeda <futa.ikeda@gmail.com>
2020-08-31 16:18:48 -0400

    Add permissions logic to redirect

--- lib/registries/addon/branded/moderation/route.ts
+++ lib/registries/addon/branded/moderation/route.ts
@@ -1,16 +1,27 @@
 import { action } from '@ember/object';
 import Route from '@ember/routing/route';
+import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 import DS from 'ember-data';
 
 import Analytics from 'ember-osf-web/services/analytics';
+import CurrentUserService from 'ember-osf-web/services/current-user';
 
 export default class BrandedModerationRoute extends Route {
     @service analytics!: Analytics;
+    @service currentUser!: CurrentUserService;
+    @service router!: RouterService;
     @service store!: DS.Store;
 
     model() {
         return this.modelFor('branded');
+    }
+
+    afterModel() {
+        const { user } = this.currentUser;
+        if (!user || !user.canViewReviews) {
+            this.transitionTo('page-not-found', window.location.pathname.slice(1));
+        }
     }
 
     @action
