dbbbd64c32a60ab60f2fc318c2def08fa47c596c
Baylee Swenson <baylee@cos.io>
2020-09-03 00:07:30 -0400

    Add moderator adapter, update model to use enum for permissionGroup

--- app/models/moderator.ts
+++ app/models/moderator.ts
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 
 import OsfModel from './osf-model';
+import ProviderModel from './provider';
 
-const { attr } = DS;
+const { attr, belongsTo } = DS;
 
 export enum PermissionGroup {
     Admin = 'admin',
@@ -13,6 +14,9 @@ export default class ModeratorModel extends OsfModel {
     @attr('string') permissionGroup!: PermissionGroup;
     @attr('string') fullName!: string;
     @attr('string') email!: string;
+
+    @belongsTo('moderator')
+    provider?: ProviderModel;
 }
 
 declare module 'ember-data/types/registries/model' {
--- app/models/provider.ts
+++ app/models/provider.ts
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 import LicenseModel from './license';
+import ModeratorModel from './moderator';
 import OsfModel from './osf-model';
 import SubjectModel from './subject';
 
@@ -44,4 +45,7 @@ export default abstract class ProviderModel extends OsfModel {
 
     @hasMany('license', { inverse: null })
     licensesAcceptable!: DS.PromiseManyArray<LicenseModel>;
+
+    @hasMany('moderator')
+    moderators!: DS.PromiseManyArray<ModeratorModel>;
 }
