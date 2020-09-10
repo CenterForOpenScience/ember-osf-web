7bb6a1721bdd98fc0890e0f82f950a1856704907
Fabrice Mizero <fabrice@cos.io>
2020-09-10 13:53:15 -0400

    Fix 404ing recent registrations links

--- lib/registries/addon/components/registries-recent-list/component.ts
+++ lib/registries/addon/components/registries-recent-list/component.ts
@@ -2,43 +2,34 @@ import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { localClassNames } from 'ember-css-modules';
-import Store from 'ember-data/store';
+import config from 'ember-get-config';
 
 import { layout } from 'ember-osf-web/decorators/component';
-import RegistrationSchema from 'ember-osf-web/models/registration-schema';
+import Registration from 'ember-osf-web/models/registration';
 import Analytics from 'ember-osf-web/services/analytics';
-import { ShareRegistration } from 'registries/services/share-search';
 import template from './template';
+
+const {
+    OSF: {
+        url: osfUrl,
+    },
+} = config;
 
 @layout(template)
 @localClassNames('RecentList')
 export default class RegistriesRecentList extends Component {
-    static positionalParams = ['items'];
-
     @service analytics!: Analytics;
-    @service store!: Store;
+    recentRegistrations!: Registration[];
 
-    items!: ShareRegistration[];
-
-    @computed('items')
+    @computed('recentRegistrations.[]')
     get registrations() {
-        const schemaLookup = this.store.peekAll('registration-schema').reduce(
-            (acc, schema) => ({ ...acc, [schema.name]: schema }),
-            { } as { [key: string]: RegistrationSchema },
-        );
-
-        return this.items.map(item => {
-            let registrationURL = item.mainLink;
-
-            if (!registrationURL.endsWith('/')) {
-                registrationURL += '/';
-            }
-
-            if (schemaLookup[item.registrationType]) {
-                registrationURL += `register/${schemaLookup[item.registrationType].id}`;
-            }
-
-            return { ...item, registrationURL };
-        });
+        return this.recentRegistrations.map(({ id, title, contributors }) => ({
+            id,
+            title,
+            url: `${osfUrl}/${id}`,
+            contributors: contributors
+                .filterBy('bibliographic')
+                .map(({ fullName }) => ({ name: fullName })),
+        }));
     }
 }
