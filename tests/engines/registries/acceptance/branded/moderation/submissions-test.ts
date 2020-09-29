8db16992042b6410a04b8410b72849d28291d343
Futa Ikeda <futa.ikeda@gmail.com>
2020-09-29 17:13:47 -0400

    Add moar tests

--- tests/engines/registries/acceptance/branded/moderation/submissions-test.ts
+++ tests/engines/registries/acceptance/branded/moderation/submissions-test.ts
@@ -40,7 +40,7 @@ module('Registries | Acceptance | branded.moderation | submissions', hooks => {
             provider: this.registrationProvider,
         }, 'asModerator');
         await visit('/registries/sbmit/moderation/submissions');
-        await percySnapshot('moderation submissions page');
+        await percySnapshot('moderation submissions page: no registrations');
         assert.equal(currentRouteName(), 'registries.branded.moderation.submissions',
             'On the submissions page of registries reviews');
 
@@ -82,9 +82,9 @@ module('Registries | Acceptance | branded.moderation | submissions', hooks => {
 
     test('Submissions pending: many registrations', async function(this: ModerationSubmissionsTestContext, assert) {
         server.createList('registration', 12, { machineState: 'pending', provider: this.registrationProvider });
-        server.createList('registration', 3, { machineState: 'accepted', provider: this.registrationProvider });
+        server.createList('registration', 2, { machineState: 'accepted', provider: this.registrationProvider });
         server.createList('registration', 3, { machineState: 'rejected', provider: this.registrationProvider });
-        server.createList('registration', 3, { machineState: 'withdrawn', provider: this.registrationProvider });
+        server.createList('registration', 4, { machineState: 'withdrawn', provider: this.registrationProvider });
         const currentUser = server.create('user', 'loggedIn');
         server.create('moderator', {
             id: currentUser.id,
@@ -92,21 +92,50 @@ module('Registries | Acceptance | branded.moderation | submissions', hooks => {
             provider: this.registrationProvider,
         }, 'asAdmin');
         await visit('/registries/sbmit/moderation/submissions');
-        await percySnapshot('moderation submissions page');
+        await percySnapshot('moderation submissions page: many registrations');
         assert.equal(currentRouteName(), 'registries.branded.moderation.submissions',
             'On the submissions page of registries reviews');
 
         // Pending tab
-        // check for pagination
-        // check for icon
-        // check for title
-        // check for time submitted
-        // check for time admin action taken
+        assert.dom('[data-test-ascending-sort]').exists({ count: 2 }, 'Sort button exists');
+        assert.dom('[data-test-registration-list-card]').exists({ count: 10 }, '10 pending registrations shown');
+        assert.dom('[data-test-registration-list-card-icon="pending"]').exists({ count: 10 }, 'Proper icons shown');
+        assert.dom('[data-test-registration-list-card-title]').exists({ count: 10 }, 'Title shown');
+        assert.dom('[data-test-registration-list-card-submitted]').exists({ count: 10 }, 'Submitted info shown');
+        assert.dom('[data-test-registration-list-card-moderated]').doesNotExist('Moderator action is not shown');
+        assert.dom('[data-test-next-page-button]').exists('Pagination shown');
+        await click('[data-test-next-page-button]');
+        assert.dom('[data-test-next-page-button]').hasAttribute('disabled', 'Pagination stops at 2 pages');
+        assert.dom('[data-test-registration-list-card]').exists({ count: 2 }, '2 more pending registrations shown');
 
         // Accepted tab
+        await click('[data-test-next-page-button]');
+        assert.dom('[data-test-ascending-sort]').exists({ count: 2 }, 'Sort button exists');
+        assert.dom('[data-test-registration-list-card]').exists({ count: 2 }, '2 accepted registrations shown');
+        assert.dom('[data-test-registration-list-card-icon="accepted"]').exists({ count: 2 }, 'Proper icons shown');
+        assert.dom('[data-test-registration-list-card-title]').exists({ count: 2 }, 'Title shown');
+        assert.dom('[data-test-registration-list-card-submitted]').exists({ count: 2 }, 'Submitted info shown');
+        assert.dom('[data-test-registration-list-card-moderated]').exists({ count: 2 }, 'Moderator action is shown');
+        assert.dom('[data-test-next-page-button]').doesNotExist('No pagination shown');
 
         // Rejected tab
+        await click('[data-test-submissions-type="rejected"]');
+        assert.dom('[data-test-ascending-sort]').exists({ count: 2 }, 'Sort button exists');
+        assert.dom('[data-test-registration-list-card]').exists({ count: 3 }, '3 rejected registrations shown');
+        assert.dom('[data-test-registration-list-card-icon="rejected"]').exists({ count: 3 }, 'Proper icons shown');
+        assert.dom('[data-test-registration-list-card-title]').exists({ count: 3 }, 'Title shown');
+        assert.dom('[data-test-registration-list-card-submitted]').exists({ count: 3 }, 'Submitted info shown');
+        assert.dom('[data-test-registration-list-card-moderated]').exists({ count: 3 }, 'Moderator action is shown');
+        assert.dom('[data-test-next-page-button]').doesNotExist('No pagination shown');
 
         // Withdrawn tab
+        await click('[data-test-submissions-type="withdrawn"]');
+        assert.dom('[data-test-ascending-sort]').exists({ count: 2 }, 'Sort button exists');
+        assert.dom('[data-test-registration-list-card]').exists({ count: 4 }, '4 withdrawn registrations shown');
+        assert.dom('[data-test-registration-list-card-icon="withdrawn"]').exists({ count: 4 }, 'Proper icons shown');
+        assert.dom('[data-test-registration-list-card-title]').exists({ count: 4 }, 'Title shown');
+        assert.dom('[data-test-registration-list-card-submitted]').exists({ count: 4 }, 'Submitted info shown');
+        assert.dom('[data-test-registration-list-card-moderated]').doesNotExist('Moderator action is not shown');
+        assert.dom('[data-test-next-page-button]').doesNotExist('No pagination shown');
     });
 });
