30d68f165e3a27cc65fa4db50a1e84e13f88bac0
Yuhuai Liu <adlius45@hotmail.com>
2020-12-03 11:31:41 -0500

    Apply suggestions from code review
    Co-authored-by: Fabrice Mizero <fabrice@cos.io>

--- tests/integration/components/registries/registration-list/component-test.ts
+++ tests/integration/components/registries/registration-list/component-test.ts
@@ -9,9 +9,6 @@ import { setupRenderingTest } from 'ember-qunit';
 import { TestContext } from 'ember-test-helpers';
 import { OsfLinkRouterStub } from '../../../helpers/osf-link-router-stub';
 
-interface RegistrationListTestContext extends TestContext {
-    intl: Intl;
-}
 
 module('Integration | Component | registration-list', hooks => {
     setupRenderingTest(hooks);
@@ -21,11 +18,9 @@ module('Integration | Component | registration-list', hooks => {
     hooks.beforeEach(function(this: RegistrationListTestContext) {
         this.owner.register('service:router', OsfLinkRouterStub);
         this.store = this.owner.lookup('service:store');
-        this.intl = this.owner.lookup('service:intl');
     });
 
-    // tslint:disable-next-line: only-arrow-functions
-    test('list renders and paginates', async function(assert) {
+    test('list renders and paginates', async function(this: TestContext, assert) {
         const registrations = server.createList('registration', 13, 'isPending');
         server.create('registration-provider', { id: 'stayc', registrations });
         const providerModel = this.store.findRecord('registration-provider', 'stayc');
@@ -43,7 +38,7 @@ module('Integration | Component | registration-list', hooks => {
         assert.dom('[data-test-registration-list-card]').exists({ count: 3 }, 'Second page has 3 elements');
     });
 
-    test('list renders when no registration is available', async function(this: RegistrationListTestContext, assert) {
+    test('list renders when no registration is available', async function(this: TestContext, assert) {
         server.create('registration-provider', { id: 'stayc' });
         const providerModel = this.store.findRecord('registration-provider', 'stayc');
         this.set('provider', providerModel);
