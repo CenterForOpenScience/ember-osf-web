64822b416d527e0fbd439c4f01289ea2f9226c38
Yuhuai Liu <yuhuai@cos.io>
2021-01-13 15:53:57 -0500

    Add education and employment dropdown to user card

--- lib/osf-components/addon/components/contributors/card/readonly/component.ts
+++ lib/osf-components/addon/components/contributors/card/readonly/component.ts
@@ -1,11 +1,23 @@
 import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { layout } from 'ember-osf-web/decorators/component';
+import ContributorModel from 'ember-osf-web/models/contributor';
 import styles from './styles';
 import template from './template';
 
 @layout(template, styles)
 @tagName('')
 export default class ContributorsCardReadonly extends Component {
+    // required
+    contributor!: ContributorModel;
+
+    @tracked showDropdown = false;
+
+    @action
+    toggleDropdown() {
+        this.toggleProperty('showDropdown');
+    }
 }
