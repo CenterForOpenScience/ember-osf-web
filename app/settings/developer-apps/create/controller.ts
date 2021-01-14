984bc7d543a4712c7c42d3bc54b0219d417be520
Futa Ikeda <futa.ikeda@gmail.com>
2021-01-14 14:35:14 -0500

    Use setupController over controller init

--- app/settings/developer-apps/create/controller.ts
+++ app/settings/developer-apps/create/controller.ts
@@ -1,21 +1,6 @@
 import Controller from '@ember/controller';
-import RouterService from '@ember/routing/router-service';
-import { inject as service } from '@ember/service';
-import Intl from 'ember-intl/services/intl';
-import Toast from 'ember-toastr/services/toast';
-
 import DeveloperAppModel from 'ember-osf-web/models/developer-app';
-import Analytics from 'ember-osf-web/services/analytics';
 
 export default class SettingsApplicationCreateController extends Controller {
-    @service analytics!: Analytics;
-    @service intl!: Intl;
-    @service router!: RouterService;
-    @service toast!: Toast;
     developerApp!: DeveloperAppModel;
-
-    init() {
-        super.init();
-        this.developerApp = this.store.createRecord('developer-app');
-    }
 }
