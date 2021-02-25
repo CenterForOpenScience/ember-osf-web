db52321e42811f9f86baf75647972477be239fab
Yuhuai Liu <yuhuai@cos.io>
2021-02-25 10:12:29 -0500

    styling fixes. Added dropdown to editable card

--- lib/osf-components/addon/components/contributors/card/editable/component.ts
+++ lib/osf-components/addon/components/contributors/card/editable/component.ts
@@ -1,6 +1,8 @@
 import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
 
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { layout } from 'ember-osf-web/decorators/component';
 import { Permission } from 'ember-osf-web/models/osf-model';
 import ContributorsManager from 'osf-components/components/contributors/manager/component';
@@ -15,4 +17,10 @@ export default class ContributorsCardEditable extends Component {
 
     // private properties
     permissionOptions = Object.values(Permission);
+    @tracked showDropdown = false;
+
+    @action
+    toggleDropdown() {
+        this.toggleProperty('showDropdown');
+    }
 }
