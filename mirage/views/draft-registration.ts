3f27920730dff904cb17637f48d963a1958b3394
Fabrice Mizero <fabrice@cos.io>
2021-03-03 16:57:05 -0500

    Fix tests

--- mirage/views/draft-registration.ts
+++ mirage/views/draft-registration.ts
@@ -1,5 +1,7 @@
 import { HandlerContext, NormalizedRequestAttrs, Schema } from 'ember-cli-mirage';
 import { MirageDraftRegistration } from 'ember-osf-web/mirage/factories/draft-registration';
+import { NodeCategory } from 'ember-osf-web/models/node';
+import { Permission } from 'ember-osf-web/models/osf-model';
 
 export function createDraftRegistration(this: HandlerContext, schema: Schema) {
     const attrs = this.normalizedRequestAttrs('draft-registration') as NormalizedRequestAttrs<MirageDraftRegistration>;
@@ -19,9 +21,17 @@ export function createDraftRegistration(this: HandlerContext, schema: Schema) {
             datetimeUpdated: now,
             initiator: currentUser,
             tags: [],
+            category: NodeCategory.Uncategorized,
             hasProject: false,
             branchedFrom: branchedFromNode,
             ...attrs,
+        });
+        schema.contributors.create({
+            index: 0,
+            draftRegistration,
+            users: currentUser,
+            bibliographic: true,
+            permission: Permission.Admin,
         });
     } else {
         draftRegistration = schema.draftRegistrations.create({
