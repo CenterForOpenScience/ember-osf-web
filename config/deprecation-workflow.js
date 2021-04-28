6505b5b694588d9d3bd52c5ed985ff866ea62c1d
Fabrice Mizero <fabrice@cos.io>
2021-04-28 15:02:22 -0400

    Catch and silence deprecations

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,28 @@
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+    workflow: [
+        { handler: 'silence', matchId: 'ember.globals-resolver' },
+        { handler: 'silence', matchId: 'ember-source.deprecation-without-for' },
+        { handler: 'silence', matchId: 'ember-source.deprecation-without-since' },
+        { handler: 'silence', matchId: 'ember-inflector.globals' },
+        { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+        { handler: 'silence', matchId: 'computed-property.volatile' },
+        { handler: 'silence', matchId: 'deprecate-router-events' },
+        { handler: 'silence', matchId: 'implicit-injections' },
+        { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
+        { handler: 'silence', matchId: 'this-property-fallback' },
+        { handler: 'silence', matchId: '@fortawesome/ember-fontawesome.no-positional-params' },
+        { handler: 'silence', matchId: 'ember-glimmer.link-to.positional-arguments' },
+        { handler: 'silence', matchId: 'ember-views.curly-components.jquery-element' },
+        { handler: 'silence', matchId: 'ember-runtime.deprecate-copy-copyable' },
+        { handler: 'silence', matchId: 'ember-bootstrap.subclassing#Alert' },
+        { handler: 'silence', matchId: 'routing.transition-methods' },
+        { handler: 'silence', matchId: 'autotracking.mutation-after-consumption' },
+        { handler: 'silence', matchId: 'ember-click-outside.action-prop' },
+        { handler: 'silence', matchId: 'computed-property.override' },
+        { handler: 'silence', matchId: 'ember-utils.try-invoke' },
+        { handler: 'silence', matchId: 'ember-engines.deprecation-camelized-engine-names' },
+        { handler: 'silence', matchId: 'ember-data:legacy-test-helper-support' },
+        { handler: 'silence', matchId: 'autotracking.mutation-after-consumption' },
+    ],
+};
