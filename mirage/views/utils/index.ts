96e6b6de0bbc9a132cbb885e107bbcbe40deb5e0
Futa Ikeda <futa.ikeda@gmail.com>
2021-05-03 11:48:01 -0400

    Fix broad condition to check for null field in mirage

--- mirage/views/utils/index.ts
+++ mirage/views/utils/index.ts
@@ -43,9 +43,8 @@ export function filter(model: ModelInstance, request: Request) {
                     if (field === 'id') {
                         return compareIds(model.id, val, toOperator(operator));
                     }
-                    if (val === '') {
-                        const actualValue = model[`${field}Ids`] || model[`${field}Id`] || '';
-                        return compareIds(actualValue, val, toOperator(operator));
+                    if (model[`${field}Ids`] === null || model[`${field}Id`] === null) {
+                        return compareIds('', val, toOperator(operator));
                     }
                     if (model[`${field}Ids`] || model[`${field}Id`]) {
                         return compareIds(model[`${field}Ids`] || model[`${field}Id`], val, toOperator(operator));
