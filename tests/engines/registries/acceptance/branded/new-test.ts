1295ea3dc85fd228ecfb3586166578ef77219f00
Yuhuai Liu <yuhuai@cos.io>
2021-06-18 11:57:23 -0400

    modify tests

--- tests/engines/registries/acceptance/branded/new-test.ts
+++ tests/engines/registries/acceptance/branded/new-test.ts
@@ -28,35 +28,23 @@ module('Registries | Acceptance | branded.new', hooks => {
         assert.equal(currentRouteName(), 'registries.branded.new', 'At the correct route: branded.new');
     });
 
-    test('redirects to page-not-found if provider.allowSubmissions: false', async assert => {
+    test('serves branded.new route if provider.allowSubmissions: false and user is moderator',
+        async assert => {
+            const brandedProvider = server.create('registration-provider', {
+                assets: {
+                    favicon: 'fakelink',
+                },
+            }, 'withBrand', 'submissionsNotAllowed', 'currentUserIsModerator');
+            await visit(`/registries/${brandedProvider.id}/new`);
+            await percySnapshot(assert);
+            assert.ok(document.querySelector('link[rel="icon"][href="fakelink"]'));
+            assert.equal(currentRouteName(), 'registries.branded.new', 'At the correct route: branded.new');
+        });
+
+    test('redirects to page-not-found if provider.allowSubmissions: false and user not moderator', async assert => {
         const brandedProvider = server.create('registration-provider',
             'withBrand', 'submissionsNotAllowed');
         await visit(`/registries/${brandedProvider.id}/new`);
-        assert.equal(currentRouteName(), 'registries.page-not-found', 'At the correct route: page-not-found');
-    });
-
-    test('only serves EGAP brand.new if egapAdmins feature flag is enabled', async function(assert) {
-        const brandedProvider = server.create('registration-provider', {
-            id: 'egap',
-            assets: {
-                favicon: 'fakelink',
-            },
-        }, 'withBrand');
-        const { featureFlagNames: { egapAdmins } } = config;
-        const features = this.owner.lookup('service:features') as Features;
-
-        await visit(`/registries/${brandedProvider.id}/new`);
-        assert.equal(currentRouteName(), 'registries.page-not-found', 'At the correct route: page-not-found');
-
-        features.enable(egapAdmins);
-        await visit(`/registries/${brandedProvider.id}/new`);
-        assert.ok(features.isEnabled(egapAdmins), 'egapAdmins flag is enabled');
-        assert.equal(currentRouteName(), 'registries.branded.new', 'At the correct route: EGAP branded.new');
-        assert.ok(document.querySelector('link[rel="icon"][href="fakelink"]'));
-
-        features.disable(egapAdmins);
-        await visit(`/registries/${brandedProvider.id}/new`);
-        assert.notOk(features.isEnabled(egapAdmins), 'egapAdmins flag is disabled');
         assert.equal(currentRouteName(), 'registries.page-not-found', 'At the correct route: page-not-found');
     });
 
