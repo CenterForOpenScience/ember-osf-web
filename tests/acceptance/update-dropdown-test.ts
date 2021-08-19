0fc07ff11656aaa2c6d63dc5c9ae397283cc3600
Ashley Robinson <ashley@cos.io>
2021-08-18 22:02:32 -0400

    added unit and acceptance tests for versioning.

---
+++ tests/acceptance/update-dropdown-test.ts
@@ -0,0 +1,52 @@
+import { visit } from '@ember/test-helpers';
+
+import { ModelInstance } from 'ember-cli-mirage';
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { percySnapshot } from 'ember-percy';
+import { module, test } from 'qunit';
+
+import Registration from 'ember-osf-web/models/registration';
+import { click, setupOSFApplicationTest } from 'ember-osf-web/tests/helpers';
+import RevisionModel from 'ember-osf-web/models/revision';
+import { assert } from '@ember/debug';
+import decaf from 'ember-osf-web/mirage/scenarios/registrations';
+
+interface Args {
+    registration?: RegistrationModel;
+};
+
+// get all accepted revisions on a particular Registration; sort by date in descending order (most recent edits first) 
+// an issue/blocker here though is the link to 
+function getAllRevisionsTest(revision: ModelInstance<RevisionModel>){
+    return revision.models
+    .filter((revisions: { decaf: any; }) => revisions.decaf)
+    .sort((a,b) => parseInt(b.dateCreated) - parseInt(a.dateCreated)); // because only approved edits are created
+}
+
+module('Acceptance | update dropdown', hooks => {
+    setupOSFApplicationTest(hooks);
+    setupMirage(hooks);
+
+    test('update dropdown component', async assert => {
+
+        const revisions = server.createList('revision', 10, 'withChildren');
+        const beforeCount = revisions.length;
+
+        // TODO update to proper route name once on test
+        await visit('/decaf');
+
+        // for the component
+        assert.dom('[data-test-verify-update-dropdown-element]').exists();
+        await percySnapshot(assert);
+        // for the list of revisions
+        assert.dom('[data-test-verify-update-dropdown-list]').doesNotExist(); // TODO create data test link
+        await percySnapshot(assert);
+
+        await click('[data-test-verify-update-dropdown-list]');
+        assert.dom('[data-test-verify-update-dropdown-list').exists();
+        decaf.reload();
+        assert.dom('[data-test-verify-update-dropdown-list]').doesNotExist();
+        assert.equal(revisions.length, beforeCount, 'Correct number of revisions');
+        assert.ok(revisions.every(revision => revision.dateCreated), 'All approved revisions present by date.');
+    });
+ });
