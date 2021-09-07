239b4aa51f47b81a7eb83b465d679c0e214db611
Ashley Robinson <ashley@cos.io>
2021-09-07 16:13:35 -0400

    added function call for allRevisions.

--- tests/acceptance/update-dropdown-test.ts
+++ tests/acceptance/update-dropdown-test.ts
@@ -5,22 +5,17 @@ import { setupMirage } from 'ember-cli-mirage/test-support';
 import { percySnapshot } from 'ember-percy';
 import { module, test } from 'qunit';
 
-import Registration from 'ember-osf-web/models/registration';
+import Registration, { RegistrationReviewStates } from 'ember-osf-web/models/registration';
+import { RevisionReviewStates } from 'ember-osf-web/models/revision';
 import { click, setupOSFApplicationTest } from 'ember-osf-web/tests/helpers';
-import RevisionModel from 'ember-osf-web/models/revision';
-import { assert } from '@ember/debug';
-import decaf from 'ember-osf-web/mirage/scenarios/registrations';
 
-interface Args {
-    registration?: RegistrationModel;
-};
+import user from 'ember-osf-web/mirage/factories/user';
+import { Permission } from 'ember-osf-web/models/osf-model';
 
-// get all accepted revisions on a particular Registration; sort by date in descending order (most recent edits first) 
-// an issue/blocker here though is the link to 
-function getAllRevisionsTest(revision: ModelInstance<RevisionModel>){
-    return revision.models
-    .filter((revisions: { decaf: any; }) => revisions.decaf)
-    .sort((a,b) => parseInt(b.dateCreated) - parseInt(a.dateCreated)); // because only approved edits are created
+function allRevisions(registration: ModelInstance<Registration>){
+    return registration.revisions
+        .filter(revision => !revision.versionNumber)
+        .sort((a,b) => parseInt(b.id, 10) - parseInt(a.id, 10));
 }
 
 module('Acceptance | update dropdown', hooks => {
@@ -28,25 +23,48 @@ module('Acceptance | update dropdown', hooks => {
     setupMirage(hooks);
 
     test('update dropdown component', async assert => {
+        const userServer = server.create('user', 'loggedIn');
 
-        const revisions = server.createList('revision', 10, 'withChildren');
-        const beforeCount = revisions.length;
+        const node = server.create('node', {
+            id: 'cobalt',
+            title: 'Outcome Reporting Testing',
+            currentUserPermissions: [Permission.Read],
+            currentUserIsContributor: true,
+            registration: true,
+            public: true,
+        });
+        const registration = server.create('registration', {
+            registeredFrom: node,
+            reviewsState: RegistrationReviewStates.Accepted,
+            revisionState: RevisionReviewStates.Approved,
+        });
+        server.create('draft-registration', {
+            branchedFrom: node,
+        }, 'currentUserIsReadAndWrite');
+        const revisions = allRevisions(registration);
+        server.create('revision', {
+            reviewState: RevisionReviewStates.Approved,
+            versionNumber: 1,
+        });
 
+        // const revisions = server.createList('revision', 10, 'withChildren');
+        const beforeCount = user.registration.revisions.length;
+        const url = `/${node.id}/registrations`;
         // TODO update to proper route name once on test
-        await visit('/decaf');
+        await visit(url);
 
         // for the component
-        assert.dom('[data-test-verify-update-dropdown-element]').exists();
+        assert.dom('[data-test-update-button]').exists();
         await percySnapshot(assert);
         // for the list of revisions
-        assert.dom('[data-test-verify-update-dropdown-list]').doesNotExist(); // TODO create data test link
+        assert.dom('[data-test-update-list]').doesNotExist();
         await percySnapshot(assert);
 
-        await click('[data-test-verify-update-dropdown-list]');
-        assert.dom('[data-test-verify-update-dropdown-list').exists();
-        decaf.reload();
-        assert.dom('[data-test-verify-update-dropdown-list]').doesNotExist();
+        await click('[data-test-update-list]');
+        assert.dom('[data-test-list-view]').exists();
         assert.equal(revisions.length, beforeCount, 'Correct number of revisions');
-        assert.ok(revisions.every(revision => revision.dateCreated), 'All approved revisions present by date.');
+        assert.ok(revisions.every(revision => revision.versionNumber), 'Version number present.');
+        userServer.reload();
+        assert.dom('[data-test-list-view]').doesNotExist();
     });
- });
+});
