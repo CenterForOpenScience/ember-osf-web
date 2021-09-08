f5f7bf72903e258c2c2f87d49f1eaeddf003dba1
Ashley Robinson <ashley@cos.io>
2021-09-08 10:21:26 -0400

    removed unused functions.

--- tests/integration/components/registries/update-dropdown/component-test.ts
+++ tests/integration/components/registries/update-dropdown/component-test.ts
@@ -1,21 +1,14 @@
 import { visit } from '@ember/test-helpers';
 
-import { ModelInstance } from 'ember-cli-mirage';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { percySnapshot } from 'ember-percy';
 import { module, test } from 'qunit';
 
-import Registration, { RegistrationReviewStates } from 'ember-osf-web/models/registration';
+import { RegistrationReviewStates } from 'ember-osf-web/models/registration';
 import { RevisionReviewStates } from 'ember-osf-web/models/revision';
 import { click, setupOSFApplicationTest } from 'ember-osf-web/tests/helpers';
 
 import { Permission } from 'ember-osf-web/models/osf-model';
-
-function allRevisions(registration: ModelInstance<Registration>){
-    return registration.revisions
-        .filter(revision => !revision.versionNumber)
-        .sort((a,b) => parseInt(b.id, 10) - parseInt(a.id, 10));
-}
 
 module('Acceptance | update dropdown', hooks => {
     setupOSFApplicationTest(hooks);
@@ -52,11 +45,6 @@ module('Acceptance | update dropdown', hooks => {
             reviewState: RevisionReviewStates.Approved,
             versionNumber: 3,
         });
-        const beforeCount = userServer.registrations.length;
-
-        const revisions = server.createList('revision', 10, 'withChildren');
-
-        const revisions = allRevisions(registration);
 
         const url = `/${node.id}/registrations`;
         await visit(url);
@@ -70,8 +58,6 @@ module('Acceptance | update dropdown', hooks => {
         await percySnapshot(assert);
         await click('[data-test-update-list]');
         assert.dom('[data-test-list-view]').exists();
-        assert.equal(registration.revisions.length, beforeCount, 'Correct number of revisions');
-        assert.ok(revisions.every(revision => revision.versionNumber), 'Version number present.');
         userServer.reload();
         assert.dom('[data-test-list-view]').doesNotExist();
     });
