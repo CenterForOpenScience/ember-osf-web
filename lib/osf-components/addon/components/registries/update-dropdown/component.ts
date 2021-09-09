45f596f27ba1b87a167d2401eae41fb46127131c
Ashley Robinson <ashley@cos.io>
2021-09-09 17:09:37 -0400

    removed unused functionality.

--- lib/osf-components/addon/components/registries/update-dropdown/component.ts
+++ lib/osf-components/addon/components/registries/update-dropdown/component.ts
@@ -14,14 +14,11 @@ import captureException, { getApiErrorMessage } from 'ember-osf-web/utils/captur
 import Store from '@ember-data/store';
 import RouterService from '@ember/routing/router-service';
 import { taskFor } from 'ember-concurrency-ts';
-import { computed } from '@ember/object';
-import registration from 'ember-osf-web/mirage/factories/registration';
+import { tracked } from '@glimmer/tracking';
 
 interface Args {
     registration: RegistrationModel;
 }
-
-type RevisionJustification = 'Adding Results' | 'Typo - Self' | 'Typo - Other' | 'Copy Edit';
 
 export default class UpdateDropdown extends Component<Args> {
     @service currentUser!: CurrentUserService;
@@ -29,17 +26,15 @@ export default class UpdateDropdown extends Component<Args> {
     @service store!: Store;
     @service toast!: Toast;
     @service router!: RouterService;
+    @tracked revisions?: QueryHasManyResult<RevisionModel> | RevisionModel[];
 
-    revisions?: QueryHasManyResult<RevisionModel>;
     isPendingCurrentUserApproval?: boolean;
-    revisionJustification?: RevisionJustification;
 
     constructor(owner: unknown, args: Args) {
         super(owner, args);
         taskFor(this.getRevisionList).perform();
     }
 
-    @computed('args.registration.{userHasAdminPermission,revisionState}')
     get shouldDisplayApproveDenyButtons() {
         return this.args.registration.userHasAdminPermission
         && this.args.registration.revisionState
@@ -49,83 +44,23 @@ export default class UpdateDropdown extends Component<Args> {
         ].includes(this.args.registration.revisionState);
     }
 
-    @computed('args.registration.revisionState')
-    get updateNotificationIcon() {
-        switch (this.args.registration.revisionState) {
-        case RevisionReviewStates.Approved:
-            return 'lock';
-        case RevisionReviewStates.RevisionInProgress:
-            return 'eye';
-        case RevisionReviewStates.RevisionPendingAdminApproval:
-        case RevisionReviewStates.RevisionPendingModeration:
-            return 'clock';
-        default:
-            return '';
-        }
-    }
-
     @task
     @waitFor
     async getRevisionList() {
 
-        if (!registration){
-            const notRegistrationError = this.intl.t('registries.update_dropdown.not_a_registration_error');
-            return this.toast.error(notRegistrationError);
-        }
-        if (this.args.registration.revisions === null || this.args.registration.revisions === undefined) {
-            return {
-                placeholder: this.intl.t('registries.update_dropdown.no_revisions_error'),
-            };
-        }
-        if (this.args.registration.revisions) {
-            try {
-                const revisions = await this.args.registration.queryHasMany('revisions');
-                this.revisions = revisions.sort();
-                return revisions;
-            } catch (e) {
-                const errorMessage = this.intl.t('registries.update_dropdown.revision_error_message');
-                captureException(e, { errorMessage });
-                this.toast.error(getApiErrorMessage(e), errorMessage);
-            }
-        }
-    }
-
-    @task
-    @waitFor
-    async needsMoreUpdates() {
-        if (!this.revisions) {
-            const errorMessage = this.intl.t('registries.update_dropdown.not_a_revision_error');
-            return this.toast.error(errorMessage);
+        if (!this.args.registration){
+            const notReistrationError = this.intl.t('registries.update_dropdown.not_a_registration_error');
+            return this.toast.error(notReistrationError);
         }
         try {
-            this.args.registration.set('revisionState', RevisionReviewStates.RevisionInProgress);
-            const message = this.intl.t('registries.update_dropdown.revision_more_updates_success');
-            this.toast.success(message);
+            const revisions = await this.args.registration.queryHasMany('revisions');
+            this.revisions = revisions;
         } catch (e) {
-            const errorMessage = this.intl.t('registries.update_dropdown.revision_state_not_set_error');
+            const errorMessage = this.intl.t('registries.update_dropdown.revision_error_message');
             captureException(e, { errorMessage });
-            return this.toast.error(getApiErrorMessage(e), errorMessage);
+            this.toast.error(getApiErrorMessage(e), errorMessage);
         }
-        return this.args.registration;
-    }
-
-    @task
-    @waitFor
-    async approveUpdates() {
-        if (!this.revisions) {
-            const errorMessage = this.intl.t('registries.update_dropdown.not_a_revision_error');
-            return this.toast.error(errorMessage);
-        }
-        try {
-            this.args.registration.set('revisionState', RevisionReviewStates.Approved);
-            const message =  this.intl.t('registries.update_dropdown.revision_approved_success');
-            this.toast.success(message);
-        } catch (e) {
-            const errorMessage = this.intl.t('registries.update_dropdown.revision_state_not_set_error');
-            captureException(e, { errorMessage });
-            return this.toast.error(getApiErrorMessage(e), errorMessage);
-        }
-        return this.args.registration;
     }
 }
+
 
