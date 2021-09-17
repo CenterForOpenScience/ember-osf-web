56b9abb5aa9ee80ac96c1ccb2044b8e7cc0fd151
Yuhuai Liu <yuhuai@cos.io>
2021-09-17 00:10:14 -0400

    err modals

--- lib/registries/addon/branded/moderation/-components/upload-csv/component.ts
+++ lib/registries/addon/branded/moderation/-components/upload-csv/component.ts
@@ -2,11 +2,27 @@ import config from 'ember-get-config';
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import Intl from 'ember-intl/services/intl';
+import { SafeString } from '@ember/template/-private/handlebars';
+import Toast from 'ember-toastr/services/toast';
 
 const { OSF: { apiUrl } } = config;
 
 interface Args {
     providerId: string;
+}
+
+interface ErrorMessage {
+    cell: string;
+    title: string;
+    detail: string | SafeString;
+}
+interface ApiErrorDetail {
+    header: string;
+    column_index: string;
+    row_index: string;
+    type: string;
 }
 export default class UploadCsvComponent extends Component<Args> {
     dropzoneOptions = {
@@ -17,10 +33,13 @@ export default class UploadCsvComponent extends Component<Args> {
         acceptDirectories: false,
     };
 
+    @service intl!: Intl;
+    @service toast!: Toast;
+
+    @tracked errorMessages: ErrorMessage[] = [];
+    @tracked shouldShowErrorModal = false;
     @tracked dropping = false;
     @tracked uploading: any[] = [];
-    @tracked filename = '';
-    clickable = [];
 
     @action
     buildUrl(files: any) {
@@ -38,14 +57,36 @@ export default class UploadCsvComponent extends Component<Args> {
     }
 
     @action
-    error(_: any, __: any, file: any, ___: any) {
+    error(_: any, __: any, file: any, { errors }: { errors: ApiErrorDetail[]}) {
         this.uploading.removeObject(file);
-        // some error handling code here
+        this.shouldShowErrorModal = true;
+        for (const error of errors) {
+            this.errorMessages.push(
+                {
+                    cell: error.column_index + error.row_index,
+                    title: this.intl.t(`registries.moderation.settings.${error.type}.title`),
+                    detail: this.intl.t(`registries.moderation.settings.${error.type}.detail`, { htmlSafe: true }),
+                },
+            );
+        }
     }
 
     @action
     success(_: any, __: any, file: any, ___: any) {
         this.uploading.removeObject(file);
         // some success handling code here
+    }
+
+    @action
+    closeErrorModal() {
+        this.shouldShowErrorModal = false;
+        this.errorMessages = [];
+    }
+
+    @action
+    async copyToClipboard(elementId: string) {
+        const mainElement = document.getElementById(elementId);
+        await navigator.clipboard.writeText(mainElement!.textContent!);
+        this.toast.success(this.intl.t('registries.moderation.settings.copyToClipboardSuccess'));
     }
 }
