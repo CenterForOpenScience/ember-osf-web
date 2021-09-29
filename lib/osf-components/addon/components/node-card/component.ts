8c84a9dd33ead7c1691f0b26f3ca33fc1505ed05
Ashley Robinson <ashley@cos.io>
2021-09-29 17:12:42 -0400

    linting: removed comment.

--- lib/osf-components/addon/components/node-card/component.ts
+++ lib/osf-components/addon/components/node-card/component.ts
@@ -1,5 +1,5 @@
 import { tagName } from '@ember-decorators/component';
-import Component from '@glimmer/component';
+import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 import config from 'ember-get-config';
@@ -10,28 +10,28 @@ import Node, { NodeType } from 'ember-osf-web/models/node';
 import Registration from 'ember-osf-web/models/registration';
 import Analytics from 'ember-osf-web/services/analytics';
 import pathJoin from 'ember-osf-web/utils/path-join';
+import Toast from 'ember-toastr/services/toast';
 
 import { task } from 'ember-concurrency';
 import { waitFor } from '@ember/test-waiters';
 import RouterService from '@ember/routing/router-service';
-import RegistrationModel from 'ember-osf-web/models/registration';
 import SchemaResponseModel from 'ember-osf-web/models/schema-response';
-
+import Intl from 'ember-intl/services/intl';
+import RegistrationModel from 'ember-osf-web/models/registration';
+import { taskFor } from 'ember-concurrency-ts';
+import captureException, { getApiErrorMessage } from 'ember-osf-web/utils/capture-exception';
 import template from './template';
 import styles from './styles';
-
-interface Args {
-    registration: RegistrationModel;
-}
-
 const { OSF: { url: baseURL } } = config;
 
 @layout(template, styles)
 @tagName('')
-export default class NodeCard extends Component<Args> {
+export default class NodeCard extends Component {
     @service analytics!: Analytics;
     @service router!: RouterService;
     @service store!: Store;
+    @service toast!: Toast;
+    @service intl!: Intl;
     // Optional parameters
     node?: Node | Registration;
     delete?: (node: Node) => void;
@@ -41,10 +41,6 @@ export default class NodeCard extends Component<Args> {
     // Private properties
     searchUrl = pathJoin(baseURL, 'search');
 
-    constructor(owner: unknown, args: Args) {
-        super(owner, args);
-    }
-
     @computed('readOnly', 'node', 'node.{nodeType,userHasWritePermission}')
     get showDropdown() {
         return !this.readOnly && this.node && this.node.nodeType === NodeType.Fork && this.node.userHasWritePermission;
@@ -52,9 +48,43 @@ export default class NodeCard extends Component<Args> {
 
     @task
     @waitFor
+    async getLatestRevision(registration: RegistrationModel) {
+        const schemaResponseDates: Date[] = [];
+        if (!registration){
+            const notReistrationError = this.intl.t('registries.update_dropdown.not_a_registration_error');
+            return this.toast.error(notReistrationError);
+        }
+        try {
+            const revisionList : SchemaResponseModel[] = await registration.queryHasMany('schemaResponses');
+            revisionList.forEach(revision => {
+                const date: Date = revision.dateModified;
+                schemaResponseDates.push(date);
+            });
+            const latestDate = schemaResponseDates.sort((a: any, b: any) =>
+                new Date(b).valueOf() - new Date(a).valueOf())[0];
+
+            const latestSchemaResponse = revisionList.filter(revision => revision.dateModified === latestDate);
+            registration.set('latestSchemaResponse', latestSchemaResponse);
+
+        } catch (e) {
+            const errorMessage = this.intl.t('node.schema_response_error');
+            captureException(e, {errorMessage});
+            this.toast.error(getApiErrorMessage(e), errorMessage);
+        }
+    }
+
+    didReceiveAttrs() {
+        if (this.node?.isRegistration) {
+            taskFor(this.getLatestRevision).perform(this.node as Registration);
+        }
+    }
+
+    // TODO add to button for Update when clicked, add route
+    @task
+    @waitFor
     async createNewSchemaResponse() {
         const newRevision: SchemaResponseModel = this.store.createRecord('schema-response', {
-            registration: this.args.registration,
+            registration: this.node,
         });
         await newRevision.save();
         this.router.transitionTo('registries.edit-revision', newRevision.id);
