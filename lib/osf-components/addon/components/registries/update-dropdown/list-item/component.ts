c05ea6c7c12f344208824f7e01eaf2ffc454bc41
Futa Ikeda <futa.ikeda@gmail.com>
2021-10-13 13:17:18 -0400

    Remove unneeded checks to registration state

--- lib/osf-components/addon/components/registries/update-dropdown/list-item/component.ts
+++ lib/osf-components/addon/components/registries/update-dropdown/list-item/component.ts
@@ -1,11 +1,9 @@
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import Intl from 'ember-intl/services/intl';
-import RegistrationModel, { RegistrationReviewStates } from 'ember-osf-web/models/registration';
 import SchemaResponseModel, { RevisionReviewStates } from 'ember-osf-web/models/schema-response';
 
 interface Args {
-    registration: RegistrationModel;
     revision: SchemaResponseModel;
     isModeratorMode: boolean;
     index: number;
@@ -16,15 +14,11 @@ export default class ListItem extends Component<Args> {
     @service intl!: Intl;
 
     get shouldShow() {
-        const { registration, revision, isModeratorMode } = this.args;
-        if ([RegistrationReviewStates.Pending, RegistrationReviewStates.Initial].includes(registration.reviewsState)
-        && revision.isOriginalResponse) {
-            return true;
-        }
+        const { revision, isModeratorMode } = this.args;
         if (isModeratorMode) {
             return [RevisionReviewStates.RevisionPendingModeration,
                 RevisionReviewStates.Approved].includes(revision.reviewsState);
         }
-        return revision.reviewsState === RevisionReviewStates.Approved;
+        return revision.isOriginalResponse || revision.reviewsState === RevisionReviewStates.Approved;
     }
 }
