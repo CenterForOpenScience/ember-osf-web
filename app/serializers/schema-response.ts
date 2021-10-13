10b47c2ffc6110de26a67920a8563bb60389c98c
Yuhuai Liu <yuhuai@cos.io>
2021-10-13 11:26:31 -0400

    add normalize method to serializer

--- app/serializers/schema-response.ts
+++ app/serializers/schema-response.ts
@@ -1,6 +1,27 @@
+import Model from '@ember-data/model';
+import {
+    NormalizedRegistrationResponse,
+    RegistrationResponse,
+} from 'ember-osf-web/packages/registration-schema';
+import { normalizeRegistrationResponses } from 'ember-osf-web/serializers/draft-registration';
+import { mapKeysAndValues } from 'ember-osf-web/utils/map-keys';
+import { Resource } from 'osf-api';
 import OsfSerializer from './osf-serializer';
 
 export default class SchemaResponseSerializer extends OsfSerializer {
+    normalize(modelClass: Model, resourceHash: Resource) {
+        if (resourceHash.attributes) {
+            const revisionResponses = resourceHash.attributes.revision_responses as RegistrationResponse;
+            // @ts-ignore: TODO: fix types
+            // eslint-disable-next-line no-param-reassign
+            resourceHash.attributes.revision_responses = mapKeysAndValues(
+                revisionResponses || {},
+                key => key,
+                value => normalizeRegistrationResponses(value, this.store),
+            ) as NormalizedRegistrationResponse;
+        }
+        return super.normalize(modelClass, resourceHash) as { data: Resource };
+    }
 }
 
 declare module 'ember-data/types/registries/serializer' {
