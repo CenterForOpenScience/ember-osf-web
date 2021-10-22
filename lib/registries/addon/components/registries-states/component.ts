9645a3a5367654820e635cac0e773d013f47693d
Futa Ikeda <futa.ikeda@gmail.com>
2021-10-22 13:52:54 -0400

    Update wording for registration states

--- lib/registries/addon/components/registries-states/component.ts
+++ lib/registries/addon/components/registries-states/component.ts
@@ -10,6 +10,7 @@ import pathJoin from 'ember-osf-web/utils/path-join';
 
 import { layout } from 'ember-osf-web/decorators/component';
 import RegistrationModel, { RegistrationReviewStates } from 'ember-osf-web/models/registration';
+import { RevisionReviewStates } from 'ember-osf-web/models/schema-response';
 import styles from './styles';
 import template from './template';
 
@@ -44,7 +45,7 @@ export default class RegistriesStates extends Component {
     /* eslint-disable max-len */
     @computed(
         'isModeratorMode', 'projectUrl',
-        'registration.{embargoEndDate,pendingEmbargoApproval,pendingRegistrationApproval,reviewsState,userHasAdminPermission}',
+        'registration.{embargoEndDate,pendingEmbargoApproval,pendingRegistrationApproval,reviewsState,revisionState,userHasAdminPermission}',
         'stateIcon',
     )
     /* eslint-enable max-len */
@@ -57,6 +58,8 @@ export default class RegistriesStates extends Component {
             stateKey = 'pendingRegistrationApproval';
         } else if (this.registration.pendingEmbargoApproval) {
             stateKey = 'pendingEmbargoApproval';
+        } else if (this.registration.revisionState !== RevisionReviewStates.Approved) {
+            stateKey = camelize(this.registration.revisionState);
         } else {
             stateKey = camelize(this.registration.reviewsState);
         }
