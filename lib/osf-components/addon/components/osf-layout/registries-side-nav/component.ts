1ba77c0ce4732578354411ebdd8a6daff19dc788
Yuhuai Liu <yuhuai@cos.io>
2021-11-12 14:14:34 -0500

    Feature/registries delete revision button (#1348)
    * added translation file, resolved merge conflicts.
    
    * added delete button to registries side nav, mirage to test, and proper template strings.
    
    * move delete button to edit-revision route
    
    * minor fixes
    
    Co-authored-by: Ashley Robinson <ashley@cos.io>

--- lib/osf-components/addon/components/osf-layout/registries-side-nav/component.ts
+++ lib/osf-components/addon/components/osf-layout/registries-side-nav/component.ts
@@ -10,6 +10,7 @@ import { layout } from 'ember-osf-web/decorators/component';
 import styles from './styles';
 import template from './template';
 
+
 @tagName('')
 @layout(template, styles)
 export default class RegistriesSideNav extends Component {
