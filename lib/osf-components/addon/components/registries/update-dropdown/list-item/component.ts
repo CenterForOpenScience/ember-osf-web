e4bc7ffc9a67d64d83f7bf7854400e2d22d64398
Futa Ikeda <futa.ikeda@gmail.com>
2021-12-07 16:23:58 -0500

    Account for AVOLs in update dropdown

--- lib/osf-components/addon/components/registries/update-dropdown/list-item/component.ts
+++ lib/osf-components/addon/components/registries/update-dropdown/list-item/component.ts
@@ -8,17 +8,18 @@ interface Args {
     isModeratorMode: boolean;
     index: number;
     totalRevisions: number;
+    isAnonymous: boolean;
 }
 
 export default class ListItem extends Component<Args> {
     @service intl!: Intl;
 
     get shouldShow() {
-        const { revision, isModeratorMode } = this.args;
+        const { revision, isModeratorMode, isAnonymous} = this.args;
         const visibleStates = [RevisionReviewStates.Approved];
         if (isModeratorMode) {
             visibleStates.push(RevisionReviewStates.RevisionPendingModeration);
         }
-        return revision.isOriginalResponse || visibleStates.includes(revision.reviewsState);
+        return revision.isOriginalResponse || isAnonymous || visibleStates.includes(revision.reviewsState);
     }
 }
