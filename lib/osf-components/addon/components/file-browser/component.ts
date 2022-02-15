7012f8f4d4c59c8d97f4945f5947d00f3cd3f1a3
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-02-15 11:28:12 -0500

    Add styling for mobile view (#1405)

--- lib/osf-components/addon/components/file-browser/component.ts
+++ lib/osf-components/addon/components/file-browser/component.ts
@@ -1,6 +1,9 @@
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { taskFor } from 'ember-concurrency-ts';
+import Media from 'ember-responsive';
+
 import OsfStorageManager from 'osf-components/components/storage-provider-manager/osf-storage-manager/component';
 
 interface Args {
@@ -8,6 +11,12 @@ interface Args {
 }
 
 export default class FileBrowser extends Component<Args> {
+    @service media!: Media;
+
+    get isMobile() {
+        return this.media.isMobile;
+    }
+
     @action
     handleInput(event: any) {
         taskFor(this.args.manager.changeFilter).perform(event.target.value);
