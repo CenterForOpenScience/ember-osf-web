cad7acba7550eaf3e832d8010bcb77f1d2783d33
Brian Geiger <bgeiger@cos.io>
2022-02-21 16:30:17 -0500

    Improve accessibility

--- tests/integration/components/file-browser/breadcrumbs/component-test.ts
+++ tests/integration/components/file-browser/breadcrumbs/component-test.ts
@@ -9,34 +9,45 @@ module('Integration | Component | file-browser / breadcrumbs', hooks => {
 
     test('it renders', async function(assert) {
         const breadcrumbsOne = [
-            {name: 'osfstorage'},
+            {
+                name: 'osfstorage',
+            },
         ];
         const breadcrumbsTwo = [
             {name: 'osfstorage'},
-            {name: 'folder one'},
+            {name: 'folder_one'},
         ];
         const breadcrumbsThree = [
             {name: 'osfstorage'},
-            {name: 'folder one'},
-            {name: 'folder two'},
+            {name: 'folder_one'},
+            {name: 'folder_two'},
         ];
 
-        this.set('breadcrumbs', breadcrumbsOne);
-        await render(hbs`<FileBrowser::Breadcrumbs @breadcrumbs={{this.breadcrumbs}} />`);
-        assert.dom('[data-test-breadcrumbs]')
-            .hasText('OSF Storage', 'Breadcrumbs render properly with just storage provider');
+        this.set('manager', {breadcrumbs: breadcrumbsOne});
+        await render(hbs`<FileBrowser::Breadcrumbs @manager={{this.manager}} />`);
+        assert.dom('[data-test-go-to-folder="osfstorage"]')
+            .exists();
+        assert.dom('[data-test-go-to-folder="folder_one"]')
+            .doesNotExist();
+        assert.dom('[data-test-go-to-folder="folder_two"]')
+            .doesNotExist();
 
-        this.set('breadcrumbs', breadcrumbsTwo);
-        await render(hbs`<FileBrowser::Breadcrumbs @breadcrumbs={{this.breadcrumbs}} />`);
-        assert.dom('[data-test-breadcrumbs]')
-            .hasText('OSF Storage > folder one', 'Breadcrumbs render properly with provider and one folder');
+        this.set('manager', {breadcrumbs: breadcrumbsTwo});
+        await render(hbs`<FileBrowser::Breadcrumbs @manager={{this.manager}} />`);
+        assert.dom('[data-test-go-to-folder="osfstorage"]')
+            .exists();
+        assert.dom('[data-test-go-to-folder="folder_one"]')
+            .exists();
+        assert.dom('[data-test-go-to-folder="folder_two"]')
+            .doesNotExist();
 
-        this.set('breadcrumbs', breadcrumbsThree);
-        await render(hbs`<FileBrowser::Breadcrumbs @breadcrumbs={{this.breadcrumbs}} />`);
-        assert.dom('[data-test-breadcrumbs]')
-            .hasText(
-                'OSF Storage > folder one > folder two',
-                'Breadcrumbs render properly with provider and two folders',
-            );
+        this.set('manager', {breadcrumbs: breadcrumbsThree});
+        await render(hbs`<FileBrowser::Breadcrumbs @manager={{this.manager}} />`);
+        assert.dom('[data-test-go-to-folder="osfstorage"]')
+            .exists();
+        assert.dom('[data-test-go-to-folder="folder_one"]')
+            .exists();
+        assert.dom('[data-test-go-to-folder="folder_two"]')
+            .exists();
     });
 });
