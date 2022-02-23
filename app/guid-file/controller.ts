4b86ef82b221d9cea0004db3aa9f38b77f2a2f3f
chth0n1x <82047646+chth0n1x@users.noreply.github.com>
2022-02-23 16:22:29 -0500

    HTML for File Detail Page (#1391)
    * added beginning html skeletal structure for file detail page.
    
    * updated side nav html list, troublshooting hbs file rendering.
    
    * added tab one view for starter html.
    
    * added html for file detail tab one, updated guid file hbs template, translation for checkout button.
    
    * updated version, added unordered list for file type, converted placeholder divs to buttons and added some bootstrap formatting.
    
    * corrected file title, changed formatting, placed powerselect.
    
    * added data test selectors for testing.
    
    * updated page changes off of design, began to add CSS, troubleshooting divs.
    
    * updated translation files for file detail page.
    
    * vertically aligned side nav buttons, updated grid layout for unclicked sidenav logic.
    
    * removed non-translated strings, code clean-up.
    
    * updated to ResponsiveDropdown for hidden buttons.
    
    * updated file detail route to newer html, added method calls for button navigation on side nav, updated hbs to use osf layout.
    
    * updated controller logic for sidenav slide, duplicated and moved x inside revisions, entering debug for revision dropdown, adding css.
    
    * updated logic for navbar slide to display revisions and tag panels properly.
    
    * added hidden attribute to version and tag slide hbs file to obscure elements  on page load.
    
    * updated html rows for bootstrap div alignment, added css flexbox properties to properly display slide, simplified and updated JS for slide display and transition.
    
    * added data test selectors, analytics names, removed unused code.
    
    * Removing file-detail.html file
    
    Removing file-detail.html file
    
    * Deleting file-detail-new-tab.html
    
    Deleting file-detail-new-tab.html
    
    * removed rouge space from registration link on L9.
    
    * added yarn.lock file with updated dependencies.
    
    * reverted translation file's methods and measures back to original, corrected template string for file renderer.
    
    * removed em element tags, updated css for font-weight, entering troubleshoot for font-weight, updated tags slide for positioning, file name header tags and padding for file name.
    
    * Removed unused strings for the File Viewer
    
    Removed unused strings for the File Viewer
    
    * Removing unused strings for File Detail
    
    Removing unused strings for File Detail
    
    * Removed unnecessary space
    
    Removed unnecessary space
    
    * Removed show url bar unused string
    
    Removed show url bar unused string
    
    * Readding toast_events
    
    Readding toast_events
    
    * updating controller.ts file to remove unused methods, properties.
    
    * resolved merge conflicts.
    
    Co-authored-by: Ashley Robinson <ashley@cos.io>

--- app/guid-file/controller.ts
+++ app/guid-file/controller.ts
@@ -1,5 +1,116 @@
+/* eslint-disable no-console */
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+import Media from 'ember-responsive';
+
 
 export default class GuidFile extends Controller {
+    @service media!: Media;
 
+    revisionClicked = false;
+    tagsClicked = false;
+
+    get isMobile() {
+        return this.media.isMobile;
+    }
+
+    get isTablet() {
+        return this.media.isTablet;
+    }
+
+    @action
+    toggleVersions() {
+        this.toggleProperty('revisionClicked');
+
+        if (this.tagsClicked === true) {
+            this.closeTags();
+        }
+        if (this.revisionClicked === true) {
+            this.openVersions();
+        }
+        if (this.revisionClicked === false) {
+            this.closeVersions();
+        }
+    }
+
+    @action
+    toggleTags() {
+        this.toggleProperty('tagsClicked');
+
+        if (this.revisionClicked === true) {
+            this.closeVersions();
+        }
+        if (this.tagsClicked === true) {
+            this.openTags();
+        }
+        if (this.tagsClicked === false) {
+            this.closeTags();
+        }
+    }
+
+    openVersions() {
+        if (this.tagsClicked === true) {
+            this.closeTags();
+            this.tagsClicked = false;
+        }
+        const mainPanel = document.getElementById('mainPanel');
+        if (mainPanel) {
+            mainPanel.classList.replace('col-lg-12', 'col-lg-8');
+            mainPanel.style.marginLeft = '450px';
+        }
+        const versionSlide = document.getElementById('versions');
+        if (versionSlide) {
+            versionSlide.hidden = false;
+            versionSlide.classList.add('col-lg-4');
+            versionSlide.style.width = '450px';
+        }
+    }
+
+    closeVersions() {
+        const versionSlide = document.getElementById('versions');
+        if (versionSlide) {
+            versionSlide.hidden = true;
+            versionSlide.classList.remove('col-lg-4');
+            versionSlide.style.width = '0px';
+        }
+        const mainPanel = document.getElementById('mainPanel');
+        if (mainPanel) {
+            mainPanel.classList.replace('col-lg-8', 'col-lg-12');
+            mainPanel.style.marginLeft = '0px';
+        }
+    }
+
+    openTags() {
+        if (this.revisionClicked === true) {
+            this.closeVersions();
+            this.revisionClicked = false;
+        }
+        const mainPanel = document.getElementById('mainPanel');
+        if (mainPanel) {
+            mainPanel.classList.replace('col-lg-12', 'col-lg-8');
+            mainPanel.style.marginLeft = '450px';
+        }
+        const tagsSlide = document.getElementById('tags');
+        if (tagsSlide) {
+            tagsSlide.hidden = false;
+            tagsSlide.classList.add('col-lg-4');
+            tagsSlide.style.width = '450px';
+        }
+    }
+
+    closeTags() {
+        const tagsSlide = document.getElementById('tags');
+        if (tagsSlide) {
+            tagsSlide.hidden = true;
+            tagsSlide.classList.remove('col-lg-4');
+            tagsSlide.style.width = '0px';
+        }
+        const mainPanel = document.getElementById('mainPanel');
+        if (mainPanel) {
+            mainPanel.classList.replace('col-lg-8', 'col-lg-12');
+            mainPanel.style.marginLeft = '0px';
+        }
+    }
 }
