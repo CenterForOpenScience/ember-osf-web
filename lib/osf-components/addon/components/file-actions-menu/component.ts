f7c2162ade09ca76a8b6e1268c118b69d1fd8ea1
Futa Ikeda <futa.ikeda@gmail.com>
2022-05-20 17:17:21 -0400

    Remove move option for file detail page

--- lib/osf-components/addon/components/file-actions-menu/component.ts
+++ lib/osf-components/addon/components/file-actions-menu/component.ts
@@ -2,9 +2,12 @@ import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import File from 'ember-osf-web/packages/files/file';
+import StorageManager from 'osf-components/components/storage-provider-manager/storage-manager/component';
 
 interface Args {
     item: File;
+    onDelete: () => void;
+    manager?: StorageManager; // No manager for file-detail page
 }
 
 export default class FileActionsMenu extends Component<Args> {
