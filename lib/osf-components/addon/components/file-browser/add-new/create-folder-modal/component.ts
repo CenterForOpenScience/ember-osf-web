3b72d81fa795629cf69be1397e4c3e1f1fc5cfa9
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-05-24 17:49:46 -0400

    [No ticket] Create folder error handling (#1494)
    ## Purpose
    - Add disable logic and error handling for create-folder modal
    
    ## Summary of Changes
    - Rearrange and add translation files for create-folder modal
    - Add `disable` tag to the `Create Folder` button when the task is already running

--- lib/osf-components/addon/components/file-browser/add-new/create-folder-modal/component.ts
+++ lib/osf-components/addon/components/file-browser/add-new/create-folder-modal/component.ts
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { taskFor } from 'ember-concurrency-ts';
 
 import StorageManager from 'osf-components/components/storage-provider-manager/storage-manager/component';
 
@@ -10,4 +11,8 @@ interface Args {
 
 export default class CreateFolderModal extends Component<Args> {
     @tracked newFolderName = '';
+
+    get shouldDisable() {
+        return taskFor(this.args.manager.createNewFolder).isRunning || !this.newFolderName;
+    }
 }
