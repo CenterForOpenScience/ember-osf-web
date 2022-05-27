581e253d9ffd8b8f3889ea8c58d05645581faa7f
Futa Ikeda <futa.ikeda@gmail.com>
2022-05-27 18:08:28 -0400

    Add dropdown z-index only for mobile

--- lib/osf-components/addon/components/file-actions-menu/component.ts
+++ lib/osf-components/addon/components/file-actions-menu/component.ts
@@ -1,6 +1,8 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import Media from 'ember-responsive';
 import File from 'ember-osf-web/packages/files/file';
 import StorageManager from 'osf-components/components/storage-provider-manager/storage-manager/component';
 
@@ -11,6 +13,8 @@ interface Args {
 }
 
 export default class FileActionsMenu extends Component<Args> {
+    @service media!: Media;
+
     @tracked isDeleteModalOpen = false;
     @tracked moveModalOpen = false;
 
