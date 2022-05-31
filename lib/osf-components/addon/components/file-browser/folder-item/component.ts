ff071b9a74dfe45b9256d2a9bb7557b951deeaa7
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-05-31 10:07:12 -0400

    Prevent dropdown and content from being obscured (#1503)
    ## Purpose
    - Allow users to select file actions in mobile view
    
    ## Summary of Changes
    - Add z-index to file/folder action dropdown triggers for mobile/tablet

--- lib/osf-components/addon/components/file-browser/folder-item/component.ts
+++ lib/osf-components/addon/components/file-browser/folder-item/component.ts
@@ -1,5 +1,8 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
+import Media from 'ember-responsive';
+
 import File from 'ember-osf-web/packages/files/file';
 import StorageManager from 'osf-components/components/storage-provider-manager/storage-manager/component';
 
@@ -9,6 +12,8 @@ interface Args {
 }
 
 export default class FolderItem extends Component<Args> {
+    @service media!: Media;
+
     @tracked isDeleteModalOpen = false;
     @tracked moveModalOpen = false;
     @tracked useCopyModal = false;
