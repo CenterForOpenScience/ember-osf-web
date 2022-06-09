79a7db6e8c96b6aaf2e14163db4d1f7a05eb412a
chth0n1x <82047646+chth0n1x@users.noreply.github.com>
2022-06-09 10:01:47 -0400

    [ENG-3720] Rename File Modal (#1490)
    * updated logic for file rename modal, added aria labels, resolved focus bug on user input, resolved file parameter when passed as an argument, resolved cancel functionality, entering debug for save upon enter and finishing tests.
    
    * added tests for enabled and disabled logic for matching file name and user input, added data test selectors to handlebars file.
    
    * added and updated values for file rename modal aria label and message notifications.
    
    * adding missing merged files from hard reset.
    
    * adding back removed tests.
    
    * adding back removed acceptance test for files-test.
    
    * adding assests back.
    
    * Deleting test file
    
    Deleing added test file
    
    * Readded deleted parameters
    
    Readded deleted parameters to ember-composable-helpers
    
    * Correcting assets
    
    Adding back the missing 'assets/images/addons/icons' from the merge
    
    * readded user input, added formatting issues for the modal on mobile, began debugging test failure, updated translations strings and file name, removed stubbed objects in Mirage test file for file item.
    
    * readded formatting for mobile view, updated OsfStorageManger to StorageManager with import.
    
    * removed RenameLink, converted OsfLink to Button, removed href and input value, removed if template logic, added DropdownItem class to button, updated validate function to get tracked property, removed unnecessary tracked properties.
    
    * removed file item component class, removed export
     for file item component, removed id for userInput, repositioned invocation of FileRenameModal to within FileActio
    nsMenu.
    
    * removed toggleRenameModal parameter inside FileActionsMenu invocation, removed unnecessary formatting for the FileRenameModal, removed currentUserPermissions within the modal, repositioned button to within currentUserCanDelete logic for FileActionsMenu template file.
    
    * removed styles for RenameModal.
    
    * Reverting changes to test.
    
    Reverting changes to test to match Develop
    
    * Updating test from feature/files-page-redesign branch
    
    Updating test from code found on Develop to code found on feature/file-page-redesign.
    
    * updated test code with feature/files-page-redesign branch instead of develop, added trailing space after FileActionsMenu template file.
    
    * revert test file
    
    * remove excessive line
    
    * fix tests
    
    * address CR
    
    Co-authored-by: Ashley Robinson <ashley@cos.io>
    Co-authored-by: Yuhuai Liu <yuhuai@cos.io>

---
+++ lib/osf-components/addon/components/file-browser/file-rename-modal/component.ts
@@ -0,0 +1,56 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import Toast from 'ember-toastr/services/toast';
+import { restartableTask } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
+import Intl from 'ember-intl/services/intl';
+import File from 'ember-osf-web/packages/files/file';
+
+interface Args {
+    manager: StorageManager;
+    item: File;
+}
+
+export default class FileRenameModal extends Component<Args> {
+    @service toast!: Toast;
+    @service intl!: Intl;
+
+    @tracked newFileName?: string;
+    @tracked originalFileName?: string;
+
+    constructor(owner: unknown, args: Args) {
+        super(owner, args);
+        this.newFileName = this.args.item.name;
+        this.originalFileName = this.args.item.name;
+    }
+
+    get isValid() {
+        return this.newFileName !== this.originalFileName;
+    }
+
+    @action
+    resetFileNameValue() {
+        this.newFileName =  this.originalFileName;
+    }
+
+    @restartableTask
+    @waitFor
+    async updateFileName() {
+        const newName = this.newFileName;
+        const successMessage = this.intl.t('osf-components.file-browser.file_rename_modal.success_message');
+        if (!newName) {
+            return;
+        }
+
+        try {
+            const trimmedName = newName.trim();
+            await this.args.item.rename(trimmedName, 'replace');
+            this.toast.success(successMessage);
+        } catch(e) {
+            this.toast.error(this.intl.t('osf-components.file-browser.file_rename_modal.retry_message'));
+        }
+        return newName;
+    }
+}
