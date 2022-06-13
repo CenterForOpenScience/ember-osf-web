25f7a62fbb81df9aed20d8e4480e5c3d3529d79f
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-06-13 16:57:11 -0400

    Prevent replace on rename conflict (#1540)
    -   Ticket: [ENG-3811]
    -   Feature flag: n/a
    
    ## Purpose
    - Don't replace files/folders when there is a name conflict
      - instead appends (1), (2), etc depending on number of duplicate file/folder names discovered (similar to macOS's handling of dupe file names)

--- lib/osf-components/addon/components/file-browser/file-rename-modal/component.ts
+++ lib/osf-components/addon/components/file-browser/file-rename-modal/component.ts
@@ -46,7 +46,7 @@ export default class FileRenameModal extends Component<Args> {
 
         try {
             const trimmedName = newName.trim();
-            await this.args.item.rename(trimmedName, 'replace');
+            await this.args.item.rename(trimmedName, '');
             this.toast.success(successMessage);
         } catch(e) {
             this.toast.error(this.intl.t('osf-components.file-browser.file_rename_modal.retry_message'));
