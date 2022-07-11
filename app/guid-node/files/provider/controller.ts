90617e28ab1259465dc63ddc2176131f76ccff1d
Futa Ikeda <futa.ikeda@gmail.com>
2022-07-11 19:48:32 -0400

    Hide node-files page links based on permission

--- app/guid-node/files/provider/controller.ts
+++ app/guid-node/files/provider/controller.ts
@@ -2,8 +2,11 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import Media from 'ember-responsive';
 
+import CurrentUser from 'ember-osf-web/services/current-user';
+
 export default class GuidNodeFilesProvider extends Controller {
     @service media!: Media;
+    @service currentUser!: CurrentUser;
 
     get isDesktop() {
         return this.media.isDesktop;
