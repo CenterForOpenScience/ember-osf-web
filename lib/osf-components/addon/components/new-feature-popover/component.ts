a846287a66a380e2cd1fe0c491c93f427d849ac5
Yuhuai Liu <yuhuai@cos.io>
2022-07-25 13:54:41 -0400

    [ENG-3911] New feature popover (#1571)
    * new feature popover
    
    * CR follow up. Add invocation
    
    * add tests
    
    * change popover body language

---
+++ lib/osf-components/addon/components/new-feature-popover/component.ts
@@ -0,0 +1,31 @@
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import Cookies from 'ember-cookies/services/cookies';
+import moment from 'moment';
+import { tracked } from '@glimmer/tracking';
+
+interface Args {
+    triggerElement: string;
+    featureCookie: string;
+}
+
+export default class NewFeaturePopover extends Component<Args> {
+    @service cookies!: Cookies;
+    @tracked notAgain = false;
+
+    get shouldShow() {
+        return !this.cookies.exists(this.args.featureCookie);
+    }
+
+    @action
+    onAccept() {
+        if (this.notAgain) {
+            this.cookies.write(this.args.featureCookie, 1, {
+                expires: moment().add(10, 'years').toDate(),
+                path: '/',
+            });
+        }
+    }
+
+}
