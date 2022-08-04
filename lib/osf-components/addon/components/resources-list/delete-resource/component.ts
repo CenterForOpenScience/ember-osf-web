ca42b6c983e7ee384a1bfbb46de0ee37ff0658d3
Yuhuai Liu <yuhuai@cos.io>
2022-08-04 14:15:09 -0400

    [ENG-3949] Delete resource (#1598)
    * prelim
    
    * delete resource
    
    * add tests
    
    * CR followup
    
    * CR followup

---
+++ lib/osf-components/addon/components/resources-list/delete-resource/component.ts
@@ -0,0 +1,33 @@
+import { inject as service } from '@ember/service';
+import Component from '@glimmer/component';
+import { task } from 'ember-concurrency';
+import Intl from 'ember-intl/services/intl';
+import { waitFor } from '@ember/test-waiters';
+
+import ResourceModel from 'ember-osf-web/models/resource';
+import { getApiErrorMessage } from 'ember-osf-web/utils/capture-exception';
+
+interface Args {
+    resource: ResourceModel;
+    onDelete: () => void;
+}
+
+export default class DeleteResource extends Component<Args> {
+    @service intl!: Intl;
+    @service toast!: Toastr;
+
+    @task
+    @waitFor
+    async deleteResource() {
+        try {
+            await this.args.resource.destroyRecord();
+            this.toast.info(this.intl.t('osf-components.resources-list.delete_resource.delete_success'));
+            this.args.onDelete();
+        } catch (e) {
+            this.toast.error(
+                getApiErrorMessage(e),
+                this.intl.t('osf-components.resources-list.delete_resource.delete_failure'),
+            );
+        }
+    }
+}
