0dbf29c690a282575c5a2f931fe152b664940ba3
Ashley <ashley@Tlaloc.local>
2022-08-05 12:05:24 -0400

    updated overview and completed resource component files, improved CSS for icon and label display, entering debug for OsfLink.

--- lib/osf-components/addon/components/resource/component.ts
+++ lib/osf-components/addon/components/resource/component.ts
@@ -2,33 +2,38 @@ import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import Intl from 'ember-intl/services/intl';
 
-import ResourceModel from 'ember-osf-web/models/resource';
+import { ResourceTypes } from 'ember-osf-web/models/resource';
 import { getBadgeIcon, getBadgeIconDisabled } from 'ember-osf-web/helpers/open-badges-icon-map';
+import Toast from 'ember-toastr/services/toast';
+import RegistrationModel from 'ember-osf-web/models/registration';
 
 interface ResourceArgs {
-    resource: ResourceModel;
+    registration: RegistrationModel;
+    resourceType: ResourceTypes;
+    hasResource: boolean;
 }
 
 export default class ResourceComponent extends Component<ResourceArgs> {
     @service intl!: Intl;
+    @service toast!: Toast;
 
     constructor(owner: unknown, args: ResourceArgs) {
         super(owner, args);
     }
 
     get getSrc() {
-        if (!this.args.resource) {
+        if (!this.args.resourceType) {
             return '';
         }
-        const resourceType = this.args.resource.resourceType;
-        const isFinal = this.args.resource.finalized;
 
-        if (!isFinal) {
-            return getBadgeIconDisabled(resourceType);
-        } else if (isFinal) {
-            return getBadgeIcon(resourceType);
-        } else {
-            return new Error('Badge state nebulous.');
+        try {
+            if (this.args.hasResource === true) {
+                return getBadgeIcon(this.args.resourceType);
+            } else {
+                return getBadgeIconDisabled(this.args.resourceType);
+            }
+        } catch (e) {
+            return this.toast.error(this.intl.t('registries.overview.resources.' + this.args.resourceType + '.error'));
         }
     }
 }
