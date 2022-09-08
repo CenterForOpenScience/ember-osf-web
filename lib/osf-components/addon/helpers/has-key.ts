29f4d9f2aed7e2d2f7623b46a82a1a4da684b3ab
Yuhuai Liu <yuhuai@cos.io>
2022-09-08 15:54:27 -0400

    fix tests

--- lib/osf-components/addon/helpers/has-key.ts
+++ lib/osf-components/addon/helpers/has-key.ts
@@ -1,7 +1,10 @@
 import { helper } from '@ember/component/helper';
 
 export function hasKey([object, key]: [any, any]): any {
-    return (key in object);
+    if (object) {
+        return (key in object);
+    }
+    return false;
 }
 
 export default helper(hasKey);
