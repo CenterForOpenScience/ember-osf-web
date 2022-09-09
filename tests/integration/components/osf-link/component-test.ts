71ae2a6ba588d4594ea5909b79ca9fa762ba8aca
Brian Pilati <brian.pilati@cos.io>
2022-09-09 10:07:20 -0600

    Added an error test

--- tests/integration/components/osf-link/component-test.ts
+++ tests/integration/components/osf-link/component-test.ts
@@ -1,4 +1,5 @@
-import { render } from '@ember/test-helpers';
+import { render} from '@ember/test-helpers';
+import Ember from 'ember';
 // import { pauseTest } from '@ember/test-helpers/setup-context';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'ember-qunit';
@@ -7,6 +8,14 @@ import { module, test } from 'qunit';
 
 module('Integration | Component | osf-link', hooks => {
     setupRenderingTest(hooks);
+
+    let orgOnError: any;
+    hooks.beforeEach(function() {
+        orgOnError = Ember.onerror;
+    });
+    hooks.afterEach(function() {
+        Ember.onerror = orgOnError;
+    });
 
     test('it renders blocks the right way', async assert => {
         await render(hbs`
@@ -27,5 +36,26 @@ module('Integration | Component | osf-link', hooks => {
         assert.dom('[data-test-get-started-button]').hasClass('btn');
         assert.dom('[data-test-get-started-button]').hasClass('btn-primary');
         assert.dom('[data-test-get-started-button][href$="https://osf.io"]').exists('The href is correct');
+    });
+
+    test('it should throw an error without an array @models', async assert => {
+        Ember.onerror = (error: Error) => {
+            assert.equal(error.message,
+                'Assertion Failed: `@models` must be undefined or an array. Consider using the `array` helper.');
+        };
+
+        await render(hbs`
+            <OsfLink
+                @models='error'
+                data-test-get-started-button
+                data-analytics-name='Get started button'
+                class='btn btn-primary'
+                local-class='startButton'
+                @target='_blank'
+                @href='https://osf.io'
+            >
+                {{t 'dashboard.getting_started'}}
+            </OsfLink>
+        `);
     });
 });
