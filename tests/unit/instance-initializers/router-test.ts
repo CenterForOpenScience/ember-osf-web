a0bbc1c40bcf8a8dedd0f83fe6c1af7018c754f4
Futa Ikeda <futa.ikeda@gmail.com>
2022-09-23 20:07:19 -0400

    Update app instance initializer router test

--- tests/unit/instance-initializers/router-test.ts
+++ tests/unit/instance-initializers/router-test.ts
@@ -1,16 +1,28 @@
 import Application from '@ember/application';
+import ApplicationInstance from '@ember/application/instance';
+import { run } from '@ember/runloop';
+import Resolver from 'ember-resolver';
+import { TestContext } from 'ember-test-helpers';
+import config from 'ember-get-config';
+import { module, test } from 'qunit';
+import sinon from 'sinon';
 
 import { initialize } from 'ember-osf-web/instance-initializers/router';
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
-import destroyApp from '../../helpers/destroy-app';
 
-module('Unit | Instance Initializer | router', function(hooks) {
-    setupTest(hooks);
+interface InitializerTestContext extends TestContext {
+    TestApplication: any;
+    application: Application;
+    instance: ApplicationInstance;
+}
 
-    // TODO: Actually test
-    hooks.beforeEach(function() {
-        this.TestApplication = Application.extend();
+const { modulePrefix } = config;
+
+module('Unit | Instance Initializer | router', function(this: InitializerTestContext, hooks) {
+    hooks.beforeEach(function(this: InitializerTestContext) {
+        this.TestApplication = class TestApplication extends Application {
+            modulePrefix = modulePrefix;
+            Resolver = Resolver;
+        };
         this.TestApplication.instanceInitializer({
             name: 'initializer under test',
             initialize,
@@ -18,15 +30,19 @@ module('Unit | Instance Initializer | router', function(hooks) {
         this.application = this.TestApplication.create({ autoboot: false });
         this.instance = this.application.buildInstance();
     });
-    hooks.afterEach(function() {
-        destroyApp(this.application);
-        destroyApp(this.instance);
+
+    hooks.afterEach(function(this: InitializerTestContext) {
+        run(this.instance, 'destroy');
+        run(this.application, 'destroy');
     });
 
-    // Replace this with your real tests.
-    test('it works', async function(assert) {
+    test('it sets handlers to router service', async function(this: InitializerTestContext, assert) {
+        const routerService = this.instance.lookup('service:router');
+        const routerOn = sinon.stub(routerService, 'on');
+        assert.ok(routerOn.notCalled, 'Event handlers are not yet set on router service');
         await this.instance.boot();
-
-        assert.ok(true);
+        assert.ok(routerOn.calledTwice, 'handlers set');
+        assert.ok(routerOn.calledWith('routeWillChange', sinon.match.func), 'routeWillChange handler set');
+        assert.ok(routerOn.calledWith('routeDidChange', sinon.match.func), 'routeDidChange handler set');
     });
 });
