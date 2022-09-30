4a06b40bf122c0440814893b240d20bbabede52f
Futa Ikeda <futa.ikeda@gmail.com>
2022-09-30 10:53:20 -0400

    Update registration finalize modal text

--- tests/integration/components/registries/finalize-registration-modal/component-test.ts
+++ tests/integration/components/registries/finalize-registration-modal/component-test.ts
@@ -110,7 +110,7 @@ module('Integration | Component | finalize-registration-modal', hooks => {
         this.set('isOpen', false);
     });
 
-    test('almost done modal content: no moderation with project', async function(assert) {
+    test('almost done modal content: no moderation', async function(assert) {
         this.store = this.owner.lookup('service:store');
         const noModerationProvider = server.create('registration-provider', { reviewsWorkflow: null });
         const node = server.create('node', 'currentUserAdmin');
@@ -142,9 +142,9 @@ module('Integration | Component | finalize-registration-modal', hooks => {
         // Click submit button
         await click('[data-test-submit-registration-button]');
 
-        const opts = { learnMoreLink: 'aaa.aa', htmlSafe: true };
+        const opts = { htmlSafe: true };
         assert.dom('[data-test-finalize-main]').hasTextContaining(
-            stripHtmlTags(t('registries.finalizeRegistrationModal.notice.noModerationFromProject', opts).toString()),
+            stripHtmlTags(t('registries.finalizeRegistrationModal.notice.noModeration', opts).toString()),
             'modal shows warning',
         );
         assert.dom('[data-test-finalize-main]').doesNotHaveTextContaining(
@@ -152,7 +152,7 @@ module('Integration | Component | finalize-registration-modal', hooks => {
         );
     });
 
-    test('almost done modal content: with moderation with project', async function(assert) {
+    test('almost done modal content: with moderation', async function(assert) {
         this.store = this.owner.lookup('service:store');
         const withModerationProvider = server.create('registration-provider');
         const node = server.create('node', 'currentUserAdmin');
@@ -188,92 +188,9 @@ module('Integration | Component | finalize-registration-modal', hooks => {
         // Click submit button
         await click('[data-test-submit-registration-button]');
 
-        const opts = { learnMoreLink: 'aaa.aa', htmlSafe: true };
+        const opts = { htmlSafe: true };
         assert.dom('[data-test-finalize-main]').hasTextContaining(
-            stripHtmlTags(t('registries.finalizeRegistrationModal.notice.withModerationFromProject', opts).toString()),
-            'modal shows warning with moderation for moderated providers',
-        );
-    });
-
-    test('almost done modal content: no moderation no project', async function(assert) {
-        this.store = this.owner.lookup('service:store');
-        const noModerationProvider = server.create('registration-provider', { reviewsWorkflow: null });
-        const noModRegistration = server.create(
-            'registration',
-            { provider: noModerationProvider },
-        );
-        const draftRegistration = server.create('draft-registration', { hasProject: false });
-
-        const registrationModel = await this.store.findRecord('registration', noModRegistration.id);
-        this.set('draftManager', {
-            provider: noModerationProvider,
-            draftRegistration,
-            validateAllVisitedPages: () => { /* noop */ },
-        });
-        this.set('model', registrationModel);
-        this.set('isOpen', true);
-        await render(hbs`
-            <Registries::FinalizeRegistrationModal::Manager
-                @registration={{this.model}}
-                @draftManager={{this.draftManager}}
-                as |manager|
-            >
-                <Registries::FinalizeRegistrationModal @isOpen={{this.isOpen}} @manager={{manager}} />
-            </Registries::FinalizeRegistrationModal::Manager>
-        `);
-        // Click immediate radio button
-        await click('[data-test-immediate-button]');
-        // Click submit button
-        await click('[data-test-submit-registration-button]');
-
-        const opts = { learnMoreLink: 'aaa.aa', htmlSafe: true };
-        assert.dom('[data-test-finalize-main]').hasTextContaining(
-            stripHtmlTags(t('registries.finalizeRegistrationModal.notice.noModerationNoProject', opts).toString()),
-            'modal shows warning',
-        );
-        assert.dom('[data-test-finalize-main]').doesNotHaveTextContaining(
-            'A moderator must review and approve', 'modal does not mention moderation for unmoderated providers',
-        );
-    });
-
-    test('almost done modal content: with moderation no project', async function(assert) {
-        this.store = this.owner.lookup('service:store');
-        const withModerationProvider = server.create('registration-provider');
-        const withModRegistration = server.create(
-            'registration',
-            { provider: withModerationProvider },
-        );
-        const draftRegistration = server.create('draft-registration', { hasProject: false });
-
-        const registrationModel = await this.store.findRecord('registration', withModRegistration.id);
-        this.set(
-            'draftManager',
-            {
-                provider: withModerationProvider,
-                reviewsWorkflow: 'pre-moderation',
-                draftRegistration,
-                validateAllVisitedPages: () => { /* noop */ },
-            },
-        );
-        this.set('model', registrationModel);
-        this.set('isOpen', true);
-        await render(hbs`
-            <Registries::FinalizeRegistrationModal::Manager
-                @registration={{this.model}}
-                @draftManager={{this.draftManager}}
-                as |manager|
-            >
-                <Registries::FinalizeRegistrationModal @isOpen={{this.isOpen}} @manager={{manager}} />
-            </Registries::FinalizeRegistrationModal::Manager>
-        `);
-        // Click immediate radio button
-        await click('[data-test-immediate-button]');
-        // Click submit button
-        await click('[data-test-submit-registration-button]');
-
-        const opts = { learnMoreLink: 'aaa.aa', htmlSafe: true };
-        assert.dom('[data-test-finalize-main]').hasTextContaining(
-            stripHtmlTags(t('registries.finalizeRegistrationModal.notice.withModerationNoProject', opts).toString()),
+            stripHtmlTags(t('registries.finalizeRegistrationModal.notice.withModeration', opts).toString()),
             'modal shows warning with moderation for moderated providers',
         );
     });
