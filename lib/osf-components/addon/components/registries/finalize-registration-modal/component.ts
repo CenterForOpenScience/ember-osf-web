d89c992c39a3f68927b969ab5454c22838cf0bcf
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-09-30 14:25:31 -0400

    [ENG-4053] Update registration finalize modal text (#1666)
    -   Ticket: [ENG-4053]
    
    ## Purpose
    - Reword finalize registration modal
    
    ## Summary of Changes
    - Update translation file
      - Reworked modal text so there is no need to change text based on project-based or no-project registration

--- lib/osf-components/addon/components/registries/finalize-registration-modal/component.ts
+++ lib/osf-components/addon/components/registries/finalize-registration-modal/component.ts
@@ -3,7 +3,6 @@ import Component from '@ember/component';
 import { assert } from '@ember/debug';
 import { action, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
-import config from 'ember-get-config';
 import Intl from 'ember-intl/services/intl';
 import moment from 'moment';
 
@@ -26,7 +25,6 @@ export default class FinalizeRegisrationModalComponent extends Component {
     makePublicOption = '';
     embargoRangeStartDate: Date = moment().add(3, 'days').toDate();
     embargoRangeEndDate: Date = moment().add(1460, 'days').toDate();
-    learnMoreLink = config.helpLinks.linkToAProject;
 
     didReceiveAttrs() {
         assert('finalize-registration-modal requires @manager!', Boolean(this.manager));
@@ -39,21 +37,13 @@ export default class FinalizeRegisrationModalComponent extends Component {
         }
     }
 
-    @computed('learnMoreLink', 'manager.draftManager.{draftRegistration.hasProject,reviewsWorkflow}')
+    @computed('manager.draftManager.reviewsWorkflow')
     get noticeText() {
-        const translationOptions = { learnMoreLink: this.learnMoreLink, htmlSafe: true };
-        let translationString = '';
-
-        if (this.manager.draftManager.draftRegistration.hasProject) {
-            translationString = this.manager.draftManager.reviewsWorkflow
-                ? 'withModerationFromProject'
-                : 'noModerationFromProject';
-        } else {
-            translationString = this.manager.draftManager.reviewsWorkflow
-                ? 'withModerationNoProject'
-                : 'noModerationNoProject';
-        }
-        return this.intl.t(`registries.finalizeRegistrationModal.notice.${translationString}`, translationOptions);
+        const translationOptions = { htmlSafe: true };
+        const translationString  = this.manager.draftManager.reviewsWorkflow
+            ? 'registries.finalizeRegistrationModal.notice.withModeration'
+            : 'registries.finalizeRegistrationModal.notice.noModeration';
+        return this.intl.t(translationString, translationOptions);
     }
 
     @computed('manager.{hasEmbargoEndDate,submittingRegistration}', 'makePublicOption')
