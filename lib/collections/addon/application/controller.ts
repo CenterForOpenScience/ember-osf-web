8da773d5dcf96351e8d1245753fca08caf662815
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-10-13 16:56:19 -0400

    Add moderation button to branded-navbar (#1671)

--- lib/collections/addon/application/controller.ts
+++ lib/collections/addon/application/controller.ts
@@ -1,17 +1,31 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
+import Features from 'ember-feature-flags/services/features';
 import config from 'ember-get-config';
 import CurrentUser from 'ember-osf-web/services/current-user';
 import Theme from 'ember-osf-web/services/theme';
 import pathJoin from 'ember-osf-web/utils/path-join';
 import $ from 'jquery';
 
+const {
+    featureFlagNames: {
+        routes: routeFlags,
+    },
+} = config;
+
 export default class Application extends Controller {
     @service currentUser!: CurrentUser;
     @service theme!: Theme;
+    @service features!: Features;
 
     signupUrl = `${pathJoin(config.OSF.url, 'register')}?${$.param({ next: window.location.href })}`;
+    currentUserIsModerator = true;
+
+    get isDisplayModerationButton() {
+        const moderationFlag = routeFlags['collections.moderation'];
+        return this.currentUserIsModerator && this.features.isEnabled(moderationFlag);
+    }
 
     @action
     login() {
