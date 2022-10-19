1a46769921b0008c0c2450a4df06fb720aa34160
Ashley <ashley@Tlaloc.local>
2022-10-19 10:47:42 -0400

    updated CSS stylings for mobile and tablet formats, code clean up, entering debug for helper function.

--- lib/osf-components/addon/components/carousel/component.ts
+++ lib/osf-components/addon/components/carousel/component.ts
@@ -7,32 +7,23 @@ import CarouselItem from 'osf-components/components/carousel/x-item/component';
 
 import { inject as service } from '@ember/service';
 import Intl from 'ember-intl/services/intl';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Toast from 'ember-toastr/services/toast';
-=======
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
-=======
-import Toast from 'ember-toastr/services/toast';
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
 
 import styles from './styles';
 import template from './template';
+
+declare global {
+    interface Window {
+      api?: any;
+    }
+}
 
 @layout(template, styles)
 @tagName('')
 export default class Carousel extends Component {
     @service intl!: Intl;
-<<<<<<< HEAD
-<<<<<<< HEAD
     @service toast!: Toast;
 
-=======
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
-=======
-    @service toast!: Toast;
-
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
     // Private properties
     carouselItems: CarouselItem[] = [];
 
@@ -44,6 +35,10 @@ export default class Carousel extends Component {
     @computed('carouselItems.{length,@each.isActive}')
     get activeSlide() {
         return this.carouselItems.findBy('isActive');
+    }
+
+    getIndexValue(index: number) {
+        return index + 1;
     }
 
     @action
@@ -77,78 +72,33 @@ export default class Carousel extends Component {
         this.carouselItems[newIndex].set('isActive', true);
     }
 
-<<<<<<< HEAD
     // @action
-    setUpLiveRegion(message: string) {
-        const w = window || null;
-        const document = w.document;
-        console.log(document);
-        const carousel = document.getElementById('carouselInner');
-        const liveregion = document.createElement('div');
-        liveregion.setAttribute('arialive', 'polite');
-        liveregion.setAttribute('ariaatomic', 'true');
-        liveregion.setAttribute('class', 'liveregion visuallyhidden');
-        if (carousel && liveregion) {
-            let subject = 'You';
-            let msg: string = subject + message;
+    setUpLiveRegion(region: string) {
+        const message = this.intl.t(region + 'new-home.voice_over.live_region.register_keyboard.message');
+        const homeMenu = document.getElementById('menu');
+        const liveRegion = document.createElement('div');
+        liveRegion.setAttribute('aria-live', 'polite');
+        liveRegion.setAttribute('aria-atomic', 'true');
+        liveRegion.setAttribute('id', 'liveRegion');
+        liveRegion.setAttribute('class', 'live-region visually-hidden');
+
+        // get active region, voice help for that region
+        if (homeMenu && liveRegion) {
+            const subject = 'You';
+            const msg: string = subject + message;
             const data = document.createTextNode(msg);
-            console.log('Spoken: ', msg);
-            liveregion.appendChild(data);
-            carousel.appendChild(liveregion);
+            liveRegion.appendChild(data);
+            homeMenu.appendChild(liveRegion);
         }
+
+        window.setTimeout(() => {
+            const menu = document.getElementById('menu');
+            if (menu) {
+                menu.textContent = message;
+            }
+        }, 3000);
     }
 
-    //@action
-    const speech = {
-        wrap: <HTMLElement>document.getElementById('swrap'),
-        btn: <HTMLElement>document.getElementById('sbtn'),
-        recog: null,
-        init: () => {
-            this.speech.wrap = <HTMLElement>document.getElementById('swrap');
-            this.speech.btn = <HTMLElement>document.getElementById('sbtn');
-
-            // get mic permission
-            navigator.mediaDevices.getUserMedia({ audio: true}).then((stream) => {
-                const speech = SpeechRecognition || webkitSpeechRecognition;
-                this.speech.recog = new SpeechRecognition();
-                speech.recog.lang = 'en-US';
-                speech.recog.continuous = false;
-                speech.interimResults = false;
-
-                speech.recog.onresult = (evt: any) => {
-                    let said = evt.results[0][0].transcript.toLowerCase();
-                    if cmd([said]) { cmd[said]
-                    else { said += " (command not found)"; }
-                    speech.wrap.innerHTML = said;
-                    speech.stop();
-
-                }
-                speech.recog.onerror = (err: string) => { this.toast.error(err)};
-                
-                speech.btn.disabled = false;
-                speech.stop();
-
-            }).catch((err) => {
-                console.log(err);
-                this.speech.wrap.innerHTML = 'Microphone access required for voice navigation.';
-            });
-        },
-
-        start: () => {
-            speech.recog.start()
-            speech.btn.onclick = speech.stop;
-            speech.btn.value = 'click or press Enter to speak selections';
-        }
-
-        stop: () => {
-            speech.onclick() = speech.start();
-            speech.btn.value = 'press to speak selections'
-        }
-    } 
-
-
-=======
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
     /** registerKeyboard() is a function that enables users to navigate certain
      *  components with a keyboard rather than using a mouse. The keybindings are
      *  designed for optimal ergonomic comfort as well as improving overall user
@@ -158,15 +108,9 @@ export default class Carousel extends Component {
      *  @date : Sat Oct 01 2022 14:26:06 GMT-0400
      */
     registerKeyboard() {
-<<<<<<< HEAD
         // set up voice over
-        let msg = 'Keyboard navigation enabled. Press lower case h to hear more.';
-        // this.setUpLiveRegion(msg);
-        console.log('voice over set to: ', msg);
-        
+        // this.setUpliveRegion(region);
 
-=======
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
         // locate elements
         const carousel: HTMLElement = document.querySelector('[data-test-carousel-container]') as HTMLElement;
         const leftNav: HTMLElement = document.getElementById('leftNav') as HTMLElement;
@@ -183,25 +127,14 @@ export default class Carousel extends Component {
         document.addEventListener('keydown', event => {
             const name = event.key;
             const code = event.code;
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
-=======
-
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
             // do not override native browser or SR controls
             if (name === 'Control' || name === 'Meta') {
                 return;
             }
 
             // switch operand based on user input
-            // current wirings are for left, right and dot slide navigation
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
+            // current wirings are for left, right and dot navigation
             try {
                 if (event.ctrlKey) {
                     event.preventDefault();
@@ -231,23 +164,6 @@ export default class Carousel extends Component {
                     default:
                         w.toastr.error(this.intl.t('osf-components.carousel.keyboard_error_message'));
                         break;
-<<<<<<< HEAD
-=======
-            if (event.ctrlKey) {
-                switch(name) {
-                case('f'):
-                    console.log('navigating to previous slide.');
-                    if (leftNav) {
-                        leftNav.click();
-                    }
-                    break;
-                case('j'):
-                    console.log('navigating to next slide.');
-                    if (rightNav) {
-                        rightNav.click();
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
-=======
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
                     }
                 } else if (event.altKey) {
                     event.preventDefault();
@@ -276,8 +192,6 @@ export default class Carousel extends Component {
                             break;
                         }
                     }
-<<<<<<< HEAD
-<<<<<<< HEAD
                 } else {
                     w.toastr.error(this.intl.t('osf-components.carousel.dot_nav_error_message'));
                 }
@@ -286,52 +200,5 @@ export default class Carousel extends Component {
                 w.toastr.error(this.intl.t('osf-components.carousel.key_name_and_code'));
             }
         }, true);
-=======
-                    break;
-                // TODO re-add registerKeyboard()
-                // case('k'):
-                    // re-open keyboard menu
-                default:
-                    throw new Error(this.intl.t('osf-components.carousel.keyboard_error_message'));
-                    break;
-=======
-                } else {
-                    w.toastr.error(this.intl.t('osf-components.carousel.dot_nav_error_message'));
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
-                }
-
-            } catch(e) {
-                w.toastr.error(this.intl.t('osf-components.carousel.key_name_and_code'));
-            }
-        }, true);
-<<<<<<< HEAD
-
-        if (dotNav) {
-            const buttonElements = dotNav.children;
-            const buttonOne = document.querySelectorAll("['data-test-navigation-button']")[0];
-            const buttonTwo : Element = buttonElements[1].children[0] || null;
-            const buttonThree : Element = buttonElements[2].children[0] || null;
-
-            document.addEventListener('keyup', event => {
-                if (event.code === '49') {
-                    if (buttonOne) {
-                        buttonOne.classList.add('active'); // TODO test
-                    }
-                }
-                if (event.code === '50') {
-                    if (buttonTwo) {
-                        buttonTwo.classList.add('active');
-                    }
-                }
-                if (event.code === '51') {
-                    if (buttonThree) {
-                        buttonThree.classList.add('active');
-                    }
-                }
-            });
-        }
->>>>>>> 25f9da9c7 (removed unnecessary styling, removed handlebars mobile queries, made dotnav buttons fully focusable, moved addLiveRegion code to separate branch, improved error message, removed unnecessary console log statements, updated tabindex to relevant elements.)
-=======
->>>>>>> 1606e3f51 (added dot navigation wirings for slides, updated aria labels and error messages, updated error handling, added try/catch block, added intl translation string helper, updated codes, entering debug for toastr.)
     }
 }
