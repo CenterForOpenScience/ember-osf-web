3c7051d1d31503d7c23cb6fe2b9e307a713151db
Futa Ikeda <futa.ikeda@gmail.com>
2022-10-19 14:18:49 -0400

    Update environment variables for mirage server

--- config/environment.js
+++ config/environment.js
@@ -18,7 +18,7 @@ const {
     BACKEND: backend = 'local',
     CAS_URL: casUrl = 'http://192.168.168.167:8080',
     CLIENT_ID: clientId,
-    COLLECTIONS_ENABLED = false,
+    COLLECTIONS_ENABLED = true,
     REGISTRIES_ENABLED = true,
     TESTS_ENABLED = false,
     FB_APP_ID,
@@ -26,7 +26,7 @@ const {
     GOOGLE_ANALYTICS_ID,
     KEEN_CONFIG: keenConfig,
     LINT_ON_BUILD: lintOnBuild = false,
-    MIRAGE_ENABLED = false,
+    MIRAGE_ENABLED = true,
     MIRAGE_SCENARIOS = [
         'loggedIn',
         'dashboard',
