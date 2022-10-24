56979bed5677c2b96d069058508ce54e682787a0
Brian Pilati <brianpilati@cos.io>
2022-10-24 09:26:30 -0600

    Added an accepted project collection scenario

--- mirage/scenarios/default.ts
+++ mirage/scenarios/default.ts
@@ -1,7 +1,7 @@
 import { Server } from 'ember-cli-mirage';
 import config from 'ember-get-config';
 
-import { collectionScenario, collectionPendingScenario} from './collections';
+import { collectionScenario, collectionPendingScenario, collectionAcceptedScenario} from './collections';
 import { dashboardScenario } from './dashboard';
 import { forksScenario } from './forks';
 import { meetingsScenario } from './meetings';
@@ -40,6 +40,7 @@ export default function(server: Server) {
     if (mirageScenarios.includes('collections')) {
         collectionScenario(server, currentUser);
         collectionPendingScenario(server, currentUser);
+        collectionAcceptedScenario(server, currentUser);
     }
     if (mirageScenarios.includes('forks')) {
         forksScenario(server, currentUser);
