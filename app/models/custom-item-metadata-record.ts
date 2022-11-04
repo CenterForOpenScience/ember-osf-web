3d30c93955f9852fa01296eda19225af26aa63a9
Brian J. Geiger <bgeiger@cos.io>
2022-11-04 09:50:58 -0400

    [ENG-4156] Add models for metadata (#1696)
    -   Ticket: [ENG-4156]
    
    ## Purpose
    
    Add some models for the guid-metadata project.
    
    ## Summary of Changes
    
    1. Modify guid model to have a link to new metadata models
    2. Add custom-file-metadata-record model
    3. Add custom-item-metadata-record model
    4. Add tests

---
+++ app/models/custom-item-metadata-record.ts
@@ -0,0 +1,24 @@
+import { attr } from '@ember-data/model';
+import CustomMetadataModel from 'ember-osf-web/models/custom-metadata';
+
+
+export interface Funder {
+    funderName?: string;
+    funderIdentifier?: string;
+    funderIdentifierType?: string;
+    awardNumber?: string;
+    awardUri?: string;
+    awardTitle?: string;
+}
+
+export default class CustomItemMetadataRecordModel extends CustomMetadataModel {
+    @attr('fixstring') resource_type_general?: string;
+    @attr('fixstring') language?: string;
+    @attr('array') funders?: Funder[];
+}
+
+declare module 'ember-data/types/registries/model' {
+    export default interface ModelRegistry {
+        'custom-item-metadata-record': CustomItemMetadataRecordModel;
+    } // eslint-disable-line semi
+}
