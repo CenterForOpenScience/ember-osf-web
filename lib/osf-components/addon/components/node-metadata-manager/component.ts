45053c0a4b67f7d0fc21662ff5ab63558829e48c
Brian Geiger <bgeiger@cos.io>
2022-11-18 10:24:47 -0500

    Add node metadata manager to guid-node

--- lib/osf-components/addon/components/node-metadata-manager/component.ts
+++ lib/osf-components/addon/components/node-metadata-manager/component.ts
@@ -1,7 +1,7 @@
 import Store from '@ember-data/store';
 import { assert } from '@ember/debug';
-import { action } from '@ember/object';
-import { alias, or } from '@ember/object/computed';
+import { action, notifyPropertyChange } from '@ember/object';
+import { alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
 import { waitFor } from '@ember/test-waiters';
 import Component from '@glimmer/component';
@@ -16,6 +16,7 @@ import CustomItemMetadataRecordModel from 'ember-osf-web/models/custom-item-meta
 import { Permission } from 'ember-osf-web/models/osf-model';
 import { getApiErrorMessage } from 'ember-osf-web/utils/capture-exception';
 import buildChangeset from 'ember-osf-web/utils/build-changeset';
+import { tracked } from '@glimmer/tracking';
 
 interface Args {
     node: (AbstractNodeModel);
@@ -40,19 +41,15 @@ export default class NodeMetadataManagerComponent extends Component<Args> {
     @service intl!: Intl;
     @service toast!: Toast;
 
-    metadata!: CustomItemMetadataRecordModel;
+    @tracked metadata!: CustomItemMetadataRecordModel;
     node: (AbstractNodeModel) = this.args.node;
-    changeset!: BufferedChangeset;
-    inEditMode = false;
-    userCanEdit!: boolean;
-    @or(
-        'getGuidMetadata.isRunning',
-        'getNode.isRunning',
-    )
-    isGatheringData!: boolean;
+    @tracked changeset!: BufferedChangeset;
+    @tracked inEditMode = false;
+    @tracked userCanEdit!: boolean;
+    @alias( 'getGuidMetadata.isRunning') isGatheringData!: boolean;
     @alias('changeset.isDirty') isDirty!: boolean;
     @alias('save.isRunning') isSaving!: boolean;
-    guidType!: string | undefined;
+    @tracked guidType!: string | undefined;
 
     constructor(owner: unknown, args: Args) {
         super(owner, args);
@@ -63,7 +60,6 @@ export default class NodeMetadataManagerComponent extends Component<Args> {
         try {
             taskFor(this.getGuidMetadata).perform();
             this.userCanEdit = this.node.currentUserPermissions.includes(Permission.Write);
-            this.changeset = buildChangeset(this.metadata, null);
         } catch (e) {
             const errorTitle = this.intl.t('osf-components.item-metadata-manager.error-getting-metadata');
             this.toast.error(getApiErrorMessage(e), errorTitle);
@@ -75,11 +71,13 @@ export default class NodeMetadataManagerComponent extends Component<Args> {
     async getGuidMetadata() {
         if (this.node) {
             const guidRecord = await this.store.findRecord('guid', this.node.id, {
-                include: 'metadata',
+                include: 'custom_metadata',
                 resolve: false,
             });
             this.guidType = guidRecord.referentType;
             this.metadata = guidRecord.customMetadata as CustomItemMetadataRecordModel;
+            notifyPropertyChange(this, 'metadata');
+            this.changeset = buildChangeset(this.metadata, null);
         }
     }
 
