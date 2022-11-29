8f6fb4543e722370830a1549403621dc8b64390e
Brian Pilati <brianpilati@cos.io>
2022-11-29 13:57:58 -0700

    Added additional validations

--- tests/engines/collections/acceptance/moderation/settings-test.ts
+++ tests/engines/collections/acceptance/moderation/settings-test.ts
@@ -9,7 +9,7 @@ module('Collections | Acceptance | moderation | settings', hooks => {
     setupEngineApplicationTest(hooks, 'collections');
     setupMirage(hooks);
 
-    test('it renders settings for collection moderators', async assert => {
+    test('it renders settings for collection moderators', async function(assert) {
         server.create('user', 'loggedIn');
         const primaryCollection = server.create('collection');
         const provider = server.create('collection-provider', {
@@ -18,8 +18,22 @@ module('Collections | Acceptance | moderation | settings', hooks => {
         }, 'currentUserIsAdmin');
 
         await visit(`/collections/${provider.id}/moderation/settings`);
-        // await pauseTest();
+        assert.dom('[data-test-collections-moderation-settings-heading]').hasText(
+            'Configure review state notification preferences',
+            'The setting heading text is not correct',
+        );
+
+        assert.dom('[data-test-collections-moderation-settings-paragraph]').hasText(
+            'To configure other notification preferences visit your user settings.',
+            'The other notification text is not correct',
+        );
+
         assert.dom('[data-test-collections-moderation-settings-tab]').hasClass('active', 'settings is active tab');
+
+        const eventNames = this.element.querySelectorAll('[data-test-subscription-event-name]');
+        assert.equal(eventNames.length, 1, 'The expected event names do not exist.');
+        assert.dom(eventNames[0]).hasText('New pending submissions',
+            'The new pending submission notification is missing.');
 
         await percySnapshot(assert);
     });
