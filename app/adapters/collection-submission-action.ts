80f222609302eb9871c92df220f8b8774591000f
Futa Ikeda <futa.ikeda@gmail.com>
2022-11-30 14:37:57 -0500

    Move POST requests of collection-submission-actions to top level endpoint

--- app/adapters/collection-submission-action.ts
+++ app/adapters/collection-submission-action.ts
@@ -1,6 +1,10 @@
 import ActionAdapter from './action';
 
 export default class CollectionSubmissionActionAdapter extends ActionAdapter {
+    urlForCreateRecord() {
+        // Eventually remove this when we can just post to /v2/collection_submissions/:id/actions
+        return `${this.urlPrefix()}/collection_submission_actions/`;
+    }
 }
 
 declare module 'ember-data/types/registries/adapter' {
