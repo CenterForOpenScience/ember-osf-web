15d261edde7ff8db46aa074bee810ff5397571ef
Futa Ikeda <futa.ikeda@gmail.com>
2022-12-06 17:34:56 -0500

    Clean up

--- lib/collections/addon/provider/moderation/moderators/controller.ts
+++ lib/collections/addon/provider/moderation/moderators/controller.ts
@@ -1,20 +1,13 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 import Store from '@ember-data/store';
 import { waitFor } from '@ember/test-waiters';
 import { task } from 'ember-concurrency';
-import { taskFor } from 'ember-concurrency-ts';
 
 export default class CollectionsModerationModeratorsController extends Controller {
     @service router!: RouterService;
     @service store!: Store;
-
-    @action
-    afterSelfRemoval() {
-        taskFor(this.refetchProvider).perform(this.model.id);
-    }
 
     @task
     @waitFor
--- lib/registries/addon/branded/moderation/moderators/controller.ts
+++ lib/registries/addon/branded/moderation/moderators/controller.ts
@@ -1,19 +1,12 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 import Store from '@ember-data/store';
 import { waitFor } from '@ember/test-waiters';
 import { task } from 'ember-concurrency';
-import { taskFor } from 'ember-concurrency-ts';
 export default class BrandedModerationModeratorsController extends Controller {
     @service router!: RouterService;
     @service store!: Store;
-
-    @action
-    afterSelfRemoval() {
-        taskFor(this.refetchProvider).perform(this.model.id);
-    }
 
     @task
     @waitFor
