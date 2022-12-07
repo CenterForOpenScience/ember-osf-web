d00c0c0b1c3a0c07c81f25efd878702940db02bd
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2022-12-07 16:13:23 -0500

    [ENG-4196] Reload provider permissions after self-remove (#1729)
    -   Ticket: [ENG-4196]
    -   Feature flag: n/a
    
    ## Purpose
    - Hide "Moderation" button in navbar if you removed yourself as a moderator
    
    ## Summary of Changes
    - Reload provider after removing yourself as a moderator
    - Make `currentUserCanReview` a computed property

--- lib/collections/addon/provider/moderation/moderators/controller.ts
+++ lib/collections/addon/provider/moderation/moderators/controller.ts
@@ -1,13 +1,18 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
+import Store from '@ember-data/store';
+import { waitFor } from '@ember/test-waiters';
+import { task } from 'ember-concurrency';
 
 export default class CollectionsModerationModeratorsController extends Controller {
     @service router!: RouterService;
+    @service store!: Store;
 
-    @action
-    afterSelfRemoval() {
-        this.transitionToRoute('provider.discover', this.model.id);
+    @task
+    @waitFor
+    async refetchProvider(id: string) {
+        await this.store.findRecord('collection-provider', id, { reload: true });
+        this.router.transitionTo('collections.provider.discover', id);
     }
 }
--- lib/registries/addon/branded/moderation/moderators/controller.ts
+++ lib/registries/addon/branded/moderation/moderators/controller.ts
@@ -1,13 +1,17 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
-
+import Store from '@ember-data/store';
+import { waitFor } from '@ember/test-waiters';
+import { task } from 'ember-concurrency';
 export default class BrandedModerationModeratorsController extends Controller {
     @service router!: RouterService;
+    @service store!: Store;
 
-    @action
-    afterSelfRemoval() {
-        this.transitionToRoute('branded.discover', this.model.id);
+    @task
+    @waitFor
+    async refetchProvider(id: string) {
+        await this.store.findRecord('registration-provider', id, { reload: true });
+        this.router.transitionTo('registries.branded.discover', id);
     }
 }
