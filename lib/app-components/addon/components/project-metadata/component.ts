372ce16a103786827e931d7a2b4c06661e6ed795
Futa Ikeda <futa.ikeda@gmail.com>
2022-12-29 12:27:33 -0500

    Prevent users from continuing submission when license is invalid

--- lib/app-components/addon/components/project-metadata/component.ts
+++ lib/app-components/addon/components/project-metadata/component.ts
@@ -4,15 +4,34 @@ import Component from '@ember/component';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { waitFor } from '@ember/test-waiters';
+import { validatePresence } from 'ember-changeset-validations/validators';
 import { task } from 'ember-concurrency';
 import Intl from 'ember-intl/services/intl';
 import Toast from 'ember-toastr/services/toast';
+import { BufferedChangeset } from 'validated-changeset';
 
 import { layout, requiredAction } from 'ember-osf-web/decorators/component';
 import Node from 'ember-osf-web/models/node';
+import { validateNodeLicense } from 'ember-osf-web/packages/registration-schema/validations';
 import Analytics from 'ember-osf-web/services/analytics';
+import buildChangeset from 'ember-osf-web/utils/build-changeset';
 import styles from './styles';
 import template from './template';
+
+const nodeValidations = {
+    title: [
+        validatePresence({ presence: true, ignoreBlank: true, type: 'empty' }),
+    ],
+    description: [
+        validatePresence({ presence: true, ignoreBlank: true, type: 'empty' }),
+    ],
+    license: [
+        validatePresence({ presence: true, ignoreBlank: true, type: 'mustSelect' }),
+    ],
+    nodeLicense: [
+        validateNodeLicense(),
+    ],
+};
 
 @layout(template, styles)
 @tagName('')
@@ -23,14 +42,36 @@ export default class ProjectMetadata extends Component {
     @service toast!: Toast;
 
     node!: Node;
+    changeset!: BufferedChangeset;
 
     @requiredAction continue!: () => void;
+
+    init() {
+        super.init();
+        this.changeset = buildChangeset(this.node, nodeValidations);
+    }
 
     @task
     @waitFor
     async reset() {
         this.node.rollbackAttributes();
         await this.node.reload();
+    }
+
+    @task
+    @waitFor
+    async save() {
+        this.changeset.validate();
+        if (this.changeset.isValid) {
+            try {
+                await this.changeset.save();
+                this.onSave();
+            } catch (e) {
+                this.onError();
+            }
+        } else {
+            this.toast.error(this.intl.t('app_components.project_metadata.save_error'));
+        }
     }
 
     @action
@@ -44,6 +85,7 @@ export default class ProjectMetadata extends Component {
         this.analytics.click('button', 'Collections - Submit - Remove tag');
         this.node.set('tags', this.node.tags.slice().removeAt(index));
     }
+
 
     @action
     onSave() {
