787235811f52d9138ff1f986ed67a0bb6f34552a
Yuhuai Liu <yuhuai@cos.io>
2023-01-05 16:00:41 -0500

    moar

--- lib/osf-components/addon/components/funding-metadata/component.ts
+++ lib/osf-components/addon/components/funding-metadata/component.ts
@@ -3,14 +3,32 @@ import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { BufferedChangeset } from 'validated-changeset';
-import { Funder } from 'ember-osf-web/models/custom-item-metadata-record'
+import { Funder } from 'ember-osf-web/models/custom-item-metadata-record';
 
 interface Args {
     changeset: BufferedChangeset;
 }
 
 export default class FundingMetadata extends Component<Args> {
-    // noop
-    @tracked fundingObjects: Funder[] = [];
+    @tracked funderObjects: Funder[] = [];
+    constructor(owner: unknown, args: Args) {
+        super(owner, args);
+        this.funderObjects = [...this.args.changeset.funders as Funder[]];
+    }
 
+    @action
+    saveToChangeset() {
+        this.args.changeset.set('funders', this.funderObjects);
+    }
+
+    @action
+    addAnother() {
+        this.funderObjects.pushObject({});
+    }
+
+    @action
+    delete(item: Funder) {
+        this.funderObjects.removeObject(item);
+        this.saveToChangeset();
+    }
 }
