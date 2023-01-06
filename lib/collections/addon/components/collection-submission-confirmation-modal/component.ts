44a1790f4378e3fc9bb229d70fcf1756e5701fad
Brian Pilati <brianpilati@cos.io>
2023-01-06 10:03:34 -0700

    Added an optional comment when resubmitting a project that has been removed or rejected by a collection

--- lib/collections/addon/components/collection-submission-confirmation-modal/component.ts
+++ lib/collections/addon/components/collection-submission-confirmation-modal/component.ts
@@ -1,9 +1,12 @@
 import Component from '@ember/component';
+import { tracked } from '@glimmer/tracking';
 
 import { layout } from 'ember-osf-web/decorators/component';
 
+import style from './styles';
 import template from './template';
 
-@layout(template)
+@layout(template, style)
 export default class CollectionSubmissionConfirmationModal extends Component {
+    @tracked removeReason?: string;
 }
