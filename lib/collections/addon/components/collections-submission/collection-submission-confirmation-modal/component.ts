b3e49bbb2f142590c5f5208c6e8a44b03606d451
Brian Pilati <brianpilati@cos.io>
2023-01-09 15:55:04 -0700

    Fixed two of the tests

--- lib/collections/addon/components/collections-submission/collection-submission-confirmation-modal/component.ts
+++ lib/collections/addon/components/collections-submission/collection-submission-confirmation-modal/component.ts
@@ -1,12 +1,7 @@
-import Component from '@ember/component';
+import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
-import { layout } from 'ember-osf-web/decorators/component';
-
-import style from './styles';
-import template from './template';
-
-@layout(template, style)
 export default class CollectionSubmissionConfirmationModal extends Component {
     @tracked removeReason?: string;
 }
+
