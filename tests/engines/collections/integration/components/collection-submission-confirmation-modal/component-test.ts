cd56ee3f646111d7b7cecc4b173abfa32d45ab89
Brian Pilati <brianpilati@cos.io>
2023-01-10 12:13:46 -0700

    Added more bdd comments

--- tests/engines/collections/integration/components/collection-submission-confirmation-modal/component-test.ts
+++ tests/engines/collections/integration/components/collection-submission-confirmation-modal/component-test.ts
@@ -48,7 +48,7 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
                 'The modal body test is wrong',
             );
 
-        // And I assert the body moderated text doesnot appear
+        // And I assert the body moderated text does not appear
         assert.dom('[data-test-collection-submission-confirmation-modal-body]').doesNotHaveTextContaining(
             t('collections.collection_submission_confirmation_modal.body_moderated'),
         );
@@ -71,6 +71,7 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
     });
 
     test('it renders for moderated', async function(assert) {
+        // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
                 @collectionIsModerated=true
@@ -80,15 +81,23 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
                 @cancel={{ this.externalCancelAction }}
             />`,
         );
-        assert.dom('[data-test-collection-submission-confirmation-modal-body]').containsText(
-            t('collections.collection_submission_confirmation_modal.body'),
-        );
-        assert.dom('[data-test-collection-submission-confirmation-modal-moderated-body]').containsText(
-            t('collections.collection_submission_confirmation_modal.body_moderated'),
-        );
+
+        // And I assert the modal body text
+        assert.dom('[data-test-collection-submission-confirmation-modal-body]')
+            .hasText(
+                // eslint-disable-next-line max-len
+                'Once submitted to the collection, the project will be made public. It can later be made private again.',
+                'The modal body test is wrong',
+            );
+
+        // And I assert the moderated body text
+        assert.dom('[data-test-collection-submission-confirmation-modal-moderated-body]')
+            .hasText('A moderator will review your submission before it is included in the collection.',
+                'The moderated text is not correct');
     });
 
     test('Add to collection button calls addToCollection action', async function(assert) {
+        // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
                 @openModal=true
@@ -98,14 +107,21 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
                 @cancel={{ this.externalCancelAction }}
             />`,
         );
+
+        // Then I assert the add to collection button is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-add-button]')
             .hasText('Add to collection', 'The to collection button is wrong');
+
+        // When I click on the add button
         await click('[ data-test-collection-submission-confirmation-modal-add-button]');
+
+        // Then I verify the action was handled
         assert.true(externalSaveClicked);
 
     });
 
     test('Cancel button calls cancel action', async function(assert) {
+        // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
                 @openModal=true
@@ -114,13 +130,21 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
                 @cancel={{ this.externalCancelAction}}
             />`,
         );
+
+        // Then I assert the cancel button text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-cancel-button]')
             .hasText('Cancel', 'The cancel button is wrong');
+
+        // When I click on the cancel button
+
         await click('[data-test-collection-submission-confirmation-modal-cancel-button]');
+
+        // Then I verify the action was handled
         assert.true(externalCancelClicked);
     });
 
     test('Resubmit workflow', async function(assert) {
+        // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
                 @resubmitToCollection={{ this.externalResubmitAction }}
@@ -130,19 +154,31 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
                 @cancel={{ this.externalCancelAction }}
             />`,
         );
+
+        // Then I verify the modal resubmit text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-resubmit]').hasText(
             'This project was removed or rejected from this collection. Do you want to resubmit it?',
             'The text is valid',
         );
+
+        // And I verify the modal reason text area exists
         assert.dom('[data-test-collection-submission-confirmation-modal-reason]')
             .exists('The collection submission confirmation modal reason does not exist.');
+
+        // And I verify the standard modal submit body text does not exist
         assert.dom('[data-test-collection-submission-confirmation-modal-body]').doesNotExist();
+
+        // And I verify the add button does not exist
         assert.dom('[data-test-collection-submission-confirmation-modal-add-button]').doesNotExist();
 
+        // And I verify the resubmit button text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-resubmit-button]')
             .hasText('Resubmit', 'The resubmit button is wrong');
 
+        // When I click on the resubmit button
         await click('[data-test-collection-submission-confirmation-modal-resubmit-button]');
+
+        // Then I verify the action was handled
         assert.true(externalResubmitClicked);
     });
 });
