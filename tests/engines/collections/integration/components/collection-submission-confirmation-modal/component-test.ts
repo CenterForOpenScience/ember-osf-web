eaa30e10858c0baefa4ae2ff2bf3c3e7b8a1d04b
Brian Pilati <brianpilati@cos.io>
2023-01-11 14:03:17 -0700

    Refactored the tests to use Sinon and fixed the test output

--- tests/engines/collections/integration/components/collection-submission-confirmation-modal/component-test.ts
+++ tests/engines/collections/integration/components/collection-submission-confirmation-modal/component-test.ts
@@ -3,27 +3,22 @@ import { hbs } from 'ember-cli-htmlbars';
 import { t, TestContext } from 'ember-intl/test-support';
 import { setupEngineRenderingTest } from 'ember-osf-web/tests/helpers/engines';
 import { module, test } from 'qunit';
+import { SinonSpy }  from 'sinon';
+import sinon from 'sinon';
 
 module('Integration | Component | collection-submission-confirmation-modal', hooks => {
+    interface ThisTestContext extends TestContext {
+        externalResubmitAction: SinonSpy;
+        externalSaveAction: SinonSpy;
+        externalCancelAction: SinonSpy;
+    }
+
     setupEngineRenderingTest(hooks, 'collections');
-    let externalSaveClicked = false;
-    let externalCancelClicked = false;
-    let externalResubmitClicked = false;
 
-    hooks.beforeEach(async function(this: TestContext) {
-        externalSaveClicked = false;
-        externalCancelClicked = false;
-        externalResubmitClicked = false;
-
-        this.set('externalResubmitAction', () => {
-            externalResubmitClicked = true;
-        });
-        this.set('externalSaveAction', () => {
-            externalSaveClicked = true;
-        });
-        this.set('externalCancelAction', () => {
-            externalCancelClicked = true;
-        });
+    hooks.beforeEach(async function(this: ThisTestContext) {
+        this.externalResubmitAction = sinon.fake();
+        this.externalSaveAction = sinon.fake();
+        this.externalCancelAction = sinon.fake();
     });
 
     test('it renders', async function(this: TestContext, assert) {
@@ -38,14 +33,14 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
         );
         // Then I assert the modal header text
         assert.dom('[data-test-collection-submission-confirmation-modal-header]')
-            .hasText('Submit project', 'The modal header is wrong');
+            .hasText('Submit project', 'The modal header is correct');
 
         // And I assert the modal body text
         assert.dom('[data-test-collection-submission-confirmation-modal-body]')
             .hasText(
                 // eslint-disable-next-line max-len
                 'Once submitted to the collection, the project will be made public. It can later be made private again.',
-                'The modal body test is wrong',
+                'The modal body text is correct',
             );
 
         // And I assert the body moderated text does not appear
@@ -55,19 +50,19 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
 
         // And I assert the cancel button text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-cancel-button]')
-            .hasText('Cancel', 'The cancel button text is missing');
+            .hasText('Cancel', 'The cancel button text is correct');
 
         // And I assert the add to collection button is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-add-button]')
-            .hasText('Add to collection', 'The add to collection button is not correct.');
+            .hasText('Add to collection', 'The add to collection button is correct.');
 
         // And I assert he modal resubmit does not exist
         assert.dom('[data-test-collection-submission-confirmation-modal-resubmit]')
-            .doesNotExist('The resubmit text is visible');
+            .doesNotExist('The resubmit text is not visible');
 
         // And The modal reason text does not exit
         assert.dom('[data-test-collection-submission-confirmation-modal-reason]')
-            .doesNotExist('The collection submission confirmation modal reason does exists.');
+            .doesNotExist('The collection submission confirmation modal reason does not exist.');
     });
 
     test('it renders for moderated', async function(assert) {
@@ -87,16 +82,16 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
             .hasText(
                 // eslint-disable-next-line max-len
                 'Once submitted to the collection, the project will be made public. It can later be made private again.',
-                'The modal body test is wrong',
+                'The modal body text is correct',
             );
 
         // And I assert the moderated body text
         assert.dom('[data-test-collection-submission-confirmation-modal-moderated-body]')
             .hasText('A moderator will review your submission before it is included in the collection.',
-                'The moderated text is not correct');
+                'The moderated text is correct');
     });
 
-    test('Add to collection button calls addToCollection action', async function(assert) {
+    test('Add to collection button calls addToCollection action', async function(this: ThisTestContext, assert) {
         // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
@@ -109,17 +104,17 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
 
         // Then I assert the add to collection button is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-add-button]')
-            .hasText('Add to collection', 'The to collection button is wrong');
+            .hasText('Add to collection', 'The add to collection button is correct');
 
         // When I click on the add button
         await click('[ data-test-collection-submission-confirmation-modal-add-button]');
 
         // Then I verify the action was handled
-        assert.true(externalSaveClicked);
+        assert.ok(this.externalSaveAction.calledWith());
 
     });
 
-    test('Cancel button calls cancel action', async function(assert) {
+    test('Cancel button calls cancel action', async function(this: ThisTestContext, assert) {
         // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
@@ -132,18 +127,17 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
 
         // Then I assert the cancel button text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-cancel-button]')
-            .hasText('Cancel', 'The cancel button is wrong');
+            .hasText('Cancel', 'The cancel button is correct');
 
         // When I click on the cancel button
 
         await click('[data-test-collection-submission-confirmation-modal-cancel-button]');
 
         // Then I verify the action was handled
-        assert.true(externalCancelClicked);
+        assert.ok(this.externalCancelAction.calledWith());
     });
 
-    test('Resubmit workflow', async function(assert) {
-
+    test('Resubmit workflow', async function(this: ThisTestContext, assert) {
         // Given the component is rendered
         await render(
             hbs`<CollectionsSubmission::CollectionSubmissionConfirmationModal
@@ -158,12 +152,12 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
         // Then I verify the modal resubmit text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-resubmit]').hasText(
             'This project was removed or rejected from this collection. Do you want to resubmit it?',
-            'The text is valid',
+            'The text is correct',
         );
 
         // And I verify the modal reason text area exists
         assert.dom('[data-test-collection-submission-confirmation-modal-reason]')
-            .exists('The collection submission confirmation modal reason does not exist.');
+            .exists('The collection submission confirmation modal reason does exists.');
 
         // And I verify the standard modal submit body text does not exist
         assert.dom('[data-test-collection-submission-confirmation-modal-body]').doesNotExist();
@@ -173,11 +167,11 @@ module('Integration | Component | collection-submission-confirmation-modal', hoo
 
         // And I verify the resubmit button text is correct
         assert.dom('[data-test-collection-submission-confirmation-modal-resubmit-button]')
-            .hasText('Resubmit', 'The resubmit button is wrong');
+            .hasText('Resubmit', 'The resubmit button is correct');
 
         // When I click on the resubmit button
         await click('[data-test-collection-submission-confirmation-modal-resubmit-button]');
 
-        assert.true(externalResubmitClicked);
+        assert.ok(this.externalResubmitAction.calledWith());
     });
 });
