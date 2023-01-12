c9921ffc5c54fc0a8e869bc12913c0ee37dd4fbc
Yuhuai Liu <yuhuai@cos.io>
2023-01-12 15:02:51 -0500

    finalize

--- lib/osf-components/addon/components/funding-metadata/component.ts
+++ lib/osf-components/addon/components/funding-metadata/component.ts
@@ -5,6 +5,9 @@ import { tracked } from '@glimmer/tracking';
 import { BufferedChangeset } from 'validated-changeset';
 import { Funder } from 'ember-osf-web/models/custom-item-metadata-record';
 import DS from 'ember-data';
+import { restartableTask, timeout } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
+import CrossrefFunderModel from 'ember-osf-web/models/crossref-funder';
 
 interface Args {
     changeset: BufferedChangeset;
@@ -18,13 +21,27 @@ export default class FundingMetadata extends Component<Args> {
         this.funderObjects = [...this.args.changeset.funders as Funder[]];
     }
 
+    @restartableTask
+    @waitFor
+    async searchFunders(name: string) {
+        await timeout(500);
+        const funders = await this.store.query('crossref-funder', {
+            query: name,
+        });
+        return funders;
+    }
+
     @action
     saveToChangeset() {
         this.args.changeset.set('funders', this.funderObjects);
-        // debugger;
-        this.store.query('crossref-funder', {
-            query: 'national',
-        });
+    }
+
+    @action
+    selectFunder(item: Funder, funder: CrossrefFunderModel) {
+        item.funder_identifier = funder.uri;
+        item.funder_identifierType = 'Crossref Funder URI';
+        item.funder_name = funder.name;
+        this.saveToChangeset();
     }
 
     @action
