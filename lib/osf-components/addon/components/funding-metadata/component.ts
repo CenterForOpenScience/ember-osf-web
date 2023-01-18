856d780cf91d66e559678a31039297e212b1dcc0
Yuhuai Liu <yuhuai@cos.io>
2023-01-17 23:45:36 -0500

    validations

--- lib/osf-components/addon/components/funding-metadata/component.ts
+++ lib/osf-components/addon/components/funding-metadata/component.ts
@@ -1,4 +1,4 @@
-import { action } from '@ember/object';
+import { action, notifyPropertyChange } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
@@ -8,17 +8,27 @@ import DS from 'ember-data';
 import { restartableTask, timeout } from 'ember-concurrency';
 import { waitFor } from '@ember/test-waiters';
 import CrossrefFunderModel from 'ember-osf-web/models/crossref-funder';
+import IntlService from 'ember-intl/services/intl';
 
 interface Args {
     changeset: BufferedChangeset;
 }
 
+interface FunderObjects extends Funder {
+    errors: string[];
+}
+
 export default class FundingMetadata extends Component<Args> {
     @service store!: DS.Store;
-    @tracked funderObjects: Funder[] = [];
+    @service intl!: IntlService;
+    @tracked funderObjects: FunderObjects[] = [];
     constructor(owner: unknown, args: Args) {
         super(owner, args);
-        this.funderObjects = [...this.args.changeset.funders as Funder[]];
+        this.funderObjects = (this.args.changeset.funders as Funder[]).map((item: Funder) => ({...item, errors : []}));
+    }
+
+    get isValid() {
+        return this.funderObjects.every(item => item.errors.length === 0);
     }
 
     @restartableTask
@@ -32,25 +42,52 @@ export default class FundingMetadata extends Component<Args> {
     }
 
     @action
-    saveToChangeset() {
-        this.args.changeset.set('funders', this.funderObjects);
+    validateFunderObjects() {
+        for (const item of this.funderObjects) {
+            if (item.award_number || item.award_title || item.award_uri || item.funder_name) {
+                item.errors.removeObject(this.intl.t('osf-components.funding-metadata.error'));
+            } else if (item.errors.length === 0) {
+                item.errors.pushObject(this.intl.t('osf-components.funding-metadata.error'));
+            }
+        }
+        notifyPropertyChange(this, 'funderObjects');
+        if (!this.isValid) {
+            return Promise.reject();
+        }
+        return true;
     }
 
     @action
-    selectFunder(item: Funder, funder: CrossrefFunderModel) {
+    saveToChangeset() {
+        this.validateFunderObjects();
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        this.args.changeset.set('funders', this.funderObjects.map(({ errors, ...props }) => props));
+    }
+
+    @action
+    selectFunder(item: FunderObjects, funder: CrossrefFunderModel) {
         item.funder_identifier = funder.uri;
         item.funder_identifierType = 'Crossref Funder URI';
         item.funder_name = funder.name;
         this.saveToChangeset();
+        this.validateFunderObjects();
     }
 
     @action
     addAnother() {
-        this.funderObjects.pushObject({});
+        this.funderObjects.pushObject({
+            funder_name: '',
+            funder_identifier: '',
+            funder_identifierType: '',
+            award_number: '',
+            award_title: '',
+            award_uri: '',
+            errors: [],
+        });
     }
 
     @action
-    delete(item: Funder) {
+    delete(item: FunderObjects) {
         this.funderObjects.removeObject(item);
         this.saveToChangeset();
     }
