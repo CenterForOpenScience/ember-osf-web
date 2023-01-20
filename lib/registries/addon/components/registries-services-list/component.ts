6d4ef4a5dfbb289cebc83cc48581569a5f60c590
Brian Pilati <brianpilati@cos.io>
2023-01-20 11:47:40 -0700

    Removed the analytics service

--- lib/registries/addon/components/registries-services-list/component.ts
+++ lib/registries/addon/components/registries-services-list/component.ts
@@ -1,10 +1,6 @@
 import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
 import config from 'ember-get-config';
 
-import Analytics from 'ember-osf-web/services/analytics';
-
 export default class RegistriesServiceList extends Component {
-    @service analytics!: Analytics;
     assetsPrefix = config.assetsPrefix;
 }
