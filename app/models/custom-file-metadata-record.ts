76be8f3abb1ee850c3ce0b81ec04f19bcd1d54e6
Ashley Robinson <ashley@cos.io>
2023-03-09 20:36:36 -0500

    Added JSON-LD Google Structured Data to the head element.

--- app/models/custom-file-metadata-record.ts
+++ app/models/custom-file-metadata-record.ts
@@ -1,11 +1,25 @@
 import { attr } from '@ember-data/model';
 import CustomMetadataModel from 'ember-osf-web/models/custom-metadata';
 
+export interface Funder {
+    funder_name?: string;
+    funder_identifier?: string;
+    funder_identifier_type?: string;
+    award_number?: string;
+    award_uri?: string;
+    award_title?: string;
+    funder_type?: string;
+}
 
 export default class CustomFileMetadataRecordModel extends CustomMetadataModel {
-    @attr('fixstring') title?: string;
+    @attr('fixstring') creatorType?: string;
+    @attr('fixstring') dataType?: string;
     @attr('fixstring') description?: string;
+    @attr('fixstring') funderName?: Funder['funder_name'];
     @attr('fixstring') language?: string;
+    @attr('fixstring') modifiedDate?: string;
+    @attr('fixstring') publishedDate?: string;
+    @attr('fixstring') title?: string;
 }
 
 declare module 'ember-data/types/registries/model' {
