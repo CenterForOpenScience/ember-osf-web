a942d65b05cca3237649fb9108644fa649f70a12
Abram Booth <aaxelb@users.noreply.github.com>
2023-03-27 10:46:50 -0400

    use osf metrics for node analytics (#1829)
    ## Purpose
    keep the node analytics page running (without keen)
    
    ## Summary of Changes
    * load data from osf-metrics "node analytics" api
    * remove keen js dependencies
    * add c3 dependency (for compatibility; used by keen-dataviz)

--- mirage/config.ts
+++ mirage/config.ts
@@ -22,7 +22,7 @@ import { createFork, createRegistrationFork } from './views/fork';
 import { guidDetail } from './views/guid';
 import { identifierCreate } from './views/identifier';
 import { summaryMetrics } from './views/institution';
-import { postCountedUsage } from './views/metrics';
+import { postCountedUsage, getNodeAnalytics } from './views/metrics';
 import { addCollectionModerator, addRegistrationModerator } from './views/moderator';
 import { createNode, storageStatus } from './views/node';
 import { osfNestedResource, osfResource, osfToManyRelationship } from './views/osf-resource';
@@ -394,4 +394,7 @@ export default function(this: Server) {
 
     // usage reporting
     this.post('/metrics/events/counted_usage/', postCountedUsage);
+
+    // node analytics
+    this.get('/metrics/query/node_analytics/:nodeID/:timespan', getNodeAnalytics);
 }
