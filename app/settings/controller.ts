e4d5a17e19356da7d333dd4415ac7e100c1f4084
Brian J. Geiger <bgeiger@cos.io>
2023-04-24 10:37:49 -0400

    [ENG-4416] Remove BsCollapse (#1848)
    ## Purpose
    
    Stop using the BsCollapse component in the codebase.
    
    ## Summary of Changes
    
    1. Remove BsCollapse from osf-navbar
    2. Remove BsCollapse from settings page
    3. Remove BsCollapse from Collections branded navbar
    4. Remove BsCollapse from registration's node navbar
    5. Fix styling on login button
    6. Fix mirage factory that prevented people from using ember-osf-web when not logged in
    
    ## Side Effects
    
    The tablet size is just a tiny bit smaller now for navbars.

--- app/settings/controller.ts
+++ app/settings/controller.ts
@@ -3,10 +3,20 @@ import { inject as service } from '@ember/service';
 
 import Analytics from 'ember-osf-web/services/analytics';
 import CurrentUser from 'ember-osf-web/services/current-user';
+import Media from 'ember-responsive';
+import { tracked } from 'tracked-built-ins';
 
 export default class SettingsController extends Controller {
     @service analytics!: Analytics;
     @service currentUser!: CurrentUser;
+    @service media!: Media;
 
-    navCollapsed = true;
+    @tracked navCollapsed = true;
+
+    get shouldShowNavLinks() {
+        if (this.media.isMobile || this.media.isTablet){
+            return !this.navCollapsed;
+        }
+        return true;
+    }
 }
