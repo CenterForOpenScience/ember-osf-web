f69ae5902c1d751e6eab1238b8bc2cf50132bd5a
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-04-24 14:29:04 -0400

    [ENG-4492] Add data-test selectors to registration components and links page (#1851)
    * Add data-test selectors to registration components and links page
    
    * Fix tests

---
+++ tests/engines/registries/acceptance/overview/links-page-test.ts
@@ -0,0 +1,40 @@
+import { setupMirage } from 'ember-cli-mirage/test-support';
+import { module, test } from 'qunit';
+
+import { visit } from 'ember-osf-web/tests/helpers';
+import { setupEngineApplicationTest } from 'ember-osf-web/tests/helpers/engines';
+
+module('Registries | Acceptance | overview.links', hooks => {
+    setupEngineApplicationTest(hooks, 'registries');
+    setupMirage(hooks);
+
+    test('With linked node and no linked registration', async assert => {
+        const registration = server.create('registration');
+        const linkedNode = server.create('node', {
+            title: 'Linked node',
+        });
+
+        registration.update({
+            linkedNodes: [linkedNode],
+        });
+        await visit(`/${registration.id}/links`);
+        assert.dom(`[data-test-node-title="${linkedNode.id}"]`).containsText('Linked node', 'Linked node is shown');
+        assert.dom('[data-test-no-linked-registrations]').exists('No linked registrations message is shown');
+    });
+
+    test('With linked registration and no linked node', async assert => {
+        const registration = server.create('registration');
+        const linkedRegistration = server.create('registration', {
+            title: 'Linked registration',
+        });
+
+        registration.update({
+            linkedRegistrations: [linkedRegistration],
+        });
+        await visit(`/${registration.id}/links`);
+
+        assert.dom(`[data-test-node-title="${linkedRegistration.id}"]`)
+            .containsText('Linked registration', 'Linked registration is shown');
+        assert.dom('[data-test-no-linked-nodes]').exists('No linked nodes message is shown');
+    });
+});
