ce40fcaf52b88cd5c02141ac7a7b156449e1aa3e
Futa Ikeda <futa.ikeda@gmail.com>
2023-04-28 16:34:09 -0400

    Add label getter to metadata-record

--- app/models/metadata-record.ts
+++ app/models/metadata-record.ts
@@ -1,4 +1,6 @@
+import { inject as service } from '@ember/service';
 import Model, { AsyncHasMany, attr, hasMany } from '@ember-data/model';
+import IntlService from 'ember-intl/services/intl';
 
 export interface LanguageText {
     '@language': string;
@@ -6,12 +8,30 @@ export interface LanguageText {
 }
 
 export default class MetadataRecordModel extends Model {
+    @service intl!: IntlService;
+
     @attr('array') resourceType!: string[];
     @attr('array') resourceIdentifier!: string[];
     @attr('object') resourceMetadata!: any;
 
     @hasMany('metadata-record', { inverse: null })
     relatedRecordSet!: AsyncHasMany<MetadataRecordModel> & MetadataRecordModel[];
+
+    get label() {
+        const { resourceMetadata } = this;
+        const preferredLanguage = this.intl.locale;
+        const labels = resourceMetadata?.label;
+        if (labels) {
+            const languageAppropriateLabel = labels.filter((label: any) => label['@language'] === preferredLanguage);
+            // give the locale appropriate label if it exists, otherwise give the first label
+            if (languageAppropriateLabel.length > 0) {
+                return labels.filter((label: any) => label['@language'] === preferredLanguage)[0]['@value'];
+            } else if (labels.length > 0) {
+                return labels[0]['@value'];
+            }
+        }
+        return 'No label found';
+    }
 }
 
 declare module 'ember-data/types/registries/model' {
