b6cb9c4d9cb17f8c934edaa52d4ab59bd72bf6d7
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-05-15 11:03:09 -0400

    [ENG-4465] Left panel facets manager (#1858)
    -   Ticket: [ENG-4465] [ENG-4466]
    -   Feature flag: n/a
    
    - Add logic to search page controller to handle active filters and list of filterable properties
    - Add a component to handle fetching values in a filterable properties in the search page
    
    - Add a `filter-facet` component
      - takes care of fetching filterable property values
      - `See more` modal

--- app/models/metadata-record.ts
+++ app/models/metadata-record.ts
@@ -1,4 +1,6 @@
+import { inject as service } from '@ember/service';
 import Model, { AsyncHasMany, attr, hasMany } from '@ember-data/model';
+import IntlService from 'ember-intl/services/intl';
 
 export interface LanguageText {
     '@language': string;
@@ -6,12 +8,46 @@ export interface LanguageText {
 }
 
 export default class MetadataRecordModel extends Model {
+    @service intl!: IntlService;
+
     @attr('array') resourceType!: string[];
     @attr('array') resourceIdentifier!: string[];
     @attr('object') resourceMetadata!: any;
 
     @hasMany('metadata-record', { inverse: null })
     relatedRecordSet!: AsyncHasMany<MetadataRecordModel> & MetadataRecordModel[];
+
+    get label(): string {
+        const { resourceMetadata } = this;
+        const preferredLanguage = this.intl.locale;
+        const labels = resourceMetadata?.label;
+        if (labels) {
+            const languageAppropriateLabel = labels.filter((label: any) => label['@language'] === preferredLanguage);
+            // give the locale appropriate label if it exists, otherwise give the first label
+            if (languageAppropriateLabel.length > 0) {
+                return labels.filter((label: any) => label['@language'] === preferredLanguage)[0]['@value'];
+            } else if (labels.length > 0) {
+                return labels[0]['@value'];
+            }
+        }
+        return this.intl.t('search.metadata-record.no-label');
+    }
+
+    get title(): string {
+        const { resourceMetadata } = this;
+        const preferredLanguage = this.intl.locale;
+        const titles = resourceMetadata?.title;
+        if (titles) {
+            const languageAppropriateTitle = titles.filter((title: any) => title['@language'] === preferredLanguage);
+            // give the locale appropriate title if it exists, otherwise give the first title
+            if (languageAppropriateTitle.length > 0) {
+                return titles.filter((title: any) => title['@language'] === preferredLanguage)[0]['@value'];
+            } else if (titles.length > 0) {
+                return titles[0]['@value'];
+            }
+        }
+        return this.intl.t('search.metadata-record.no-title');
+    }
 }
 
 declare module 'ember-data/types/registries/model' {
--- app/search/route.ts
+++ app/search/route.ts
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
 
+import SearchController from './controller';
+
 export default class Search extends Route {
     buildRouteInfoMetadata() {
         return {
