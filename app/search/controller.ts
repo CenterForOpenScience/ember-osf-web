73cf0cb6b2b8bdfd9964962c5f88b624d2d8b7ef
Futa Ikeda <futa.ikeda@gmail.com>
2023-05-15 14:49:22 -0400

    Add object filter and sort dropdown to search

--- app/search/controller.ts
+++ app/search/controller.ts
@@ -7,29 +7,86 @@ import { waitFor } from '@ember/test-waiters';
 import { tracked } from '@glimmer/tracking';
 import { task, timeout } from 'ember-concurrency';
 import { taskFor } from 'ember-concurrency-ts';
+import Intl from 'ember-intl/services/intl';
 import Media from 'ember-responsive';
 
 import MetadataPropertySearchModel from 'ember-osf-web/models/metadata-property-search';
+import SearchResultModel from 'ember-osf-web/models/search-result';
 export interface Filter {
     property: string;
+    value: string;
+}
+
+interface ResourceTypeOption {
+    display: string;
+    value: string;
+}
+
+interface SortOption {
+    display: string;
     value: string;
 }
 
 const searchDebounceTime = 100;
 
 export default class SearchController extends Controller {
+    @service intl!: Intl;
     @service store!: Store;
     @service media!: Media;
     @service toast!: Toastr;
 
-    queryParams = ['q', 'page', 'sort'];
+    queryParams = ['q', 'page', 'sort', 'resourceType'];
     @tracked q?: string = '';
     @tracked seachBoxText?: string = '';
 
     @tracked page?: number = 1;
-    @tracked sort?: string = '-relevance';
+
+    // Resource type
+    resourceTypeOptions: ResourceTypeOption[] = [
+        { display: this.intl.t('search.resource-type.all'), value: 'All' },
+        { display: this.intl.t('search.resource-type.projects'), value: 'Projects' },
+        { display: this.intl.t('search.resource-type.registrations'), value: 'Registrations' },
+        { display: this.intl.t('search.resource-type.preprints'), value: 'Preprints' },
+        { display: this.intl.t('search.resource-type.files'), value: 'Files' },
+        { display: this.intl.t('search.resource-type.users'), value: 'Users' },
+    ];
+
+    @tracked resourceType = this.resourceTypeOptions[0].value;
+
+    get selectedResourceTypeOption() {
+        return this.resourceTypeOptions.find(option => option.value === this.resourceType);
+    }
+
+    @action
+    updateResourceType(resourceTypeOption: ResourceTypeOption) {
+        this.resourceType = resourceTypeOption.value;
+        taskFor(this.search).perform();
+    }
+
+    // Sort
+    sortOptions: SortOption[] = [
+        { display: this.intl.t('search.sort.relevance'), value: '-relevance' },
+        { display: this.intl.t('search.sort.created-date-descending'), value: '-date_created' },
+        { display: this.intl.t('search.sort.created-date-ascending'), value: 'date_created' },
+        { display: this.intl.t('search.sort.modified-date-descending'), value: '-date_modified' },
+        { display: this.intl.t('search.sort.modified-date-ascending'), value: 'date_modified' },
+    ];
+
+    @tracked sort: string = this.sortOptions[0].value;
+
+    get selectedSortOption() {
+        return this.sortOptions.find(option => option.value === this.sort);// || this.sortOptions[0];
+    }
+
+    @action
+    updateSort(sortOption: SortOption) {
+        this.sort = sortOption.value;
+        taskFor(this.search).perform();
+    }
+
     @tracked activeFilters = A<Filter>([]);
 
+    @tracked searchResults?: SearchResultModel[];
     @tracked propertySearch?: MetadataPropertySearchModel;
 
     get showSidePanelToggle() {
@@ -76,11 +133,12 @@ export default class SearchController extends Controller {
     @waitFor
     async search() {
         try {
-            const { q, page, sort, activeFilters } = this;
+            const { q, page, sort, activeFilters, resourceType } = this;
             const filterQueryObject = activeFilters.reduce((acc, filter) => {
                 acc[filter.property] = filter.value;
                 return acc;
             }, {} as { [key: string]: string });
+            filterQueryObject['resourceType'] = resourceType;
             const searchResult = await this.store.queryRecord('metadata-record-search', {
                 q,
                 page,
@@ -88,7 +146,8 @@ export default class SearchController extends Controller {
                 filter: filterQueryObject,
             });
 
-            this.propertySearch = searchResult.relatedPropertySearch;
+            this.propertySearch = await searchResult.relatedPropertySearch;
+            this.searchResults =  searchResult.searchResultPage.toArray();
         } catch (e) {
             this.toast.error(e);
         }
