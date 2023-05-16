b6d88068816d9acbf8effa28cb74fd207a8f5989
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-05-16 12:12:35 -0400

    Add route analytics metadata (#1865)

--- app/search/route.ts
+++ app/search/route.ts
@@ -3,6 +3,14 @@ import Route from '@ember/routing/route';
 import SearchController from './controller';
 
 export default class Search extends Route {
+    buildRouteInfoMetadata() {
+        return {
+            osfMetrics: {
+                isSearch: true,
+            },
+        };
+    }
+
     setupController(controller: SearchController, _model: any, _transition: any) {
         super.setupController(controller, _model, _transition);
         controller.ingestQueryParams();
