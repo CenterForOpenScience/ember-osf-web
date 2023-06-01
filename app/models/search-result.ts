fe9f2befbc7f27adb2e52c0d2e5b85793b75d0ba
Yuhuai Liu <yuhuai@cos.io>
2023-05-31 22:53:00 -0400

    prelim

--- app/models/search-result.ts
+++ app/models/search-result.ts
@@ -1,4 +1,4 @@
-import Model, { AsyncBelongsTo, attr, belongsTo } from '@ember-data/model';
+import Model, { attr, belongsTo } from '@ember-data/model';
 
 import IndexCardModel from './index-card';
 
@@ -19,7 +19,45 @@ export default class SearchResultModel extends Model {
     @attr('number') recordResultCount!: number;
 
     @belongsTo('index-card', { inverse: null })
-    indexCard!: AsyncBelongsTo<IndexCardModel> | IndexCardModel;
+    indexCard!: IndexCardModel;
+
+    // TODO: double check how matchEvidence works
+    get context() {
+        return this.matchEvidence.reduce(
+            (acc, current) => acc.concat(
+                `${current.propertyPath}:
+                ${current['@type'] === 'TextMatchEvidence' ? current.matchingHighlight : current.matchingIri}`,
+            ),
+            '',
+        );
+    }
+
+    get displayTitle() {
+        if (this.indexCard.resourceType.includes('foaf:person')) {
+            return this.indexCard.resourceMetadata['foaf:name'];
+        } else if (this.indexCard.resourceType.includes('osf:File')) {
+            return this.indexCard.resourceMetadata['osf:fileName'];
+        }
+        return this.indexCard.resourceMetadata['dcterms:title'];
+    }
+
+    // returns list of contributors for osf objects
+    // returns list of affiliated institutions for osf users
+    get affiliatedEntities() {
+        if (this.indexCard.resourceType.includes('foaf:person')) {
+            // return something
+        } else {
+            // return something else
+        }
+    }
+
+    get dateFields() {
+        if (this.indexCard.resourceType.includes('foaf:person')) {
+            return [];
+        } else if (this.indexCard.resourceType) {
+            return [];
+        }
+    }
 }
 
 declare module 'ember-data/types/registries/model' {
