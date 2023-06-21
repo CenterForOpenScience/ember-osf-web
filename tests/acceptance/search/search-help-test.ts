e87ec377f19b4e1c520fc399934c5692b1702bbb
Ashley Robinson <ashley@cos.io>
2023-06-21 10:19:07 -0400

    Removed translation strings from test file.

--- tests/acceptance/search/search-help-test.ts
+++ tests/acceptance/search/search-help-test.ts
@@ -1,31 +1,12 @@
 import { click, currentURL, visit } from '@ember/test-helpers';
 import { setupMirage } from 'ember-cli-mirage/test-support';
 import { module, test } from 'qunit';
-import { setupIntl } from 'ember-intl/test-support';
 import { setupOSFApplicationTest } from 'ember-osf-web/tests/helpers';
 import { EnginesIntlTestContext } from 'ember-engines/test-support';
 
 module('Integration | Component | Search help', hooks => {
     setupOSFApplicationTest(hooks);
     setupMirage(hooks);
-    setupIntl(hooks, {
-        search_help_test: {
-            heading_1_test: 'Improved OSF Search',
-            heading_2_test: 'OSF Smart Facets',
-            heading_3_test: 'Add Metadata',
-            body_1_test: 'Enter any term in the search box and filter by specific object types.',
-            body_2_test: `Narrow the source, discipline, and more. For example, 
-                find content supported by a specific funder or view only datasets.`,
-            body_3_test: `Remember to add metadata and resources to your own work on OSF to
-                 make it more discoverable!`,
-            skip: 'Skip',
-            next: 'Next',
-            done: 'Done',
-            enumeration_1_test: '1 of 3',
-            enumeration_2_test: '2 of 3',
-            enumeration_3_test: '3 of 3',
-        },
-    });
 
     test('ember popover renders', async function(this: EnginesIntlTestContext, assert) {
         await visit('/search');
@@ -39,41 +20,46 @@ module('Integration | Component | Search help', hooks => {
         assert.dom('[data-test-search-help-1]').exists();
         // verify skip button present
         assert.dom('[data-test-help-skip-1]').exists();
-        assert.dom('[data-test-help-skip-1]').hasText(this.intl.t('search_help_test.skip'));
+        assert.dom('[data-test-help-skip-1]').hasText('Skip');
         // verify next button works
         assert.dom('[data-test-help-next-1]').exists();
-        assert.dom('[data-test-help-next-1]').hasText(this.intl.t('search_help_test.next'));
+        assert.dom('[data-test-help-next-1]').hasText('Next');
         // verify first popover content
         assert.dom('[data-test-help-heading-1]').exists();
-        assert.dom('[data-test-help-heading-1]').hasText(this.intl.t('search_help_test.heading_1_test'));
+        assert.dom('[data-test-help-heading-1]').hasText('Improved OSF Search');
         assert.dom('[data-test-help-body-1]').exists();
-        assert.dom('[data-test-help-body-1]').hasText(this.intl.t('search_help_test.body_1_test'));
+        assert.dom('[data-test-help-body-1]').hasText(`Enter any term in the search box 
+            and filter by specific object types.`);
         assert.dom('[data-test-help-enumeration-1]').exists();
-        assert.dom('[data-test-help-enumeration-1]').hasText(this.intl.t('search_help_test.enumeration_1_test'));
+        assert.dom('[data-test-help-enumeration-1]').hasText('1 of 3');
 
         // verify second popover displays
         await click('[data-test-help-next-1]');
         assert.dom('[data-test-search-help-2]').exists();
         // verify second popover content
         assert.dom('[data-test-help-heading-2]').exists();
-        assert.dom('[data-test-help-heading-2]').hasText(this.intl.t('search_help_test.heading_2_test'));
+        assert.dom('[data-test-help-heading-2]').hasText('OSF Smart Facets');
         assert.dom('[data-test-help-body-2]').exists();
-        assert.dom('[data-test-help-body-2]').hasText(this.intl.t('search_help_test.body_2_test'));
+        assert.dom('[data-test-help-body-2]').hasText(`Narrow the source, discipline, and more. 
+            For example, find content supported by a specific funder or view only datasets.`);
         assert.dom('[data-test-help-enumeration-2]').exists();
-        assert.dom('[data-test-help-enumeration-2]').hasText(this.intl.t('search_help_test.enumeration_2_test'));
+        assert.dom('[data-test-help-enumeration-2]').hasText('2 of 3');
 
         // verify third popover displays
         await click('[data-test-help-next-2]');
         assert.dom('[data-test-search-help-3]').exists();
         // verify third popover content
         assert.dom('[data-test-help-heading-3]').exists();
-        assert.dom('[data-test-help-heading-3]').hasText(this.intl.t('search_help_test.heading_3_test'));
+        assert.dom('[data-test-help-heading-3]').hasText('Add Metadata');
         assert.dom('[data-test-help-body-3]').exists();
-        assert.dom('[data-test-help-body-3]').hasText(this.intl.t('search_help_test.body_3_test'));
+        assert.dom('[data-test-help-body-3]').hasText(`Remember to add metadata and resources 
+            to your own work on OSF to make it more discoverable!`);
         assert.dom('[data-test-help-enumeration-3]').exists();
-        assert.dom('[data-test-help-enumeration-3]').hasText(this.intl.t('search_help_test.enumeration_3_test'));
+        assert.dom('[data-test-help-enumeration-3]').hasText('3 of 3');
 
         // verify popover closes
+        assert.dom('[data-test-help-done]').exists();
+        assert.dom('[data-test-help-done]').hasText('Done');
         await click('[data-test-help-done]');
         assert.dom('[data-test-search-help-1]').isNotVisible();
     });
