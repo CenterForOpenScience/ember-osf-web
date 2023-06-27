0de159bdb0c4ac18c155154c2bef40680e5bdd59
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-06-27 11:19:34 -0400

    [ENG-4568] Componentize search page (#1886)
    -   Ticket: [ENG-4568]
    -   Feature flag: n/a
    
    - Componentize search page for reuse in branded pages
    
    - Move logic and templating from search page route to `search-page` component
    - No logic for branding and default query-params yet in this PR

--- app/search/route.ts
+++ app/search/route.ts
@@ -1,7 +1,5 @@
 import Route from '@ember/routing/route';
 
-import SearchController from './controller';
-
 export default class Search extends Route {
     buildRouteInfoMetadata() {
         return {
