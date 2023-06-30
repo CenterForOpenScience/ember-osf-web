e74d57d412cba818565940a30520ce360edc0b01
Futa Ikeda <futa.ikeda@gmail.com>
2023-06-30 11:22:40 -0400

    Remove unused services from search controller

--- app/search/controller.ts
+++ app/search/controller.ts
@@ -1,16 +1,11 @@
-import Store from '@ember-data/store';
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
-import Intl from 'ember-intl/services/intl';
 import Media from 'ember-responsive';
 
 export default class SearchController extends Controller {
-    @service intl!: Intl;
-    @service store!: Store;
     @service media!: Media;
-    @service toast!: Toastr;
 
     @tracked q?: string = '';
     @tracked sort?: string =  '-relevance';
