4add18a806ffb0de53549bd211b12b32bbd3cd59
Futa Ikeda <futa.ikeda@gmail.com>
2023-07-05 16:26:06 -0400

    Error handling and theme resetting

--- app/preprints/discover/route.ts
+++ app/preprints/discover/route.ts
@@ -1,5 +1,6 @@
 import Store from '@ember-data/store';
 import Route from '@ember/routing/route';
+import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 
 import Theme from 'ember-osf-web/services/theme';
@@ -7,6 +8,7 @@ import Theme from 'ember-osf-web/services/theme';
 export default class PreprintDiscoverRoute extends Route {
     @service store!: Store;
     @service theme!: Theme;
+    @service router!: RouterService;
 
     buildRouteInfoMetadata() {
         return {
@@ -18,9 +20,18 @@ export default class PreprintDiscoverRoute extends Route {
     }
 
     async model(args: any) {
-        // Error handling?
-        this.theme.providerType = 'preprint';
-        this.theme.id = args.provider_id;
-        return await this.store.findRecord('preprint-provider', args.provider_id);
+        try {
+            const provider = await this.store.findRecord('preprint-provider', args.provider_id);
+            this.theme.providerType = 'preprint';
+            this.theme.id = args.provider_id;
+            return provider;
+        } catch (e) {
+            this.router.transitionTo('not-found', `preprints/${args.provider_id}/discover`);
+            return null;
+        }
+    }
+
+    deactivate() {
+        this.theme.reset();
     }
 }
