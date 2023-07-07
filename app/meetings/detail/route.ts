7cd4dfd5702b2438114191da4b7dfa64f6979ac1
Brian J. Geiger <bgeiger@pobox.com>
2023-07-07 08:52:36 -0400

    Fix some problems with the upgrade

--- app/meetings/detail/route.ts
+++ app/meetings/detail/route.ts
@@ -2,11 +2,13 @@ import Route from '@ember/routing/route';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 import { waitFor } from '@ember/test-waiters';
+import Store from '@ember-data/store';
 import { task } from 'ember-concurrency';
 import { taskFor } from 'ember-concurrency-ts';
 
 export default class MeetingsDetail extends Route {
     @service router!: RouterService;
+    @service store!: Store;
 
     @task
     @waitFor
