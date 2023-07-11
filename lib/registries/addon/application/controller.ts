dc7b4bf01519987ce7b4e63cb9edba1e5e49d112
Yuhuai Liu <yuhuai@cos.io>
2023-07-11 15:22:35 -0400

    remove unused action and variable on registries application route

--- lib/registries/addon/application/controller.ts
+++ lib/registries/addon/application/controller.ts
@@ -1,24 +1,9 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 import Features from 'ember-feature-flags/services/features';
 
-import { OSFService } from 'osf-components/components/osf-navbar/component';
-
 export default class Application extends Controller {
     @service features!: Features;
     @service router!: RouterService;
-
-    activeService = OSFService.REGISTRIES;
-
-    @action
-    search(query: string) {
-        this.router.transitionTo('search', {
-            queryParams: {
-                q: query,
-                resourceType: 'osf:Registration',
-            },
-        });
-    }
 }
