333ed1340b914113a56ce630827431099b82ec0e
Brian J. Geiger <bgeiger@pobox.com>
2023-07-25 15:08:49 -0400

    Add our very own copy-button

---
+++ lib/osf-components/addon/components/copy-button/component.ts
@@ -0,0 +1,33 @@
+import { assert } from '@ember/debug';
+import Component from '@glimmer/component';
+import { task } from 'ember-concurrency';
+
+interface Args {
+    success?: () => void;
+    error?: () => void;
+    disabled: Boolean;
+    clipboardText: string;
+}
+
+export default class CopyButton extends Component<Args> {
+    disabled = false;
+
+    @task
+    async copy() {
+        assert('clipboardText is a required parameter.', Boolean(this.args.clipboardText));
+        if(!this.args.disabled){
+            try {
+                await navigator.clipboard.writeText(this.args.clipboardText);
+                if(this.args.success) {
+                    this.args.success();
+                }
+            } catch(e) {
+                if(this.args.error) {
+                    this.args.error();
+                } else {
+                    throw(e);
+                }
+            }
+        }
+    }
+}
