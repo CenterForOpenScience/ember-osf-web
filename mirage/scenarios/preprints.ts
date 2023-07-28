04050d2710e0c37d88138541e3909c7ccddf7a69
Futa Ikeda <futa.ikeda@gmail.com>
2023-07-28 12:19:31 -0400

    Use preprints brand for navbar color and hero background

--- mirage/scenarios/preprints.ts
+++ mirage/scenarios/preprints.ts
@@ -8,7 +8,11 @@ export function preprintsScenario(
     currentUser: ModelInstance<User>,
 ) {
     const thesisCommons = server.schema.preprintProviders.find('thesiscommons') as ModelInstance<PreprintProvider>;
-    const brand = server.create('brand');
+    const brand = server.create('brand', {
+        primaryColor: '#821e1e',
+        secondaryColor: '#94918e',
+        heroBackgroundImage: 'https://singlecolorimage.com/get/94918e/1000x1000',
+    });
     const currentUserModerator = server.create('moderator',
         { id: currentUser.id, user: currentUser, provider: thesisCommons }, 'asAdmin');
 
