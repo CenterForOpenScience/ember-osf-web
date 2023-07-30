bd5cfdd8f8ff180d115007ec49ecc53b3743b2cf
Yuhuai Liu <yuhuai@cos.io>
2023-07-30 03:12:14 -0400

    some moar

--- lib/osf-components/addon/components/search-result-card/component.ts
+++ lib/osf-components/addon/components/search-result-card/component.ts
@@ -1,25 +1,12 @@
 import { action } from '@ember/object';
-import Component from '@glimmer/component'
-import { tracked } from '@glimmer/tracking'
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 import SearchResultModel from 'ember-osf-web/models/search-result';
 import { inject as service } from '@ember/service';
 import Intl from 'ember-intl/services/intl';
 
 interface Args {
     result: SearchResultModel;
-    // displayName: string;
-    // nameFields: string[];
-    // dateFields: string[];
-    // context: string;
-    // secondaryMetadata: any;
-}
-
-const ResourceTypeCardLabelMap = {
-    'osf:Registration': 'registration',
-    'osf:RegistrationComponent': 'registration_component',
-    'osf:Project': 'project',
-    'osf:ProjectComponent': 'project_component',
-    'osf:File': 'file',
 }
 
 export default class SearchResultCard extends Component<Args> {
