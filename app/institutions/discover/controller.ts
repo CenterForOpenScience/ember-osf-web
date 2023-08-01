691326b47c5fbd89c5800923ad61eea6cf9dd717
Ashley Robinson <82047646+chth0n1x@users.noreply.github.com>
2023-08-01 13:43:08 -0400

    [ENG-4575]  Institution dashboard page re-write (#1902)
    -   Ticket: https://openscience.atlassian.net/browse/ENG-4575
    -   Git branch: feature/search-institutions-rewrite
    
    ## Purpose
    
    The purpose of these changes is to implement the new search page component for the Institutions Discover page.
    
    ## Summary of Changes
    
    -Used the new search-page component to re-implement the Institutions dashboard/discover page
    
    -Added institution header with description and banner for desktop, logo and description for mobile
    
    -Added logic for institution colors and default OSF colors when none provided
    
    -Added an affiliated institution filter applied by default to SHARE queries

--- app/institutions/discover/controller.ts
+++ app/institutions/discover/controller.ts
@@ -1,7 +1,30 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import CurrentUser from 'ember-osf-web/services/current-user';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import pathJoin from 'ember-osf-web/utils/path-join';
+import config from 'ember-get-config';
 
 export default class InstitutionDiscoverController extends Controller {
     @service currentUser!: CurrentUser;
+
+    @tracked q?: string = '';
+    @tracked sort?: string =  '-relevance';
+    @tracked resourceType?: string = 'All';
+
+    queryParams = ['q', 'page', 'sort', 'resourceType'];
+
+    get defaultQueryOptions() {
+        return {
+            publisher: pathJoin(config.OSF.url, 'institutions', this.model.id),
+        };
+    }
+
+    @action
+    onSearch(queryOptions: Record<string, string>) {
+        this.q = queryOptions.q;
+        this.sort = queryOptions.sort;
+        this.resourceType = queryOptions.resourceType;
+    }
 }
