60d67d04086ebb925efe6cbbdc8cf8bdb8da9f1d
Brian Pilati <brianpilati@cos.io>
2023-08-07 12:12:44 -0600

    Added more styling for mobile

--- lib/osf-components/addon/components/preprints/taxonomy-top-list/component.ts
+++ lib/osf-components/addon/components/preprints/taxonomy-top-list/component.ts
@@ -1,5 +1,7 @@
 import Component from '@glimmer/component';
 import SubjectModel from 'ember-osf-web/models/subject';
+import Media from 'ember-responsive';
+import { inject as service } from '@ember/service';
 
 interface InputArgs {
     list: SubjectModel[];
@@ -11,7 +13,11 @@ interface PairModel {
 }
 
 export default class TaxonomyTopList extends Component<InputArgs> {
-    routerPrefix = 'http://localhost:4200';
+    @service media!: Media;
+
+    get isMobile(): boolean {
+        return this.media.isMobile;
+    }
 
     get sortedList() {
         if (!this.args.list) {
@@ -19,25 +25,40 @@ export default class TaxonomyTopList extends Component<InputArgs> {
         }
         const sortedList = this.args.list.sortBy('text');
         const pairedList = [] as  PairModel[][];
-        for (let i = 0; i < sortedList.get('length'); i += 2) {
-            const pair: PairModel[] = [];
-            // path in pair needs to be a list because that's what the
-            // subject param in the discover controller is expecting
-            const subjectOdd = sortedList.objectAt(i) as SubjectModel;
-            pair.pushObject({
-                path: [subjectOdd?.taxonomyName],
-                text: subjectOdd?.text,
-            } as PairModel);
 
-            if (sortedList.objectAt(i + 1)) {
-                const subjectEven = sortedList.objectAt(i + 1) as SubjectModel;
+        if (this.isMobile) {
+            for (let i = 0; i < sortedList.get('length'); i += 1) {
+                const pair: PairModel[] = [];
+                const subject= sortedList.objectAt(i) as SubjectModel;
                 pair.pushObject({
-                    path: [subjectEven?.taxonomyName],
-                    text: subjectEven?.text,
-                });
+                    path: [subject?.taxonomyName],
+                    text: subject?.text,
+                } as PairModel);
+                pairedList.pushObject(pair);
             }
-            pairedList.pushObject(pair);
+        } else {
+            for (let i = 0; i < sortedList.get('length'); i += 2) {
+                const pair: PairModel[] = [];
+                // path in pair needs to be a list because that's what the
+                // subject param in the discover controller is expecting
+                const subjectOdd = sortedList.objectAt(i) as SubjectModel;
+                pair.pushObject({
+                    path: [subjectOdd?.taxonomyName],
+                    text: subjectOdd?.text,
+                } as PairModel);
+
+                if (sortedList.objectAt(i + 1)) {
+                    const subjectEven = sortedList.objectAt(i + 1) as SubjectModel;
+                    pair.pushObject({
+                        path: [subjectEven?.taxonomyName],
+                        text: subjectEven?.text,
+                    });
+                }
+                pairedList.pushObject(pair);
+            }
+
         }
+
         return pairedList;
     }
 }
