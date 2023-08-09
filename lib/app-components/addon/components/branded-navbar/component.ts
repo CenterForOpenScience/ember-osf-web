b1a3bf363ebf1c38b801c098253a102173cb5fd5
Futa Ikeda <futa.ikeda@gmail.com>
2023-08-09 13:45:11 -0400

    Remove unneeded comment

--- lib/app-components/addon/components/branded-navbar/component.ts
+++ lib/app-components/addon/components/branded-navbar/component.ts
@@ -56,7 +56,6 @@ export default class BrandedNavbar extends Component {
     @alias('theme.provider.id') providerId!: string;
     @alias('theme.provider.brand.primaryColor') brandPrimaryColor!: BrandModel;
 
-    // @computed('theme.{providerType,provider.id}')
     get useWhiteBackground(): boolean {
         const { provider } = this.theme;
         if (provider) {
