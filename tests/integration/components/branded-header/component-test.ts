eaf0d97c9ec761e0adfbcfc047fe9fdcc49df655
Brian Pilati <brianpilati@cos.io>
2023-08-15 11:38:48 -0600

    Added additional tests for the branded header

--- tests/integration/components/branded-header/component-test.ts
+++ tests/integration/components/branded-header/component-test.ts
@@ -26,28 +26,51 @@ module('Integration | Component | branded-header', hooks => {
                 @showHelp=true
                 @searchPlaceholder={{this.searchPlaceholder}}
                 as |branded-header|
-            ></BrandedHeader>
+            >
+                {{#branded-header.lead}}
+                    <div data-test-lead-yield>
+                        This is the lead
+                    </div>
+                {{/branded-header.lead}}
+                {{#branded-header.row}}
+                    <div data-test-row-yield>
+                        This is the row 
+                    </div>
+                {{/branded-header.row}} 
+            </BrandedHeader>
         `);
 
         // Then the header container is verified
         const headerContainer = this.element.querySelector('[data-test-header-container]');
         assert.dom(headerContainer).exists();
 
-        // Then the header container is verified
+        // And the brand logo container is verified
         assert.dom(this.element.querySelector('[data-test-brand-logo]')).hasAttribute('aria-label', 'OSF Preprints');
 
+        // And the perform search button is verified
         // eslint-disable-next-line max-len
         assert.dom(this.element.querySelector('[data-test-perform-search-button]')).hasAttribute('aria-label', 'Perform search');
         assert.dom(this.element.querySelector('[data-test-perform-search-button]')).hasAttribute('type', 'button');
 
+        // And the search icon is verified
         // eslint-disable-next-line max-len
         assert.dom(this.element.querySelector('[data-test-search-icon]')).hasAttribute('data-icon', 'search');
 
+        // And the search box is verified
         // eslint-disable-next-line max-len
         assert.dom(this.element.querySelector('[data-test-search-box]')).hasAttribute('placeholder', 'preprints.header.search_placeholder');
 
+        // And the search help button is verified
         // eslint-disable-next-line max-len
         assert.dom(this.element.querySelector('[data-test-search-help-button]')).hasAttribute('aria-label', 'Search help');
         assert.dom(this.element.querySelector('[data-test-search-help-button]')).hasAttribute('type', 'button');
+
+        // And the lead yield is verified
+        // eslint-disable-next-line max-len
+        assert.dom(this.element.querySelector('[data-test-lead-yield]')).hasText('This is the lead', 'The lead in yielded correctly');
+
+        // And the row yield is verified
+        // eslint-disable-next-line max-len
+        assert.dom(this.element.querySelector('[data-test-row-yield]')).hasText('This is the row', 'The lead in yielded correctly');
     });
 });
