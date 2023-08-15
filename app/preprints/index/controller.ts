3c553feeb933e843b3b6fbe411510cde57c1b6a8
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2023-08-15 14:52:11 -0400

    Revert "Fixed the css for the steering committee"
    This reverts commit 99853dc70bd32ad56585e5fa8014533400a31d8d.

--- app/preprints/index/controller.ts
+++ app/preprints/index/controller.ts
@@ -10,10 +10,6 @@ export default class Preprints extends Controller {
     @service theme!: Theme;
     @service router!: RouterService;
 
-    get isOsf(): boolean {
-        return this.theme?.provider?.id === 'osf';
-    }
-
     @action
     onSearch(query: string) {
         let route = 'search';
