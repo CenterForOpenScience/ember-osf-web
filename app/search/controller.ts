b0dce1e5547d01364f39dfb97e9614196147e205
Futa Ikeda <futa.ikeda@gmail.com>
2023-08-16 16:24:23 -0400

    Rename cardSearchText queryParam to q

--- app/search/controller.ts
+++ app/search/controller.ts
@@ -4,16 +4,16 @@ import { tracked } from '@glimmer/tracking';
 import { OnSearchParams, ResourceTypeFilterValue } from 'osf-components/components/search-page/component';
 
 export default class SearchController extends Controller {
-    @tracked cardSearchText?: string = '';
+    @tracked q?: string = '';
     @tracked sort?: string =  '-relevance';
     @tracked resourceType?: ResourceTypeFilterValue | null = null;
     @tracked page?: string = '';
 
-    queryParams = ['cardSearchText', 'page', 'sort', 'resourceType'];
+    queryParams = ['q', 'page', 'sort', 'resourceType'];
 
     @action
     onSearch(queryOptions: OnSearchParams) {
-        this.cardSearchText = queryOptions.cardSearchText;
+        this.q = queryOptions.cardSearchText;
         this.sort = queryOptions.sort;
         this.resourceType = queryOptions.resourceType;
         this.page = queryOptions.page;
