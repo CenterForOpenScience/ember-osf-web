62924f0e84f968376a944260e7a6e35d797c6c20
Futa Ikeda <futa.ikeda@gmail.com>
2023-08-16 11:57:59 -0400

    Properly support nested filters

--- app/models/related-property-path.ts
+++ app/models/related-property-path.ts
@@ -22,13 +22,25 @@ export default class RelatedPropertyPathModel extends OsfModel {
     getLocalizedString = new GetLocalizedPropertyHelper(getOwner(this));
 
     get shortFormLabel() {
-        const hash = this.propertyPath[0] as unknown as Record<string, LanguageText[]>;
-        const label = this.getLocalizedString.compute([hash, 'shortFormLabel']);
-        return label || '';
+        const labelArray = [];
+        // propertyPath is likely an array of length 1,
+        // unless it is nested property(e.g. file's isContainedBy.funder, file's isContainedBy.license)
+        for (const property of this.propertyPath) {
+            const label = this.getLocalizedString.compute(
+                [property as unknown as Record<string, LanguageText[]>, 'shortFormLabel'],
+            );
+            if (label) {
+                labelArray.push(label);
+            }
+        }
+        return labelArray.join(',');
     }
 
     get displayLabel() {
-        const hash = this.propertyPath[0] as unknown as Record<string, LanguageText[]>;
+        // propertyPath is likely an array of length 1,
+        // unless it is nested property(e.g. file's isContainedBy.funder, file's isContainedBy.license)
+        // in which case we just use the last property
+        const hash = this.propertyPath[this.propertyPath.length-1] as unknown as Record<string, LanguageText[]>;
         const label = this.getLocalizedString.compute([hash, 'displayLabel']);
         return label || '';
     }
