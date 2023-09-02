50096dd2f5d8d58d4dc6153f927500b0d864146e
Yuhuai Liu <yuhuai@cos.io>
2023-09-02 01:06:26 -0400

    cont.

--- tests/integration/components/osf-link/component-test.ts
+++ tests/integration/components/osf-link/component-test.ts
@@ -1,12 +1,14 @@
 import { render} from '@ember/test-helpers';
 import Ember from 'ember';
 import { hbs } from 'ember-cli-htmlbars';
+import { setupIntl } from 'ember-intl/test-support';
 import { setupRenderingTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
 
 module('Integration | Component | osf-link', hooks => {
     setupRenderingTest(hooks);
+    setupIntl(hooks);
 
     let orgOnError: any;
     hooks.beforeEach(function() {
