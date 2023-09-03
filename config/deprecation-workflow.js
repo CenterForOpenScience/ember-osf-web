c26733a20d1cdb404532065bec7636bd7f41bd2c
Yuhuai Liu <yuhuai@cos.io>
2023-09-02 20:11:13 -0400

    Remove commented out deprecations
    Remove commented out deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -9,8 +9,6 @@ self.deprecationWorkflow.config = {
         { handler: 'silence', matchId: 'ember-cli-mirage-config-routes-only-export' },
         { handler: 'silence', matchId: 'ember-engines.deprecation-router-service-from-host'},
         { handler: 'silence', matchId: 'ember-test-waiters-legacy-module-name'},
-        // { handler: 'silence', matchId: 'deprecated-run-loop-and-computed-dot-access'},
-        // { handler: 'silence', matchId: 'ember.built-in-components.legacy-attribute-arguments'},
         { handler: 'silence', matchId: 'ember.built-in-components.reopen'},
         { handler: 'silence', matchId: 'ember.component.reopen'},
         { handler: 'silence', matchId: 'ember-global'},
