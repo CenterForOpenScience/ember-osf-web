5b68e1ca93e855b7e2aa0bcc1911ed5a75ee36ca
Yuhuai Liu <yuhuai@cos.io>
2023-09-05 14:26:13 -0400

    activeFilters query param

--- app/search/controller.ts
+++ app/search/controller.ts
@@ -1,19 +1,21 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { OnSearchParams, ResourceTypeFilterValue } from 'osf-components/components/search-page/component';
+import { Filter, OnSearchParams, ResourceTypeFilterValue } from 'osf-components/components/search-page/component';
 
 export default class SearchController extends Controller {
     @tracked q?: string = '';
     @tracked sort?: string =  '-relevance';
     @tracked resourceType?: ResourceTypeFilterValue | null = null;
+    @tracked activeFilters?: Filter[] = [];
 
-    queryParams = ['q', 'sort', 'resourceType'];
+    queryParams = ['q', 'sort', 'resourceType', 'activeFilters'];
 
     @action
     onSearch(queryOptions: OnSearchParams) {
         this.q = queryOptions.cardSearchText;
         this.sort = queryOptions.sort;
         this.resourceType = queryOptions.resourceType;
+        this.activeFilters = queryOptions.activeFilters;
     }
 }
