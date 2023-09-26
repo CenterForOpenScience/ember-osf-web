4182198790e5e3f047796c091b8ac32df1008e2a
Futa Ikeda <futa.ikeda@gmail.com>
2023-09-26 18:13:14 -0400

    Reroute for registry not found

--- lib/registries/addon/branded/discover/route.ts
+++ lib/registries/addon/branded/discover/route.ts
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 
+import RegistrationProviderModel from 'ember-osf-web/models/registration-provider';
 import { notFoundURL } from 'ember-osf-web/utils/clean-url';
 
 export default class BrandedRegistriesDiscoverRoute extends Route {
