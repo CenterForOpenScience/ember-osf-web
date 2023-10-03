5655dfba3338b0000d74ed9d0639b53b6d870603
Futa Ikeda <futa.ikeda@gmail.com>
2023-10-03 15:11:16 -0400

    Move accessService logic to share-adapter

--- app/adapters/share-adapter.ts
+++ app/adapters/share-adapter.ts
@@ -1,7 +1,17 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
-import config from 'ember-get-config';
+import config from 'ember-osf-web/config/environment';
+
+const osfUrl = config.OSF.url;
 
 export default class ShareAdapter extends JSONAPIAdapter {
     host = config.OSF.shareBaseUrl.replace(/\/$/, ''); // Remove trailing slash to avoid // in URLs
     namespace = 'api/v3';
+
+    queryRecord(store: any, type: any, query: any) {
+        // check if we aren't serving locally, otherwise add accessService query param to card/value searches
+        if (['index-card-search', 'index-value-search'].includes(type.modelName) && !osfUrl.includes('localhost')) {
+            query.cardSearchFilter['accessService'] = osfUrl;
+        }
+        return super.queryRecord(store, type, query);
+    }
 }
