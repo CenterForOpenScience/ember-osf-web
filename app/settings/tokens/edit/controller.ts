1fad4d20303f32083713a24f64b5dbb7a5f44a9f
Ashley Robinson <ashley@cos.io>
2023-10-12 16:43:42 -0400

    Updated refresh method to refreshToken, updated action to task.

--- app/settings/tokens/edit/controller.ts
+++ app/settings/tokens/edit/controller.ts
@@ -1,5 +1,4 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
@@ -8,6 +7,8 @@ import Toast from 'ember-toastr/services/toast';
 
 import Token from 'ember-osf-web/models/token';
 import Analytics from 'ember-osf-web/services/analytics';
+import { task } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
 
 export default class SettingsTokensEditController extends Controller {
     @service analytics!: Analytics;
@@ -20,8 +21,9 @@ export default class SettingsTokensEditController extends Controller {
     @reads('model.taskInstance.value')
     token?: Token;
 
-    @action
-    refresh() {
+    @task
+    @waitFor
+    async refreshToken() {
         this.clearTokenValue();
 
         // Send action to route
