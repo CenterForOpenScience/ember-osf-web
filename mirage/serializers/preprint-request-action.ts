7148607bc20290457eb89568be8c16a652a40758
Brian Pilati <brianpilati@cos.io>
2023-10-17 09:37:20 -0600

    Removed all the `import config from ember-get-config`

--- mirage/serializers/preprint-request-action.ts
+++ mirage/serializers/preprint-request-action.ts
@@ -1,5 +1,5 @@
 import { ModelInstance } from 'ember-cli-mirage';
-import config from 'ember-get-config';
+import config from 'ember-osf-web/config/environment';
 import PreprintRequestActionModel from 'ember-osf-web/models/preprint-request-action';
 import ApplicationSerializer, { SerializedRelationships } from './application';
 
