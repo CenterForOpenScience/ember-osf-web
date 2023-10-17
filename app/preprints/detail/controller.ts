6e4afa254a73dda7cabb7860d780579bb9db92cb
Brian Pilati <brianpilati@cos.io>
2023-10-17 13:26:33 -0600

    Pruned unused code

--- app/preprints/detail/controller.ts
+++ app/preprints/detail/controller.ts
@@ -46,12 +46,6 @@ export default class PrePrintsDetailController extends Controller {
     @service intl!: Intl;
     @service media!: Media;
 
-    metricsStartDate = config.OSF.metricsStartDate;
-
-    queryParams_Altered = {
-        chosenFile: 'file',
-    };
-
     @tracked fullScreenMFR = false;
 
     get hyperlink(): string {
