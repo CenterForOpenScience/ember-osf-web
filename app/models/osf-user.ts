1721500439928da11c63d386dd520ba80b994a73
Futa Ikeda <futa.ikeda@gmail.com>
2023-11-10 13:45:48 -0500

    Remove unneeded model

--- app/models/osf-user.ts
+++ app/models/osf-user.ts
@@ -1,6 +1,5 @@
 import { AsyncHasMany, hasMany } from '@ember-data/model';
 
-import AddonExternalAccountModel from './addon-external-account';
 import AuthorizedStorageAccountModel from './authorized-storage-account';
 import OsfResourceModel from './osf-resource';
 import OsfModel from './osf-model';
@@ -8,9 +7,6 @@ import OsfModel from './osf-model';
 export default class OsfUserModel extends OsfModel {
     @hasMany('authorized-storage-account', { inverse: 'configuringUser' })
     authorizedStorageAccounts!: AsyncHasMany<AuthorizedStorageAccountModel> & AuthorizedStorageAccountModel[];
-
-    @hasMany('external-account')
-    externalAccounts!: AsyncHasMany<AddonExternalAccountModel> & AddonExternalAccountModel[];
 
     @hasMany('osf-resource')
     configuredResources!: AsyncHasMany<OsfResourceModel> & OsfResourceModel[];
