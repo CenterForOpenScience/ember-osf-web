36dbad15554cd429c7e357efdeae36a62c595503
Brian Pilati <brianpilati@cos.io>
2023-12-15 13:08:13 -0700

    Fixed the namespace

--- app/adapters/cedar-metadata-record.ts
+++ app/adapters/cedar-metadata-record.ts
@@ -1,6 +1,8 @@
 import OsfAdapter from './osf-adapter';
 
-export default class CedarMetadataRecordAdapter extends OsfAdapter { }
+export default class CedarMetadataRecordAdapter extends OsfAdapter {
+    namespace = '_';
+}
 
 declare module 'ember-data/types/registries/adapter' {
     export default interface AdapterRegistry {
--- app/adapters/cedar-metadata-template.ts
+++ app/adapters/cedar-metadata-template.ts
@@ -1,6 +1,7 @@
 import OsfAdapter from './osf-adapter';
 
 export default class CedarMetadataTemplateAdapter extends OsfAdapter {
+    namespace = '_';
 }
 
 declare module 'ember-data/types/registries/adapter' {
