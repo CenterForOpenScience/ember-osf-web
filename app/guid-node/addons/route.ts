4516be755121855a89cd3cee0c0328dfa6cfc486
Futa Ikeda <futa.ikeda@gmail.com>
2023-12-18 10:25:22 -0500

    Updates to addon route and manager

--- app/guid-node/addons/route.ts
+++ app/guid-node/addons/route.ts
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import Store from '@ember-data/store';
 
 export default class GuidNodeAddons extends Route {
+    @service store: Store;
+
+    async model() {
+        return await this.modelFor('guid-node').taskInstance;
+    }
 }
