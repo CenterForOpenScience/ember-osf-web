53c29c63387605079a2973b0bdcecabed48c4809
Brian Pilati <brianpilati@cos.io>
2024-01-04 13:15:31 -0700

    Updates per PR requests

--- mirage/serializers/cedar-metadata-template.ts
+++ mirage/serializers/cedar-metadata-template.ts
@@ -1,12 +1,13 @@
+import { ModelInstance } from 'ember-cli-mirage';
 import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
 import config from 'ember-osf-web/config/environment';
 import ApplicationSerializer from './application';
 const { OSF: { apiUrl } } = config;
 
 export default class CedarMetadataTemplateMirageSerializer extends ApplicationSerializer<CedarMetadataTemplateModel> {
-    buildNormalLinks() {
+    buildNormalLinks(model: ModelInstance<CedarMetadataTemplateModel>) {
         return {
-            self: `${apiUrl}/_/cedar_metadata_templates/`,
+            self: `${apiUrl}/_/cedar_metadata_templates/${model.id}`,
         };
     }
 }
