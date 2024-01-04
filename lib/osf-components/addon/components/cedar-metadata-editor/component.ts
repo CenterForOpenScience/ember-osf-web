c38b27af0bb502151fdd7d8238652242ab43cb50
Brian Pilati <brianpilati@cos.io>
2024-01-04 15:41:55 -0700

    For debugging added the configuration to the component

--- lib/osf-components/addon/components/cedar-metadata-editor/component.ts
+++ lib/osf-components/addon/components/cedar-metadata-editor/component.ts
@@ -10,6 +10,29 @@ interface Args {
 }
 
 export default class CedarMetadataEditor extends Component<Args> {
+
+    config = {
+        sampleTemplateLocationPrefix: 'http://localhost:4240/cedar-embeddable-editor-sample-templates/',
+        loadSampleTemplateName: '01',
+        expandedSampleTemplateLinks: true,
+        showTemplateRenderingRepresentation: true,
+        showInstanceDataCore: true,
+        expandedInstanceDataCore: false,
+        showMultiInstanceInfo: true,
+        expandedMultiInstanceInfo: false,
+        expandedTemplateRenderingRepresentation: false,
+        showInstanceDataFull: false,
+        expandedInstanceDataFull: false,
+        showTemplateSourceData: true,
+        expandedTemplateSourceData: false,
+        showHeader: true,
+        showFooter: true,
+        defaultLanguage: 'en',
+        fallbackLanguage: 'en',
+        collapseStaticComponents: false,
+        showStaticText: true,
+    };
+
     @action
     save() {
         /*
