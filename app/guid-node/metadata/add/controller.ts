c06919aaa43b5917a3d864213b2e33e31f071f88
Brian Pilati <brianpilati@cos.io>
2024-01-04 11:47:34 -0700

    Added a new component to handle the cedar editor

--- app/guid-node/metadata/add/controller.ts
+++ app/guid-node/metadata/add/controller.ts
@@ -4,20 +4,36 @@ import Media from 'ember-responsive';
 import config from 'ember-osf-web/config/environment';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import RouterService from '@ember/routing/router-service';
+import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
 
 export default class GuidMetadataAdd extends Controller {
     @service media!: Media;
+    @service router!: RouterService;
 
     supportEmail = config.support.supportEmail;
 
     @tracked displaySelectionOptions = true;
+    @tracked selectedTemplate = undefined;
 
     @action
-    // eslint-disable-next-line
     selectTemplate(cedarId: string): void {
         this.displaySelectionOptions = false;
-        // console.log(this.model.templates.length);
-        // console.log('selected', cedarId);
+
+        // eslint-disable-next-line max-len
+        const selectedTemplate = this.model.templates.filter((template: CedarMetadataTemplateModel) =>  cedarId === template.cedarId);
+
+        if (selectedTemplate) {
+            this.selectedTemplate = selectedTemplate.pop();
+        } else {
+            this.router.transitionTo('not-found', `${this.model.guidNode.id}/metadata`);
+        }
+    }
+
+    @action
+    closeCedarEditor(): void {
+        this.displaySelectionOptions = true;
+        this.selectedTemplate = undefined;
     }
 
     get isMobile() {
