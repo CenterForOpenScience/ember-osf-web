ee1a8dfe8efd673e04a269e78b04f48225160296
Brian Pilati <brianpilati@cos.io>
2024-01-04 11:47:34 -0700

    Added a new component to handle the cedar editor

---
+++ app/guid-node/metadata/-components/metadata-cedar/component.ts
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import Media from 'ember-responsive';
+
+export default class MetadataCedar extends Component {
+    @service media!: Media;
+
+    get isMobile() {
+        return this.media.isMobile;
+    }
+}
--- mirage/fixtures/cedar-metadata-templates.ts
+++ mirage/fixtures/cedar-metadata-templates.ts
@@ -5,7 +5,7 @@ const cedarMetadataTemplates: Array<Partial<CedarMetadataTemplateModel>> = [
     {
         id: '2',
         schemaName: 'OSF Registries - 1',
-        cedarId: 'abc-123',
+        cedarId: 'https://repo.metadatacenter.net/templates/58098c2d-dea9-4ec6-8f5d-d85d5f1b4f6f',
         active: true,
         template: Object({
             description: 'A template description may or may not exist on the cedar template object',
