a60a20bc9da386a42b41bea8bf10e37c89531227
Brian Pilati <brianpilati@cos.io>
2024-01-05 11:41:38 -0700

    Added metadata information to the tabs

--- app/guid-node/metadata/add/controller.ts
+++ app/guid-node/metadata/add/controller.ts
@@ -6,26 +6,37 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import RouterService from '@ember/routing/router-service';
 import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
+import Intl from 'ember-intl/services/intl';
 
 export default class GuidMetadataAdd extends Controller {
     @service media!: Media;
     @service router!: RouterService;
+    @service intl!: Intl;
 
     supportEmail = config.support.supportEmail;
 
     @tracked displaySelectionOptions = true;
     @tracked selectedTemplate?: CedarMetadataTemplateModel;
+    @tracked tabTitle = this.intl.t('metadata.add-flow.select');
 
     @action
     selectTemplate(cedarMetadataTemplate: CedarMetadataTemplateModel): void {
         this.displaySelectionOptions = false;
         this.selectedTemplate = cedarMetadataTemplate;
+        this.updateTitle();
     }
 
     @action
     closeCedarEditor(): void {
         this.displaySelectionOptions = true;
         this.selectedTemplate = undefined;
+        this.updateTitle();
+    }
+
+    private updateTitle(): void {
+        // eslint-disable-next-line max-len
+        const translation = this.displaySelectionOptions ? 'metadata.add-flow.tab-title-select' : 'metadata.add-flow.tab-title-add';
+        this.tabTitle = this.intl.t(translation);
     }
 
     get isMobile() {
