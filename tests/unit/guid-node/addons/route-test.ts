e0c3571ed4522d89ab962dae948003069ed0b5a1
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2024-01-05 14:17:46 -0500

    [No ticket] Updates to the addons templates (#2101)
    ## Purpose
    - Update route structure for addons
    
    ## Summary of Changes
    - Move addons list page to `guid-node.addons.index` from `guid-node.addons`
    - Update left nav link to use in-app transition
      - Will now show active route state

--- tests/unit/guid-node/addons/route-test.ts
+++ tests/unit/guid-node/addons/route-test.ts
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | guid-node/addons', function(hooks) {
+module('Unit | Route | guid-node/addons/index', function(hooks) {
     setupTest(hooks);
 
     test('it exists', function(assert) {
-        const route = this.owner.lookup('route:guid-node/addons');
+        const route = this.owner.lookup('route:guid-node/addons/index');
         assert.ok(route);
     });
 });
