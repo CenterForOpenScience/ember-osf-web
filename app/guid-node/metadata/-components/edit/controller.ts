bf2687b554362ff192fd8f71fb9ae6c22bf4a9c9
Brian Pilati <brianpilati@cos.io>
2024-01-17 13:45:21 -0700

    Added more edit flow components

---
+++ app/guid-node/metadata/-components/edit/controller.ts
@@ -0,0 +1,45 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import Media from 'ember-responsive';
+import config from 'ember-osf-web/config/environment';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import RouterService from '@ember/routing/router-service';
+import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
+import Intl from 'ember-intl/services/intl';
+
+export default class GuidMetadataEdit extends Controller {
+    @service media!: Media;
+    @service router!: RouterService;
+    @service intl!: Intl;
+
+    supportEmail = config.support.supportEmail;
+
+    @tracked displaySelectionOptions = true;
+    @tracked selectedTemplate?: CedarMetadataTemplateModel;
+    @tracked tabTitle = this.intl.t('metadata.add-flow.select');
+
+    @action
+    selectTemplate(cedarMetadataTemplate: CedarMetadataTemplateModel): void {
+        this.displaySelectionOptions = false;
+        this.selectedTemplate = cedarMetadataTemplate;
+        this.updateTitle();
+    }
+
+    @action
+    closeCedarEditor(): void {
+        this.displaySelectionOptions = true;
+        this.selectedTemplate = undefined;
+        this.updateTitle();
+    }
+
+    private updateTitle(): void {
+        // eslint-disable-next-line max-len
+        const translation = this.displaySelectionOptions ? 'metadata.add-flow.tab-title-select' : 'metadata.add-flow.tab-title-add';
+        this.tabTitle = this.intl.t(translation);
+    }
+
+    get isMobile() {
+        return this.media.isMobile;
+    }
+}
