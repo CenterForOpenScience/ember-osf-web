641bde96c9be4c89188aceea072a877ff7ef946a
Futa Ikeda <futa.ikeda@gmail.com>
2024-01-22 17:22:54 -0500

    Remove v2 addon model

--- mirage/serializers/addon.ts
+++
@@ -1,6 +0,0 @@
-import AddonModel from 'ember-osf-web/models/addon';
-
-import ApplicationSerializer from './application';
-
-export default class AddonSerializer extends ApplicationSerializer<AddonModel> {
-}
