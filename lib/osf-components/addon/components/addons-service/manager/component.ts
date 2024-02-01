bf8996acf4f954fd515e3bed9c3df039f801dfe4
Futa Ikeda <futa.ikeda@gmail.com>
2024-02-01 09:23:35 -0500

    One route to rule them all

--- lib/osf-components/addon/components/addons-service/manager/component.ts
+++ lib/osf-components/addon/components/addons-service/manager/component.ts
@@ -1,3 +1,4 @@
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import { waitFor } from '@ember/test-waiters';
 import Store from '@ember-data/store';
@@ -5,6 +6,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { task } from 'ember-concurrency';
 import { taskFor } from 'ember-concurrency-ts';
+import IntlService from 'ember-intl/services/intl';
 
 import InternalResourceModel from 'ember-osf-web/models/internal-resource';
 import NodeModel from 'ember-osf-web/models/node';
@@ -19,11 +21,71 @@ interface Args {
 export default class AddonsServiceManagerComponent extends Component<Args> {
     @service store!: Store;
     @service currentUser!: CurrentUserService;
+    @service intl!: IntlService;
 
     node = this.args.node;
     @tracked addonServiceNode?: InternalResourceModel;
 
     @tracked addonProviders: Provider[] = [];
+
+    @tracked showTerms = false;
+    @tracked showAccountSetup = false;
+    @tracked showConfirm = false;
+    @tracked showConfigure = false;
+    @tracked selectedProvider?: Provider;
+
+    @action
+    configureProvider(provider: Provider) {
+        this.cancelSetup();
+        this.selectedProvider = provider;
+        this.showConfigure = true;
+    }
+
+    @action
+    beginAccountSetup(provider: Provider) {
+        this.cancelSetup();
+        this.showTerms = true;
+        this.selectedProvider = provider;
+    }
+
+    @action
+    acceptTerms() {
+        this.showAccountSetup = true;
+        this.showTerms = false;
+        this.showConfirm = false;
+        this.showConfigure = false;
+    }
+
+    @action
+    authorizeSelectedAccount() {
+        this.showConfirm = true;
+        this.showTerms = false;
+        this.showAccountSetup = false;
+        this.showConfigure = false;
+    }
+
+    @action
+    confirmAccountSetup() {
+        this.showConfigure = true;
+        this.showTerms = false;
+        this.showAccountSetup = false;
+        this.showConfirm = false;
+    }
+
+    @action
+    cancelSetup() {
+        this.showTerms = false;
+        this.showAccountSetup = false;
+        this.showConfirm = false;
+        this.showConfigure = false;
+        this.selectedProvider = undefined;
+    }
+
+    @action
+    save() {
+        this.cancelSetup();
+        // TODO: Actually save the provider
+    }
 
     constructor(owner: unknown, args: Args) {
         super(owner, args);
@@ -66,6 +128,24 @@ export default class AddonsServiceManagerComponent extends Component<Args> {
 
     get isLoading() {
         return taskFor(this.getAddonProviders).isRunning;
+    }
+
+    get headingText() {
+        const providerName = this.selectedProvider?.provider.name;
+        let heading = this.intl.t('addons.heading');
+        if (this.showTerms) {
+            heading = this.intl.t('addons.terms.heading', { providerName });
+        }
+        if (this.showAccountSetup) {
+            heading = this.intl.t('addons.accountSelect.heading', { providerName });
+        }
+        if (this.showConfirm) {
+            heading = this.intl.t('addons.confirm.heading', { providerName });
+        }
+        if (this.showConfigure) {
+            heading = this.intl.t('addons.configure.heading', { providerName });
+        }
+        return heading;
     }
 
     // Service API Methods
