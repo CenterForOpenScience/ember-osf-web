d2a6f7cf0b2a1d0d7954f696079f491862438386
Brian Pilati <brianpilati@cos.io>
2024-02-08 10:48:03 -0700

    Added the async on route transition

--- lib/osf-components/addon/components/metadata/metadata-add/component.ts
+++ lib/osf-components/addon/components/metadata/metadata-add/component.ts
@@ -3,10 +3,11 @@ import { inject as service } from '@ember/service';
 import Media from 'ember-responsive';
 import config from 'ember-osf-web/config/environment';
 import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
-import { action } from '@ember/object';
 import RouterService from '@ember/routing/router-service';
 import FileModel from 'ember-osf-web/models/file';
 import AbstractNodeModel from 'ember-osf-web/models/abstract-node';
+import { waitFor } from '@ember/test-waiters';
+import { task } from 'ember-concurrency';
 
 interface AddArgs {
     templates: CedarMetadataTemplateModel[];
@@ -24,14 +25,15 @@ export default class MetadataAdd extends Component<AddArgs> {
         return this.media.isMobile;
     }
 
-    @action
-    public cancel(): void {
+    @task
+    @waitFor
+    async cancel() {
         if (this.args.target.modelName === 'node') {
-            this.router.transitionTo('guid-node.metadata', this.args.target.id);
+            await this.router.transitionTo('guid-node.metadata.detail', this.args.target.id);
         } else if (this.args.target.modelName === 'registration') {
-            this.router.transitionTo('registries.overview.metadata', this.args.target.id);
+            await this.router.transitionTo('registries.overview.metadata.detail', this.args.target.id);
         } else {
-            this.router.transitionTo('guid-file', this.args.target.id);
+            await this.router.transitionTo('guid-file', this.args.target.id);
         }
     }
 }
