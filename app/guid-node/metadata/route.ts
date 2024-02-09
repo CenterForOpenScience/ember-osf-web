98997b3ed749a86a3a67fbeab6cda434ef10fb3a
Brian Pilati <brianpilati@cos.io>
2024-02-09 11:15:54 -0700

    Fixed the persist because the parent route was not run on an add or edit

--- app/guid-node/metadata/route.ts
+++ app/guid-node/metadata/route.ts
@@ -2,7 +2,6 @@ import Store from '@ember-data/store';
 import Route from '@ember/routing/route';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
-import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
 
 export default class MetadataDetailRoute extends Route {
     @service store!: Store;
@@ -12,20 +11,8 @@ export default class MetadataDetailRoute extends Route {
         const guidNode = this.modelFor('guid-node');
         const target = await guidNode.taskInstance;
 
-        const cedarMetadataRecords = await target.queryHasMany('cedarMetadataRecords', {
-            // embed: 'template',
-            'page[size]': 20,
-        });
-
-        for(const cedarMetadataRecord of cedarMetadataRecords) {
-            const template = await cedarMetadataRecord.template as CedarMetadataTemplateModel;
-            template.recordCreated = true;
-            cedarMetadataRecord.templateName = template.schemaName;
-        }
-
         return {
             target,
-            cedarMetadataRecords,
         };
     }
 }
--- lib/registries/addon/overview/metadata/detail/route.ts
+++ lib/registries/addon/overview/metadata/detail/route.ts
@@ -3,6 +3,7 @@ import Route from '@ember/routing/route';
 import RouterService from '@ember/routing/router-service';
 import { inject as service } from '@ember/service';
 import CedarMetadataRecordModel from 'ember-osf-web/models/cedar-metadata-record';
+import CedarMetadataTemplateModel from 'ember-osf-web/models/cedar-metadata-template';
 
 
 export default class MetadataDetailRoute extends Route {
@@ -13,14 +14,25 @@ export default class MetadataDetailRoute extends Route {
         const parentModel = this.modelFor('overview.metadata');
         let defaultIndex = 0;
 
-        parentModel.cedarMetadataRecords.sort(
+        const cedarMetadataRecords = await parentModel.target.queryHasMany('cedarMetadataRecords', {
+            // embed: 'template',
+            'page[size]': 20,
+        });
+
+        for(const cedarMetadataRecord of cedarMetadataRecords) {
+            const template = await cedarMetadataRecord.template as CedarMetadataTemplateModel;
+            template.recordCreated = true;
+            cedarMetadataRecord.templateName = template.schemaName;
+        }
+
+        cedarMetadataRecords.sort(
             (a: CedarMetadataRecordModel, b: CedarMetadataRecordModel) =>
                 a.templateName > b.templateName ? 1 : -1,
         );
 
         if (params.recordId) {
             let index = 0;
-            for(const cedarMetadataRecord of parentModel.cedarMetadataRecords) {
+            for(const cedarMetadataRecord of cedarMetadataRecords) {
                 if (cedarMetadataRecord.id === params.recordId) {
                     defaultIndex = index + 1;
                 }
@@ -29,14 +41,14 @@ export default class MetadataDetailRoute extends Route {
         }
 
         if (defaultIndex > 0) {
-            const selected = parentModel.cedarMetadataRecords.splice(defaultIndex - 1, 1);
-            parentModel.cedarMetadataRecords.unshift(selected[0]);
+            const selected = cedarMetadataRecords.splice(defaultIndex - 1, 1);
+            cedarMetadataRecords.unshift(selected[0]);
             defaultIndex = 1;
         }
 
         return {
             target: parentModel.target,
-            cedarMetadataRecords: parentModel.cedarMetadataRecords,
+            cedarMetadataRecords,
             defaultIndex,
         };
     }
