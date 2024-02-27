386ce837ce238eeac6d9081260d49bd469e2fbb3
Futa Ikeda <futa.ikeda@gmail.com>
2024-02-27 18:01:46 -0500

    CR feedback

--- tests/integration/helpers/is-mobile-test.ts
+++ tests/integration/helpers/is-mobile-test.ts
@@ -8,12 +8,13 @@ import { setBreakpoint } from 'ember-responsive/test-support';
 module('Integration | Helper | is-mobile', function(hooks) {
     setupRenderingTest(hooks);
 
-    // Replace this with your real tests.
     test('it renders', async function(assert) {
         setBreakpoint('mobile');
 
         await render(hbs`
-<div data-test-div>{{if (is-mobile) 'mobile' 'not-mobile'}}</div>`);
+            {{!-- template-lint-disable block-indentation --}}
+            <div data-test-div>{{if (is-mobile) 'mobile' 'not-mobile'}}</div>
+        `);
 
         assert.dom('[data-test-div]').hasText('mobile');
 
