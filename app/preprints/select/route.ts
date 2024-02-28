fad5bb1808546d30d024aeb80ca5f34d2a476d3a
Longze Chen <cslzchen@gmail.com>
2024-02-28 16:49:43 -0500

    Update route.js to retrieve providers and access them on the page

--- app/preprints/select/route.ts
+++ app/preprints/select/route.ts
@@ -1,4 +1,15 @@
+import Store from '@ember-data/store';
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 
-export default class PreprintSelectRoute extends Route {}
+export default class PreprintSelectRoute extends Route {
+    @service store!: Store;
+
+    async model(){
+        const allProviders = await this.store.findAll('preprint-provider', { reload: true });
+        return {
+            allProviders,
+        };
+    }
+}
