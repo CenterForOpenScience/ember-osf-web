78d05c139fb130b6c1622513b160536bbdbf9402
Brian Pilati <brianpilati@cos.io>
2024-02-28 09:41:53 -0700

    Added additional methods to the state-machine

--- app/preprints/-components/submit/preprint-state-machine/component.ts
+++ app/preprints/-components/submit/preprint-state-machine/component.ts
@@ -1,11 +1,54 @@
 import Component from '@glimmer/component';
+import PreprintModel from 'ember-osf-web/models/preprint';
+import PreprintProviderModel from 'ember-osf-web/models/preprint-provider';
+import Store from '@ember-data/store';
 import { inject as service } from '@ember/service';
-import Media from 'ember-responsive';
+import { task } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
+import RouterService from '@ember/routing/router-service';
+import { tracked } from '@glimmer/tracking';
 
-export default class PreprintStateMachine extends Component{
-    @service media!: Media;
+/**
+ * The Submit Args
+ */
+interface SubmitArgs {
+    provider: PreprintProviderModel;
+}
 
-    get isMobile() {
-        return this.media.isMobile;
+/**
+ * The Preprint State Machine
+ */
+export default class PreprintStateMachine extends Component<SubmitArgs>{
+    @service store!: Store;
+    @service router!: RouterService;
+
+    provider = this.args.provider;
+    preprint: PreprintModel;
+    @tracked statusFlowIndex = 0;
+
+    constructor(owner: unknown, args: SubmitArgs) {
+        super(owner, args);
+
+        this.preprint = this.store.createRecord('cedar-metadata-record');
+    }
+
+    /**
+     * Callback for the action-flow component
+     */
+    @task
+    @waitFor
+    public async onCancel(): Promise<void> {
+        this.preprint.deleteRecord();
+        await this.router.transitionTo('preprints.index', this.provider.id);
+    }
+
+    /**
+     * Callback for the action-flow component
+     */
+    @task
+    @waitFor
+    public async onSave(): Promise<void> {
+        await this.preprint.save();
+        await this.router.transitionTo('preprints.detail', this.provider.id, this.preprint.id );
     }
 }
