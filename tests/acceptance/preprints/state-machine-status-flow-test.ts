1a279f54470c5b8d959bb6f807b4d0b301b927b4
Brian Pilati <brianpilati@cos.io>
2024-03-08 09:51:02 -0700

    Fixed tests

--- tests/acceptance/preprints/state-machine-status-flow-test.ts
+++ tests/acceptance/preprints/state-machine-status-flow-test.ts
@@ -10,6 +10,12 @@ module('Acceptance | preprints | state-machine | status-flow', hooks => {
     setupRenderingTest(hooks);
     setupIntl(hooks);
 
+    function findSelectedClass(testAgent: EnginesTestContext, dataTestSelector: string): boolean {
+        const testElement = testAgent.element.querySelector(`[${dataTestSelector}]`);
+        const regexp = new RegExp(/.*?selected.*?/, 'g');
+        return regexp.test(testElement?.className || '');
+    }
+
     hooks.beforeEach(function(this: EnginesTestContext) {
         this.owner.unregister('service:router');
         this.owner.register('service:router', OsfLinkRouterStub);
@@ -18,145 +24,227 @@ module('Acceptance | preprints | state-machine | status-flow', hooks => {
     test('it displays the title and file status', async function(this: EnginesTestContext,assert) {
         // Given the statusFlowIndex is 1
         // And the displayAuthorAssertions is true
-        this.set('statusFlowIndex', 1);
-        this.set('displayAuthorAssertions', true);
+        this.set('manager', Object({
+            statusFlowIndex: 1,
+            displayAuthorAssertions: true,
+        }));
         // When the component is rendered
         await render(hbs`
-            <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                @statusFlowIndex={{this.statusFlowIndex}}
-                @displayAuthorAssertions={{this.displayAuthorAssertions}}
-            />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
         `);
 
         // Then the title and file is verified
         assert.dom('[data-test-title-and-file]').containsText('Title and File');
-        assert.dom('[data-test-metadata]').doesNotExist();
+        assert.true(findSelectedClass(this, 'data-test-title-and-file'), 'The select class exists');
+
+        // And the other fields are verified
+        assert.dom('[data-test-metadata]').containsText('Metadata');
+        assert.false(findSelectedClass(this, 'data-test-metadata'), 'The select class does not exist');
+
+        assert.dom('[data-test-author-assertions]').containsText('Author Assertions');
+        assert.false(findSelectedClass(this, 'data-test-author-assertions'), 'The select class does not exist');
+
+        assert.dom('[data-test-supplements]').containsText('Supplements');
+        assert.false(findSelectedClass(this, 'data-test-supplements'), 'The select class does not exist');
+
+        assert.dom('[data-test-review]').containsText('Review');
+        assert.false(findSelectedClass(this, 'data-test-review'), 'The select class does not exist');
     });
 
     test('it displays the metadata status', async function(this: EnginesTestContext,assert) {
         // Given the statusFlowIndex is 2
         // And the displayAuthorAssertions is true
-        this.set('statusFlowIndex', 2);
-        this.set('displayAuthorAssertions', true);
+        this.set('manager', Object({
+            statusFlowIndex: 2,
+            displayAuthorAssertions: true,
+        }));
         // When the component is rendered
         await render(hbs`
-            <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                @statusFlowIndex={{this.statusFlowIndex}}
-                @displayAuthorAssertions={{this.displayAuthorAssertions}}
-            />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
         `);
 
         // Then the metadata is verified
-        assert.dom('[data-test-title-and-file]').doesNotExist();
         assert.dom('[data-test-metadata]').containsText('Metadata');
+        assert.true(findSelectedClass(this, 'data-test-metadata'), 'The select class exists');
+
+        // And the other fields are verified
+        assert.dom('[data-test-title-and-file]').containsText('Title and File');
+        assert.false(findSelectedClass(this, 'data-test-title-and-file'), 'The select class does not exist');
+
+        assert.dom('[data-test-author-assertions]').containsText('Author Assertions');
+        assert.false(findSelectedClass(this, 'data-test-author-assertions'), 'The select class does not exist');
+
+        assert.dom('[data-test-supplements]').containsText('Supplements');
+        assert.false(findSelectedClass(this, 'data-test-supplements'), 'The select class does not exist');
+
+        assert.dom('[data-test-review]').containsText('Review');
+        assert.false(findSelectedClass(this, 'data-test-review'), 'The select class does not exist');
     });
 
     test('it displays the author assertions status', async function(this: EnginesTestContext,assert) {
-        // Given the statusFlowIndex is 2
+        // Given the statusFlowIndex is 3
         // And the displayAuthorAssertions is true
-        this.set('statusFlowIndex', 3);
-        this.set('displayAuthorAssertions', true);
+        this.set('manager', Object({
+            statusFlowIndex: 3,
+            displayAuthorAssertions: true,
+        }));
         // When the component is rendered
         await render(hbs`
-            <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                @statusFlowIndex={{this.statusFlowIndex}}
-                @displayAuthorAssertions={{this.displayAuthorAssertions}}
-            />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
         `);
 
         // Then the author assertions is verified
-        assert.dom('[data-test-title-and-file]').doesNotExist();
-        assert.dom('[data-test-metadata]').doesNotExist();
         assert.dom('[data-test-author-assertions]').containsText('Author Assertions');
-        assert.dom('[data-test-supplements]').doesNotExist();
-        assert.dom('[data-test-review]').doesNotExist();
+        assert.true(findSelectedClass(this, 'data-test-author-assertions'), 'The select class exists');
+
+
+        // And the other fields are verified
+        assert.dom('[data-test-title-and-file]').containsText('Title and File');
+        assert.false(findSelectedClass(this, 'data-test-title-and-file'), 'The select class does not exist');
+
+        assert.dom('[data-test-metadata]').containsText('Metadata');
+        assert.false(findSelectedClass(this, 'data-test-metadata'), 'The select class does not exist');
+
+        assert.dom('[data-test-supplements]').containsText('Supplements');
+        assert.false(findSelectedClass(this, 'data-test-supplements'), 'The select class does not exist');
+
+        assert.dom('[data-test-review]').containsText('Review');
+        assert.false(findSelectedClass(this, 'data-test-review'), 'The select class does not exist');
     });
 
     test('it displays the supplements status', async function(this: EnginesTestContext,assert) {
         // Given the statusFlowIndex is 4
         // And the displayAuthorAssertions is true
-        this.set('statusFlowIndex', 4);
-        this.set('displayAuthorAssertions', true);
+        this.set('manager', Object({
+            statusFlowIndex: 4,
+            displayAuthorAssertions: true,
+        }));
         // When the component is rendered
         await render(hbs`
-            <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                @statusFlowIndex={{this.statusFlowIndex}}
-                @displayAuthorAssertions={{this.displayAuthorAssertions}}
-            />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
         `);
 
-        // Then the metadata is verified
-        assert.dom('[data-test-title-and-file]').doesNotExist();
-        assert.dom('[data-test-metadata]').doesNotExist();
-        assert.dom('[data-test-author-assertions]').doesNotExist();
+        // Then the supplements is verified
         assert.dom('[data-test-supplements]').containsText('Supplements');
-        assert.dom('[data-test-review]').doesNotExist();
+        assert.true(findSelectedClass(this, 'data-test-supplements'), 'The select class exists');
+
+        // And the other fields are verified
+        assert.dom('[data-test-title-and-file]').containsText('Title and File');
+        assert.false(findSelectedClass(this, 'data-test-title-and-file'), 'The select class does not exist');
+
+        assert.dom('[data-test-metadata]').containsText('Metadata');
+        assert.false(findSelectedClass(this, 'data-test-metadata'), 'The select class does not exist');
+
+        assert.dom('[data-test-author-assertions]').containsText('Author Assertions');
+        assert.false(findSelectedClass(this, 'data-test-author-assertions'), 'The select class does not exist');
+
+        assert.dom('[data-test-review]').containsText('Review');
+        assert.false(findSelectedClass(this, 'data-test-review'), 'The select class does not exist');
     });
 
     test('it display the supplements status when displayAuthorAssertions is false',
         async function(this: EnginesTestContext,assert) {
             // Given the statusFlowIndex is 2
             // And the displayAuthorAssertions is false
-            this.set('statusFlowIndex', 3);
-            this.set('displayAuthorAssertions', false);
+            this.set('manager', Object({
+                statusFlowIndex: 3,
+                displayAuthorAssertions: false,
+            }));
             // When the component is rendered
             await render(hbs`
-                <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                    @statusFlowIndex={{this.statusFlowIndex}}
-                    @displayAuthorAssertions={{this.displayAuthorAssertions}}
-                />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
             `);
 
-            // Then the author assertions is verified
-            assert.dom('[data-test-title-and-file]').doesNotExist();
-            assert.dom('[data-test-metadata]').doesNotExist();
-            assert.dom('[data-test-author-assertions]').doesNotExist();
+            // Then the supplements is verified
             assert.dom('[data-test-supplements]').containsText('Supplements');
-            assert.dom('[data-test-review]').doesNotExist();
+            assert.true(findSelectedClass(this, 'data-test-supplements'), 'The select class exists');
+
+            // And the other fields are verified
+            assert.dom('[data-test-title-and-file]').containsText('Title and File');
+            assert.false(findSelectedClass(this, 'data-test-title-and-file'), 'The select class does not exist');
+
+            assert.dom('[data-test-metadata]').containsText('Metadata');
+            assert.false(findSelectedClass(this, 'data-test-metadata'), 'The select class does not exist');
+
+            assert.dom('[data-test-author-assertions]').doesNotExist();
+
+            assert.dom('[data-test-review]').containsText('Review');
+            assert.false(findSelectedClass(this, 'data-test-review'), 'The select class does not exist');
         });
 
 
     test('it displays the Review status', async function(this: EnginesTestContext,assert) {
-        // Given the statusFlowIndex is 2
+        // Given the statusFlowIndex is 5
         // And the displayAuthorAssertions is true
-        this.set('statusFlowIndex', 5);
-        this.set('displayAuthorAssertions', true);
+        this.set('manager', Object({
+            statusFlowIndex: 5,
+            displayAuthorAssertions: true,
+        }));
         // When the component is rendered
         await render(hbs`
-            <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                @statusFlowIndex={{this.statusFlowIndex}}
-                @displayAuthorAssertions={{this.displayAuthorAssertions}}
-            />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
         `);
 
-        // Then the metadata is verified
-        assert.dom('[data-test-title-and-file]').doesNotExist();
-        assert.dom('[data-test-metadata]').doesNotExist('Metadata');
-        assert.dom('[data-test-author-assertions]').doesNotExist();
-        assert.dom('[data-test-supplements]').doesNotExist();
+        // Then the review is verified
         assert.dom('[data-test-review]').containsText('Review');
+        assert.true(findSelectedClass(this, 'data-test-review'), 'The select class exists');
+
+        // And the other fields are verified
+        assert.dom('[data-test-title-and-file]').containsText('Title and File');
+        assert.false(findSelectedClass(this, 'data-test-title-and-file'), 'The select class does not exist');
+
+        assert.dom('[data-test-metadata]').containsText('Metadata');
+        assert.false(findSelectedClass(this, 'data-test-metadata'), 'The select class does not exist');
+
+        assert.dom('[data-test-author-assertions]').containsText('Author Assertions');
+        assert.false(findSelectedClass(this, 'data-test-author-assertions'), 'The select class does not exist');
+
+        assert.dom('[data-test-supplements]').containsText('Supplements');
+        assert.false(findSelectedClass(this, 'data-test-supplements'), 'The select class does not exist');
+
     });
 
     test('it display the review status when displayAuthorAssertions is false',
         async function(this: EnginesTestContext,assert) {
             // Given the statusFlowIndex is 4
             // And the displayAuthorAssertions is false
-            this.set('statusFlowIndex', 4);
-            this.set('displayAuthorAssertions', false);
+            this.set('manager', Object({
+                statusFlowIndex: 4,
+                displayAuthorAssertions: false,
+            }));
             // When the component is rendered
             await render(hbs`
-                <Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
-                    @statusFlowIndex={{this.statusFlowIndex}}
-                    @displayAuthorAssertions={{this.displayAuthorAssertions}}
-                />
+<Preprints::-Components::Submit::PreprintStateMachine::StatusFlow
+    @manager={{this.manager}}
+/>
             `);
 
-            // Then the author assertions is verified
-            assert.dom('[data-test-title-and-file]').doesNotExist();
-            assert.dom('[data-test-metadata]').doesNotExist();
-            assert.dom('[data-test-author-assertions]').doesNotExist();
-            assert.dom('[data-test-supplements]').doesNotExist();
+            // Then the review is verified
             assert.dom('[data-test-review]').containsText('Review');
-        });
+            assert.true(findSelectedClass(this, 'data-test-review'), 'The select class exists');
 
+            // And the other fields are verified
+            assert.dom('[data-test-title-and-file]').containsText('Title and File');
+            assert.false(findSelectedClass(this, 'data-test-title-and-file'), 'The select class does not exist');
+
+            assert.dom('[data-test-metadata]').containsText('Metadata');
+            assert.false(findSelectedClass(this, 'data-test-metadata'), 'The select class does not exist');
+
+            assert.dom('[data-test-author-assertions]').doesNotExist();
+
+            assert.dom('[data-test-supplements]').containsText('Supplements');
+            assert.false(findSelectedClass(this, 'data-test-supplements'), 'The select class does not exist');
+        });
 });
