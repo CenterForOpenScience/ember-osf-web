932bb8df228bb607eb9281834b48e50708fc74e9
Brian Pilati <brianpilati@cos.io>
2024-03-14 14:39:46 -0600

    Added a title and file component

--- app/preprints/-components/submit/component.ts
+++ app/preprints/-components/submit/component.ts
@@ -1,5 +1,6 @@
 import Component from '@glimmer/component';
-import PreprintStateMachine from 'ember-osf-web/preprints/-components/submit/preprint-state-machine/component';
+import PreprintStateMachine, { PreprintStatusTypeEnum } from
+    'ember-osf-web/preprints/-components/submit/preprint-state-machine/component';
 
 /**
  * The Submit Args
@@ -14,5 +15,14 @@ interface SubmitArgs {
 export default class Submit extends Component<SubmitArgs>{
     constructor(owner: unknown, args: SubmitArgs) {
         super(owner, args);
+    }
+
+    public get isTitleAndFileActive(): boolean {
+        return this.isSelected(PreprintStatusTypeEnum.titleAndFile);
+
+    }
+
+    private isSelected(type: string): boolean {
+        return this.args.manager.isSelected(type);
     }
 }
