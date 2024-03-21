ef280b926689faefd1e9903b27fe83a3e1c12fd7
Longze Chen <cslzchen@gmail.com>
2024-03-21 12:08:14 -0400

    Update mirage fixtures to allow submissions for all

--- mirage/fixtures/preprint-providers.ts
+++ mirage/fixtures/preprint-providers.ts
@@ -21,6 +21,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         footerLinks: 'fake footer links',
         reviewsWorkflow: PreprintProviderReviewsWorkFlow.PRE_MODERATION,
         allowCommenting: true,
+        allowSubmissions: true,
     },
     {
         id: 'thesiscommons',
@@ -28,6 +29,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         assets: randomAssets(2),
         // eslint-disable-next-line max-len
         footerLinks: '<p style="text-align: center;">LawArXiv:&nbsp;<a href="mailto:support+lawarxiv@osf.io">Support&nbsp;</a>|&nbsp;<a href="mailto:contact+lawarxiv@osf.io">Contact&nbsp;</a>|&nbsp;<a href="https://twitter.com/lawarxiv" target="_blank" title="LawArXiv on Twitter"><span class="fa fa-twitter fa-2x" style="vertical-align: middle;">&nbsp;</span></a></p>\n <p>LawrXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on LawArXiv by Cornell University or arXiv.</p>',
+        allowSubmissions: true,
     },
     {
         id: 'preprintrxiv',
@@ -38,6 +40,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         footerLinks: 'Removed in mirage scenario',
         reviewsCommentsPrivate: true,
         reviewsWorkflow: PreprintProviderReviewsWorkFlow.PRE_MODERATION,
+        allowSubmissions: true,
     },
     {
         id: 'paperxiv',
@@ -46,6 +49,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         assets: randomAssets(4),
         // eslint-disable-next-line max-len
         footerLinks: '<p style="text-align: right;" data-mce-style="text-align: right;">AgriXiv:&nbsp;<a href="mailto:support+agrixiv@osf.io" data-mce-href="mailto:support+agrixiv@osf.io">Support&nbsp;</a>|&nbsp;<a href="mailto:contact+agrixiv@osf.io" data-mce-href="mailto:contact+agrixiv@osf.io">Contact&nbsp;</a>|&nbsp;<a href="https://twitter.com/AgriXiv" target="_blank" title="AgriXiv on Twitter" rel="noopener" data-mce-href="https://twitter.com/AgriXiv"><span class="fa fa-twitter fa-2x" style="vertical-align: middle;" data-mce-style="vertical-align: middle;">&nbsp;</span></a>&nbsp;<a href="https://www.facebook.com/AgriXiv/" target="_blank" title="AgriXiv on Facebook" rel="noopener" data-mce-href="https://www.facebook.com/AgriXiv/"><span class="fa fa-facebook fa-2x" style="vertical-align: middle;" data-mce-style="vertical-align: middle;">&nbsp;</span></a>&nbsp;<a href="https://www.instagram.com/agrixiv/" target="_blank" title="AgriXiv on Instagram" rel="noopener" data-mce-href="https://www.instagram.com/agrixiv/"><span class="fa fa-2x fa-instagram" style="vertical-align: middle;" data-mce-style="vertical-align: middle;">&nbsp;</span></a><br data-mce-bogus="1"></p><p style="text-align: right;" data-mce-style="text-align: right;">arXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on AgriXiv by Cornell University or arXiv.</p>',
+        allowSubmissions: true,
     },
     {
         id: 'thesisrxiv',
@@ -54,6 +58,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         assets: randomAssets(5),
         // eslint-disable-next-line max-len
         footerLinks: '<p style="text-align: left;" data-mce-style="text-align: left;">AgriXiv:&nbsp;<a href="mailto:support+agrixiv@osf.io" data-mce-href="mailto:support+agrixiv@osf.io">Support&nbsp;</a>|&nbsp;<a href="mailto:contact+agrixiv@osf.io" data-mce-href="mailto:contact+agrixiv@osf.io">Contact&nbsp;</a>|&nbsp;<a href="https://twitter.com/AgriXiv" target="_blank" title="AgriXiv on Twitter" rel="noopener" data-mce-href="https://twitter.com/AgriXiv"><span class="fa fa-twitter fa-2x" style="vertical-align: middle;" data-mce-style="vertical-align: middle;">&nbsp;</span></a>&nbsp;<a href="https://www.facebook.com/AgriXiv/" target="_blank" title="AgriXiv on Facebook" rel="noopener" data-mce-href="https://www.facebook.com/AgriXiv/"><span class="fa fa-facebook fa-2x" style="vertical-align: middle;" data-mce-style="vertical-align: middle;">&nbsp;</span></a>&nbsp;<a href="https://www.instagram.com/agrixiv/" target="_blank" title="AgriXiv on Instagram" rel="noopener" data-mce-href="https://www.instagram.com/agrixiv/"><span class="fa fa-2x fa-instagram" style="vertical-align: middle;" data-mce-style="vertical-align: middle;">&nbsp;</span></a><br data-mce-bogus="1"></p><p style="text-align: left;" data-mce-style="text-align: left;">arXiv is a trademark of Cornell University, used under license. This license should not be understood to indicate endorsement of content on AgriXiv by Cornell University or arXiv.</p>',
+        allowSubmissions: true,
     },
     {
         id: 'workrxiv',
@@ -61,6 +66,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         preprintWord: 'work',
         assets: randomAssets(6),
         footerLinks: 'fake footer links',
+        allowSubmissions: true,
     },
     {
         id: 'docrxiv',
@@ -68,6 +74,7 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         preprintWord: 'default',
         assets: randomAssets(7),
         footerLinks: 'fake footer links',
+        allowSubmissions: true,
     },
     {
         id: 'agrixiv',
@@ -75,30 +82,35 @@ const preprintProviders: Array<Partial<PreprintProvider>> = [
         preprintWord: 'preprint',
         assets: randomAssets(8),
         reviewsWorkflow: PreprintProviderReviewsWorkFlow.POST_MODERATION,
+        allowSubmissions: true,
     },
     {
         id: 'biohackrxiv',
         name: 'BioHackrXiv',
         preprintWord: 'preprint',
         assets: randomAssets(9),
+        allowSubmissions: true,
     },
     {
         id: 'nutrixiv',
         name: 'NutriXiv',
         preprintWord: 'preprint',
         assets: randomAssets(10),
+        allowSubmissions: true,
     },
     {
         id: 'paleorxiv',
         name: 'PaleoRrxiv',
         preprintWord: 'preprint',
         assets: randomAssets(10, false),
+        allowSubmissions: true,
     },
     {
         id: 'sportrxiv',
         name: 'Sport-Rxiv',
         preprintWord: 'paper',
         assets: randomAssets(10),
+        allowSubmissions: true,
     },
 ];
 
