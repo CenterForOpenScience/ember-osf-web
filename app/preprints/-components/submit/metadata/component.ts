792147348b439d68c2a1ccf95efe1eb0ce8f74ca
Brian Pilati <brianpilati@cos.io>
2024-04-01 11:43:43 -0600

    Added the license component ENG-5310

--- app/preprints/-components/submit/metadata/component.ts
+++ app/preprints/-components/submit/metadata/component.ts
@@ -3,9 +3,16 @@ import Component from '@glimmer/component';
 import PreprintStateMachine from 'ember-osf-web/preprints/-components/submit/preprint-state-machine/component';
 import { action } from '@ember/object';
 import { ValidationObject } from 'ember-changeset-validations';
-import { validateFormat } from 'ember-changeset-validations/validators';
+import { validateFormat, validatePresence } from 'ember-changeset-validations/validators';
 import buildChangeset from 'ember-osf-web/utils/build-changeset';
 import { DOIRegex } from 'ember-osf-web/utils/doi';
+import Store from '@ember-data/store';
+import { inject as service } from '@ember/service';
+import { taskFor } from 'ember-concurrency-ts';
+import { task } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
+import LicenseModel from 'ember-osf-web/models/license';
+import { tracked } from '@glimmer/tracking';
 
 /**
  * The Metadata Args
@@ -18,6 +25,7 @@ interface MetadataForm {
     publicationDoi: string;
     publicationYear: number;
     publicationCitation: string;
+    license: LicenseModel;
 }
 
 const MetadataFormValidation: ValidationObject<MetadataForm> = {
@@ -35,14 +43,37 @@ const MetadataFormValidation: ValidationObject<MetadataForm> = {
         regex: /^((?!(0))[0-9]{4})$/,
         type: 'year_format',
     }),
+    license: validatePresence({
+        presence: true,
+        ignoreBlank: true,
+        type: 'empty',
+    }),
 };
 
 /**
  * The Metadata Component
  */
 export default class Metadata extends Component<MetadataArgs>{
+    @service store!: Store;
     metadataFormChangeset = buildChangeset(this.args.manager.preprint, MetadataFormValidation);
     showAddContributorWidget = true;
+    @tracked licenses = [] as LicenseModel[];
+    @tracked license!: LicenseModel;
+
+
+    constructor(owner: unknown, args: MetadataArgs) {
+        super(owner, args);
+
+        taskFor(this.loadLicenses).perform();
+    }
+
+    @task
+    @waitFor
+    private async loadLicenses()  {
+        this.licenses = await this.store.findAll('license', {
+            sort: '-name',
+        });
+    }
 
     @action
     toggleAddContributorWidget() {
