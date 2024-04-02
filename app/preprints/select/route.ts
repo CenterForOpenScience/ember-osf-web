5cab35a18ce1e4522f981e17cc4cbdd9d5212938
Brian Pilati <brianpilati@cos.io>
2024-04-02 14:41:10 -0600

    Fixed some filtering

--- app/preprints/select/route.ts
+++ app/preprints/select/route.ts
@@ -8,8 +8,11 @@ export default class PreprintSelectRoute extends Route {
     @service store!: Store;
 
     async model(){
-        const allProviders: PreprintProviderModel[] = await this.store.findAll('preprint-provider', {reload: true});
-        const submissionProviders: PreprintProviderModel[] = allProviders.filter(item => item.allowSubmissions);
+        const submissionProviders: PreprintProviderModel[] = await this.store.findAll('preprint-provider', {
+            reload: true,
+            adapterOptions: { 'filter[allowSubmissions]': 'true' },
+        });
+
         return {
             submissionProviders,
         };
