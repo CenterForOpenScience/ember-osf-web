1124ea0062a46a4288337ef369786bd895408d9a
Brian Pilati <brianpilati@cos.io>
2024-04-02 12:58:44 -0600

    Added the subjects widget

--- mirage/scenarios/preprints.ts
+++ mirage/scenarios/preprints.ts
@@ -315,7 +315,9 @@ function buildThesisCommons(
     currentUser: ModelInstance<User>,
 ) {
     const thesisCommons = server.schema.preprintProviders.find('thesiscommons') as ModelInstance<PreprintProvider>;
+
     const brand = server.create('brand', {
+
         primaryColor: '#821e1e',
         secondaryColor: '#94918e',
         heroBackgroundImage: 'https://singlecolorimage.com/get/94918e/1000x1000',
@@ -332,6 +334,7 @@ function buildThesisCommons(
 
     thesisCommons.update({
         highlightedSubjects: subjects,
+        subjects,
         brand,
         licensesAcceptable: server.schema.licenses.all(),
         moderators: [currentUserModerator],
