18926c8f50f3ffde0aaca97544cda02527c394b7
Brian Pilati <brianpilati@cos.io>
2024-04-05 09:10:05 -0600

    Updates for yuhuai to debug

--- app/preprints/-components/submit/author-assertions/data-link-widget/component.ts
+++ app/preprints/-components/submit/author-assertions/data-link-widget/component.ts
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import PreprintStateMachine from 'ember-osf-web/preprints/-components/submit/preprint-state-machine/component';
-import { action } from '@ember/object';
+import { action, notifyPropertyChange } from '@ember/object';
 import { inject as service } from '@ember/service';
 import Intl from 'ember-intl/services/intl';
 import { tracked } from '@glimmer/tracking';
@@ -29,6 +29,7 @@ export default class DataLinkWidget extends Component<DataLinkWidgetArgs>{
 
     @action
     public async addLink(): Promise<void> {
-        this.links = [...this.links, ''];
+        this.links.push('');
+        notifyPropertyChange(this, 'links');
     }
 }
