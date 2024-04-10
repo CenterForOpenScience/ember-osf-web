f2ddaef992a290039bb533dd56e0056f2d0a5f73
Futa Ikeda <futa.ikeda@gmail.com>
2024-04-10 11:56:33 -0400

    Add apiBaseUrl to AuthorizedStorageAccount

--- app/models/authorized-storage-account.ts
+++ app/models/authorized-storage-account.ts
@@ -19,6 +19,7 @@ export default class AuthorizedStorageAccountModel extends OsfModel {
     @attr('fixstring') externalUserDisplayName!: string;
     @attr('fixstringarray') scopes!: string[];
     @attr('object') credentials?: AddonCredentialFields; // write-only
+    @attr('fixstring') apiBaseUrl!: string; // Only applicable when ExternalStorageService.configurableApiRoot
     @attr('fixstring') readonly authUrl!: string; // Only returned when POSTing to /authorized-storage-accounts
     @attr('boolean') readonly credentialsAvailable!: boolean;
 
