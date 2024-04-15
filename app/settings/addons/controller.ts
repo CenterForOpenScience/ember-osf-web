d393234ed7007469bed7cf7a71fa9fc30fe69497
Futa Ikeda <futa.ikeda@gmail.com>
2024-04-15 17:45:33 -0400

    Update user settings page to show lists of addons and lists of accounts

--- app/settings/addons/controller.ts
+++ app/settings/addons/controller.ts
@@ -1,8 +1,14 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import Media from 'ember-responsive';
 
 import CurrentUser from 'ember-osf-web/services/current-user';
 
 export default class SettingsAddonsController extends Controller {
     @service currentUser!: CurrentUser;
+    @service media!: Media;
+
+    get isMobile(): boolean {
+        return this.media.isMobile;
+    }
 }
