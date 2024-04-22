6ab1790fa1cf75abfb4568c5a9a68c730a61a1f3
Futa Ikeda <futa.ikeda@gmail.com>
2024-04-22 17:14:32 -0400

    Use ember-responsive native helpers

--- app/settings/addons/controller.ts
+++ app/settings/addons/controller.ts
@@ -1,14 +1,8 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import Media from 'ember-responsive';
 
 import CurrentUser from 'ember-osf-web/services/current-user';
 
 export default class SettingsAddonsController extends Controller {
     @service currentUser!: CurrentUser;
-    @service media!: Media;
-
-    get isMobile(): boolean {
-        return this.media.isMobile;
-    }
 }
