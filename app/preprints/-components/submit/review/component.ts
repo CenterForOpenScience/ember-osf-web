4b70356e1c387c7d184a6fde21ebc545ff70d262
Brian Pilati <brianpilati@cos.io>
2024-04-29 10:00:58 -0600

    Added finishing upates for the pr comments

--- app/preprints/-components/submit/review/component.ts
+++ app/preprints/-components/submit/review/component.ts
@@ -6,6 +6,8 @@ import { taskFor } from 'ember-concurrency-ts';
 import PreprintStateMachine from 'ember-osf-web/preprints/-components/submit/preprint-state-machine/component';
 import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
+import moment from 'moment-timezone';
+import Intl from 'ember-intl/services/intl';
 
 /**
  * The Review Args
@@ -24,6 +26,7 @@ export default class Review extends Component<ReviewArgs>{
     @tracked license?: any;
     @tracked contributors?: any;
     @tracked subjects?: any;
+    @service intl!: Intl;
 
     constructor(owner: unknown, args: ReviewArgs) {
         super(owner, args);
@@ -34,14 +37,26 @@ export default class Review extends Component<ReviewArgs>{
     @task
     @waitFor
     private async loadPreprint()  {
+        this.preprint = await this.store.findRecord('preprint', 'osf-not-contributor');
         // this.preprint = await this.store.findRecord('preprint', 'osf-approved-admin');
-        this.provider = this.preprint.provider;
+        this.provider = this.preprint.provider.content;
         this.license = this.preprint.license;
         this.subjects = await this.preprint.queryHasMany('subjects');
     }
 
     public get providerLogo(): string | undefined {
         return this.provider.get('assets')?.square_color_no_transparent;
+    }
 
+    public  get displayPublicationDate(): string {
+        return this.preprint.originalPublicationDate
+            ? moment(this.preprint.originalPublicationDate).format('YYYY-MM-DD')
+            : this.intl.t('general.not-applicable');
+    }
+
+    public  get displayPublicationCitation(): string {
+        return this.preprint.originalPublicationCitation
+            ? this.preprint.originalPublicationCitation
+            : this.intl.t('general.not-applicable');
     }
 }
