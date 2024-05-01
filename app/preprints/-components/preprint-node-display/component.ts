08d911321f31c40721dd7b8e2fdfc79f6d67a103
Brian Pilati <brianpilati@cos.io>
2024-05-01 12:49:42 -0600

    Added the supplements to the review page

---
+++ app/preprints/-components/preprint-node-display/component.ts
@@ -0,0 +1,48 @@
+import Component from '@glimmer/component';
+import PreprintModel from 'ember-osf-web/models/preprint';
+import { inject as service } from '@ember/service';
+import Intl from 'ember-intl/services/intl';
+import { task } from 'ember-concurrency';
+import { waitFor } from '@ember/test-waiters';
+import Store from '@ember-data/store';
+import { taskFor } from 'ember-concurrency-ts';
+import NodeModel from 'ember-osf-web/models/node';
+
+interface NodeDisplayArgs {
+    preprint: PreprintModel;
+    preprintWord: string;
+}
+
+export default class PreprintNodeDisplay extends Component<NodeDisplayArgs> {
+    @service intl!: Intl;
+    @service store!: Store;
+
+    preprint = this.args.preprint;
+    node!: NodeModel;
+    nodeId!: string;
+
+    constructor(owner: unknown, args: NodeDisplayArgs) {
+        super(owner, args);
+
+        this.nodeId = this.preprint.belongsTo('node').id();
+        taskFor(this.loadNode).perform();
+
+    }
+
+    get nodeDisplay(): string {
+        if (this.nodeId) {
+            return this.node.title;
+        } else {
+            return this.intl.t('preprints.submit.step-five.supplement-na',
+                { singularPreprintWord: this.args.preprintWord});
+        }
+    }
+
+    @task
+    @waitFor
+    private async loadNode()  {
+        if (this.nodeId) {
+            this.node = await this.store.findRecord('node', this.nodeId);
+        }
+    }
+}
