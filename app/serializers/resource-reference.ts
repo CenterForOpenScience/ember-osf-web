8bcc1d9923b912ff32a96e2535e747862dbf99f0
Futa Ikeda <futa.ikeda@gmail.com>
2024-05-02 16:07:07 -0400

    Test fix

--- app/serializers/resource-reference.ts
+++ app/serializers/resource-reference.ts
@@ -1,6 +1,10 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
+import { dasherize } from '@ember/string';
 
 export default class ResourceReferenceSerializer extends JSONAPISerializer {
+    keyForRelationship(key: string) {
+        return dasherize(key);
+    }
 }
 
 declare module 'ember-data/types/registries/serializer' {
