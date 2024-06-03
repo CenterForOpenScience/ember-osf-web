8f1aca10f581bbd199ad4285dd3273ac35e9536f
Brian Pilati <brianpilati@cos.io>
2024-06-03 12:16:18 -0600

    2 quick clean-up items

--- app/preprints/-components/submit/title-and-abstract/component.ts
+++ app/preprints/-components/submit/title-and-abstract/component.ts
@@ -31,11 +31,6 @@ export default class TitleAndAbstract extends Component<TitleAndAbstractArgs>{
             type: 'empty',
         }),
         description: [
-            validatePresence({
-                presence: true,
-                ignoreBlank: true,
-                type: 'empty',
-            }),
             validateLength({
                 min: 20,
                 type: 'greaterThanOrEqualTo',
