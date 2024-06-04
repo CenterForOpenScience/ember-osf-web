edfcf6e1f34fcf1137634fad33ff94119b6aa5da
Uditi Mehta <uditimehta@COSs-MacBook-Pro.local>
2024-06-04 15:21:04 -0400

    Fix errors in subject model

--- mirage/models/subject.ts
+++ mirage/models/subject.ts
@@ -1,6 +1,6 @@
 import { Model, belongsTo, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
-    parent: belongsTo('subject'),
+    parent: belongsTo('subject', { inverse: 'children' }),
     children: hasMany('subject'),
 });
