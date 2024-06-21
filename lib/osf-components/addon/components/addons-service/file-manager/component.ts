c6b4d8a0cbac8a1898422a2895aba68b73d40fab
Futa Ikeda <futa.ikeda@gmail.com>
2024-06-21 10:07:18 -0400

    Add file-manager for addon-service

---
+++ lib/osf-components/addon/components/addons-service/file-manager/component.ts
@@ -0,0 +1,85 @@
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+import { waitFor } from '@ember/test-waiters';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { task } from 'ember-concurrency';
+import { taskFor } from 'ember-concurrency-ts';
+import IntlService from 'ember-intl/services/intl';
+import Toast from 'ember-toastr/services/toast';
+
+import { Item } from 'ember-osf-web/models/addon-operation-invocation';
+import ConfiguredStorageAddonModel, { OperationKwargs } from 'ember-osf-web/models/configured-storage-addon';
+
+interface Args {
+    configuredStorageAddon: ConfiguredStorageAddonModel;
+    defaultKwargs?: OperationKwargs;
+    startingFolderId: string;
+}
+
+export default class FileManager extends Component<Args> {
+    @service intl!: IntlService;
+    @service toast!: Toast;
+
+    @tracked currentPath: Item[] = [];
+    @tracked currentItems: Item[] = [];
+    @tracked currentFolderId = this.args.startingFolderId;
+
+    @tracked cursor = '';
+    @tracked hasMore = false;
+
+    private lastInvocation: any = null;
+
+    get isLoading() {
+        return taskFor(this.args.configuredStorageAddon.getFolderItems).isRunning;
+    }
+
+    constructor(owner: unknown, args: Args) {
+        super(owner, args);
+        taskFor(this.getItems).perform();
+    }
+
+
+    @action
+    goToRoot() {
+        this.cursor = '';
+        this.currentPath = [];
+        this.currentFolderId = this.args.startingFolderId;
+        taskFor(this.getItems).perform();
+    }
+
+    @action
+    goToFolder(folder: Item) {
+        this.cursor = '';
+        if (this.currentPath.includes(folder)) {
+            this.currentPath = this.currentPath.slice(0, this.currentPath.indexOf(folder) + 1);
+        } else {
+            this.currentPath = [...this.currentPath, folder];
+        }
+        this.currentFolderId = folder.itemId;
+        taskFor(this.getItems).perform();
+    }
+
+    @action
+    getMore() {
+        this.cursor = this.lastInvocation?.operationResult.cursor;
+        taskFor(this.getItems).perform();
+    }
+
+    @task
+    @waitFor
+    async getItems() {
+        const kwargs = this.args.defaultKwargs || {};
+        kwargs.itemId = this.currentFolderId;
+        kwargs.pageCursor = this.cursor;
+        try {
+            const invocation = await taskFor(this.args.configuredStorageAddon.getFolderItems).perform(kwargs);
+            this.lastInvocation = invocation;
+            const { operationResult } = invocation;
+            this.currentItems = this.cursor ? [...this.currentItems, ...operationResult.items] : operationResult.items;
+            this.hasMore = Boolean(invocation.operationResult.cursor);
+        } catch (e) {
+            this.toast.error(this.intl.t('osf-components.addons-service.file-manager.error'));
+        }
+    }
+}
