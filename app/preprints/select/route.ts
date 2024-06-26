bddebc3058f0713df51bb830f02835b2f81bd035
Brian Pilati <brianpilati@cos.io>
2024-06-26 13:08:57 -0500

    Fixed the branded issue

--- app/preprints/select/route.ts
+++ app/preprints/select/route.ts
@@ -3,16 +3,20 @@ import Route from '@ember/routing/route';
 import Store from '@ember-data/store';
 import PreprintProviderModel from 'ember-osf-web/models/preprint-provider';
 import requireAuth from 'ember-osf-web/decorators/require-auth';
+import Theme from 'ember-osf-web/services/theme';
 
 @requireAuth()
 export default class PreprintSelectRoute extends Route {
     @service store!: Store;
+    @service theme!: Theme;
 
     async model(){
         const submissionProviders: PreprintProviderModel[] = await this.store.findAll('preprint-provider', {
             reload: true,
             adapterOptions: { 'filter[allowSubmissions]': 'true' },
         });
+
+        this.theme.reset();
 
         return {
             submissionProviders,
