c9c1a1ee81a1e563a9eb5fd0da5b38f76dc9268b
Brian Pilati <brianpilati@cos.io>
2024-08-07 15:19:58 -0500

    Added more tests and updates

--- app/preprints/-components/preprint-institutions/institution-manager/component-test.ts
+++ app/preprints/-components/preprint-institutions/institution-manager/component-test.ts
@@ -14,27 +14,53 @@ module('Integration | Preprint | Component | Institution Manager', hooks => {
     setupMirage(hooks);
     setupIntl(hooks);
 
-    test('it renders', async function(assert) {
+    test('it renders with 4 user institutions and 1 affiliated preprint institution', async function(assert) {
+        // Given the providers are loaded
         server.loadFixtures('preprint-providers');
+        const store = this.owner.lookup('service:store');
         const osf = server.schema.preprintProviders.find('osf') as ModelInstance<PreprintProvider>;
 
+        // And create a preprint with affiliated institutions
         const preprintMock = server.create('preprint', { provider: osf }, 'withAffiliatedInstitutions');
 
-        const store = this.owner.lookup('service:store');
+        // And retrieve the preprint from the store
         const preprint: PreprintModel = await store.findRecord('preprint', preprintMock.id);
+
+        // And create a user for the service with institutions
+        const user = server.create('user', { id: 'institution-user' }, 'withInstitutions');
+
+        // And find the osf institutions from the preprint
+        const institution = server.schema.institutions.find('osf');
+        // And update the user with the union
+        user.institutions.models.push(institution);
+        user.save();
+
+        // And find and set the user for the service
+        const currentUserModel = await store.findRecord('user', 'institution-user');
+
+        this.owner.lookup('service:current-user').setProperties({
+            testUser: currentUserModel, currentUserId: currentUserModel.id,
+        });
+
+        // And bind the preprint to the test
         this.set('preprintMock', preprint);
 
-
+        // When the component is rendered
         await render(hbs`
 <Preprints::-Components::PreprintInstitutions::InstitutionManager
         @preprint={{this.preprintMock}} as |manager|>
-    {{#each manager.affiliatedList as |affiliate index|}}
-        <div data-test-institution={{index}}>
-            {{affiliate.name}}
-        </div>
-    {{/each}}
+    <Preprints::-Components::PreprintInstitutions::InstitutionSelectList
+                @manager={{manager}} />
 </Preprints::-Components::PreprintInstitutions::InstitutionManager> `);
 
-        assert.dom('[data-test-institution="0"]').hasText('Main OSF Test Institution');
+        // Then the first attribute is verified by name and selected
+        assert.dom('[data-test-institution-name="0"]').hasText('Main OSF Test Institution');
+        assert.dom('[data-test-institution-input="0"]').isChecked();
+
+        // And the other institutions are verified as not selected
+        assert.dom('[data-test-institution-input="1"]').isNotChecked();
+        assert.dom('[data-test-institution-input="2"]').isNotChecked();
+        assert.dom('[data-test-institution-input="3"]').isNotChecked();
+        assert.dom('[data-test-institution-input="4"]').doesNotExist();
     });
 });
