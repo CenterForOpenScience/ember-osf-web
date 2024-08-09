b70e96fd73d78032e3044104526932abbf85b9f9
Brian Pilati <brianpilati@cos.io>
2024-08-09 11:30:07 -0500

    Added a description for the institutional affiliations

--- app/preprints/-components/preprint-institutions/institution-select-list/component.ts
+++ app/preprints/-components/preprint-institutions/institution-select-list/component.ts
@@ -1,4 +1,6 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import Intl from 'ember-intl/services/intl';
 import InstitutionsManagerComponent from '../institution-manager/component';
 
 
@@ -10,10 +12,17 @@ interface InstitutionSelectListArgs {
 }
 
 export default class InstitutionSelectList extends Component<InstitutionSelectListArgs> {
+    @service intl!: Intl;
+
     // Required
     manager = this.args.manager;
 
     public get displayComponent(): boolean {
         return this.args.manager.institutions.length > 0;
+    }
+
+    public get descriptionDisplay(): string {
+        return this.intl.t('preprints.submit.step-metadata.institutions.description',
+            { singularPreprintWord: this.manager.preprintWord, htmlSafe: true}) as string;
     }
 }
