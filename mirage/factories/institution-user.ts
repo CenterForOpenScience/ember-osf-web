e7c719fdd172f9096ab3407efcb035fc9afd8550
John Tordoff <>
2024-08-26 11:28:55 -0400

    update user model for institutional dashboard

--- mirage/factories/institution-user.ts
+++ mirage/factories/institution-user.ts
@@ -14,6 +14,36 @@ export default Factory.extend<InstitutionUser>({
     privateProjects() {
         return faker.random.number({ min: 0, max: 99 });
     },
+    publicRegistrationCount() {
+        return faker.random.number({ min: 0, max: 50 });
+    },
+    embargoedRegistrationCount() {
+        return faker.random.number({ min: 0, max: 50 });
+    },
+    publishedPreprintCount() {
+        return faker.random.number({ min: 0, max: 50 });
+    },
+    publicFileCount() {
+        return faker.random.number({ min: 0, max: 100 });
+    },
+    storageByteCount() {
+        return faker.random.number({ min: 1e7, max: 1e9 }); // Between 10MB and 1GB
+    },
+    totalObjectCount() {
+        return faker.random.number({ min: 0, max: 1000 });
+    },
+    monthLastLogin() {
+        return faker.date.past(1); // Any date within the past year
+    },
+    monthLastActive() {
+        return faker.date.past(1); // Any date within the past year
+    },
+    accountCreationDate() {
+        return faker.date.past(10); // Any date within the past 10 years
+    },
+    orcidId() {
+        return faker.random.uuid(); // Simulate an ORCID ID
+    },
     afterCreate(institutionUser, server) {
         if (!institutionUser.userName && !institutionUser.userGuid) {
             const user = server.create('user');
