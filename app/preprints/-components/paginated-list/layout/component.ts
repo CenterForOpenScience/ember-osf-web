37ec5681f8e983993961fb8dc6fc3a63c24bd93e
Uditi Mehta <uditimehta@coss-mbp.myfiosgateway.com>
2024-09-03 11:09:50 -0400

    Moved changes to preprints-paginated-list branch

---
+++ app/preprints/-components/paginated-list/layout/component.ts
@@ -0,0 +1,47 @@
+import Component from '@ember/component';
+import { computed } from '@ember/object';
+
+import { layout, requiredAction } from 'ember-osf-web/decorators/component';
+
+import template from './template';
+
+@layout(template)
+export default class PaginatedList extends Component {
+    // Required arguments
+    items?: unknown[];
+    page!: number;
+    pageSize!: number;
+    @requiredAction next!: () => void;
+    @requiredAction previous!: () => void;
+    @requiredAction doReload!: () => void;
+
+    // Optional arguments
+    loading = false;
+    errorShown = false;
+    totalCount?: number;
+
+    // Private properties
+    @computed('totalCount', 'pageSize')
+    get maxPage() {
+        if (typeof this.totalCount === 'undefined') {
+            return undefined;
+        }
+        return Math.ceil(this.totalCount / this.pageSize);
+    }
+
+    @computed('maxPage', 'page', 'pageSize', 'totalCount')
+    get placeholderCount() {
+        if (typeof this.maxPage === 'undefined' || typeof this.totalCount === 'undefined') {
+            return this.pageSize / 2;
+        }
+        if (this.page < this.maxPage || !(this.totalCount % this.pageSize)) {
+            return this.pageSize;
+        }
+        return this.totalCount % this.pageSize;
+    }
+
+    @computed('items.length', 'loading', 'placeholderCount')
+    get paginatorShown(): boolean {
+        return Boolean((this.items && this.items.length) || (this.loading && this.placeholderCount));
+    }
+}
