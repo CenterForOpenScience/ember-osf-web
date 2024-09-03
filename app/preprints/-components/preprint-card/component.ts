0e5b586ac6fa45bedeaff1f7ad6e49a9a7451d10
Uditi Mehta <uditimehta@coss-mbp.myfiosgateway.com>
2024-09-03 11:20:28 -0400

    remove unused classes and services

--- app/preprints/-components/preprint-card/component.ts
+++ app/preprints/-components/preprint-card/component.ts
@@ -1,18 +1,12 @@
 import { tagName } from '@ember-decorators/component';
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
 import config from 'ember-osf-web/config/environment';
-import Store from '@ember-data/store';
 
 import { layout } from 'ember-osf-web/decorators/component';
 import Preprint from 'ember-osf-web/models/preprint';
-import Analytics from 'ember-osf-web/services/analytics';
 import pathJoin from 'ember-osf-web/utils/path-join';
 import { Permission } from 'ember-osf-web/models/osf-model';
-import Toast from 'ember-toastr/services/toast';
 
-import RouterService from '@ember/routing/router-service';
-import Intl from 'ember-intl/services/intl';
 import template from './template';
 import styles from './styles';
 
@@ -21,11 +15,6 @@ const { OSF: { url: baseURL } } = config;
 @layout(template, styles)
 @tagName('')
 export default class PreprintCard extends Component {
-    @service analytics!: Analytics;
-    @service router!: RouterService;
-    @service store!: Store;
-    @service toast!: Toast;
-    @service intl!: Intl;
 
     preprint?: Preprint;
     delete?: (preprint: Preprint) => void;
