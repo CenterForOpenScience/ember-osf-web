c32c821f1f2cac85f02fc64b99aad2d6637a6628
John Tordoff <>
2024-09-04 16:01:21 -0400

    update for linting

--- app/components/sort-arrow/component.ts
+++ app/components/sort-arrow/component.ts
@@ -1,5 +1,4 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 interface SortArrowArgs {
@@ -10,27 +9,25 @@ interface SortArrowArgs {
 
 export default class SortArrow extends Component<SortArrowArgs> {
 
-  get sortBy() {
-    return this.args.sortBy || 'user_name';
-  }
-
-  get isCurrentAscending() {
-    return this.args.sort === this.args.sortBy;
-  }
-
-  get isCurrentDescending() {
-    return this.args.sort === `-${this.sortBy}`;
-  }
-
-  get isSelected() {
-    return this.isCurrentAscending || this.isCurrentDescending;
-  }
-
-  @action
-  handleSort() {
-    if (this.args.sortAction) {  // Make sure sortAction is a function
-      this.args.sortAction(this.args.sortBy);
+    get sortBy() {
+        return this.args.sortBy || 'user_name';
     }
-  }
+
+    get isCurrentAscending() {
+        return this.args.sort === this.args.sortBy;
+    }
+
+    get isCurrentDescending() {
+        return this.args.sort === `-${this.sortBy}`;
+    }
+
+    get isSelected() {
+        return this.isCurrentAscending || this.isCurrentDescending;
+    }
+
+    @action
+    handleSort() {
+        this.args.sortAction(this.args.sortBy);
+    }
 }
 
--- tests/integration/components/sort-arrow/component-test.ts
+++ tests/integration/components/sort-arrow/component-test.ts
@@ -1,26 +1,82 @@
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import { setupRenderingTest } from 'ember-qunit';
 import { module, test } from 'qunit';
 
-module('Integration | Component | sort-arrow', hooks => {
+module('Integration | Component | sort-arrow', function(hooks) {
     setupRenderingTest(hooks);
 
-    test('it renders', async function(assert) {
-        // Set any properties with this.set('myProperty', 'value');
-        // Handle any actions with this.set('myAction', function(val) { ... });
+    test('it renders without any content by default', async function(assert) {
+        await render(hbs`<SortArrow />`);
 
-        await render(hbs`{{sort-arrow}}`);
+        assert.dom('[data-test-sort]').exists('The sort arrow renders correctly');
+        assert
+            .dom('[data-test-sort]')
+            .hasNoText('It has no text by default');
+    });
 
-        assert.dom(this.element).hasText('');
-
-        // Template block usage:
+    test('it renders with block content', async function(assert) {
         await render(hbs`
-            {{#sort-arrow}}
-                template block text
-            {{/sort-arrow}}
-        `);
+      <SortArrow>
+        Block content
+      </SortArrow>
+    `);
 
-        assert.dom(this.element).hasText('template block text');
+        assert
+            .dom(this.element)
+            .hasText('Block content', 'The sort arrow renders block content correctly');
+    });
+
+    test('it renders the correct icon and state when ascending', async function(assert) {
+        this.set('sortBy', 'user_name');
+        this.set('sort', 'user_name');
+        await render(hbs`<SortArrow @sort={{this.sort}} @sortBy={{this.sortBy}} />`);
+
+        assert
+            .dom('[data-test-sort="user_name"] .fa-icon')
+            .hasClass('fa-arrow-up', 'Displays the correct arrow-up icon when sort is ascending');
+    });
+
+    test('it renders the correct icon and state when descending', async function(assert) {
+        this.set('sortBy', 'user_name');
+        this.set('sort', '-user_name');
+        await render(hbs`<SortArrow @sort={{this.sort}} @sortBy={{this.sortBy}} />`);
+
+        assert
+            .dom('[data-test-sort="user_name"] .fa-icon')
+            .hasClass('fa-arrow-down', 'Displays the correct arrow-down icon when sort is descending');
+    });
+
+    test('it triggers the sort action on click', async function(assert) {
+        assert.expect(1);
+
+        this.set('sortBy', 'user_name');
+        this.set('sortAction', sortField => {
+            assert.strictEqual(
+                sortField,
+                'user_name',
+                'sortAction was called with the correct sort field',
+            );
+        });
+
+        await render(hbs`<SortArrow @sortBy={{this.sortBy}} @sortAction={{this.sortAction}} />`);
+
+        await click('[data-test-sort="user_name"]');
+    });
+
+    test('it applies the correct attributes and classes', async function(assert) {
+        this.set('sortBy', 'user_name');
+        this.set('sort', 'user_name');
+        await render(hbs`<SortArrow @sort={{this.sort}} @sortBy={{this.sortBy}} />`);
+
+        assert
+            .dom('[data-test-sort="user_name"]')
+            .hasAttribute('data-analytics-name', 'Sort user_name', 'The correct data-analytics-name is applied');
+        assert
+            .dom('[data-test-sort="user_name"]')
+            .hasAttribute('title', 'Sort Descending', 'The correct title attribute is applied when ascending');
+        assert
+            .dom('[data-test-sort="user_name"]')
+            .hasAttribute('aria-label', 'Sort Descending', 'The correct aria-label is applied when ascending');
     });
 });
