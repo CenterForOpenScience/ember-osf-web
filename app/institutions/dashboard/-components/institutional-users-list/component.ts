dd4e5218396b210eee1c56e7bb5fa3af5f34f4b3
John Tordoff <>
2024-09-11 14:14:36 -0400

    fix indentation for linting and spruce up tests

--- app/institutions/dashboard/-components/institutional-users-list/component.ts
+++ app/institutions/dashboard/-components/institutional-users-list/component.ts
@@ -11,155 +11,159 @@ import InstitutionDepartmentsModel from 'ember-osf-web/models/institution-depart
 import Analytics from 'ember-osf-web/services/analytics';
 
 interface Column {
-  key: string;
-  value: boolean;
-  label: string;
-  type: 'string' | 'date_by_month' | 'osf_link' | 'user_name';
+    key: string;
+    value: boolean;
+    label: string;
+    type: 'string' | 'date_by_month' | 'osf_link' | 'user_name';
 }
 
 interface InstitutionalUsersListArgs {
-  institution: InstitutionModel;
-  departmentMetrics: InstitutionDepartmentsModel[];
+    institution: InstitutionModel;
+    departmentMetrics: InstitutionDepartmentsModel[];
 }
 
 export default class InstitutionalUsersList extends Component<InstitutionalUsersListArgs> {
-  @service analytics!: Analytics;
-  @service intl!: Intl;
+    @service analytics!: Analytics;
+    @service intl!: Intl;
 
-  // Properties
-  @tracked department = this.defaultDepartment;
-  @tracked sort = 'user_name';
-  @tracked selectedDepartments: string[] = [];
-  @tracked selectedColumns: string[] = this.defaultSelectedColumns;
-  @tracked filteredUsers = [];
+    institution?: InstitutionModel;
 
-  @tracked columns: Column[] = [
-      {
-          key: 'user_name',
-          sort_key: 'user_name',
-          label: this.intl.t('institutions.dashboard.users_list.name'),
-          value: true,
-          type: 'user_name',
-      },
-      {
-          key: 'department',
-          sort_key: 'department',
-          label: this.intl.t('institutions.dashboard.users_list.department'),
-          value: true,
-          type: 'string',
-      },
-      {
-          key: 'osf_link',
-          sort_key: false,
-          label: this.intl.t('institutions.dashboard.users_list.osf_link'),
-          value: true,
-          type: 'osf_link',
-      },
-      {
-          key: 'publicProjects',
-          sort_key: 'public_projects',
-          label: this.intl.t('institutions.dashboard.users_list.public_projects'),
-          value: true,
-          type: 'string',
-      },
-      {
-          key: 'privateProjects',
-          sort_key: 'private_projects',
-          label: this.intl.t('institutions.dashboard.users_list.private_projects'),
-          value: true,
-          type: 'string',
-      },
-      {
-          key: 'publicRegistrationCount',
-          sort_key: 'public_registration_count',
-          label: this.intl.t('institutions.dashboard.users_list.public_registration_count'),
-          value: true,
-          type: 'string',
-      },
-      {
-          key: 'embargoedRegistrationCount',
-          sort_key: 'embargoed_registration_count',
-          label: this.intl.t('institutions.dashboard.users_list.private_registration_count'),
-          value: true,
-          type: 'string',
-      },
-      {
-          key: 'publishedPreprintCount',
-          sort_key: 'published_preprint_count',
-          label: this.intl.t('institutions.dashboard.users_list.published_preprint_count'),
-          value: true,
-          type: 'string',
-      },
-      {
-          key: 'publicFileCount',
-          sort_key: 'public_file_count',
-          label: this.intl.t('institutions.dashboard.users_list.public_file_count'),
-          value: false,
-          type: 'string',
-      },
-      {
-          key: 'userDataUsage',
-          sort_key: 'storage_byte_count',
-          label: this.intl.t('institutions.dashboard.users_list.storage_byte_count'),
-          value: false,
-          type: 'string',
-      },
-      {
-          key: 'accountCreationDate',
-          sort_key: 'account_creation_date',
-          label: this.intl.t('institutions.dashboard.users_list.account_created'),
-          value: false,
-          type: 'date_by_month',
-      },
-      {
-          key: 'monthLastLogin',
-          sort_key: 'month_last_login',
-          label: this.intl.t('institutions.dashboard.users_list.month_last_login'),
-          value: false,
-          type: 'date_by_month',
-      },
-      {
-          key: 'monthLastActive',
-          sort_key: 'month_last_active',
-          label: this.intl.t('institutions.dashboard.users_list.month_last_active'),
-          value: false,
-          type: 'date_by_month',
-      },
-  ];
+    departmentMetrics?: InstitutionDepartmentsModel[];
 
-  @tracked selectedColumns: string[] = this.columns.filter(col => col.value).map(col => col.key);
-  // Private properties
-  @tracked department = this.intl.t('institutions.dashboard.select_default');
-  @tracked sort = 'user_name';
-  @tracked hasOrcid = false;
+    // Properties
+    @tracked department = this.defaultDepartment;
+    @tracked sort = 'user_name';
+    @tracked selectedDepartments: string[] = [];
+    @tracked selectedColumns: string[] = this.defaultSelectedColumns;
+    @tracked filteredUsers = [];
 
-  reloadUserList?: () => void;
+    @tracked columns: Column[] = [
+        {
+            key: 'user_name',
+            sort_key: 'user_name',
+            label: this.intl.t('institutions.dashboard.users_list.name'),
+            value: true,
+            type: 'user_name',
+        },
+        {
+            key: 'department',
+            sort_key: 'department',
+            label: this.intl.t('institutions.dashboard.users_list.department'),
+            value: true,
+            type: 'string',
+        },
+        {
+            key: 'osf_link',
+            sort_key: false,
+            label: this.intl.t('institutions.dashboard.users_list.osf_link'),
+            value: true,
+            type: 'osf_link',
+        },
+        {
+            key: 'publicProjects',
+            sort_key: 'public_projects',
+            label: this.intl.t('institutions.dashboard.users_list.public_projects'),
+            value: true,
+            type: 'string',
+        },
+        {
+            key: 'privateProjects',
+            sort_key: 'private_projects',
+            label: this.intl.t('institutions.dashboard.users_list.private_projects'),
+            value: true,
+            type: 'string',
+        },
+        {
+            key: 'publicRegistrationCount',
+            sort_key: 'public_registration_count',
+            label: this.intl.t('institutions.dashboard.users_list.public_registration_count'),
+            value: true,
+            type: 'string',
+        },
+        {
+            key: 'embargoedRegistrationCount',
+            sort_key: 'embargoed_registration_count',
+            label: this.intl.t('institutions.dashboard.users_list.private_registration_count'),
+            value: true,
+            type: 'string',
+        },
+        {
+            key: 'publishedPreprintCount',
+            sort_key: 'published_preprint_count',
+            label: this.intl.t('institutions.dashboard.users_list.published_preprint_count'),
+            value: true,
+            type: 'string',
+        },
+        {
+            key: 'publicFileCount',
+            sort_key: 'public_file_count',
+            label: this.intl.t('institutions.dashboard.users_list.public_file_count'),
+            value: false,
+            type: 'string',
+        },
+        {
+            key: 'userDataUsage',
+            sort_key: 'storage_byte_count',
+            label: this.intl.t('institutions.dashboard.users_list.storage_byte_count'),
+            value: false,
+            type: 'string',
+        },
+        {
+            key: 'accountCreationDate',
+            sort_key: 'account_creation_date',
+            label: this.intl.t('institutions.dashboard.users_list.account_created'),
+            value: false,
+            type: 'date_by_month',
+        },
+        {
+            key: 'monthLastLogin',
+            sort_key: 'month_last_login',
+            label: this.intl.t('institutions.dashboard.users_list.month_last_login'),
+            value: false,
+            type: 'date_by_month',
+        },
+        {
+            key: 'monthLastActive',
+            sort_key: 'month_last_active',
+            label: this.intl.t('institutions.dashboard.users_list.month_last_active'),
+            value: false,
+            type: 'date_by_month',
+        },
+    ];
 
-  @action
-  toggleColumnSelection(columnKey: string) {
-      const column = this.columns.find(col => col.key === columnKey);
-      if (column) {
-          column.value = !column.value;
-      }
-  }
+    @tracked selectedColumns: string[] = this.columns.filter(col => col.value).map(col => col.key);
+    // Private properties
+    @tracked department = this.intl.t('institutions.dashboard.select_default');
+    @tracked sort = 'user_name';
+    @tracked hasOrcid = false;
 
-  get defaultDepartment() {
-      return this.intl.t('institutions.dashboard.select_default');
-  }
+    reloadUserList?: () => void;
 
-  get departments() {
-      let departments = [this.defaultDepartment];
+    @action
+    toggleColumnSelection(columnKey: string) {
+        const column = this.columns.find(col => col.key === columnKey);
+        if (column) {
+            column.value = !column.value;
+        }
+    }
 
-      if (this.args.institution && this.args.departmentMetrics) {
-          const institutionDepartments = this.args.departmentMetrics.map((x: InstitutionDepartmentsModel) => x.name);
-          departments = departments.concat(institutionDepartments);
-      }
-      return departments;
-  }
+    get defaultDepartment() {
+        return this.intl.t('institutions.dashboard.select_default');
+    }
 
-  get isDefaultDepartment() {
-      return this.department === this.defaultDepartment;
-  }
+    get departments() {
+        let departments = [this.defaultDepartment];
+
+        if (this.args.institution && this.args.departmentMetrics) {
+            const institutionDepartments = this.args.departmentMetrics.map((x: InstitutionDepartmentsModel) => x.name);
+            departments = departments.concat(institutionDepartments);
+        }
+        return departments;
+    }
+
+    get isDefaultDepartment() {
+        return this.department === this.defaultDepartment;
+    }
 
     get queryUsers() {
         const query = {} as Record<string, string>;
@@ -175,83 +179,59 @@ export default class InstitutionalUsersList extends Component<InstitutionalUsers
         return query;
     }
 
-  get filteredColumns() {
-      return this.columns.filter(column => this.selectedColumns.includes(column.key));
-  }
+    @restartableTask
+    @waitFor
+    async searchDepartment(name: string) {
+        await timeout(500);
+        if (this.institution) {
+            const depts: InstitutionDepartmentsModel[] = await this.args.institution.queryHasMany('departmentMetrics', {
+                filter: {
+                    name,
+                },
+            });
+            return depts.map(dept => dept.name);
+        }
+        return [];
+    }
 
+    @action
+    onSelectChange(department: string) {
+        this.department = department;
+    }
 
-  @restartableTask
-  @waitFor
-  async searchDepartment(name: string) {
-      await timeout(500);
-      if (this.institution) {
-          const depts: InstitutionDepartmentsModel[] = await this.args.institution.queryHasMany('departmentMetrics', {
-              filter: { name },
-          });
-          return depts.map(dept => dept.name);
-      }
-      return [];
-  }
+    @action
+    sortInstitutionalUsers(sortBy: string) {
+        if (this.sort === sortBy) {
+            // If the current sort is ascending, toggle to descending
+            this.sort = `-${sortBy}`;
+        } else if (this.sort === `-${sortBy}`) {
+            // If the current sort is descending, toggle to ascending
+            this.sort = sortBy;
+        } else {
+            // Set to descending if it's a new sort field
+            this.sort = `-${sortBy}`;
+        }
+    }
 
-  @action
-  sortInstitutionalUsers(sortBy: string) {
-      if (this.sort === sortBy) {
-          this.sort = `-${sortBy}`; // Toggle to descending
-      } else if (this.sort === `-${sortBy}`) {
-          this.sort = sortBy; // Toggle to ascending
-      } else {
-          this.sort = `-${sortBy}`; // New field, default to descending
-      }
-  }
+    @action
+    cancelSelection() {
+        this.selectedDepartments = [];
+        this.isAllSelected = false;
+    }
 
-  @action
-  toggleDepartmentSelection(department: string) {
-      if (this.selectedDepartments.includes(department)) {
-          this.selectedDepartments = this.selectedDepartments.filter(d => d !== department);
-      } else {
-          this.selectedDepartments = [...this.selectedDepartments, department];
-      }
-  }
+    @action
+    applyColumnSelection() {
+        this.selectedColumns = this.columns.filter(col => col.value).map(col => col.key);
+    }
 
-  @action
-  onSelectChange(selectedDepartment) {
-      this.department = selectedDepartment;
-  }
+    @action
+    toggleOrcidFilter(hasOrcid: boolean) {
+        this.hasOrcid = hasOrcid;
+    }
 
-  @action
-  toggleAllDepartments(event: Event) {
-      this.isAllSelected = (event.target as HTMLInputElement).checked;
-      this.selectedDepartments = this.isAllSelected ? [...this.departments] : [];
-  }
+    @action
+    clickToggleOrcidFilter(hasOrcid: boolean) {
+        this.hasOrcid = !hasOrcid;
+    }
 
-  @action
-  applyDepartmentSelection() {
-      if (this.selectedDepartments.length && !this.isAllSelected) {
-          this.filteredUsers = this.args.departmentMetrics.filter(user =>
-              this.selectedDepartments.includes(user.department));
-      } else {
-          this.filteredUsers = this.args.departmentMetrics;
-      }
-  }
-
-  @action
-  cancelSelection() {
-      this.selectedDepartments = [];
-      this.isAllSelected = false;
-  }
-
-  @action
-  applyColumnSelection() {
-      this.selectedColumns = this.columns.filter(col => col.value).map(col => col.key);
-  }
-
-  @action
-  toggleOrcidFilter(hasOrcid: boolean) {
-    this.hasOrcid = hasOrcid;
-  }
-
-  @action
-  clickToggleOrcidFilter(hasOrcid: boolean) {
-    this.hasOrcid = !hasOrcid;
-  }
 }
