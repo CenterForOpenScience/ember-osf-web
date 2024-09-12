50857bd2c24b52a9e1546659f4ae2c1637336065
John Tordoff <>
2024-09-12 09:53:53 -0400

    refactor Column `value` to `selected`

--- app/institutions/dashboard/-components/institutional-users-list/component.ts
+++ app/institutions/dashboard/-components/institutional-users-list/component.ts
@@ -12,7 +12,7 @@ import Analytics from 'ember-osf-web/services/analytics';
 
 interface Column {
     key: string;
-    value: boolean;
+    selected: boolean;
     label: string;
     type: 'string' | 'date_by_month' | 'osf_link' | 'user_name';
 }
@@ -42,96 +42,96 @@ export default class InstitutionalUsersList extends Component<InstitutionalUsers
             key: 'user_name',
             sort_key: 'user_name',
             label: this.intl.t('institutions.dashboard.users_list.name'),
-            value: true,
+            selected: true,
             type: 'user_name',
         },
         {
             key: 'department',
             sort_key: 'department',
             label: this.intl.t('institutions.dashboard.users_list.department'),
-            value: true,
+            selected: true,
             type: 'string',
         },
         {
             key: 'osf_link',
             sort_key: false,
             label: this.intl.t('institutions.dashboard.users_list.osf_link'),
-            value: true,
+            selected: true,
             type: 'osf_link',
         },
         {
             key: 'publicProjects',
             sort_key: 'public_projects',
             label: this.intl.t('institutions.dashboard.users_list.public_projects'),
-            value: true,
+            selected: true,
             type: 'string',
         },
         {
             key: 'privateProjects',
             sort_key: 'private_projects',
             label: this.intl.t('institutions.dashboard.users_list.private_projects'),
-            value: true,
+            selected: true,
             type: 'string',
         },
         {
             key: 'publicRegistrationCount',
             sort_key: 'public_registration_count',
             label: this.intl.t('institutions.dashboard.users_list.public_registration_count'),
-            value: true,
+            selected: true,
             type: 'string',
         },
         {
             key: 'embargoedRegistrationCount',
             sort_key: 'embargoed_registration_count',
             label: this.intl.t('institutions.dashboard.users_list.private_registration_count'),
-            value: true,
+            selected: true,
             type: 'string',
         },
         {
             key: 'publishedPreprintCount',
             sort_key: 'published_preprint_count',
             label: this.intl.t('institutions.dashboard.users_list.published_preprint_count'),
-            value: true,
+            selected: true,
             type: 'string',
         },
         {
             key: 'publicFileCount',
             sort_key: 'public_file_count',
             label: this.intl.t('institutions.dashboard.users_list.public_file_count'),
-            value: false,
+            selected: false,
             type: 'string',
         },
         {
             key: 'userDataUsage',
             sort_key: 'storage_byte_count',
             label: this.intl.t('institutions.dashboard.users_list.storage_byte_count'),
-            value: false,
+            selected: false,
             type: 'string',
         },
         {
             key: 'accountCreationDate',
             sort_key: 'account_creation_date',
             label: this.intl.t('institutions.dashboard.users_list.account_created'),
-            value: false,
+            selected: false,
             type: 'date_by_month',
         },
         {
             key: 'monthLastLogin',
             sort_key: 'month_last_login',
             label: this.intl.t('institutions.dashboard.users_list.month_last_login'),
-            value: false,
+            selected: false,
             type: 'date_by_month',
         },
         {
             key: 'monthLastActive',
             sort_key: 'month_last_active',
             label: this.intl.t('institutions.dashboard.users_list.month_last_active'),
-            value: false,
+            selected: false,
             type: 'date_by_month',
         },
     ];
 
-    @tracked selectedColumns: string[] = this.columns.filter(col => col.value).map(col => col.key);
+    @tracked selectedColumns: string[] = this.columns.filter(col => col.selected).map(col => col.key);
     // Private properties
     @tracked department = this.intl.t('institutions.dashboard.select_default');
     @tracked sort = 'user_name';
@@ -143,7 +143,7 @@ export default class InstitutionalUsersList extends Component<InstitutionalUsers
     toggleColumnSelection(columnKey: string) {
         const column = this.columns.find(col => col.key === columnKey);
         if (column) {
-            column.value = !column.value;
+            column.selected = !column.selected;
         }
     }
 
@@ -221,7 +221,7 @@ export default class InstitutionalUsersList extends Component<InstitutionalUsers
 
     @action
     applyColumnSelection() {
-        this.selectedColumns = this.columns.filter(col => col.value).map(col => col.key);
+        this.selectedColumns = this.columns.filter(col => col.selected).map(col => col.key);
     }
 
     @action
