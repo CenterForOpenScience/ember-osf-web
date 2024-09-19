736150126e25f1c2b87a441a81ad94400a6669ba
Brian Pilati <brianpilati@cos.io>
2024-09-19 12:26:45 -0500

    Added all the new attributes

--- app/models/institution-summary-metric.ts
+++ app/models/institution-summary-metric.ts
@@ -7,6 +7,12 @@ export default class InstitutionSummaryMetricModel extends OsfModel {
     @attr('number') userCount!: number;
     @attr('number') publicRegistrationCount!: number;
     @attr('number') preprintCount!: number;
+    @attr('number') embargoedRegistrationCount!: number;
+    @attr('number') storageByteCount!: number;
+    @attr('number') publicFileCount!: number;
+    @attr('number') monthlyLoggedInUserCount!: number;
+    @attr('number') monthlyActiveUserCount!: number;
+
 }
 
 declare module 'ember-data/types/registries/model' {
