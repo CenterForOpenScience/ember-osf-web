8ffb312cbcdcbf11ddd39aa0bde94b467d901f04
Futa Ikeda <futa.ikeda@gmail.com>
2024-09-27 10:52:09 -0400

    Add filtering logic to object list

--- app/institutions/dashboard/-components/object-list/component.ts
+++ app/institutions/dashboard/-components/object-list/component.ts
@@ -1,8 +1,10 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 
 import InstitutionModel from 'ember-osf-web/models/institution';
 import SearchResultModel from 'ember-osf-web/models/search-result';
+import { Filter } from 'osf-components/components/search-page/component';
 
 interface ValueColumn {
     name: string;
@@ -25,24 +27,38 @@ export type ObjectListColumn = ValueColumn | LinkColumn | ComponentColumn;
 
 interface InstitutionalObjectListArgs {
     institution: InstitutionModel;
-    defaultQueryOptions: Record<string, any>;
+    defaultQueryOptions: Record<'cardSearchFilter', Record<string, string[]>>;
     columns: ObjectListColumn[];
 }
 
 export default class InstitutionalObjectList extends Component<InstitutionalObjectListArgs> {
-    @tracked filterObject = {}; // TODO: ENG-6183 Implement filter and type this
+    @tracked activeFilters: Filter[] = [];
     @tracked page = ''; // TODO: ENG-6184 Implement pagination
     @tracked sort = '-relevance'; // TODO: ENG-6184 Implement sorting
 
     get queryOptions() {
-        return {
-            ...this.args.defaultQueryOptions,
-            // TODO: ENG-6183 Implement filter
-            // chance that this may overwrite the defaultQueryOptions.check SearchPageComponent for reference
-            cardSearchFilter: this.filterObject,
+        const options = {
+            ... this.args.defaultQueryOptions,
             'page[cursor]': this.page,
             'page[size]': 10, // TODO: ENG-6184 Implement pagination
             sort: this.sort,
+
         };
+        const fullQueryOptions = this.activeFilters.reduce((acc, filter: Filter) => {
+            const currentValue = acc.cardSearchFilter[filter.propertyPathKey];
+            acc.cardSearchFilter[filter.propertyPathKey] =
+                currentValue ? [...currentValue, filter.value] : [filter.value];
+            return acc;
+        }, options);
+        return fullQueryOptions;
+    }
+
+    @action
+    toggleFilter(property: Filter) {
+        if (this.activeFilters.includes(property)) {
+            this.activeFilters.removeObject(property);
+        } else {
+            this.activeFilters.pushObject(property);
+        }
     }
 }
