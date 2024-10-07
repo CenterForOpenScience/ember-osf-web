3860594f397c155cf1fd1f7ff983605449c83818
Brian Pilati <brianpilati@cos.io>
2024-10-07 13:10:34 -0500

    Added additional tests and more dynamic data

--- app/institutions/dashboard/-components/chart-kpi-wrapper/component.ts
+++ app/institutions/dashboard/-components/chart-kpi-wrapper/component.ts
@@ -5,28 +5,117 @@ import { task } from 'ember-concurrency';
 import { taskFor } from 'ember-concurrency-ts';
 import Intl from 'ember-intl/services/intl';
 import { inject as service } from '@ember/service';
+import InstitutionDepartmentModel from 'ember-osf-web/models/institution-department';
 import InstitutionSummaryMetricModel from 'ember-osf-web/models/institution-summary-metric';
+/*
+import InstitutionSummaryMetricModel from 'ember-osf-web/models/institution-summary-metric';
+*/
+
+export interface ChartDataModel {
+    label: string;
+    total: number;
+}
 
 interface TotalCountChartWrapperArgs {
     model: any;
 }
 
-interface TotalCountChartModel {
+export interface KpiChartModel {
     title: string;
-    total: number;
-    chart: string;
+    chartData: ChartDataModel[];
+    chartType: string;
 }
 
 export default class ChartKpiWrapperComponent extends Component<TotalCountChartWrapperArgs> {
     @service intl!: Intl;
     @tracked model = this.args.model;
-    @tracked totalCountCharts = [] as TotalCountChartModel[];
+    @tracked kpiCharts = [] as KpiChartModel[];
     @tracked isLoading = true;
 
     constructor(owner: unknown, args: TotalCountChartWrapperArgs) {
         super(owner, args);
 
         taskFor(this.loadData).perform();
+    }
+
+    /**
+     * loadData
+     *
+     * @description Loads all the data and builds the chart data before rendering the page
+     *
+     * @returns a void Promise
+     */
+    @task
+    @waitFor
+    private async loadData(): Promise<void> {
+        const metrics = await this.model;
+
+        this.kpiCharts.push(
+            {
+                title: this.intl.t('institutions.dashboard.kpi-chart.users-by-department'),
+                chartData: this.calculateUsersByDepartment(metrics.departmentMetrics),
+                chartType: 'doughnut',
+            },
+            {
+                title: this.intl.t('institutions.dashboard.kpi-chart.public-vs-private-projects.title'),
+                chartData: this.calculateProjects(metrics.summaryMetrics),
+                chartType: 'pie',
+            },
+            /*
+            {
+                title: this.intl.t('institutions.dashboard.panel.registrations'),
+                chartData: [
+                    {
+                        label: 'a',
+                        total: metrics.summaryMetrics.userCount,
+                    } as ChartDataModel,
+                    {
+                        label: 'a',
+                        total: metrics.summaryMetrics.userCount,
+                    } as ChartDataModel,
+                ],
+                chartType: 'bar',
+            },
+            {
+                title: this.intl.t('institutions.dashboard.panel.preprints'),
+                chartData: [
+                    {
+                        label: 'a',
+                        total: metrics.summaryMetrics.userCount,
+                    } as ChartDataModel,
+                    {
+                        label: 'a',
+                        total: metrics.summaryMetrics.userCount,
+                    } as ChartDataModel,
+                ],
+                chartType: 'line',
+            },
+            */
+        );
+
+        this.isLoading = false;
+    }
+
+    /**
+     * calculateUserByDepartments
+     *
+     * @description Abstracted method to build the ChartData model for deparments
+     * @param departmentMetrics The department metrics object
+     *
+     * @returns The users by department ChartData model
+     */
+    private calculateUsersByDepartment(departmentMetrics: InstitutionDepartmentModel[]): ChartDataModel[] {
+        const departmentData = [] as ChartDataModel[];
+
+        departmentMetrics.map((metric: InstitutionDepartmentModel ) => {
+            departmentData.push(
+                {
+                    label: metric.name,
+                    total: metric.numberOfUsers,
+                } as ChartDataModel,
+            );
+        });
+        return departmentData;
     }
 
     /**
@@ -37,39 +126,17 @@ export default class ChartKpiWrapperComponent extends Component<TotalCountChartW
      *
      * @returns The total of private and public projects
      */
-    private calculateProjects(summaryMetrics: InstitutionSummaryMetricModel): number {
-        return summaryMetrics.privateProjectCount + summaryMetrics.publicProjectCount;
-    }
-
-    @task
-    @waitFor
-    private async loadData(): Promise<void> {
-        const metrics = await this.model;
-
-        this.totalCountCharts.push(
+    private calculateProjects(summaryMetrics: InstitutionSummaryMetricModel): ChartDataModel[] {
+        return [
             {
-                title: this.intl.t('institutions.dashboard.panel.users'),
-                total: metrics.summaryMetrics.userCount,
-                chart: 'doughnut',
-            },
+                label: this.intl.t('institutions.dashboard.kpi-chart.public-vs-private-projects.public'),
+                total: summaryMetrics.publicProjectCount,
+            } as ChartDataModel,
             {
-                title: this.intl.t('institutions.dashboard.panel.projects'),
-                total: this.calculateProjects(metrics.summaryMetrics),
-                chart: 'pie',
-            },
-            {
-                title: this.intl.t('institutions.dashboard.panel.registrations'),
-                total: metrics.summaryMetrics.publicRegistrationCount,
-                chart: 'bar',
-            },
-            {
-                title: this.intl.t('institutions.dashboard.panel.preprints'),
-                total: metrics.summaryMetrics.preprintCount,
-                chart: 'line',
-            },
-        );
-
-        this.isLoading = false;
+                label: this.intl.t('institutions.dashboard.kpi-chart.public-vs-private-projects.private'),
+                total: summaryMetrics.privateProjectCount,
+            } as ChartDataModel,
+        ];
     }
 }
 
