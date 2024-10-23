6e42b4de3f8166820e7391f08b5f5aebf623fa7e
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2024-10-23 14:53:27 -0400

    [ENG-6246][ENG-6269][ENG-6275]  Add new property fields to mirage (#2353)
    -   Ticket: [ENG-6246] [ENG-6269] [ENG-6275]
    -   Feature flag: n/a
    
    ## Purpose
    - Add new metadata fields to mirage and update project list to get new properties
    
    ## Summary of Changes
    - Add new properties to mirage/views/search.ts based on the proposed changes to OSF MAP
    - Add new getters for these new metadata properties to the search-result model
    - Update project list columns to match what is actually requested in the mocks
    - Update contributor field component to show user's permission level

--- app/institutions/dashboard/-components/object-list/contributors-field/component.ts
+++ app/institutions/dashboard/-components/object-list/contributors-field/component.ts
@@ -1,32 +1,55 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import Intl from 'ember-intl/services/intl';
 
 import InstitutionModel from 'ember-osf-web/models/institution';
 import SearchResultModel from 'ember-osf-web/models/search-result';
+import { AttributionRoleIris } from 'ember-osf-web/models/index-card';
 
 interface ContributorsFieldArgs {
     searchResult: SearchResultModel;
     institution: InstitutionModel;
 }
 
+const roleIriToTranslationKey: Record<AttributionRoleIris, string> = {
+    [AttributionRoleIris.Admin]: 'general.permissions.admin',
+    [AttributionRoleIris.Write]: 'general.permissions.write',
+    [AttributionRoleIris.Read]: 'general.permissions.read',
+};
+
+
 export default class InstitutionalObjectListContributorsField extends Component<ContributorsFieldArgs> {
+    @service intl!: Intl;
+
     // Return two contributors affiliated with the institution given with highest permission levels
     get topInstitutionAffiliatedContributors() {
         const { searchResult, institution } = this.args;
-        const contributors: any[] = searchResult.resourceMetadata.creator;
+        const attributions: any[] = searchResult.resourceMetadata.qualifiedAttribution;
         const institutionIris = institution.iris;
-        const affiliatedContributors = contributors
-            .filter((contributor: any) => hasInstitutionAffiliation(contributor, institutionIris));
-        // TODO: get the two users with the highest permission level and add permission to the return object
-        return affiliatedContributors.slice(0, 2).map((contributor: any) => ({
-            name: contributor.name[0]['@value'],
-            url: contributor['@id'],
-        }));
-    }
+        const affiliatedAttributions = attributions
+            .filter((attribution: any) => hasInstitutionAffiliation(attribution, institutionIris));
+        const adminAttributions = affiliatedAttributions
+            .filter(attribution => attribution.hadRole[0]['@id'] === AttributionRoleIris.Admin);
+        const writeAttributions = affiliatedAttributions
+            .filter(attribution => attribution.hadRole[0]['@id'] === AttributionRoleIris.Write);
+        const readAttributions = affiliatedAttributions
+            .filter(attribution => attribution.hadRole[0]['@id'] === AttributionRoleIris.Read);
 
+        const prioritizedAttributions = adminAttributions.concat(writeAttributions, readAttributions);
+
+        return prioritizedAttributions.slice(0, 2).map((attribution: any) => {
+            const roleIri: AttributionRoleIris = attribution.hadRole[0]['@id'];
+            return {
+                name: attribution.agent[0].name[0]['@value'],
+                url: attribution.agent[0]['@id'],
+                permissionLevel: this.intl.t(roleIriToTranslationKey[roleIri]),
+            };
+        });
+    }
 }
 
 function hasInstitutionAffiliation(contributor: any, institutionIris: string[]) {
-    return contributor.affiliation.some(
+    return contributor.agent[0].affiliation.some(
         (affiliation: any) => affiliation.identifier.some(
             (affiliationIdentifier: any) => institutionIris.includes(affiliationIdentifier['@value']),
         ),
