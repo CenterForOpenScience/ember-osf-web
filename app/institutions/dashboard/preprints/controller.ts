defd0280667300a6aa5e5a1ff802d99ec1f5369f
futa-ikeda <51409893+futa-ikeda@users.noreply.github.com>
2024-10-30 15:21:49 -0400

    [ENG-6276][ENG-6277][ENG-6278] Add Preprints table (#2370)
    -   Ticket: [ENG-6276] [ENG-6277] [ENG-6278]
    -   Feature flag: n/a
    
    ## Purpose
    - Add table for preprints in institutional dashboard
    
    ## Summary of Changes
    - Add `object-list` component to institutional dashboard preprint tab
    - Add controller to determine which columns to show
    - Update tests

---
+++ app/institutions/dashboard/preprints/controller.ts
@@ -0,0 +1,63 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import Intl from 'ember-intl/services/intl';
+
+import { ResourceTypeFilterValue } from 'osf-components/components/search-page/component';
+import { ObjectListColumn } from '../-components/object-list/component';
+
+export default class InstitutionDashboardPreprints extends Controller {
+    @service intl!: Intl;
+
+    columns: ObjectListColumn[] = [
+        { // Title
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.title'),
+            getValue: searchResult => searchResult.displayTitle,
+        },
+        { // Link
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.link'),
+            type: 'link',
+            getHref: searchResult => searchResult.indexCard.get('osfIdentifier'),
+            getLinkText: searchResult => searchResult.indexCard.get('osfGuid'),
+        },
+        { // Date created
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.created_date'),
+            getValue: searchResult => searchResult.getResourceMetadataField('dateCreated'),
+            sortKey: 'dateCreated',
+        },
+        { // Date modified
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.modified_date'),
+            getValue: searchResult => searchResult.getResourceMetadataField('dateModified'),
+            sortKey: 'dateModified',
+        },
+        { // DOI
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.doi'),
+            type: 'doi',
+        },
+        { // License
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.license'),
+            getValue: searchResult => searchResult.license?.name,
+        },
+        { // Contributor name + permissions
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.contributor_name'),
+            type: 'contributors',
+        },
+        { // View count
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.view_count'),
+            getValue: searchResult => searchResult.usageMetrics.viewCount,
+        },
+        { // Download count
+            name: this.intl.t('institutions.dashboard.object-list.table-headers.download_count'),
+            getValue: searchResult => searchResult.usageMetrics.downloadCount,
+        },
+    ];
+
+    get defaultQueryOptions() {
+        const identifiers = this.model.institution.iris.join(',');
+        return {
+            cardSearchFilter: {
+                affiliation: identifiers,
+                resourceType: ResourceTypeFilterValue.Preprints,
+            },
+        };
+    }
+}
