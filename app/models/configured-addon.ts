1c9a48b7b5367efde0b2c0fa253100407c630d48
Yuhuai Liu <yuhuai@cos.io>
2025-01-02 21:48:33 -0500

    use render modifiers to invoke getRootFolderName task; add tracked property to model

--- app/models/configured-addon.ts
+++ app/models/configured-addon.ts
@@ -1,8 +1,12 @@
 import Model, { AsyncBelongsTo, attr, belongsTo } from '@ember-data/model';
+import { waitFor } from '@ember/test-waiters';
+import { task } from 'ember-concurrency';
 
 import UserReferenceModel from 'ember-osf-web/models/user-reference';
+import { tracked } from 'tracked-built-ins';
+import { taskFor } from 'ember-concurrency-ts';
 import { ConnectedStorageOperationNames, OperationKwargs } from './addon-operation-invocation';
-import AuthorizedAccountModel, { ConnectedCapabilities } from './authorized-account';
+
 
 export interface ConfiguredAddonEditableAttrs {
     displayName: string;
@@ -27,18 +31,28 @@ export default class ConfiguredAddonModel extends Model {
     @attr('boolean')
     currentUserIsOwner!: boolean;
 
-    async getFolderItems(this: AuthorizedAccountModel, _kwargs?: OperationKwargs) : Promise<any> {
+    async getFolderItems(this: ConfiguredAddonModel, _kwargs?: OperationKwargs) : Promise<any> {
         // To be implemented in child classes
         return;
     }
 
 
-    async getItemInfo(this: AuthorizedAccountModel, _itemId: string) : Promise<any> {
+    async getItemInfo(this: ConfiguredAddonModel, _itemId: string) : Promise<any> {
         // To be implemented in child classes
         return;
     }
 
     get hasRootFolder() {
         return true;
+
+    }
+
+    @tracked rootFolderName = '';
+
+    @task
+    @waitFor
+    async getRootFolderName(this: ConfiguredAddonModel) {
+        const response = await taskFor(this.getItemInfo).perform(this.rootFolder);
+        this.rootFolderName = response.operationResult.itemName;
     }
 }
