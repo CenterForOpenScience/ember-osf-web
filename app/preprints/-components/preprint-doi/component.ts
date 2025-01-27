b4084d063a7a90e1874076796f276440a2275ccd
Longze Chen <cslzchen@gmail.com>
2025-01-27 14:33:37 -0500

    [ENG-6813]  Preprints DOI Versioning (#2447)
    -   Ticket: [ENG-6813](https://openscience.atlassian.net/browse/ENG-6813)
    -   Feature flag: n/a
    
    ## Purpose
    - Enable Preprint DOI Versioning on the FE
    
    ## Summary of Changes
    - Add new version workflow
    - Update preprint edit workflow
    - Move preprint withdrawal button out of the edit workflow
    - Add version selection controls

--- app/preprints/-components/preprint-doi/component.ts
+++ app/preprints/-components/preprint-doi/component.ts
@@ -1,14 +1,26 @@
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
-import PreprintModel from 'ember-osf-web/models/preprint';
+import { tracked } from '@glimmer/tracking';
+
+import PreprintModel, { VersionStatusSimpleLabelKey } from 'ember-osf-web/models/preprint';
 import PreprintProviderModel from 'ember-osf-web/models/preprint-provider';
 
 interface InputArgs {
-    preprint: PreprintModel;
+    versions: PreprintModel[];
     provider: PreprintProviderModel;
+    currentVersion: PreprintModel;
 }
 
 export default class PreprintAbstract extends Component<InputArgs> {
     provider = this.args.provider;
+    documentType = this.provider.documentType.singularCapitalized;
 
-    documentType = this.provider.documentType.singular;
+    @tracked selectedVersion = this.args.currentVersion;
+
+    reviewStateLabelKeyMap = VersionStatusSimpleLabelKey;
+
+    @action
+    selectVersion(version: PreprintModel) {
+        this.selectedVersion = version;
+    }
 }
--- app/serializers/preprint.ts
+++ app/serializers/preprint.ts
@@ -1,6 +1,19 @@
+import Model from '@ember-data/model';
+import { Resource } from 'osf-api';
+
 import OsfSerializer from './osf-serializer';
 
 export default class PreprintSerializer extends OsfSerializer {
+    normalize(modelClass: Model, resourceHash: Resource) {
+        const result = super.normalize(modelClass, resourceHash);
+        // Insert a `versions` relationship to the model
+        result.data.relationships!.versions = {
+            links: {
+                related: resourceHash.links!.preprint_versions!,
+            },
+        };
+        return result;
+    }
 }
 
 declare module 'ember-data/types/registries/serializer' {
