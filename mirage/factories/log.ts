a663742de2380685bcc34c87eb15e8d845371670
Brian Pilati <brianpilati@cos.io>
2025-02-10 11:33:33 -0600

    Upates to tests

--- mirage/factories/log.ts
+++ mirage/factories/log.ts
@@ -1,15 +1,9 @@
-import { Factory, trait, Trait } from 'ember-cli-mirage';
+import { Factory } from 'ember-cli-mirage';
 import LogModel from 'ember-osf-web/models/log';
 
 import { guidAfterCreate } from './utils';
 
-export interface LogsTraits {
-    license: Trait;
-    tag: Trait;
-    fileAdded: Trait;
-}
-
-export default Factory.extend<LogModel & LogsTraits>({
+export default Factory.extend<LogModel >({
     id: 'unknown',
     action: 'unknown',
     date: new Date('2025-02-06T19:51:35.017269Z'),
@@ -21,7 +15,10 @@ export default Factory.extend<LogModel & LogsTraits>({
         },
         paramsProject: null,
         path: '/hat.jpg',
+        tag: 'Food',
+        pathType: 'file',
         pointer: null,
+        license: 'Apache License 2.0',
         preprintProvider: null,
         urls: {
             view: '/c2het/files/osfstorage/6786c8874fde462e7f1ec489/?pid=c2het',
@@ -42,62 +39,6 @@ export default Factory.extend<LogModel & LogsTraits>({
             user,
         });
     },
-
-    license: trait<LogModel>({
-        afterCreate(log) {
-            const params = log.params;
-            params.license = 'Apache License 2.0';
-            log.update({
-                params,
-            });
-        },
-    }),
-    tag: trait<LogModel>({
-        afterCreate(log) {
-            log.update({
-                params: {
-                    contributors: [],
-                    license: 'Apache License 2.0',
-                    params_node: {
-                        id: 'c2het',
-                        title: 'A new project for testing file components',
-                    },
-                    params_project: null,
-                    path: '/hat.jpg',
-                    pointer: null,
-                    preprint_provider: null,
-                    tag: 'Food',
-                    urls: {
-                        view: '/c2het/files/osfstorage/6786c8874fde462e7f1ec489/?pid=c2het',
-                        download: '/c2het/files/osfstorage/6786c8874fde462e7f1ec489/?pid=c2het?action=download',
-                    },
-                },
-            });
-        },
-    }),
-    fileAdded: trait<LogModel>({
-        afterCreate(log) {
-            log.update({
-                params: {
-                    contributors: [],
-                    license: 'Apache License 2.0',
-                    params_node: {
-                        id: 'c2het',
-                        title: 'A new project for testing file components',
-                    },
-                    params_project: null,
-                    path: '/hat.jpg',
-                    pointer: null,
-                    preprint_provider: null,
-                    tag: 'Food',
-                    urls: {
-                        view: '/c2het/files/osfstorage/6786c8874fde462e7f1ec489/?pid=c2het',
-                        download: '/c2het/files/osfstorage/6786c8874fde462e7f1ec489/?pid=c2het?action=download',
-                    },
-                },
-            });
-        },
-    }),
 });
 
 declare module 'ember-cli-mirage/types/registries/model' {
