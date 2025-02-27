a94ca8bd222f24e54055e628b71753bd281a9f64
Oleh Paduchak <opaduchak@exoft.net>
2025-02-27 13:55:31 +0200

    Added VOL support to Gv requests
    Co-authored-by: NAME <sheredko.andriy@gmail.com>

--- app/adapters/addon-service.ts
+++ app/adapters/addon-service.ts
@@ -1,5 +1,7 @@
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import config from 'ember-osf-web/config/environment';
+import { inject as service } from '@ember/service';
+import CurrentUser from 'ember-osf-web/services/current-user';
 
 const { addonServiceUrl } = config.OSF;
 
@@ -9,10 +11,25 @@ export const addonServiceAPIUrl = `${addonServiceUrl}${addonServiceNamespace}/`;
 export default class AddonServiceAdapter extends JSONAPIAdapter {
     host = addonServiceUrl.replace(/\/$/, ''); // Remove trailing slash to avoid // in URLs
     namespace = addonServiceNamespace;
+    @service currentUser!: CurrentUser;
 
     ajaxOptions(url: string, type: string, options?: any): object {
         const _ajaxopts: any = super.ajaxOptions(url, type, options);
         _ajaxopts.credentials = 'include';
         return _ajaxopts;
     }
+
+    ajax(url, method, options) {
+        return super.ajax(method === 'GET' ? this.transformUrl(url) : url, method, options);
+    }
+
+    transformUrl(url: string): string {
+        const { viewOnlyToken } = this.currentUser;
+        if (!viewOnlyToken) {
+            return url;
+        }
+        const separator = url.includes('?') ? '&' : '?';
+        return `${url}${separator}view_only=${viewOnlyToken}`;
+    }
+
 }
