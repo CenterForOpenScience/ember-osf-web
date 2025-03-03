d4e1e2ed344ea2d1dd269f7de2182bda84900e3a
abram axel booth <boothaa@gmail.com>
2025-03-03 16:18:27 -0500

    fix: remove `preprintDoiCreated` check
    rename `Preprint.verifiedDoi` to simply `preprintDoi`, since it's no
    longer verifying anything

--- app/models/preprint.ts
+++ app/models/preprint.ts
@@ -126,11 +126,8 @@ export default class PreprintModel extends AbstractNodeModel {
         return this.dateWithdrawn !== null;
     }
 
-    get verifiedDoi(): string {
-        if (this.preprintDoiCreated) {
-            return extractDoi(this.preprintDoiUrl) || '';
-        }
-        return '';
+    get preprintDoi(): string {
+        return extractDoi(this.preprintDoiUrl) || '';
     }
 
     @computed('license', 'licenseRecord')
