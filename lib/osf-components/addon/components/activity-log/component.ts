a893273f5f5e556d2e8a847c3e91f4512ac942a6
John Tordoff <Johnetordoff@users.noreply.github.com>
2025-06-09 14:20:37 -0400

    remove unnecessary group embed

--- lib/osf-components/addon/components/activity-log/component.ts
+++ lib/osf-components/addon/components/activity-log/component.ts
@@ -4,7 +4,7 @@ export default class ActivityLogComponent extends Component {
     public loadEmbeds = {
         embed:
             [
-                'group', 'linked_node', 'linked_registration', 'original_node',
+                'linked_node', 'linked_registration', 'original_node',
                 'template_node', 'user',
             ],
     };
